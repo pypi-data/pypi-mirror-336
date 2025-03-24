6904c56a2e7a1266a0ec5d29a9b2e2265a071fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:05 +0000

    Add package computer-use-agent/0.1.0/computer_use_agent-0.1.0.tar.gz

---
+++ packages/computer-use-agent/computer_use_agent-0.1.0.tar.gz/computer_use_agent-0.1.0/computer_use_agent/__init__.py
@@ -0,0 +1,5 @@
+"""
+computer-use-agent - A placeholder package
+"""
+
+__version__ = "0.1.0" 
---
+++ packages/computer-use-agent/computer_use_agent-0.1.0.tar.gz/computer_use_agent-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="computer-use-agent",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Xiangyi Li",
+    author_email="xiangyi@benchflow.ai",
+    description="A placeholder package for computer-use-agent",
+    long_description="A placeholder package for computer-use-agent",
+    url="https://github.com/placeholder/computer-use-agent",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
