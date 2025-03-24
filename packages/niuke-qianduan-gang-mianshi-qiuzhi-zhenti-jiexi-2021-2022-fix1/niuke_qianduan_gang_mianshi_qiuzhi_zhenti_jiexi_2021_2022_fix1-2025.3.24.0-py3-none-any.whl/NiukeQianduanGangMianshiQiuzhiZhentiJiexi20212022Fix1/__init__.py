68e6edc16cec66739e6fd1690dd71513d73050e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:05:37 +0000

    Add package niuke-qianduan-gang-mianshi-qiuzhi-zhenti-jiexi-2021-2022-fix1/2025.3.24.0/niuke_qianduan_gang_mianshi_qiuzhi_zhenti_jiexi_2021_2022_fix1-2025.3.24.0-py3-none-any.whl

---
+++ packages/niuke-qianduan-gang-mianshi-qiuzhi-zhenti-jiexi-2021-2022-fix1/niuke_qianduan_gang_mianshi_qiuzhi_zhenti_jiexi_2021_2022_fix1-2025.3.24.0-py3-none-any.whl/NiukeQianduanGangMianshiQiuzhiZhentiJiexi20212022Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""niuke-qianduan-gang-mianshi-qiuzhi-zhenti-jiexi-2021-2022-fix1
+https://github.com/apachecn/niuke-qianduan-gang-mianshi-qiuzhi-zhenti-jiexi-2021-2022-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.0"
