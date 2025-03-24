a306f5b36f5a460243d0a1b932ae04926a8476a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:04:16 +0000

    Add package Oned-Vlasov-Poisson-jax-solver/0.1.1/oned_vlasov_poisson_jax_solver-0.1.1.tar.gz

---
+++ packages/Oned-Vlasov-Poisson-jax-solver/oned_vlasov_poisson_jax_solver-0.1.1.tar.gz/oned_vlasov_poisson_jax_solver-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="Oned_Vlasov_Poisson_jax_solver",
+    version="0.1.1",
+    author="Jianfeng Ye",
+    author_email="xyjjeff23@gmail.com",
+    description="A fast 1D Vlasov solver using JAX",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/XYJeff23/1D_Vlasov_JAX_Solver",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[  # 指定依赖项
+        "jax",
+        "jaxlib",
+        "diffrax",
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "pandas"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
