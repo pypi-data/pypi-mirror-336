8f09960153a69901e75a778f16e2801b846db3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:05 +0000

    Add package cstore-package/0.1.0/cstore_package-0.1.0.tar.gz

---
+++ packages/cstore-package/cstore_package-0.1.0.tar.gz/cstore_package-0.1.0/cstore/__init__.py
@@ -0,0 +1,10 @@
+from .cstore import (
+    load_credentials,
+    save_credentials,
+    create_account,
+    login,
+    get_folder_structure,
+    upload_library,
+    download_library,
+    search_library
+)
