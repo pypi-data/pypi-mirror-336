75a8ff4aa8dd7dc5d2ba595ccea3f276497cf9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:40 +0000

    Add package easydsi/2.0.0/easydsi-2.0.0-py3-none-any.whl

---
+++ packages/easydsi/easydsi-2.0.0-py3-none-any.whl/easydsi/__init__.py
@@ -0,0 +1,12 @@
+# import sys
+# sys.path.append(r'D:/Projects/Python/easydsi')
+
+from easydsi.queue import *
+from easydsi.tree import *
+from easydsi.heap import *
+from easydsi.linked_list import *
+from easydsi.stack import *
+
+__version__ = "1.2.1"
+__developer__ = "Mohamed Fazil Z"
+__website__ = "https://github.com/fazil2003/easydsi"
