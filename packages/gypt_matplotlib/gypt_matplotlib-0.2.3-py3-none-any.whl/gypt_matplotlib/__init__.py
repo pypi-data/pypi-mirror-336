c28e23a0b0b1a5b1f193d2bd33a501f5215f3e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 04:04:08 +0000

    Add package gypt_matplotlib/0.2.3/gypt_matplotlib-0.2.3-py3-none-any.whl

---
+++ packages/gypt_matplotlib/gypt_matplotlib-0.2.3-py3-none-any.whl/gypt_matplotlib/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "0.2.3"
+__description__ = "A small addon for matplotlib that can be used for the GYPT."
+__license__ = "MIT"
+__authors__ = ["Keenan Noack <AlbertUnruh@pm.me>"]
+__repository__ = "https://github.com/AlbertUnruh/gypt-matplotlib/"
+
+
+# local
+from .context_managers import au_plot, auto_close, auto_save, auto_save_and_show, auto_show
+from .utils import apply_gypt_style, axes_label, tex
+
+
+__all__ = (
+    "au_plot",
+    "auto_close",
+    "auto_save",
+    "auto_save_and_show",
+    "auto_show",
+    "axes_label",
+    "tex",
+)
+
+
+# automatically apply the GYPT style
+apply_gypt_style()
