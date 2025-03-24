2320f22b0252c080c4b0b77ddd1bddd05b10a9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 04:03:36 +0000

    Add package leveup/1.0/leveup-1.0.tar.gz

---
+++ packages/leveup/leveup-1.0.tar.gz/leveup-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='leveup', 
+    version='1.0',
+    packages=find_packages(),
+    author='Nasr',
+    author_email='nasr2python@gmail.com',
+    description='Simple',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+        ]
+) 
