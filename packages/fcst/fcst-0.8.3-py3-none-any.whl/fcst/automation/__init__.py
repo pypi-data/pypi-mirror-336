ebbad7dc60f76e53b33a0a221315df02b20cf976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:14 +0000

    Add package fcst/0.8.3/fcst-0.8.3-py3-none-any.whl

---
+++ packages/fcst/fcst-0.8.3-py3-none-any.whl/fcst/automation/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+automation sub-package
+~~~~
+Automation, make it easy for common forecasting tasks.
+"""
+
+from ._forecasting_automation import run_forecasting_automation
+
+__all__ = ["run_forecasting_automation"]
