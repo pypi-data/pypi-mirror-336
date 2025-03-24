598f53fcac0becedf9fc2cc04a66149af6b6a36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 04:03:39 +0000

    Add package python-pulse/0.3.2/python_pulse-0.3.2-py3-none-any.whl

---
+++ packages/python-pulse/python_pulse-0.3.2-py3-none-any.whl/pypulse/Template/render.py
@@ -0,0 +1,23 @@
+from jinja2 import Environment, FileSystemLoader
+from .template import Template
+
+
+class RenderTemplate():
+    def __init__(self, template_route: str, variables_dict: dict = None) -> None:
+        self.template_route = template_route
+        self.variables_dict = variables_dict
+
+        self.envirenemnt = Environment(
+            loader=FileSystemLoader(Template.TEMPLATE_PATH))
+
+    def render_template(self, request):
+        request.send_response(200)
+        request.send_header('Content-type', 'text/html')
+
+        template = self.envirenemnt.get_template(self.template_route)
+        if self.variables_dict is not None:
+            output = template.render(self.variables_dict)
+        else:
+            output = template.render()
+
+        return output, False
