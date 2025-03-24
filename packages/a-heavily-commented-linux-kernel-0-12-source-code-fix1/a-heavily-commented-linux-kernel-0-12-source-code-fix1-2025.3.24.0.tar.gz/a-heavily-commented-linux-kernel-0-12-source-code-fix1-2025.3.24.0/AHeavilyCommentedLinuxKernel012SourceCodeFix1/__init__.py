7cfaf8f118ddb67b861b62cdc07211d5d4a27e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:27 +0000

    Add package a-heavily-commented-linux-kernel-0-12-source-code-fix1/2025.3.24.0/a-heavily-commented-linux-kernel-0-12-source-code-fix1-2025.3.24.0.tar.gz

---
+++ packages/a-heavily-commented-linux-kernel-0-12-source-code-fix1/a-heavily-commented-linux-kernel-0-12-source-code-fix1-2025.3.24.0.tar.gz/a-heavily-commented-linux-kernel-0-12-source-code-fix1-2025.3.24.0/AHeavilyCommentedLinuxKernel012SourceCodeFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""a-heavily-commented-linux-kernel-0-12-source-code-fix1
+https://github.com/apachecn/a-heavily-commented-linux-kernel-0-12-source-code-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.0"
