e142170f989cb64ce2fd9e4fdb41ba168be909a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 04:03:54 +0000

    Add package mteb/1.36.32/mteb-1.36.32-py3-none-any.whl

---
+++ packages/mteb/mteb-1.36.32-py3-none-any.whl/mteb/requires_package.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import importlib.util
+
+
+def _is_package_available(pkg_name: str) -> bool:
+    package_exists = importlib.util.find_spec(pkg_name) is not None
+    return package_exists
+
+
+def requires_package(
+    obj, package_name: str, model_name: str, install_instruction: str | None = None
+) -> None:
+    if not _is_package_available(package_name):
+        install_instruction = (
+            f"pip install {package_name}"
+            if install_instruction is None
+            else install_instruction
+        )
+        name = obj.__name__ if hasattr(obj, "__name__") else obj.__class__.__name__
+        raise ImportError(
+            f"{name} requires the `{package_name}` library but it was not found in your environment. "
+            + f"If you want to load {model_name} models, please `{install_instruction}` to install the package."
+        )
+
+
+def requires_image_dependencies() -> None:
+    if not _is_package_available("torchvision"):
+        raise ImportError(
+            "You are trying to running the image subset of mteb without having installed the required dependencies (`torchvision`). "
+            + "You can install the required dependencies using `pip install 'mteb[image]'` to install the required dependencies."
+        )
