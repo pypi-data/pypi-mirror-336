7f07cc8fb4fb8310a41c9b8d3109325c79c49975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:03 +0000

    Add package bu-cha-dian-de-jisuanjikexue-fix1/2025.3.24.2/bu-cha-dian-de-jisuanjikexue-fix1-2025.3.24.2.tar.gz

---
+++ packages/bu-cha-dian-de-jisuanjikexue-fix1/bu-cha-dian-de-jisuanjikexue-fix1-2025.3.24.2.tar.gz/bu-cha-dian-de-jisuanjikexue-fix1-2025.3.24.2/BuChaDianDeJisuanjikexueFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bu-cha-dian-de-jisuanjikexue-fix1
+https://github.com/apachecn/bu-cha-dian-de-jisuanjikexue-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.2"
