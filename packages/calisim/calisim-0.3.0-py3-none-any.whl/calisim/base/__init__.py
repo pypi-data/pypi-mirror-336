3a717cd222fb1f990d74b0dd7892157fd281efd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:15 +0000

    Add package calisim/0.3.0/calisim-0.3.0-py3-none-any.whl

---
+++ packages/calisim/calisim-0.3.0-py3-none-any.whl/calisim/base/__init__.py
@@ -0,0 +1,23 @@
+import importlib
+
+from .calibration_base import CalibrationMethodBase, CalibrationWorkflowBase
+from .emukit_base import EmukitBase
+from .example_model_base import ExampleModelBase
+from .history_matching_base import HistoryMatchingBase
+from .openturns_base import OpenTurnsBase
+from .surrogate_base import SurrogateBase
+
+__all__ = [
+	CalibrationMethodBase,
+	CalibrationWorkflowBase,
+	EmukitBase,
+	ExampleModelBase,
+	HistoryMatchingBase,
+	OpenTurnsBase,
+	SurrogateBase,
+]
+
+if importlib.util.find_spec("torch") is not None:
+	from .sbi_base import SimulationBasedInferenceBase
+
+	__all__.append(SimulationBasedInferenceBase)
