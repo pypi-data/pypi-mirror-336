05c0d71a2512e3c06552495d4616417310350710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:04:23 +0000

    Add package hipo/1.0.2/hipo-1.0.2-py3-none-any.whl

---
+++ packages/hipo/hipo-1.0.2-py3-none-any.whl/hipo/examples/solver.py
@@ -0,0 +1,32 @@
+# solver.py
+import pipre
+import sys, re, os
+#import json
+import commentjson as json
+
+fnA = sys.argv[1]
+fnb = sys.argv[2]
+config = sys.argv[3]
+
+params = json.load(open(config))
+
+A = pipre.ParCSRMatrix()
+A.loadFromFile(fnA)
+b = pipre.ParMatrix()
+b.loadFromFile(fnb)
+if b.getSize() == 0:
+    b.resize(A.getRows(), 1)
+    b.fill(1)
+
+# transfer the matrix and vector to gpu 0.
+dev = pipre.Device("cuda:0")
+A = A.toDevice(dev)
+b = b.toDevice(dev)
+
+# use gpu 0 to finish the computation.
+precond = pipre.createPrecond(params["preconditioner"])
+precond.setup(A)
+solver = pipre.createSolver(params["solver"])
+solver.setup(A)
+
+out = solver.solve(precond, A, b)
