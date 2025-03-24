9475a6393bfd5ccc74f680c71232f5098577bc57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:06 +0000

    Add package java-persistence-with-mybatis-3-zhongwenban-fix1/2025.3.24.0/java_persistence_with_mybatis_3_zhongwenban_fix1-2025.3.24.0-py3-none-any.whl

---
+++ packages/java-persistence-with-mybatis-3-zhongwenban-fix1/java_persistence_with_mybatis_3_zhongwenban_fix1-2025.3.24.0-py3-none-any.whl/JavaPersistenceWithMybatis3ZhongwenbanFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-persistence-with-mybatis-3-zhongwenban-fix1
+https://github.com/apachecn/java-persistence-with-mybatis-3-zhongwenban-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.0"
