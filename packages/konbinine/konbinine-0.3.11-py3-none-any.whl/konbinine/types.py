11983fd58cba46dda907da23ee8c1d2b0848bb91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:05:21 +0000

    Add package konbinine/0.3.11/konbinine-0.3.11-py3-none-any.whl

---
+++ packages/konbinine/konbinine-0.3.11-py3-none-any.whl/konbinine/types.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import datetime
+from typing import Literal, TypedDict
+
+
+class TSgUploadedMovie(TypedDict):
+    url: str
+    name: str
+    content_type: str
+    link_type: str
+    type: str
+    id: int
+
+
+class THumanUser(TypedDict):
+    id: int
+    name: str
+    type: Literal["HumanUser"]
+
+
+class TNoteThreadReadData(TypedDict):
+    type: Literal["Note", "Attachment", "Reply"]
+    id: int
+    content: str
+    user: THumanUser
+    created_by: THumanUser
+    created_at: datetime.datetime
+
+
+class TNoteThreadCustomEntityFields(TypedDict):
+    Note: list[str]
+    Reply: list[str]
+    Attachment: list[str]
