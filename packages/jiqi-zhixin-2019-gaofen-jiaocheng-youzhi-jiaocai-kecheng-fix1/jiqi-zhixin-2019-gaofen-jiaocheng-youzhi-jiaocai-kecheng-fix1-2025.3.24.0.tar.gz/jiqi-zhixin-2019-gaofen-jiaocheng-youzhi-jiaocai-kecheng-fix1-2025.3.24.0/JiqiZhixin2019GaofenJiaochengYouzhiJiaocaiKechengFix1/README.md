2bf4d42c944f1eba342fc4da75786d9221aea8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:16 +0000

    Add package jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1/2025.3.24.0/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1-2025.3.24.0.tar.gz

---
+++ packages/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1-2025.3.24.0.tar.gz/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1-2025.3.24.0/JiqiZhixin2019GaofenJiaochengYouzhiJiaocaiKechengFix1/README.md
@@ -0,0 +1,27 @@
+# 机器之心 2019 高分教程：优质教材、课程
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng
+docker run -tid -p <port>:80 apachecn0/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng
+jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g jiqi-zhixin-erlingyijiu-gaofen-jiaocheng-youzhi-jiaocai-kecheng
+jiqi-zhixin-erlingyijiu-gaofen-jiaocheng-youzhi-jiaocai-kecheng <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1-2025.3.24.0.tar.gz/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1-2025.3.24.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import JiqiZhixin2019GaofenJiaochengYouzhiJiaocaiKechengFix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('JiqiZhixin2019GaofenJiaochengYouzhiJiaocaiKechengFix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1",
+    version=JiqiZhixin2019GaofenJiaochengYouzhiJiaocaiKechengFix1.__version__,
+    url="https://github.com/apachecn/jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1",
+    author=JiqiZhixin2019GaofenJiaochengYouzhiJiaocaiKechengFix1.__author__,
+    author_email=JiqiZhixin2019GaofenJiaochengYouzhiJiaocaiKechengFix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="机器之心 2019 高分教程：优质教材、课程",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "jiqi-zhixin-2019-gaofen-jiaocheng-youzhi-jiaocai-kecheng-fix1=JiqiZhixin2019GaofenJiaochengYouzhiJiaocaiKechengFix1.__main__:main",
+            "JiqiZhixin2019GaofenJiaochengYouzhiJiaocaiKechengFix1=JiqiZhixin2019GaofenJiaochengYouzhiJiaocaiKechengFix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
