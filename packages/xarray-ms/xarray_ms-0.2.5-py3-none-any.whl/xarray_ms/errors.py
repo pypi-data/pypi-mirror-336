1954fe2616a7e3d638de36f6500a010c0635dd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 17:04:57 +0000

    Add package xarray-ms/0.2.5/xarray_ms-0.2.5-py3-none-any.whl

---
+++ packages/xarray-ms/xarray_ms-0.2.5-py3-none-any.whl/xarray_ms/errors.py
@@ -0,0 +1,23 @@
+class IrregularGridWarning(UserWarning):
+  """Warning raised when the intervals associated
+  with each timestep are not homogenous"""
+
+
+class InvalidMeasurementSet(ValueError):
+  """Raised when the Measurement Set foreign key indexing is invalid"""
+
+
+class InvalidPartitionKey(ValueError):
+  """Raised when a string representing a partition key is invalid"""
+
+
+class PartitioningError(ValueError):
+  """Raised when a logical error is encountered during Measurement Set partitioning"""
+
+
+class MissingMeasuresInfo(ValueError):
+  """Raised when Measures information is missing from the column"""
+
+
+class MissingQuantumUnits(ValueError):
+  """Raised when QuantumUnits information is missing from the column"""
