96d04443143dd2dcd8b8f7000edc9dc0cb0d0eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:05 +0000

    Add package wavx/0.1.4/wavx-0.1.4-py2.py3-none-any.whl

---
+++ packages/wavx/wavx-0.1.4-py2.py3-none-any.whl/wavx/analysis/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+WavX 音频分析模块
+
+包含各种音频分析功能的模块集合
+"""
+
+from . import amplitude
+from . import spectrogram
+
+# 为方便使用，直接导出一些常用函数
+from .amplitude import analyze_amplitude, print_amplitude_info
