efe4b7c3dcb9cca705ffd895ea51ed7759ed0e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:07 +0000

    Add package herman-code/0.0.3/herman_code-0.0.3.tar.gz

---
+++ packages/herman-code/herman_code-0.0.3.tar.gz/herman_code-0.0.3/src/herman_code/templates/cli_script.py
@@ -0,0 +1,156 @@
+"""
+A template python script that uses command-line argument parsing.
+"""
+
+import argparse
+import logging
+import os
+import pprint
+from pathlib import Path
+# Third-party packages
+import pandas as pd
+from sqlalchemy import URL
+# Local packages
+from herman_code import __version__ as drapiVersion
+from herman_code.code import (choosePathToLog,
+                              getTimestamp,
+                              makeDirPath)
+
+if __name__ == "__main__":
+    # >>> `Argparse` arguments >>>
+    parser = argparse.ArgumentParser()
+
+    # Arguments: Main
+    pass
+
+    # Arguments: Meta-parameters
+    parser.add_argument("--LOG_LEVEL",
+                        default=10,
+                        type=int,
+                        help="""Increase output verbosity. See "logging" module's log level for valid values.""")
+
+    # Arguments: SQL connection settings
+    parser.add_argument("--SERVER",
+                        type=str,
+                        help="")
+    parser.add_argument("--DATABASE",
+                        type=str,
+                        help="")
+    parser.add_argument("--USER_DOMAIN",
+                        type=str,
+                        help="")
+    parser.add_argument("--USERNAME",
+                        default=os.environ["USER"],
+                        type=str,
+                        help="")
+    parser.add_argument("--USER_ID",
+                        default=None,
+                        help="")
+    parser.add_argument("--USER_PWD",
+                        default=None,
+                        help="")
+
+    argNamespace = parser.parse_args()
+
+    # Parsed arguments: Main
+    pass
+
+    # Parsed arguments: Meta-parameters
+    LOG_LEVEL = argNamespace.LOG_LEVEL
+
+    # Parsed arguments: SQL connection settings
+    SERVER = argNamespace.SERVER
+    DATABASE = argNamespace.DATABASE
+    USER_DOMAIN = argNamespace.USER_DOMAIN
+    USERNAME = argNamespace.USERNAME
+    USER_ID = argNamespace.USER_ID
+    USER_PWD = argNamespace.USER_PWD
+    # <<< `Argparse` arguments <<<
+
+    # >>> Argument checks >>>
+    # NOTE TODO Look into handling this natively with `argparse` by using `subcommands`. See "https://stackoverflow.com/questions/30457162/argparse-with-different-modes"
+    pass
+    # <<< Argument checks <<<
+
+    # Variables: Path construction: General
+    runTimestamp = getTimestamp()
+    thisFilePath = Path(__file__)
+    thisFileStem = thisFilePath.stem
+    currentWorkingDir = Path(os.getcwd()).absolute()
+    projectDir = currentWorkingDir
+    dataDir = projectDir.joinpath("data")
+    if dataDir:
+        inputDataDir = dataDir.joinpath("input")
+        intermediateDataDir = dataDir.joinpath("intermediate")
+        outputDataDir = dataDir.joinpath("output")
+        if intermediateDataDir:
+            runIntermediateDir = intermediateDataDir.joinpath(thisFileStem, runTimestamp)
+        if outputDataDir:
+            runOutputDir = outputDataDir.joinpath(thisFileStem, runTimestamp)
+    logsDir = projectDir.joinpath("logs")
+    if logsDir:
+        runLogsDir = logsDir.joinpath(thisFileStem)
+    sqlDir = projectDir.joinpath("sql")
+
+    # Variables: Path construction: Project-specific
+    pass
+
+    # Variables: SQL Parameters
+    if USER_ID:
+        userID = USER_ID[:]
+    else:
+        userID = fr"{USER_DOMAIN}\{USERNAME}"
+    if USER_PWD:
+        userPwd = USER_PWD
+    else:
+        raise Exception("Need password.")
+    connectionString = URL.create(drivername="mssql+pymssql",
+                                  username=userID,
+                                  password=userPwd,
+                                  host=SERVER,
+                                  database=DATABASE)
+
+    # Variables: Other
+    pass
+
+    # Directory creation: General
+    makeDirPath(runIntermediateDir)
+    makeDirPath(runOutputDir)
+    makeDirPath(runLogsDir)
+
+    # Logging block
+    logpath = runLogsDir.joinpath(f"log {runTimestamp}.log")
+    logFormat = logging.Formatter("""[%(asctime)s][%(levelname)s](%(funcName)s): %(message)s""")
+
+    logger = logging.getLogger(__name__)
+
+    fileHandler = logging.FileHandler(logpath)
+    fileHandler.setLevel(9)
+    fileHandler.setFormatter(logFormat)
+
+    streamHandler = logging.StreamHandler()
+    streamHandler.setLevel(LOG_LEVEL)
+    streamHandler.setFormatter(logFormat)
+
+    logger.addHandler(fileHandler)
+    logger.addHandler(streamHandler)
+
+    logger.setLevel(9)
+
+    logger.info(f"""Begin running "{choosePathToLog(path=thisFilePath, rootPath=projectDir)}".""")
+    logger.info(f"""DRAPI-Lemur version is "{drapiVersion}".""")
+    logger.info(f"""All other paths will be reported in debugging relative to the current working directory: "{choosePathToLog(path=projectDir, rootPath=projectDir)}".""")
+
+    argList = argNamespace._get_args() + argNamespace._get_kwargs()
+    argListString = pprint.pformat(argList)  # TODO Remove secrets from list to print, e.g., passwords.
+    logger.info(f"""Script arguments:\n{argListString}""")
+
+    # >>> Begin script body >>>
+
+    _ = pd
+
+    # Output location summary
+    logger.info(f"""Script output is located in the following directory: "{choosePathToLog(path=runOutputDir, rootPath=projectDir)}".""")
+
+    # <<< End script body <<<
+    logger.info(f"""Finished running "{choosePathToLog(path=thisFilePath, rootPath=projectDir)}".""")
