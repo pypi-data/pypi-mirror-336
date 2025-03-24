89753cee37b52c6fa339cb6cde3a4baa18c862b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:04:11 +0000

    Add package liteauto/0.2.14/liteauto-0.2.14.tar.gz

---
+++ packages/liteauto/liteauto-0.2.14.tar.gz/liteauto-0.2.14/liteauto/__init__.py
@@ -0,0 +1,16 @@
+from .searchlite import google,duckduckgo,wikipedia
+from .parselite import parse
+from .visionlite import wlsplit,wlanswer,wltopk,wlsimchunks
+
+
+from .utils.common import web,compress,get_summaries,compress_sequential,summary,web_top_chunk
+from .utils.utilities import relevant_chunk,get_query_relevant_chunks_list
+from .utils import read_data,write_data
+
+from .gmaillite import GmailAutomation,gmail,automail
+
+from .project_to_prompt import project_to_prompt,project_to_markdown,ProjectToPrompt
+
+from .arxivlite import get_todays_arxiv_papers,research_paper_analysis,get_arxiv_papers,ArxivPlaywrightAdvancedSearch
+
+from .deep_research import DeepResearchSystem,deep_research_email
