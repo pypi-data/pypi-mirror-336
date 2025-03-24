872f3196d471b61080965ab92573889f262a41c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:18:05 +0000

    Add package beginnersbook-java-zifuchuan-jiaocheng-chuyi-fix1/2025.3.24.0/beginnersbook-java-zifuchuan-jiaocheng-chuyi-fix1-2025.3.24.0.tar.gz

---
+++ packages/beginnersbook-java-zifuchuan-jiaocheng-chuyi-fix1/beginnersbook-java-zifuchuan-jiaocheng-chuyi-fix1-2025.3.24.0.tar.gz/beginnersbook-java-zifuchuan-jiaocheng-chuyi-fix1-2025.3.24.0/BeginnersbookJavaZifuchuanJiaochengChuyiFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""beginnersbook-java-zifuchuan-jiaocheng-chuyi-fix1
+https://github.com/apachecn/beginnersbook-java-zifuchuan-jiaocheng-chuyi-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.0"
