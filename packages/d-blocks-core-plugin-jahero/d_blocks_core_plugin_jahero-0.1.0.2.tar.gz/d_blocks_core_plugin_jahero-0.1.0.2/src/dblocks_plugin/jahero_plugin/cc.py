7b26858bb9c32ae2a279f7fba716ac762d9e25db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 17:04:38 +0000

    Add package d-blocks-core-plugin-jahero/0.1.0.2/d_blocks_core_plugin_jahero-0.1.0.2.tar.gz

---
+++ packages/d-blocks-core-plugin-jahero/d_blocks_core_plugin_jahero-0.1.0.2.tar.gz/d_blocks_core_plugin_jahero-0.1.0.2/src/dblocks_plugin/jahero_plugin/cc.py
@@ -0,0 +1,245 @@
+import re
+from pathlib import Path
+
+from dblocks_core import exc
+from dblocks_core.config.config import logger
+from dblocks_core.model import config_model, plugin_model
+from rich.console import Console
+from rich.prompt import Prompt
+
+_VALID_SCENARIOS = ["drop", "create", "cleanup", "drop-only"]
+
+# regular expressions building blocks
+_WS = r"\s+"
+_MAYBE_WS = r"\s*"
+_CREATE = "create"
+_SET = "set +|multiset +"
+_TABLE = "table"
+_IDENTIFIER = "[a-z0-9_.]+"
+_AS = "as"
+_PATTERN = (
+    _CREATE
+    + _WS
+    + "("
+    + _SET
+    + ")?"
+    + _MAYBE_WS
+    + _TABLE
+    + _WS
+    + "(?P<IDENT>"
+    + _IDENTIFIER
+    + ")"
+)
+_PATTERN2 = (
+    _CREATE
+    + _WS
+    + _TABLE
+    + _WS
+    + "(?P<IDENT>"
+    + _IDENTIFIER
+    + ")"
+    + _AS
+    + _WS
+    + _IDENTIFIER
+)
+
+# regular expressions used to find what is needed
+_re_create = re.compile(_PATTERN, re.I)
+_re_create2 = re.compile(_PATTERN2, re.I)
+
+
+# the template inected into SQL files
+_TEMPLATE_CREATE = """-----------------------------------------------------------------------
+/*--conditional_create--*/
+select count(*) from dbc.tablesV 
+where tablename='<table>' and databasename=( select database )
+having count(*) > 0;
+
+.IF ERRORCODE <> 0 THEN .QUIT 10;
+.IF ACTIVITYCOUNT = 0 THEN .GOTO TABLE_DOES_NOT_EXIST;
+.REMARK 'tabulka EXISTUJE'
+.GOTO KONEC
+
+.LABEL TABLE_DOES_NOT_EXIST
+-----------------------------------------------------------------------
+"""
+
+_TEMPLATE_DROP = """-----------------------------------------------------------------------
+/*--conditional_create--*/
+select count(*) from dbc.tablesV 
+where tablename='<table>' and databasename=( select database )
+having count(*) > 0;
+
+.IF ERRORCODE <> 0 THEN .QUIT 10;
+.IF ACTIVITYCOUNT = 0 THEN .GOTO TABLE_DOES_NOT_EXIST;
+.REMARK 'tabulka EXISTUJE'
+drop table <table>;
+
+.LABEL TABLE_DOES_NOT_EXIST
+-----------------------------------------------------------------------
+"""
+
+
+def _load_script(file: Path, input_encoding: str):
+    """loads the script, and filters out the header and footer of the conditional create.
+
+    Args:
+        file (Path): path to the file
+        input_encoding (str): encoding used to load the file
+
+    Returns:
+        _type_: _description_
+    """
+    outLines = []
+    with open(file, "r", encoding=input_encoding) as f:
+        script = f.read()
+
+        # pokud neobsahuje ".LABEL TABLE_DOES_NOT_EXIST", není tam co řešit
+        if ".LABEL TABLE_DOES_NOT_EXIST" not in script:
+            return script
+
+        # jinak ale musíme filtrovat
+        lines = script.splitlines()
+        skipThis = True
+        readNext = False
+
+        for line in lines:
+            if line == ".LABEL TABLE_DOES_NOT_EXIST":
+                readNext = True
+                continue
+            if readNext:
+                skipThis = False
+                readNext = False
+                continue
+
+            if line == ".LABEL KONEC":
+                break
+
+            if skipThis:
+                continue
+
+            outLines.append(line)
+
+    return "\n".join(outLines)
+
+
+def _is_create_script(insides: str, file: str):
+    if m := _re_create.search(insides):
+        parts = m.group("IDENT").split(".")
+        if len(parts) == 1:
+            return True, parts[0]
+        return True, parts[1]
+    elif m := _re_create2.search(insides):
+        parts = m.group("IDENT").split(".")
+        if len(parts) == 1:
+            return True, parts[0]
+        return True, parts[1]
+    else:
+        raise exc.DOperationsError(f"did not find name of the table: {file}")
+
+
+def _change_to_conditional(
+    file: Path,
+    output_encoding: str,
+    input_encoding: str,
+    create_or_drop="create",
+):
+    try:
+        old_script = _load_script(file, input_encoding)
+    except UnicodeDecodeError as e:
+        raise exc.DOperationsError(f"encoding error: {file.as_posix()}")
+
+    is_create, creates_table = _is_create_script(old_script, file)
+
+    if not is_create:
+        return False
+
+    new_script = ""
+
+    if create_or_drop == "cleanup":
+        new_script = old_script
+
+    else:
+        TEMPLATE = _TEMPLATE_CREATE if create_or_drop == "create" else _TEMPLATE_DROP
+        inject = TEMPLATE.replace("<table>", creates_table)
+        if create_or_drop == "drop-only":
+            old_script = "--only drop the table, do not create\n\n"
+        new_script = inject + old_script + "\n" + ".LABEL KONEC"
+
+    # save
+    if new_script != old_script:
+        logger.info(f"writing: {file.as_posix()}")
+        try:
+            with open(file, "w", encoding=output_encoding) as f:
+                f.write(new_script)
+        except Exception as e:
+            logger.error(f"check this file, it may have been lost: {file.as_posix()}")
+
+
+class CC(plugin_model.PluginWalker):
+    def __init__(self):
+        self.files = []
+
+    def before(
+        self,
+        path: Path,
+        environment: str | None,
+        cfg: config_model.Config,
+        **kwargs,
+    ):
+        """This function is executed before the walk starts."""
+        pass
+
+    def walker(
+        self,
+        path: Path,
+        environment: str | None,
+        cfg: config_model.Config,
+        **kwargs,
+    ):
+        """This function is executed for each file we walk through."""
+        ext = self.suffix.lower()
+        if ext in (".bteq", ".sql"):
+            self.files.append(path)
+
+    def after(
+        self,
+        path: Path,
+        environment: str | None,
+        cfg: config_model.Config,
+        **kwargs,
+    ):
+        self._conditional_create()
+
+    def _conditional_create(
+        self,
+        scenario="create",
+        input_encoding: str = "utf-8",
+        output_encoding: str = "utf-8",
+        max_files: int = 20,
+    ):
+        """
+        Scans a folder for files with .sql or .bteq extension, and tries to insert conditional create header and footer in them.
+        Raises a ContentError when the number of files is > max_files.
+
+        Args:
+            folder (str): input folder
+            scenario (str, optional): ["drop", "create", "cleanup", "drop-only"], defaults to "create".
+            input_encoding (str, optional): defaults to "utf-8".
+            output_encoding (str, optional): defaults to "utf-8".
+            max_files (int, optional): defaults to 20.
+
+        Raises:
+            ContentError: raised when number of files is too great, or when one of the files can not be patched.
+        """
+        files = self.files
+        if len(files) > max_files:
+            console = Console()
+            console.print(f"Expected to get at max {max_files}, got {len(files)}")
+            answer = Prompt.ask("continue? [Y/n]")
+            if answer != "Y":
+                console.print("Canceled.", style="bold red")
+                return
+
+        for f in files:
+            _change_to_conditional(f, output_encoding, input_encoding, scenario)
---
+++ packages/d-blocks-core-plugin-jahero/d_blocks_core_plugin_jahero-0.1.0.2.tar.gz/d_blocks_core_plugin_jahero-0.1.0.2/src/dblocks_plugin/jahero_plugin/deploy.py
@@ -0,0 +1,252 @@
+import re
+import stat
+from pathlib import Path
+from textwrap import dedent
+
+from dblocks_core import exc
+from dblocks_core.config import config
+from dblocks_core.config.config import logger
+from dblocks_core.deployer import fsequencer
+from dblocks_core.model import config_model, plugin_model
+
+from dblocks_plugin.jahero_plugin import plug_config, plug_model
+
+
+class Dpl(plugin_model.PluginWalker):
+    def __init__(self):
+        self.batch: fsequencer.DeploymentBatch | None = None
+
+    def before(
+        self,
+        path: Path,
+        environment: str | None,
+        cfg: config_model.Config,
+        **kwargs,
+    ):
+        """This function is executed before the walk starts."""
+        # get plugin config
+        plug_cfg = plug_config.load_config(path)
+
+        # deploy
+        pkg_cfg = cfg.packager
+        if pkg_cfg.case_insensitive_dirs:
+            logger.info("case insensitive search")
+            subdirs = case_insensitive_search(path, pkg_cfg.steps_subdir)
+
+            if subdirs is None:
+                raise exc.DOperationsError(
+                    f"subdir not found: {pkg_cfg.steps_subdir}: in {path}"
+                )
+            root_dir = path / subdirs
+        else:
+            logger.warning("case SENSITIVE search")
+            root_dir = path / pkg_cfg.steps_subdir
+
+        # sanity check
+        if not root_dir.is_dir():
+            raise exc.DOperationsError(f"directory not found: {root_dir}")
+
+        # get a log directory in the package
+        log_dir = path / "log"
+        log_dir.mkdir(exist_ok=True)
+
+        self.batch = fsequencer.create_batch(root_dir=root_dir, tgr=None)
+        if len(self.batch.steps) == 0:
+            raise exc.DOperationsError("Empty batch.")
+
+        exec_scripts = []
+        deployment_dir = root_dir.parent / "_deployment"
+        log_dir = path / "log"
+
+        deployment_dir.mkdir(exist_ok=True, parents=True)
+        log_dir.mkdir(exist_ok=True, parents=True)
+
+        for step in self.batch.steps:
+            logger.info(f"creating step: {step.name}")
+            statements, prev_db = [], None
+
+            # skip empty steps
+            if len(step.files) == 0:
+                logger.error(f"empty step: {step.name}")
+                continue
+
+            bteq_file = deployment_dir / (step.name + ".bteq")
+            exec_scripts.append(_get_bteq_call(bteq_file))
+
+            for f in step.files:
+                db = f.default_db
+
+                if db is not None and db != prev_db:
+                    statements.append(f"\ndatabase {_get_database(db, plug_cfg)};")
+                statements.append(f".run file = '{f.file.as_posix()}'")
+
+                prev_db = db
+
+            if statements:
+                script = _get_header() + "\n".join(statements) + _get_footer()
+                bteq_file.write_text(script, encoding="utf-8")
+            else:
+                logger.error(f"EMPTY STEP: {step.name}")
+
+        runme_file = deployment_dir / "runme.sh"
+        script = "#!/bin/bash\n\n" + "\n\n".join(exec_scripts)
+        runme_file.write_text(script, encoding="utf-8")
+        runme_file.chmod(runme_file.stat().st_mode | stat.S_IEXEC)
+        # os.chmod(runme_file, 755)
+
+    def walker(
+        self,
+        path: Path,
+        environment: str | None,
+        cfg: config_model.Config,
+        **kwargs,
+    ):
+        pass
+
+    def after(
+        self,
+        path: Path,
+        environment: str | None,
+        cfg: config_model.Config,
+        **kwargs,
+    ):
+        pass
+
+
+def main():
+    path = Path("/home/jan/o2/feature/bidev-7434-vodwh-na/gen-sk/output")
+    plugin = "dpl"
+    environment = None
+    cfg_dict = config.load_config_dict()
+    cfg = config.load_config()
+
+    path_ = Path(path)
+
+    walker = Dpl()
+    if path_.exists():
+        if path_.is_file():
+            walker.walker(
+                path_,
+                environment,
+                cfg,  # kwargs
+                cfg_dict=cfg_dict,
+            )
+        elif path_.is_dir():
+            for f in path_.rglob("*.*"):
+                walker.walker(
+                    f,
+                    environment,
+                    cfg,  # kwargs
+                    cfg_dict=cfg_dict,
+                )
+    walker.before(
+        path_,
+        environment,
+        cfg,
+        # kwargs
+        cfg_dict=cfg_dict,
+    )
+    walker.after(
+        path_,
+        environment,
+        cfg,
+        # kwargs
+        cfg_dict=cfg_dict,
+    )
+
+
+def case_insensitive_search(root: Path, subdir: Path) -> Path | None:
+    wanted = _path_to_directories(subdir)
+    wanted = [s.lower() for s in wanted]
+    logger.info(f"searching in: {root}")
+    logger.info(f"searching for: {wanted}")
+    found_dirs = []
+
+    for i in range(len(wanted)):
+        children_dir_names = [
+            (d.name.lower(), d.name) for d in root.glob("*") if d.is_dir
+        ]
+        found = False
+        for name_lower, name in children_dir_names:
+            if name_lower == wanted[i]:
+                found = True
+                found_dirs.append(name)
+                root = root / name
+                break
+        if not found:
+            return None
+
+    retval = Path("")
+    for d in found_dirs:
+        retval = retval / d
+    return retval
+
+
+def _path_to_directories(path: Path) -> list[str]:
+    elements = []
+    curr: Path = path
+    prev: Path | None = None
+
+    while curr != prev:
+        if curr.name:
+            elements.insert(0, curr.name)
+        prev = curr
+        curr = curr.parent
+
+    return elements
+
+
+def _get_header() -> str:
+    return dedent(
+        """
+        -----------------------------------------------------------
+        .SET SESSION CHARSET 'UTF8'
+        .SET WIDTH 65531
+        .SET ERRORLEVEL UNKNOWN SEVERITY 8;
+        .SET ERROROUT STDOUT;
+        .SET MAXERROR 1
+        -----------------------------------------------------------
+        .RUN FILE='/home/jan/Vaults/o2/logon_prod.sql'
+        -----------------------------------------------------------
+        SET SESSION DATEFORM=ANSIDATE;
+        .SET ERRORLEVEL 3624 SEVERITY 0;            -- collect stats - pro neex. stat - projde
+        --.SET ERRORLEVEL 3803 SEVERITY 0;          -- projde create tabulky ktera existuje
+        --.SET ERRORLEVEL 3807 SEVERITY 0;          -- projde drop tabulky ktera neexistuje
+    """
+    )
+
+
+def _get_footer() -> str:
+    return ""
+
+
+def _get_bteq_call(f: Path) -> str:
+    name, stem = f.name, f.stem
+    log_file = f.parent / "log" / f"{stem}.log"
+    return dedent(
+        f"""
+        echo "running {stem}"
+        bteq < {f.as_posix()} &>>{log_file.as_posix()}
+        retval=$?
+        if [ $retval -ne 0 ]; then
+            echo "===============ERROR================="
+            echo "====================================="
+            exit $retval
+        fi           
+    """
+    )
+
+
+def _get_database(db: str, cfg: plug_model.PluginConfig) -> str:
+    for replacement in cfg.replacements:
+        db = re.sub(
+            replacement.replace_from,
+            replacement.replace_to,
+            db,
+            flags=re.I | re.X,
+        )
+    return db
+
+
+if __name__ == "__main__":
+    main()
