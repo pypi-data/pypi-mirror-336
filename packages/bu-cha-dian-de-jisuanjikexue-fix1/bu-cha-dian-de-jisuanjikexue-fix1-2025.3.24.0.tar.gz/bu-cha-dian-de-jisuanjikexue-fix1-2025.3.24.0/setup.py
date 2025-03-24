627ebd79deae9a5e580b14684aa0d2987a5a5f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:08 +0000

    Add package bu-cha-dian-de-jisuanjikexue-fix1/2025.3.24.0/bu-cha-dian-de-jisuanjikexue-fix1-2025.3.24.0.tar.gz

---
+++ packages/bu-cha-dian-de-jisuanjikexue-fix1/bu-cha-dian-de-jisuanjikexue-fix1-2025.3.24.0.tar.gz/bu-cha-dian-de-jisuanjikexue-fix1-2025.3.24.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import BuChaDianDeJisuanjikexueFix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('BuChaDianDeJisuanjikexueFix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="bu-cha-dian-de-jisuanjikexue-fix1",
+    version=BuChaDianDeJisuanjikexueFix1.__version__,
+    url="https://github.com/apachecn/bu-cha-dian-de-jisuanjikexue-fix1",
+    author=BuChaDianDeJisuanjikexueFix1.__author__,
+    author_email=BuChaDianDeJisuanjikexueFix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="不插电的计算机科学",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "bu-cha-dian-de-jisuanjikexue-fix1=BuChaDianDeJisuanjikexueFix1.__main__:main",
+            "BuChaDianDeJisuanjikexueFix1=BuChaDianDeJisuanjikexueFix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
