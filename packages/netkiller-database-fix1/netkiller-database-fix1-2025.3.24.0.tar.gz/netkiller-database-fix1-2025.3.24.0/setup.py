22c0ff08da491c8deb196c24c757a4b84bb703b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 04:04:04 +0000

    Add package netkiller-database-fix1/2025.3.24.0/netkiller-database-fix1-2025.3.24.0.tar.gz

---
+++ packages/netkiller-database-fix1/netkiller-database-fix1-2025.3.24.0.tar.gz/netkiller-database-fix1-2025.3.24.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import NetkillerDatabaseFix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('NetkillerDatabaseFix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="netkiller-database-fix1",
+    version=NetkillerDatabaseFix1.__version__,
+    url="https://github.com/apachecn/netkiller-database-fix1",
+    author=NetkillerDatabaseFix1.__author__,
+    author_email=NetkillerDatabaseFix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="Netkiller-Database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "netkiller-database-fix1=NetkillerDatabaseFix1.__main__:main",
+            "NetkillerDatabaseFix1=NetkillerDatabaseFix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
