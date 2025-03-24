15aa0519f97af9f8d70e5ec03b9baa007cb15114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:03:57 +0000

    Add package shared-models-stocksblitz/0.1.4/shared_models_stocksblitz-0.1.4.tar.gz

---
+++ packages/shared-models-stocksblitz/shared_models_stocksblitz-0.1.4.tar.gz/shared_models_stocksblitz-0.1.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shared_models_stocksblitz",
+    version="0.1.4",
+    packages=find_packages(),  # Automatically finds all submodules like `models`
+    install_requires=["sqlalchemy", "psycopg2"],
+    include_package_data=True,  # Include non-code files if present
+)
