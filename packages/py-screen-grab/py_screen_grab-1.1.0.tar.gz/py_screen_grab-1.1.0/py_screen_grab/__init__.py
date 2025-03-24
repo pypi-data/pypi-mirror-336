d82d7586d8db5c7523552c2d11b52d48e9b48567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:12 +0000

    Add package py-screen-grab/1.1.0/py_screen_grab-1.1.0.tar.gz

---
+++ packages/py-screen-grab/py_screen_grab-1.1.0.tar.gz/py_screen_grab-1.1.0/py_screen_grab/__init__.py
@@ -0,0 +1,8 @@
+"""
+PyScreenGrab - A simple screen recording tool
+"""
+
+from .screen_grabber import ScreenGrabber
+
+__version__ = "1.0.0"
+__all__ = ["ScreenGrabber"]
---
+++ packages/py-screen-grab/py_screen_grab-1.1.0.tar.gz/py_screen_grab-1.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+__version__ = "1.1.0"
+    
+setup(
+    name="py-screen-grab",
+    version=__version__,
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=[
+        "mss>=6.1.0",
+        "numpy>=1.19.0",
+        "opencv-python>=4.5.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "screengrab=py_screen_grab.cli:main",
+        ],
+    },
+    author="twid_yuni",
+    author_email="jyhee7935@naver.com",
+    description="A simple screen recording tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yeonhee7935/py-screen-grab",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: POSIX :: Linux', 
+    ],
+    python_requires=">=3.6",
+    test_suite="tests",
+    tests_require=[
+        "pytest>=6.0.0",
+        "pytest-cov>=2.10.0",
+    ],
+) 
