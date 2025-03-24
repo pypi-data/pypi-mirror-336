eb0020beecb8fb75aa530ddb7a9ca6a0f07eccb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:06 +0000

    Add package cstore-package/0.1.1/cstore_package-0.1.1.tar.gz

---
+++ packages/cstore-package/cstore_package-0.1.1.tar.gz/cstore_package-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cstore-package",
+    version="0.1.1",
+    url="https://github.com/AliceAX24/Cstore",
+    packages=find_packages(),
+)
