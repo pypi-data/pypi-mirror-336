77935ec4ccbe8bc9c2eed77fef46778e5e3596e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 17:04:38 +0000

    Add package ymj-tool/1.0.8/ymj_tool-1.0.8.tar.gz

---
+++ packages/ymj-tool/ymj_tool-1.0.8.tar.gz/ymj_tool-1.0.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ymj_tool",
+    version="1.0.8",
+    description="A useful tool for ...",  # 修改为你的描述
+    author="ssseVennn",
+    author_email="739369987@qq.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
