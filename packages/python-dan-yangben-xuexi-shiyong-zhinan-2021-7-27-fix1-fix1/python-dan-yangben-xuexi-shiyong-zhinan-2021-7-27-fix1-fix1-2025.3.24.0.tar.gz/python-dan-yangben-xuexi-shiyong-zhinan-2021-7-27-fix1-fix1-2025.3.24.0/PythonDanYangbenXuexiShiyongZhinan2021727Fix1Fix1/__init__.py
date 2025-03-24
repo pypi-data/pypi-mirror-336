62a81054bf283cc5dee43387806ff10767166be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 17:04:48 +0000

    Add package python-dan-yangben-xuexi-shiyong-zhinan-2021-7-27-fix1-fix1/2025.3.24.0/python-dan-yangben-xuexi-shiyong-zhinan-2021-7-27-fix1-fix1-2025.3.24.0.tar.gz

---
+++ packages/python-dan-yangben-xuexi-shiyong-zhinan-2021-7-27-fix1-fix1/python-dan-yangben-xuexi-shiyong-zhinan-2021-7-27-fix1-fix1-2025.3.24.0.tar.gz/python-dan-yangben-xuexi-shiyong-zhinan-2021-7-27-fix1-fix1-2025.3.24.0/PythonDanYangbenXuexiShiyongZhinan2021727Fix1Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""python-dan-yangben-xuexi-shiyong-zhinan-2021-7-27-fix1-fix1
+https://github.com/apachecn/python-dan-yangben-xuexi-shiyong-zhinan-2021-7-27-fix1-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.0"
