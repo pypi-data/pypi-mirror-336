031bb1feebf3ebfa27aa5ae1847a47fa8715c589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 05:03:47 +0000

    Add package haillo/0.3.0/haillo-0.3.0-py3-none-any.whl

---
+++ packages/haillo/haillo-0.3.0-py3-none-any.whl/haillo/package.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, division, print_function
+
+__version__ = "0.3.0"
+dependencies = ["flask==3.1.0",
+                "huckle>=5.7.0,<6.0.0",
+                "hcli_hai>=3.3.0,<4.0.0"]
