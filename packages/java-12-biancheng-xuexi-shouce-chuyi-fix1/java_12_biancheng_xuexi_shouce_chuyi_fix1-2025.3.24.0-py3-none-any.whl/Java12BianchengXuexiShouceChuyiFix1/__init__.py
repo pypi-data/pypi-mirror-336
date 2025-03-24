35b2e5d4c66775431a5c62062149d7dc97c21c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:05:48 +0000

    Add package java-12-biancheng-xuexi-shouce-chuyi-fix1/2025.3.24.0/java_12_biancheng_xuexi_shouce_chuyi_fix1-2025.3.24.0-py3-none-any.whl

---
+++ packages/java-12-biancheng-xuexi-shouce-chuyi-fix1/java_12_biancheng_xuexi_shouce_chuyi_fix1-2025.3.24.0-py3-none-any.whl/Java12BianchengXuexiShouceChuyiFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-12-biancheng-xuexi-shouce-chuyi-fix1
+https://github.com/apachecn/java-12-biancheng-xuexi-shouce-chuyi-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.0"
