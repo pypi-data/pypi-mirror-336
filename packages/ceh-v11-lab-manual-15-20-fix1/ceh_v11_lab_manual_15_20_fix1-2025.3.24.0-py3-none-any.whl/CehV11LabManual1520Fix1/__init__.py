7222f0d426d479d2a4568303b4b9bb972ecf133e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:00 +0000

    Add package ceh-v11-lab-manual-15-20-fix1/2025.3.24.0/ceh_v11_lab_manual_15_20_fix1-2025.3.24.0-py3-none-any.whl

---
+++ packages/ceh-v11-lab-manual-15-20-fix1/ceh_v11_lab_manual_15_20_fix1-2025.3.24.0-py3-none-any.whl/CehV11LabManual1520Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ceh-v11-lab-manual-15-20-fix1
+https://github.com/apachecn/ceh-v11-lab-manual-15-20-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.0"
