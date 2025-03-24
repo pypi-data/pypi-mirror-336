fc7d8052f0e12f3b640b59fd24ad82104c913c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 17:04:40 +0000

    Add package py-bitcoinkernel/0.1.0a3/py_bitcoinkernel-0.1.0a3-cp313-cp313-win_amd64.whl

---
+++ packages/py-bitcoinkernel/py_bitcoinkernel-0.1.0a3-cp313-cp313-win_amd64.whl/pbk/validation.py
@@ -0,0 +1,16 @@
+import pbk.capi.bindings as k
+import pbk.util.callbacks
+
+
+class ValidationInterfaceCallbacks(k.kernel_ValidationInterfaceCallbacks):
+    def __init__(self, user_data=None, **callbacks):
+        super().__init__()
+        pbk.util.callbacks._initialize_callbacks(self, user_data, **callbacks)
+
+
+default_validation_callbacks = ValidationInterfaceCallbacks(
+    user_data=None,
+    block_checked=lambda user_data, block, state: print(
+        f"block_checked: block: {block}, state: {state}"
+    ),
+)
