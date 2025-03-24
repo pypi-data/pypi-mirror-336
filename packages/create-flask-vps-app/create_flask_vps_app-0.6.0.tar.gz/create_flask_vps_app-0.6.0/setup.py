8935382dd491e6f2e505379aa7f78ed6d778e4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:05:14 +0000

    Add package create-flask-vps-app/0.6.0/create_flask_vps_app-0.6.0.tar.gz

---
+++ packages/create-flask-vps-app/create_flask_vps_app-0.6.0.tar.gz/create_flask_vps_app-0.6.0/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="create-flask-vps-app",
+    version="0.6.0",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'create_flask_vps_app': ['templates/**/*'],
+    },
+    install_requires=[
+        "jinja2",  # For rendering templates
+        "flask",
+        "python-dotenv",
+        "fabric",
+        "gunicorn",
+        "flask-cors",
+    ],
+    entry_points={
+        "console_scripts": [
+            "create-flask-vps-app=create_flask_vps_app.main:main",
+        ],
+    },
+    author="Alireza Bashiri",
+    author_email="al3rez@gmail.com",
+    description="A CLI tool to scaffold a Flask API project with deployment setup.",
+    long_description=open("README.md").read() if os.path.exists("README.md") else "",
+    long_description_content_type="text/markdown",
+    url="https://github.com/al3rez/create-flask-vps-app",
+    python_requires=">=3.6",
+)
