36f781f37e01c4188e832a30638d231d738cc6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:05:50 +0000

    Add package streambricks/0.2.5/streambricks-0.2.5-py3-none-any.whl

---
+++ packages/streambricks/streambricks-0.2.5-py3-none-any.whl/streambricks/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "0.2.5"
+
+
+from streambricks.widgets.model_widget import (
+    render_model_form as model_edit,
+    render_model_readonly as model_display,
+)
+from streambricks.widgets.multi_select import multiselect, MultiSelectItem
+from streambricks.widgets.model_selector import (
+    model_selector,
+    model_selector as llm_model_selector,
+)
+from streambricks.helpers import run
+from streambricks.state import State
+
+__all__ = [
+    "MultiSelectItem",
+    "State",
+    "llm_model_selector",
+    "model_display",
+    "model_edit",
+    "model_selector",
+    "multiselect",
+    "run",
+]
