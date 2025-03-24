6161e8e758c46195bb36437fe8fc2345821beb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:18:19 +0000

    Add package project-visualization-tool-GDeLuisi/0.2.0/project_visualization_tool_gdeluisi-0.2.0.tar.gz

---
+++ packages/project-visualization-tool-GDeLuisi/project_visualization_tool_gdeluisi-0.2.0.tar.gz/project_visualization_tool_gdeluisi-0.2.0/tests/test_helper.py
@@ -0,0 +1,5 @@
+from pytest import mark
+from src.utility.helper import infer_programming_language
+@mark.parametrize("files",[("src/app.py","tests/report.txt","tests/test_dummy.py","tests/test_file_parser.py"),(".github/workflows/test-dev.yml",".github/workflows/testpypi_publish.yml",".gitignore","LICENSE","RAD.docx","README.md","main.py","pyproject.toml","requirements.txt","src/_internal/__init__.py","src/_internal/data_preprocessing.py","src/_internal/data_typing.py","src/_internal/file_parser.py","src/_internal/git_mining.py","src/_internal/info/ext.json","src/app/__init__.py","src/app/app.py","src/app/cli.py","src/gui/__init__.py","src/gui/components.py","src/gui/pages/homepage.py","src/utility/__init__.py","src/utility/logging_configs/logs_config_file.json","src/utility/logging_configs/logs_config_file_old.json","src/utility/logs.py","tests/test_cli.py","tests/test_data_preprocessing.py","tests/test_dummy.py","tests/test_file_parser.py","tests/test_git_miner.py")])
+def test_infer_programming_language(files):
+    assert infer_programming_language(files)==[".py"]
