8a154c11d8ebe6efef062129d3dff56abc3ee2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:05 +0000

    Add package fcst/0.8.2/fcst-0.8.2.tar.gz

---
+++ packages/fcst/fcst-0.8.2.tar.gz/fcst-0.8.2/src/fcst/metrics/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+metrics sub-package
+~~~~
+Provides forecasting metrics.
+"""
+
+from ._metrics import mape, smape, mae, mse, rmse
+
+__all__ = ["mape", "smape", "mae", "mse", "rmse"]
