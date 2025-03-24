8cd79f7fc1b2dff40f91e7ba69267b14da5dbef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:02 +0000

    Add package pyrovider/10.0.3/pyrovider-10.0.3.tar.gz

---
+++ packages/pyrovider/pyrovider-10.0.3.tar.gz/pyrovider-10.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '10.0.3'
+DESCRIPTION = 'Dependency confiuse Test'
+LONG_DESCRIPTION = 'hello world'
+
+# Setting up
+setup(
+    name="pyrovider",
+    version=VERSION,
+    author="shiphero",
+    author_email="hello@shiphero.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['getpass'],
+    keywords=[]
+   )
