4abd73bd4cd5a775bd3e9e92e0d44d4946688162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 04:03:54 +0000

    Add package outro/1.0/outro-1.0.tar.gz

---
+++ packages/outro/outro-1.0.tar.gz/outro-1.0/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='outro',
+    version='1.0',
+    author='u0_a325',
+    author_email='seuemail@example.com',
+    description='py',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
