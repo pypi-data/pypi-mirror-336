ee27bc1ea7dffb2f4159984c03d06035877cc46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:05:49 +0000

    Add package fastapi-app-manage/4.0.2/fastapi_app_manage-4.0.2.tar.gz

---
+++ packages/fastapi-app-manage/fastapi_app_manage-4.0.2.tar.gz/fastapi_app_manage-4.0.2/fastapi_app_manage/enums.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class PackageManager(Enum):
+    PIP = "pip"
+    POETRY = "poetry"
+    UV = "uv"
+
+
+class DataBaseFramework(Enum):
+    TORTOISEORM = "tortoise-orm"
+    SQLMODEL = "sqlmodel"
+    NONE = "none"
+
+
+class Database(Enum):
+    POSTGRES = "PostgreSQL"
+    MYSQL = "MySQL"
