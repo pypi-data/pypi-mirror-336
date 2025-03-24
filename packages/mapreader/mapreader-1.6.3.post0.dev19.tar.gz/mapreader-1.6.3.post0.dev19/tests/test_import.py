27991772f2fce6e7beebcefd6fc845f8c800b1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:05:46 +0000

    Add package mapreader/1.6.3.post0.dev19/mapreader-1.6.3.post0.dev19.tar.gz

---
+++ packages/mapreader/mapreader-1.6.3.post0.dev19.tar.gz/mapreader-1.6.3.post0.dev19/tests/test_import.py
@@ -0,0 +1,15 @@
+
+def test_import():
+    # This is based on all the imports found in the various tutorial notebooks
+    from mapreader import (
+        Annotator,
+        ClassifierContainer,
+        load_patches,
+        AnnotationsLoader,
+        loader,
+        MapImages,
+        PatchDataset,
+        PatchContextDataset,
+        Downloader,
+        SheetDownloader,
+    )
