0cd796d79394c5f4631eade79bc65464240b8bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 05:03:44 +0000

    Add package qroissant/0.1.0/qroissant-0.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/qroissant/qroissant-0.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/qroissant/__init__.py
@@ -0,0 +1,5 @@
+from .qroissant import *
+
+__doc__ = qroissant.__doc__
+if hasattr(qroissant, "__all__"):
+    __all__ = qroissant.__all__
---
+++ packages/qroissant/qroissant-0.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/qroissant/__init__.pyi
@@ -0,0 +1,10 @@
+from typing import Any
+
+def deserialize(data: bytes) -> Any:
+    ...
+
+def deserialize_raw(data: bytes) -> Any:
+    ...
+
+def read_message(message: str) -> Any:
+    ...
