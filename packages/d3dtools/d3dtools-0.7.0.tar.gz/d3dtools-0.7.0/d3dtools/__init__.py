c4a4f209c6ef72525595db10c2e6edc05507f52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:24 +0000

    Add package d3dtools/0.7.0/d3dtools-0.7.0.tar.gz

---
+++ packages/d3dtools/d3dtools-0.7.0.tar.gz/d3dtools-0.7.0/d3dtools/__init__.py
@@ -0,0 +1,10 @@
+"""
+D3DTOOLS - A collection of tools for working with shapefiles and converting them for Delft3D modeling.
+"""
+
+__version__ = '0.6.1'
+
+from . import ncrain
+from . import shpbc2pli
+from . import shpblock2pol
+from . import shpdike2pliz
---
+++ packages/d3dtools/d3dtools-0.7.0.tar.gz/d3dtools-0.7.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="d3dtools",
+    version="0.7.0",
+    author="aaronchh",
+    author_email="aaronhsu219@gmail.com",  # Please update this with your email
+    description="A collection of tools for working with shapefiles and converting them for Delft3D modeling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AaronOET/d3dtools",  # Update with your GitHub repo URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "ncrain=d3dtools.ncrain:main",
+            "shpbc2pli=d3dtools.shpbc2pli:main",
+            "shpblock2pol=d3dtools.shpblock2pol:main",
+            "shpdike2pliz=d3dtools.shpdike2pliz:main",
+        ],
+    },
+)
