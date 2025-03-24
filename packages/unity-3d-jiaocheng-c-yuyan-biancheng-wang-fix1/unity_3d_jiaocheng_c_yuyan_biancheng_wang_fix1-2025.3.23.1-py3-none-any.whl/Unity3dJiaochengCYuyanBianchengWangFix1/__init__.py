fe8f25cf24cba08af57c9d77c5eec3a25f4d81da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:26 +0000

    Add package unity-3d-jiaocheng-c-yuyan-biancheng-wang-fix1/2025.3.23.1/unity_3d_jiaocheng_c_yuyan_biancheng_wang_fix1-2025.3.23.1-py3-none-any.whl

---
+++ packages/unity-3d-jiaocheng-c-yuyan-biancheng-wang-fix1/unity_3d_jiaocheng_c_yuyan_biancheng_wang_fix1-2025.3.23.1-py3-none-any.whl/Unity3dJiaochengCYuyanBianchengWangFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""unity-3d-jiaocheng-c-yuyan-biancheng-wang-fix1
+https://github.com/apachecn/unity-3d-jiaocheng-c-yuyan-biancheng-wang-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.23.1"
