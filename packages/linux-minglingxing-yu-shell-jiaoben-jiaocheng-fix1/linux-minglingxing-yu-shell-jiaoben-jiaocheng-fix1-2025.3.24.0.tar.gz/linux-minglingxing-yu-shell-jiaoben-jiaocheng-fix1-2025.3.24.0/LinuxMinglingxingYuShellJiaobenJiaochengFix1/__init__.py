ef6e1fe1430256645f67c386229ca91238adb0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:06:02 +0000

    Add package linux-minglingxing-yu-shell-jiaoben-jiaocheng-fix1/2025.3.24.0/linux-minglingxing-yu-shell-jiaoben-jiaocheng-fix1-2025.3.24.0.tar.gz

---
+++ packages/linux-minglingxing-yu-shell-jiaoben-jiaocheng-fix1/linux-minglingxing-yu-shell-jiaoben-jiaocheng-fix1-2025.3.24.0.tar.gz/linux-minglingxing-yu-shell-jiaoben-jiaocheng-fix1-2025.3.24.0/LinuxMinglingxingYuShellJiaobenJiaochengFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""linux-minglingxing-yu-shell-jiaoben-jiaocheng-fix1
+https://github.com/apachecn/linux-minglingxing-yu-shell-jiaoben-jiaocheng-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.0"
