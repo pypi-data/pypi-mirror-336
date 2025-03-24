843b7943fb2fadb83f96c0c2b4fa4fd287bc834e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 04:03:59 +0000

    Add package mofan-shujuchuli-xilie-jiaocheng-2020-10-31-fix1/2025.3.24.0/mofan-shujuchuli-xilie-jiaocheng-2020-10-31-fix1-2025.3.24.0.tar.gz

---
+++ packages/mofan-shujuchuli-xilie-jiaocheng-2020-10-31-fix1/mofan-shujuchuli-xilie-jiaocheng-2020-10-31-fix1-2025.3.24.0.tar.gz/mofan-shujuchuli-xilie-jiaocheng-2020-10-31-fix1-2025.3.24.0/MofanShujuchuliXilieJiaocheng20201031Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mofan-shujuchuli-xilie-jiaocheng-2020-10-31-fix1
+https://github.com/apachecn/mofan-shujuchuli-xilie-jiaocheng-2020-10-31-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.0"
