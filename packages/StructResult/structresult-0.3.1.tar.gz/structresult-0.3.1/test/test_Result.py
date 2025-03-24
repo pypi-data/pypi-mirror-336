e78d42a41645d77a87f5315c4d94c766af386240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 17:05:03 +0000

    Add package StructResult/0.3.1/structresult-0.3.1.tar.gz

---
+++ packages/StructResult/structresult-0.3.1.tar.gz/structresult-0.3.1/test/test_Result.py
@@ -0,0 +1,10 @@
+import unittest
+from StructResult import Result
+
+
+class TestType(unittest.TestCase):
+
+    def test_init(self):
+        res = Result[int](1)
+        self.assertEqual(res.value, 1)
+        self.assertEqual(list(res), [1, None])
