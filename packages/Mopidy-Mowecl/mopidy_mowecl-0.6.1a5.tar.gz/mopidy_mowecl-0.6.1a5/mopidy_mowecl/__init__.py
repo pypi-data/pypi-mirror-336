c615524f99d1ac1b8d48632d5e75ec464efe92b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:23 +0000

    Add package Mopidy-Mowecl/0.6.1a5/mopidy_mowecl-0.6.1a5.tar.gz

---
+++ packages/Mopidy-Mowecl/mopidy_mowecl-0.6.1a5.tar.gz/mopidy_mowecl-0.6.1a5/mopidy_mowecl/__init__.py
@@ -0,0 +1,80 @@
+import logging
+import pathlib
+import pkg_resources
+import sysconfig
+import re
+
+from mopidy import config, ext
+
+from tornado.web import StaticFileHandler
+from .file_server import FileServer
+from .web_api_extra  import AddToPlaylistRequestHandler, GetLastFMData
+
+__version__ = pkg_resources.get_distribution("Mopidy-Mowecl").version
+
+logger = logging.getLogger(__name__)
+
+
+class ConfigColor(config.String):
+
+    def deserialize(self, value):
+        value = super().deserialize(value)
+        if not re.fullmatch("#[0-9A-Fa-f]{6}", value):
+            raise ValueError(f"Colors must be in the #AAAAAA format; {value} is not")
+        return value
+
+
+class Extension(ext.Extension):
+
+    dist_name = "Mopidy-Mowecl"
+    ext_name = "mowecl"
+    version = __version__
+
+    def get_default_config(self):
+        return config.read(pathlib.Path(__file__).parent / "ext.conf")
+
+    def get_config_schema(self):
+        schema = super().get_config_schema()
+        schema["theme_type"] = config.String(optional=True, choices=["light", "dark"])
+        schema["background_color"] = ConfigColor(optional=True)
+        schema["text_color"] = ConfigColor(optional=True)
+        schema["primary_color"] = ConfigColor(optional=True)
+        schema["seek_update_interval"] = config.Integer()
+        schema["search_history_length"] = config.Integer()
+        schema["disable_dnd"] = config.Boolean()
+        schema["small_screen"] = config.Boolean()
+
+        schema["key_play_pause"] = config.String(optional=True)
+        schema["key_next_track"] = config.String(optional=True)
+        schema["key_previous_track"] = config.String(optional=True)
+        schema["key_rewind_track"] = config.String(optional=True)
+        schema["key_volume_up"] = config.String(optional=True)
+        schema["key_volume_down"] = config.String(optional=True)
+
+        schema["lastfm_api_key"] = config.String(optional=True)
+        schema["lastfm_api_secret"] = config.String(optional=True)
+        return schema
+
+    def setup(self, registry):
+
+        logger.info("VERSION %s", self.version)
+        registry.add(
+            "http:app",
+            {
+                "name": self.ext_name,
+                "factory": self.factory
+            },
+        )
+
+    def factory(self, config, core):
+        root = pathlib.Path(sysconfig.get_path("data")) / "mopidy_mowecl" / "static"
+        server_params = {
+            "path": root, "config": config, "mowecl_version": self.version
+        }
+        return [
+            ('/add_to_playlist', AddToPlaylistRequestHandler, {'config': config, 'core': core}),
+            ('/get_artist_data', GetLastFMData, {'config': config, 'core': core}),
+            (r"/(index.html)", server_params),
+            (r"/", FileServer, server_params),
+            (r"/(.*)", StaticFileHandler, {"path": root}), # must be at the end, otherwise precedes other routes
+        ]
