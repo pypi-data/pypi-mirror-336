e1b16d9cb2238f2d019fd75df2dc2eeba950e433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:16 +0000

    Add package scportrait/1.3.3/scportrait-1.3.3-py3-none-any.whl

---
+++ packages/scportrait/scportrait-1.3.3-py3-none-any.whl/scportrait/pipeline/_utils/helper.py
@@ -0,0 +1,32 @@
+from pathlib import PosixPath
+from typing import TypeVar
+
+import yaml
+
+T = TypeVar("T")
+
+
+def read_config(config_path: str | PosixPath) -> dict:
+    with open(config_path) as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config
+
+
+def flatten(nested_list: list[list[T]]) -> list[T | tuple[T]]:
+    """Flatten a list of lists into a single list.
+
+    Args:
+        nested_list: A list containing one or more lists as its elements
+
+    Returns:
+        A single list containing all elements from the input lists
+
+    Example:
+        >>> nested_list = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+        >>> flatten(nested_list)
+        [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    """
+    return [item for sublist in nested_list for item in sublist]
---
+++ packages/scportrait/scportrait-1.3.3-py3-none-any.whl/scportrait/tools/__init__.py
@@ -0,0 +1,3 @@
+from . import h5sc, ml, parse, spdata, stitch
+
+__all__ = ["h5sc", "ml", "parse", "stitch", "spdata"]
