b6f712e189018c9409a3696fc27bcc647520f95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:04:26 +0000

    Add package nltkor/1.2.8/nltkor-1.2.8.tar.gz

---
+++ packages/nltkor/nltkor-1.2.8.tar.gz/nltkor-1.2.8/nltkor/__init__.py
@@ -0,0 +1,18 @@
+from nltkor import alignment
+from nltkor import cider
+from nltkor import distance
+
+from nltkor import sejong
+from nltkor import metrics
+from nltkor import misc
+from nltkor import search
+from nltkor import similarity
+from nltkor import tag
+from nltkor import tokenize
+from nltkor import dataloader
+from nltkor.dataloader import dataset
+from nltkor import trans
+from nltkor import Kor_char
+from nltkor import etc
+
+__version__ = '1.2.8'
