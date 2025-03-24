8cb72218db0fc5e677f4a90caa62000abee78460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:52 +0000

    Add package retrievepg/0.1/retrievepg-0.1.tar.gz

---
+++ packages/retrievepg/retrievepg-0.1.tar.gz/retrievepg-0.1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="retrievepg",  # Replace with your package name
+    version="0.1",
+    packages=find_packages(),
+    description="A simple package to print predefined texts",
+    author="Your Name",
+    author_email="your.email@example.com",
+)
