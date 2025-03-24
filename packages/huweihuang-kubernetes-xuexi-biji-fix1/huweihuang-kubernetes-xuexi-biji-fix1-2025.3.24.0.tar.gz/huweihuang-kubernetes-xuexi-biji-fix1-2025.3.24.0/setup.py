6d6ff09a2311cf10e1ea04f7f40cd9e9ce95491c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:04:25 +0000

    Add package huweihuang-kubernetes-xuexi-biji-fix1/2025.3.24.0/huweihuang-kubernetes-xuexi-biji-fix1-2025.3.24.0.tar.gz

---
+++ packages/huweihuang-kubernetes-xuexi-biji-fix1/huweihuang-kubernetes-xuexi-biji-fix1-2025.3.24.0.tar.gz/huweihuang-kubernetes-xuexi-biji-fix1-2025.3.24.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import HuweihuangKubernetesXuexiBijiFix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('HuweihuangKubernetesXuexiBijiFix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="huweihuang-kubernetes-xuexi-biji-fix1",
+    version=HuweihuangKubernetesXuexiBijiFix1.__version__,
+    url="https://github.com/apachecn/huweihuang-kubernetes-xuexi-biji-fix1",
+    author=HuweihuangKubernetesXuexiBijiFix1.__author__,
+    author_email=HuweihuangKubernetesXuexiBijiFix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="胡伟煌 Kubernetes 学习笔记",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "huweihuang-kubernetes-xuexi-biji-fix1=HuweihuangKubernetesXuexiBijiFix1.__main__:main",
+            "HuweihuangKubernetesXuexiBijiFix1=HuweihuangKubernetesXuexiBijiFix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
