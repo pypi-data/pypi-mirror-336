a824dd174a4143f24da3cc7fbce4776b4f4b2c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:05:47 +0000

    Add package leetcode-tijie-itcharge-fix1/2025.3.24.0/leetcode-tijie-itcharge-fix1-2025.3.24.0.tar.gz

---
+++ packages/leetcode-tijie-itcharge-fix1/leetcode-tijie-itcharge-fix1-2025.3.24.0.tar.gz/leetcode-tijie-itcharge-fix1-2025.3.24.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import LeetcodeTijieItchargeFix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('LeetcodeTijieItchargeFix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="leetcode-tijie-itcharge-fix1",
+    version=LeetcodeTijieItchargeFix1.__version__,
+    url="https://github.com/apachecn/leetcode-tijie-itcharge-fix1",
+    author=LeetcodeTijieItchargeFix1.__author__,
+    author_email=LeetcodeTijieItchargeFix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="LeetCode 题解（ITCharge）",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "leetcode-tijie-itcharge-fix1=LeetcodeTijieItchargeFix1.__main__:main",
+            "LeetcodeTijieItchargeFix1=LeetcodeTijieItchargeFix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
