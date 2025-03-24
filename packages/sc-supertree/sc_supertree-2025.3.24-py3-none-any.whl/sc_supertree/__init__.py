8b146766f1fecde0112e92ce9ff185570169fcc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:16 +0000

    Add package sc-supertree/2025.3.24/sc_supertree-2025.3.24-py3-none-any.whl

---
+++ packages/sc-supertree/sc_supertree-2025.3.24-py3-none-any.whl/sc_supertree/__init__.py
@@ -0,0 +1,34 @@
+"""
+Spectral Cluster Supertree
+
+A scalable and accurate algorithm for merging rooted phylogenetic trees.
+"""
+
+__all__ = ["load_trees", "construct_supertree"]
+__copyright__ = "Copyright 2023, Robert McArthur"
+__license__ = "BSD"
+__version__ = "2025.3.24"
+
+import os
+
+from cogent3 import TreeNode, make_tree
+
+from sc_supertree.scs import construct_supertree
+
+
+def load_trees(source_tree_file: str | bytes | os.PathLike) -> list[TreeNode]:
+    """Load a line-separated file of Newick-formatted trees.
+
+    Parameters
+    ----------
+    source_tree_file : str | bytes | os.PathLike
+        The path to the source tree file.
+
+    Returns
+    -------
+    list[TreeNode]
+        A list of all source trees in the file.
+    """
+    with open(source_tree_file, "r") as f:
+        source_trees: list[TreeNode] = [make_tree(line.strip()) for line in f]  # type: ignore
+    return source_trees
