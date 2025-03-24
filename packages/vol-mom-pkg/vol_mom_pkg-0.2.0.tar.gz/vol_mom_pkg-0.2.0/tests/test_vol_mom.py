c4c511eb1e0c2d772639bf866947f0b3d5233725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 05:03:47 +0000

    Add package vol-mom-pkg/0.2.0/vol_mom_pkg-0.2.0.tar.gz

---
+++ packages/vol-mom-pkg/vol_mom_pkg-0.2.0.tar.gz/vol_mom_pkg-0.2.0/tests/test_vol_mom.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from volmompkg.vol_mom_pkg.trade import send_weekly_basket
+from volmompkg.vol_mom_pkg.broker_info import get_incremental_pnl, get_orders, get_positions, get_account, get_daily_pnl, get_transactions, get_historical_pnl
+
+
+if __name__ == "__main__":
+    # send_weekly_basket()
+    # exit()
+    # print(get_orders())
+    # print(get_positions())
+    # print(get_account())
+    # print(get_daily_pnl())
+    # print(get_transactions())
+    #print(get_historical_pnl("2025-01-01", "2025-03-21"))
+    print(get_incremental_pnl("2025-02-01", "2025-03-19"))
