6d5dde72d499e83df77b89d9c6262474c3ffe576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:04 +0000

    Add package computer-use-core/0.1.0/computer_use_core-0.1.0.tar.gz

---
+++ packages/computer-use-core/computer_use_core-0.1.0.tar.gz/computer_use_core-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="computer-use-core",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Xiangyi Li",
+    author_email="xiangyi@benchflow.ai",
+    description="A placeholder package for computer-use-core",
+    long_description="A placeholder package for computer-use-core",
+    url="https://github.com/placeholder/computer-use-core",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
