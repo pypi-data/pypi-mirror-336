fccbe77d98201e1d55f5cc698863f411caa509d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:11 +0000

    Add package rigour/0.9.7/rigour-0.9.7-py3-none-any.whl

---
+++ packages/rigour/rigour-0.9.7-py3-none-any.whl/rigour/addresses/__init__.py
@@ -0,0 +1,31 @@
+"""
+# Postal/location address handling
+
+This set of helpers is designed to help with the processing of real-world
+addresses, including composing an address from individual parts, and cleaning it up.
+
+```python
+import rigour.addresses as format_one_line
+
+address = {
+    "road": "Bahnhofstr.",
+    "house_number": "10",
+    "postcode": "86150",
+    "city": "Augsburg",
+    "state": "Bayern",
+    "country": "Germany",
+}
+address_text = format_one_line(address, country="DE")
+```
+"""
+
+from rigour.addresses.cleaning import clean_address
+from rigour.addresses.normalize import normalize_address
+from rigour.addresses.format import format_address, format_address_line
+
+__all__ = [
+    "clean_address",
+    "normalize_address",
+    "format_address",
+    "format_address_line",
+]
