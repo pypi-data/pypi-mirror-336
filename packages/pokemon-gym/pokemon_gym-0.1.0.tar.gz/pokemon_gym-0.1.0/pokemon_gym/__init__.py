8ac5626c32a780ed130f0a8252cf0fb59c201fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:04 +0000

    Add package pokemon-gym/0.1.0/pokemon_gym-0.1.0.tar.gz

---
+++ packages/pokemon-gym/pokemon_gym-0.1.0.tar.gz/pokemon_gym-0.1.0/pokemon_gym/__init__.py
@@ -0,0 +1,5 @@
+"""
+pokemon-gym - A placeholder package
+"""
+
+__version__ = "0.1.0" 
---
+++ packages/pokemon-gym/pokemon_gym-0.1.0.tar.gz/pokemon_gym-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pokemon-gym",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Xiangyi Li",
+    author_email="xiangyi@benchflow.ai",
+    description="A placeholder package for pokemon-gym",
+    long_description="A placeholder package for pokemon-gym",
+    url="https://github.com/placeholder/pokemon-gym",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
