9226aea82ea373760138ba848d5b6f2cf8d85386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:18 +0000

    Add package dongshou-xue-shendu-xuexi-pytorch-ban-2020-3-13-fix1/2025.3.24.1/dongshou-xue-shendu-xuexi-pytorch-ban-2020-3-13-fix1-2025.3.24.1.tar.gz

---
+++ packages/dongshou-xue-shendu-xuexi-pytorch-ban-2020-3-13-fix1/dongshou-xue-shendu-xuexi-pytorch-ban-2020-3-13-fix1-2025.3.24.1.tar.gz/dongshou-xue-shendu-xuexi-pytorch-ban-2020-3-13-fix1-2025.3.24.1/DongshouXueShenduXuexiPytorchBan2020313Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dongshou-xue-shendu-xuexi-pytorch-ban-2020-3-13-fix1
+https://github.com/apachecn/dongshou-xue-shendu-xuexi-pytorch-ban-2020-3-13-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.24.1"
