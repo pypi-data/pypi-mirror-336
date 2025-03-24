a436e235655d10077136f3c2b70c2b5361171733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 15:04:13 +0000

    Add package midiogre/0.1.7.dev0/midiogre-0.1.7.dev0.tar.gz

---
+++ packages/midiogre/midiogre-0.1.7.dev0.tar.gz/midiogre-0.1.7.dev0/docs/source/conf.py
@@ -0,0 +1,37 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+project = 'MIDIOgre'
+copyright = '2024'
+author = 'Ashwin Pillay'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx_autodoc_typehints',
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_extra_path = ['../../demo']  # Include the demo directory
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_type_aliases = None 
