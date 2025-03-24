5c7b3ad27aef2d899b469280b81b997ef511aa37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:26 +0000

    Add package w3school-python-jiaocheng-fix1/2025.3.23.1/w3school_python_jiaocheng_fix1-2025.3.23.1-py3-none-any.whl

---
+++ packages/w3school-python-jiaocheng-fix1/w3school_python_jiaocheng_fix1-2025.3.23.1-py3-none-any.whl/W3schoolPythonJiaochengFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""w3school-python-jiaocheng-fix1
+https://github.com/apachecn/w3school-python-jiaocheng-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.23.1"
