7ca0caa7be37baa374a967561bed43471b1d7695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:05:43 +0000

    Add package webdriver-manager-mirrored/4.0.2.dev0/webdriver_manager_mirrored-4.0.2.dev0.tar.gz

---
+++ packages/webdriver-manager-mirrored/webdriver_manager_mirrored-4.0.2.dev0.tar.gz/webdriver_manager_mirrored-4.0.2.dev0/tests/test_downloader.py
@@ -0,0 +1,47 @@
+import os
+
+import pytest
+
+from webdriver_manager.core.constants import DEFAULT_PROJECT_ROOT_CACHE_PATH
+from webdriver_manager.core.download_manager import WDMDownloadManager
+from webdriver_manager.core.file_manager import FileManager
+from webdriver_manager.core.http import WDMHttpClient
+from webdriver_manager.core.os_manager import OperationSystemManager, ChromeType
+from webdriver_manager.drivers.chrome import ChromeDriver
+
+os_manager = OperationSystemManager()
+download_manager = WDMDownloadManager()
+file_manager = FileManager(os_manager)
+
+
+def test_can_download_driver_as_zip_file(delete_drivers_dir):
+    file = download_manager.download_file("http://repo.huaweicloud.com/chromedriver/2.26/chromedriver_win32.zip")
+    assert file.filename == "chromedriver_win32.zip"
+    archive = file_manager.save_archive_file(file, DEFAULT_PROJECT_ROOT_CACHE_PATH)
+    assert archive.file_path == f"{DEFAULT_PROJECT_ROOT_CACHE_PATH}{os.sep}{file.filename}"
+    assert file_manager.unpack_archive(archive, DEFAULT_PROJECT_ROOT_CACHE_PATH) == ["chromedriver.exe"]
+
+
+def test_can_download_driver_as_tar_gz(delete_drivers_dir):
+    file = download_manager.download_file(
+        "https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux32.tar.gz")
+    assert file.filename == 'geckodriver-v0.26.0-linux32.tar.gz'
+    archive = file_manager.save_archive_file(file, DEFAULT_PROJECT_ROOT_CACHE_PATH)
+    assert archive.file_path == f"{DEFAULT_PROJECT_ROOT_CACHE_PATH}{os.sep}{file.filename}"
+    assert file_manager.unpack_archive(archive, DEFAULT_PROJECT_ROOT_CACHE_PATH) == ["geckodriver"]
+
+
+@pytest.mark.parametrize('version', ["2.26"])
+def test_can_download_chrome_driver(delete_drivers_dir, version):
+    os_sys_manager = OperationSystemManager("win32")
+    driver = ChromeDriver(name="chromedriver",
+                          driver_version=version,
+                          os_system_manager=os_sys_manager,
+                          url="http://repo.huaweicloud.com/chromedriver",
+                          latest_release_url="http://repo.huaweicloud.com/chromedriver/LATEST_RELEASE",
+                          chrome_type=ChromeType.GOOGLE, http_client=WDMHttpClient())
+
+    file = download_manager.download_file(driver.get_driver_download_url(os_sys_manager.get_os_type()))
+    assert file.filename == "chromedriver_win32.zip"
+    archive = file_manager.save_archive_file(file, DEFAULT_PROJECT_ROOT_CACHE_PATH)
+    assert "chromedriver.exe" in file_manager.unpack_archive(archive, DEFAULT_PROJECT_ROOT_CACHE_PATH)
---
+++ packages/webdriver-manager-mirrored/webdriver_manager_mirrored-4.0.2.dev0.tar.gz/webdriver_manager_mirrored-4.0.2.dev0/tests/test_opera_manager.py
@@ -0,0 +1,79 @@
+import glob
+import os
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from webdriver_manager.core.driver_cache import DriverCacheManager
+from webdriver_manager.core.os_manager import OperationSystemManager
+from webdriver_manager.opera import OperaDriverManager
+
+
+def test_opera_driver_manager_with_correct_version():
+    driver_path = OperaDriverManager("v.2.45").install()
+    assert os.path.exists(driver_path)
+
+
+def test_driver_with_ssl_verify_disabled_can_be_downloaded(ssl_verify_enable):
+    os.environ['WDM_SSL_VERIFY'] = '0'
+    custom_path = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "ssl_disabled",
+    )
+    driver_path = OperaDriverManager(cache_manager=DriverCacheManager(custom_path)).install()
+    os.environ['WDM_SSL_VERIFY'] = '1'
+    assert os.path.exists(driver_path)
+
+
+def test_operadriver_manager_with_selenium():
+    driver_path = OperaDriverManager().install()
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('w3c', True)
+    os_type = OperationSystemManager().get_os_type()
+    if os_type in ["win64", "win32"]:
+        paths = [f for f in glob.glob(
+            f"C:/Users/{os.getlogin()}/AppData/Local/Programs/Opera/**",
+            recursive=True
+        )]
+        for path in paths:
+            if os.path.isfile(path) and path.endswith("opera.exe"):
+                options.binary_location = path
+    elif (
+            (os_type in ["linux64", "linux32"])
+            and not os.path.exists('/usr/bin/opera')
+    ):
+        options.binary_location = "/usr/bin/opera"
+    elif os_type in "mac64":
+        options.binary_location = "/Applications/Opera.app/Contents/MacOS/Opera"
+    web_service = Service(driver_path)
+    web_service.start()
+
+    opera_driver = webdriver.Remote(web_service.service_url, options=options)
+    opera_driver.get("http://automation-remarks.com")
+    opera_driver.quit()
+
+
+def test_opera_driver_manager_with_wrong_version():
+    with pytest.raises(ValueError) as ex:
+        OperaDriverManager("0.2").install()
+
+    assert "There is no such driver by url " \
+           "https://api.github.com/repos/operasoftware/operachromiumdriver/" \
+           "releases/tags/0.2" in ex.value.args[0]
+
+
+@pytest.mark.parametrize('path', ['.', None])
+def test_opera_driver_manager_with_correct_version_and_token(path):
+    driver_path = OperaDriverManager(version="v.2.45", cache_manager=DriverCacheManager(path)).install()
+    assert os.path.exists(driver_path)
+
+
+@pytest.mark.parametrize('os_type', ['win32',
+                                     'win64',
+                                     'linux64',
+                                     'mac64'])
+def test_can_get_driver_from_cache(os_type):
+    OperaDriverManager(os_system_manager=OperationSystemManager(os_type)).install()
+    driver_path = OperaDriverManager(os_system_manager=OperationSystemManager(os_type)).install()
+    assert os.path.exists(driver_path)
