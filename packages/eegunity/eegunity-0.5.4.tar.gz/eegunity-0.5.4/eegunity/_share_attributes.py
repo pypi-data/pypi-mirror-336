357ba219ba660e810bb0172dcf2c6e2078eff98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 04:03:56 +0000

    Add package eegunity/0.5.4/eegunity-0.5.4.tar.gz

---
+++ packages/eegunity/eegunity-0.5.4.tar.gz/eegunity-0.5.4/eegunity/_share_attributes.py
@@ -0,0 +1,8 @@
+class _UDatasetSharedAttributes:
+    def __init__(self):
+        self._shared_attr = {}  # Use instance variables to store shared attributes
+    def get_shared_attr(self):
+        return self._shared_attr
+
+    def set_shared_attr(self, value):
+        self._shared_attr.update(value)
---
+++ packages/eegunity/eegunity-0.5.4.tar.gz/eegunity-0.5.4/eegunity/modules/batch/utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def __convert_to_serializable__(value):
+    if hasattr(value, 'isoformat'):
+        return value.isoformat()
+    elif isinstance(value, np.ndarray):
+        return value.tolist()
+    elif isinstance(value, bytes):
+        return value.decode('utf-8')
