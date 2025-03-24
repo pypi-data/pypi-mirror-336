3f3f1ac15a3b2134dd35d6f44647a4cb69738ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 14:04:11 +0000

    Add package phasedm/1.0.1/phasedm-1.0.1-cp312-cp312-win32.whl

---
+++ packages/phasedm/phasedm-1.0.1-cp312-cp312-win32.whl/phasedm/phasedm.pyi
@@ -0,0 +1,4 @@
+import numpy as np
+
+def pdm(time:np.ndarray, signal: np.ndarray, min_freq: float, max_freq: float, n_freqs: int, n_bins: int, verbose: int) -> np.ndarray:
+    """info about the function"""
