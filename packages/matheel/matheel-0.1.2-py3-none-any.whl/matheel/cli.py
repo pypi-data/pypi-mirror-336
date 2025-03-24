ca9f0fe0d4f426d333e4e3851ab6d3bc06c86678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:17:01 +0000

    Add package matheel/0.1.2/matheel-0.1.2-py3-none-any.whl

---
+++ packages/matheel/matheel-0.1.2-py3-none-any.whl/matheel/cli.py
@@ -0,0 +1,21 @@
+import click
+from .similarity import get_sim_list
+
+@click.command()
+@click.argument('zipfile', type=click.Path(exists=True))
+@click.option('--ws', default=0.7, help='Semantic Similarity Weight')
+@click.option('--wl', default=0.3, help='Levenshtein Distance Weight')
+@click.option('--wj', default=0.0, help='Jaro-Winkler Distance Weight')
+@click.option('--model', default='Salesforce/codet5-small', help='Sentence Transformer Model')
+@click.option('--pooling', default='mean', type=click.Choice(['mean', 'cls', 'max']), help='Pooling Type')
+@click.option('--threshold', default=0.0, help='Similarity Threshold')
+@click.option('--num', default=10, help='Number of Results to Display')
+
+
+def main(zipfile, ws, wl, wj, model, pooling, threshold, num):
+    """Matheel CLI - Compute Code Similarity"""
+    results = get_sim_list(zipfile, ws, wl, wj, model, pooling, threshold, num)
+    click.echo(results.to_string(index=False))
+
+if __name__ == "__main__":
+    main()
