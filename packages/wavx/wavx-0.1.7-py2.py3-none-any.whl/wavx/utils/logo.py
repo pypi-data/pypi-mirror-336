df917fc3be460ae84f09186d6f07c501ac4e8ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 16:04:10 +0000

    Add package wavx/0.1.7/wavx-0.1.7-py2.py3-none-any.whl

---
+++ packages/wavx/wavx-0.1.7-py2.py3-none-any.whl/wavx/utils/logo.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+WavX LOGO模块
+
+提供显示WavX LOGO的功能
+"""
+
+def print_logo(with_version=True):
+    """
+    打印WavX的ASCII艺术LOGO
+    
+    参数:
+        with_version: 是否显示版本信息
+    """
+    from .. import __version__
+    
+    logo = """
+ ____      ____  _  ____   ____   ____  ____  
+|_  _|    |_  _|/ \|_  _| |_  _| |_  _||_  _| 
+  \ \  /\  / / / _ \ \ \   / /     \ \  / /   
+   \ \/  \/ / / ___ \ \ \ / /       > `' <    
+    \  /\  /_/ /   \ \_\ ' /      _/ /'`\ \_  
+     \/  \/|____| |____|\_/      |____||____| 
+     
+    """
+    
+    if with_version:
+        from .. import __version__
+        logo += f"\n      v{__version__}\n"
+    
+    print(logo)
+
+def print_install_message():
+    """
+    打印安装完成后的欢迎消息和LOGO
+    """
+    print_logo()
+    print("文档: https://github.com/chord-chord/wavx")
+    print("使用 'wavx --help' 查看命令行工具的使用方法")
