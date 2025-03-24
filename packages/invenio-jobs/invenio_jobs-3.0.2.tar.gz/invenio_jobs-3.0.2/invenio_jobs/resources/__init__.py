54e3547b2752f4300f2ceeca5b7e0b64dcf22a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 17:04:44 +0000

    Add package invenio-jobs/3.0.2/invenio_jobs-3.0.2.tar.gz

---
+++ packages/invenio-jobs/invenio_jobs-3.0.2.tar.gz/invenio_jobs-3.0.2/invenio_jobs/resources/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 CERN.
+# Copyright (C) 2024 University of Münster.
+#
+# Invenio-Jobs is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Jobs resources."""
+
+from .config import JobsResourceConfig, RunsResourceConfig, TasksResourceConfig
+from .resources import JobsResource, RunsResource, TasksResource
+
+__all__ = (
+    "JobsResource",
+    "JobsResourceConfig",
+    "TasksResource",
+    "TasksResourceConfig",
+    "RunsResource",
+    "RunsResourceConfig",
+)
---
+++ packages/invenio-jobs/invenio_jobs-3.0.2.tar.gz/invenio_jobs-3.0.2/invenio_jobs/services/errors.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 CERN.
+# Copyright (C) 2024 University of Münster.
+#
+# Invenio-Jobs is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Service definitions."""
+
+from invenio_i18n import gettext as _
+
+
+class JobsError(Exception):
+    """Base class for Jobs errors."""
+
+    def __init__(self, description, *args: object):
+        """Constructor."""
+        self.description = description
+        super().__init__(*args)
+
+
+class JobNotFoundError(JobsError):
+    """Job not found error."""
+
+    def __init__(self, id):
+        """Initialise error."""
+        super().__init__(
+            description=_("Job with ID {id} does not exist.").format(id=id)
+        )
+
+
+class RunNotFoundError(JobsError):
+    """Run not found error."""
+
+    def __init__(self, id, job_id=None):
+        """Initialise error."""
+        description = _("Run with ID {id} does not exist.")
+        if job_id:
+            description = _("Run with ID {id} for job {job_id} does not exist.")
+        super().__init__(description=description.format(id=id, job_id=job_id))
+
+
+class RunStatusChangeError(JobsError):
+    """Run status change error."""
+
+    def __init__(self, run, new_status):
+        """Initialise error."""
+        self.run = run
+        self.new_status = new_status
+        super().__init__(
+            description=_("You cannot change run status from {old} to {new}.").format(
+                old=run.status, new=new_status
+            )
+        )
