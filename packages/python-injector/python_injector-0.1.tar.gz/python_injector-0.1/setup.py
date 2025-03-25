0f01f1660ba5e66b6d7e0188cca83d97569adeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 04:05:22 +0000

    Add package python-injector/0.1/python_injector-0.1.tar.gz

---
+++ packages/python-injector/python_injector-0.1.tar.gz/python_injector-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_injector',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here, e.g., 'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
