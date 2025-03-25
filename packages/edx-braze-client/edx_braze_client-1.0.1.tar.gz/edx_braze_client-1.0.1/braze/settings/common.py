4f0ab42d2128ec3078307766034e4552eedb616e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 03:16:30 +0000

    Add package edx-braze-client/1.0.1/edx_braze_client-1.0.1.tar.gz

---
+++ packages/edx-braze-client/edx_braze_client-1.0.1.tar.gz/edx_braze_client-1.0.1/braze/settings/common.py
@@ -0,0 +1,13 @@
+"""
+braze common settings.
+"""
+
+
+def plugin_settings(settings):
+    """
+    Common settings for braze app
+    """
+    # BRAZE API SETTINGS
+    settings.EDX_BRAZE_API_KEY = None
+    settings.EDX_BRAZE_API_SERVER = None
+    settings.BRAZE_COURSE_ENROLLMENT_CANVAS_ID = ""
