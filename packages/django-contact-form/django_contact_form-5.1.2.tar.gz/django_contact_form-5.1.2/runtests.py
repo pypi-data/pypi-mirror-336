b1ce65d01cdb44e7c5f2941dd7b372c696946ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 16:05:21 +0000

    Add package django-contact-form/5.1.2/django_contact_form-5.1.2.tar.gz

---
+++ packages/django-contact-form/django_contact_form-5.1.2.tar.gz/django_contact_form-5.1.2/runtests.py
@@ -0,0 +1,20 @@
+"""
+A standalone test runner script, since we don't ship a Django manage.py file.
+
+"""
+
+# SPDX-License-Identifier: BSD-3-Clause
+
+from django.core.management import execute_from_command_line
+
+
+def run_tests():
+    """
+    Run the test suite as Django's "manage.py test" would.
+
+    """
+    execute_from_command_line(["runtests.py", "test", "--verbosity", "2"])
+
+
+if __name__ == "__main__":
+    run_tests()
