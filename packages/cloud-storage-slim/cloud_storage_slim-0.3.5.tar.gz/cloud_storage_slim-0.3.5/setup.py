b1845050c0bf695e6984c53c8936a01a2a0832fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 07:04:51 +0000

    Add package cloud-storage-slim/0.3.5/cloud_storage_slim-0.3.5.tar.gz

---
+++ packages/cloud-storage-slim/cloud_storage_slim-0.3.5.tar.gz/cloud_storage_slim-0.3.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloud_storage_slim",
+    version="0.3.5",
+    author="Ying Cai",
+    author_email="i@caiying.me",
+    description="A lightweight cloud storage package for popular cloud storage services.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/yingca1/cloud-storage-slim",
+    packages=find_packages(),
+    install_requires=[],
+)
