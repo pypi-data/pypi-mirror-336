0af5e07ba73e73d4f09401335756c0e994df42e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:04:34 +0000

    Add package hmd-cli-librarian-sync-manager/0.2.116/hmd_cli_librarian_sync_manager-0.2.116-py3-none-any.whl

---
+++ packages/hmd-cli-librarian-sync-manager/hmd_cli_librarian_sync_manager-0.2.116-py3-none-any.whl/hmd_cli_librarian_sync_manager/source_handlers/__init__.py
@@ -0,0 +1,44 @@
+import logging
+from pathlib import Path
+from typing import Dict
+
+from hmd_lang_librarian_sync.hmd_lang_librarian_sync_client import (
+    HmdLangLibrarianSyncClient,
+)
+from hmd_lib_librarian_client.hmd_lib_librarian_client import HmdLibrarianClient
+
+from .manifest_source import ManifestSource
+from .source import Source
+
+logger = logging.getLogger(__name__)
+
+
+def make_source(
+    name: str,
+    librarian_sync_client: HmdLangLibrarianSyncClient,
+    librarian_client: HmdLibrarianClient,
+    hmd_home: Path,
+    hmd_repo_home: Path,
+    source: Dict,
+    timestamp_reverse: bool = False,
+) -> Source:
+    source_type = source.get("type", "default")
+    source["name"] = name
+    logger.info(f"{source['name']}: creating source of type {source_type}")
+    if source_type == "manifest":
+        return ManifestSource(
+            source,
+            librarian_sync_client,
+            librarian_client,
+            hmd_home,
+            hmd_repo_home,
+            timestamp_reverse,
+        )
+    return Source(
+        source,
+        librarian_sync_client,
+        librarian_client,
+        hmd_home,
+        hmd_repo_home,
+        timestamp_reverse,
+    )
