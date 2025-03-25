1bb79aad8c0bd9bf66d0dac354b113fe87ab0fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 16:05:49 +0000

    Add package fcst/0.8.11/fcst-0.8.11-py3-none-any.whl

---
+++ packages/fcst/fcst-0.8.11-py3-none-any.whl/fcst/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    fcst
+    ~~~~
+    fcst provides you the utilities related to forecasting;
+    for example, automatic model selection from rolling back-testing,
+    forecasting using ensemble from different models.
+"""
+
+from .version import __version__
