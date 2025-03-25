f74330bbf8c8672d6fd192095078c7f40fa4088a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 14:05:56 +0000

    Add package pyttman-plugin-mongoengine/1.0.0/pyttman_plugin_mongoengine-1.0.0.tar.gz

---
+++ packages/pyttman-plugin-mongoengine/pyttman_plugin_mongoengine-1.0.0.tar.gz/pyttman_plugin_mongoengine-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+# This call to setup() does all the work
+setup(
+    name="pyttman-plugin-mongoengine",
+    version="1.0.0",
+    description="Mongoengine (MongoDB) plugin for Pyttman apps, allowing seamless integration with MongoEngine and Pyttman.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hashmap-Software-Agency/Pyttman-Plugins",
+    author="Simon Olofsson, Pyttman framework founder and maintainer.",
+    author_email="simon@hashmap.se",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+    packages=find_packages(exclude=["*.tests", "*.tests.*",
+                                    "tests.*", "tests"]),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "py7zr",
+        "ordered_set",
+    ],
+)
