17033853984b33ef4c28e28ca12f51bba69bef90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 14:05:48 +0000

    Add package sklearn-yu-tensorflow-jiqi-xuexi-shiyong-zhinan-dier-ban-fix1-fix1/2025.3.25.0/sklearn-yu-tensorflow-jiqi-xuexi-shiyong-zhinan-dier-ban-fix1-fix1-2025.3.25.0.tar.gz

---
+++ packages/sklearn-yu-tensorflow-jiqi-xuexi-shiyong-zhinan-dier-ban-fix1-fix1/sklearn-yu-tensorflow-jiqi-xuexi-shiyong-zhinan-dier-ban-fix1-fix1-2025.3.25.0.tar.gz/sklearn-yu-tensorflow-jiqi-xuexi-shiyong-zhinan-dier-ban-fix1-fix1-2025.3.25.0/SklearnYuTensorflowJiqiXuexiShiyongZhinanDierBanFix1Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sklearn-yu-tensorflow-jiqi-xuexi-shiyong-zhinan-dier-ban-fix1-fix1
+https://github.com/apachecn/sklearn-yu-tensorflow-jiqi-xuexi-shiyong-zhinan-dier-ban-fix1-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.25.0"
