e87264ab01ec244c363bfca6c23bb89deeb8edb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 15:04:52 +0000

    Add package gseda/0.4.0/gseda-0.4.0-py3-none-any.whl

---
+++ packages/gseda/gseda-0.4.0-py3-none-any.whl/gseda/ppl/phreq_analysis.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from pathlib import Path
+
+cur_file = os.path.abspath(__file__)
+cur_file = Path(cur_file)
+sys.path.insert(0, cur_file.parent.as_posix())
+sys.path.insert(0, cur_file.parent.parent.as_posix())
+
+import argparse
+import subprocess
+from fact_table_ana import pred_baseq_and_emp_q, rq_iy_analysis
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", type=str, required=True, help="query file")
+    parser.add_argument("-t", type=str, required=True, help="target file")
+
+    args = parser.parse_args()
+
+    """
+    gsmm2 align -q $query_file -t $ref_file -p outputbam_prefix --noMar
+    gsetl --outdir $outdir aligned-bam --bam $aligned_bam --ref-file $ref_file
+    """
+
+    file_stem = Path(args.q).stem
+
+    oup_bam_prefix = f"{args.q}.gsmm2-aligned"
+    ## 1. do alignment
+    cmd_str = f"gsmm2 align -q {args.q} -t {args.t} -p {oup_bam_prefix} --noMar"
+    subprocess.check_call(cmd_str, shell=True)
+    oup_bam = f"{oup_bam_prefix}.bam"
+
+    ## 2. gsetl extract fact table
+    gsetl_outdir = f"{oup_bam}-gsetl"
+    cmd_str = f"gsetl -f --outdir {gsetl_outdir} aligned-bam --bam {oup_bam} --ref-file {args.t}"
+    subprocess.check_call(cmd_str, shell=True)
+
+    ## 3. do analysis
+
+    baseq_ana_args = {
+        "fact_table": f"{gsetl_outdir}/fact_baseq_stat.csv",
+        "o_prefix": file_stem,
+    }
+    pred_baseq_and_emp_q.main(argparse.Namespace(**baseq_ana_args))
+
+    readsq_ana_args = {
+        "fact_table": f"{gsetl_outdir}/fact_aligned_bam_bam_basic.csv",
+        "o_prefix": file_stem,
+    }
+    rq_iy_analysis.main(argparse.Namespace(**readsq_ana_args))
+
+
+if __name__ == "__main__":
+    main()
