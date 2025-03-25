836f1e1a4d44e4a723594a736512db64c413086c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 05:05:02 +0000

    Add package indico-plugin-payment-manual/3.3.2/indico_plugin_payment_manual-3.3.2-py3-none-any.whl

---
+++ packages/indico-plugin-payment-manual/indico_plugin_payment_manual-3.3.2-py3-none-any.whl/indico_payment_manual/placeholders.py
@@ -0,0 +1,112 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2002 - 2025 CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
+from decimal import Decimal
+from urllib.parse import quote_plus
+
+from indico.modules.events.registration.placeholders.registrations import IDPlaceholder
+from indico.util.placeholders import ParametrizedPlaceholder, Placeholder
+
+from indico_payment_manual import _
+
+
+class EscapablePlaceholder(ParametrizedPlaceholder):
+    param_required = False
+    param_restricted = True
+    field = None
+    basic_description = None
+
+    @classmethod
+    def render(cls, param, regform, registration):
+        rv = getattr(registration, cls.field)
+        if param == 'url':
+            rv = quote_plus(rv)
+        return rv
+
+    @classmethod
+    def iter_param_info(cls, regform, registration):
+        yield None, cls.basic_description
+        yield 'url', '{} ({})'.format(cls.basic_description, _('escaped for URLs'))
+
+
+class FirstNamePlaceholder(EscapablePlaceholder):
+    name = 'first_name'
+    basic_description = _('First name of the registrant')
+    field = 'first_name'
+
+
+class LastNamePlaceholder(EscapablePlaceholder):
+    name = 'last_name'
+    basic_description = _('Last name of the registrant')
+    field = 'last_name'
+
+
+class EmailPlaceholder(EscapablePlaceholder):
+    name = 'email'
+    basic_description = _('Email address of the registrant')
+    field = 'email'
+
+
+class RegistrationIDPlaceholder(IDPlaceholder):
+    name = 'registration_id'
+
+
+class RegistrationDatabaseIDPlaceholder(Placeholder):
+    name = 'registration_db_id'
+    description = _('The database ID of the registration')
+
+    @classmethod
+    def render(cls, regform, registration):
+        return registration.id
+
+
+class RegistrationFormIDPlaceholder(Placeholder):
+    name = 'registration_form_id'
+    description = _('The ID of the registration form')
+
+    @classmethod
+    def render(cls, regform, registration):
+        return registration.registration_form.id
+
+
+class EventIDPlaceholder(Placeholder):
+    name = 'event_id'
+    description = _('The ID of the event')
+
+    @classmethod
+    def render(cls, regform, registration):
+        return registration.registration_form.event.id
+
+
+class PricePlaceholder(ParametrizedPlaceholder):
+    param_required = False
+    param_restricted = True
+    name = 'price'
+
+    @classmethod
+    def render(cls, param, regform, registration):
+        if param == 'int':
+            return str(int(registration.price * 100))
+        elif param == 'short' and int(registration.price) == registration.price:
+            return str(int(registration.price))
+        else:
+            return str(registration.price.quantize(Decimal('.01')))
+
+    @classmethod
+    def iter_param_info(cls, regform, registration):
+        yield None, _('The price the registrant needs to pay (e.g. 100.00 or 100.25)')
+        yield 'short', _('The price without cents if possible (e.g. 100 or 100.25)')
+        yield 'int', _('The price formatted as an integer (e.g. 10000 or 10025)')
+
+
+class CurrencyPlaceholder(Placeholder):
+    name = 'currency'
+    description = _('The currency used in the registration')
+
+    @classmethod
+    def render(cls, regform, registration):
+        return registration.currency
---
+++ packages/indico-plugin-payment-manual/indico_plugin_payment_manual-3.3.2-py3-none-any.whl/indico_payment_manual/plugin.py
@@ -0,0 +1,89 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2002 - 2025 CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
+from wtforms.fields.simple import TextAreaField
+from wtforms.validators import DataRequired
+
+from indico.core import signals
+from indico.core.plugins import IndicoPlugin, IndicoPluginBlueprint, url_for_plugin
+from indico.modules.events.payment import (PaymentEventSettingsFormBase, PaymentPluginMixin,
+                                           PaymentPluginSettingsFormBase)
+from indico.util.placeholders import render_placeholder_info, replace_placeholders
+from indico.web.forms.validators import UsedIf
+
+from indico_payment_manual import _
+
+
+DETAILS_DESC = _('The details the user needs to make their payment. This usually includes the bank account details, '
+                 'the IBAN and payment reference.  You can also link to a custom payment site. In this case make sure '
+                 'to use the URL-escaped versions of the placeholders where available.')
+
+
+class PluginSettingsForm(PaymentPluginSettingsFormBase):
+    details = TextAreaField(_('Payment details'), [])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.details.description = DETAILS_DESC + '\n' + render_placeholder_info('manual-payment-details',
+                                                                                 regform=None, registration=None)
+
+
+class EventSettingsForm(PaymentEventSettingsFormBase):
+    details = TextAreaField(_('Payment details'), [UsedIf(lambda form, _: form.enabled.data), DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.details.description = DETAILS_DESC + '\n' + render_placeholder_info('manual-payment-details',
+                                                                                 regform=None, registration=None)
+
+
+class ManualPaymentPlugin(PaymentPluginMixin, IndicoPlugin):
+    """Bank Transfer
+
+    Provides a payment method where bank details etc. are shown to the user
+    who then pays manually using e.g. a wire transfer. Marking the registrant
+    as paid is then done manually by a manager of the event.
+    """
+    configurable = True
+    settings_form = PluginSettingsForm
+    event_settings_form = EventSettingsForm
+    default_settings = {'method_name': 'Bank Transfer',
+                        'details': ''}
+    default_event_settings = {'enabled': False,
+                              'method_name': None,
+                              'details': ''}
+
+    def init(self):
+        super().init()
+        self.connect(signals.core.get_placeholders, self._get_details_placeholders, sender='manual-payment-details')
+
+    def _get_details_placeholders(self, sender, regform, registration, **kwargs):
+        from indico_payment_manual.placeholders import (CurrencyPlaceholder, EmailPlaceholder, EventIDPlaceholder,
+                                                        FirstNamePlaceholder, LastNamePlaceholder, PricePlaceholder,
+                                                        RegistrationDatabaseIDPlaceholder,
+                                                        RegistrationFormIDPlaceholder, RegistrationIDPlaceholder)
+        yield FirstNamePlaceholder
+        yield LastNamePlaceholder
+        yield EmailPlaceholder
+        yield RegistrationIDPlaceholder
+        yield RegistrationDatabaseIDPlaceholder
+        yield RegistrationFormIDPlaceholder
+        yield EventIDPlaceholder
+        yield PricePlaceholder
+        yield CurrencyPlaceholder
+
+    @property
+    def logo_url(self):
+        return url_for_plugin(self.name + '.static', filename='images/logo.png')
+
+    def get_blueprints(self):
+        return IndicoPluginBlueprint('payment_manual', __name__)
+
+    def adjust_payment_form_data(self, data):
+        data['details'] = replace_placeholders('manual-payment-details', data['event_settings']['details'],
+                                               regform=data['registration'].registration_form,
+                                               registration=data['registration'])
