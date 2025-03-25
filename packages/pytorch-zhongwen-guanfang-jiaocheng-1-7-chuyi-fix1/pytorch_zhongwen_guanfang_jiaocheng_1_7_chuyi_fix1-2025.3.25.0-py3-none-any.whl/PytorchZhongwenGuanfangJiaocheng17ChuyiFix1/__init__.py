2151d5a6a14f2ad43963950223563a37b61b93a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 08:04:49 +0000

    Add package pytorch-zhongwen-guanfang-jiaocheng-1-7-chuyi-fix1/2025.3.25.0/pytorch_zhongwen_guanfang_jiaocheng_1_7_chuyi_fix1-2025.3.25.0-py3-none-any.whl

---
+++ packages/pytorch-zhongwen-guanfang-jiaocheng-1-7-chuyi-fix1/pytorch_zhongwen_guanfang_jiaocheng_1_7_chuyi_fix1-2025.3.25.0-py3-none-any.whl/PytorchZhongwenGuanfangJiaocheng17ChuyiFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pytorch-zhongwen-guanfang-jiaocheng-1-7-chuyi-fix1
+https://github.com/apachecn/pytorch-zhongwen-guanfang-jiaocheng-1-7-chuyi-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.25.0"
