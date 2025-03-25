4fe4b7e0b8b7b1331962b29e318f2542fd450175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 17:05:30 +0000

    Add package Mathmodule13547/0.1.0/mathmodule13547-0.1.0.tar.gz

---
+++ packages/Mathmodule13547/mathmodule13547-0.1.0.tar.gz/mathmodule13547-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Mathmodule13547",  
+    version="0.1.0",
+    author="Rahul Rohilla",
+    author_email="rohillarahul462@gmail.com",
+    description="Maths Functions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
