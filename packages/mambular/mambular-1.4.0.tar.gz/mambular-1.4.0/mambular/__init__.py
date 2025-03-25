a6ff4d9858e212b55f81fe653a48361816a75c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 03:16:07 +0000

    Add package mambular/1.4.0/mambular-1.4.0.tar.gz

---
+++ packages/mambular/mambular-1.4.0.tar.gz/mambular-1.4.0/mambular/__init__.py
@@ -0,0 +1,11 @@
+from . import base_models, data_utils, models, preprocessing, utils
+from .__version__ import __version__
+
+__all__ = [
+    "__version__",
+    "base_models",
+    "data_utils",
+    "models",
+    "preprocessing",
+    "utils",
+]
