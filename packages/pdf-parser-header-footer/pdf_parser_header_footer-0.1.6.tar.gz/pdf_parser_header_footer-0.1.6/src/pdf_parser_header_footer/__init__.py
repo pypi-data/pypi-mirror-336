c8b18fcd04e91ed9a535c26c7720a2a3060a8cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 05:04:45 +0000

    Add package pdf-parser-header-footer/0.1.6/pdf_parser_header_footer-0.1.6.tar.gz

---
+++ packages/pdf-parser-header-footer/pdf_parser_header_footer-0.1.6.tar.gz/pdf_parser_header_footer-0.1.6/src/pdf_parser_header_footer/__init__.py
@@ -0,0 +1,10 @@
+from .parser import PDFSectionParser
+from .config import ParserConfig, FooterBoundary
+
+__version__ = "0.1.6"
+
+__all__ = [
+    "PDFSectionParser",
+    "ParserConfig",
+    "FooterBoundary",
+]
