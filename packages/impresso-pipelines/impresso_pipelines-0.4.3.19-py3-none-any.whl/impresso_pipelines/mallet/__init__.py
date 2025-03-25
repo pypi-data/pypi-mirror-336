05d1dc3a2cf4814647885887e480bae1babef6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:04:46 +0000

    Add package impresso-pipelines/0.4.3.19/impresso_pipelines-0.4.3.19-py3-none-any.whl

---
+++ packages/impresso-pipelines/impresso_pipelines-0.4.3.19-py3-none-any.whl/impresso_pipelines/mallet/__init__.py
@@ -0,0 +1,17 @@
+try:
+    import huggingface_hub
+    import floret
+    import spacy
+    import jpype
+    import smart_open
+    import boto3
+    import dotenv
+
+    
+    # Only import this after checking dependencies
+    from .mallet_pipeline import MalletPipeline
+except ImportError:
+    raise ImportError(
+        "The mallet subpackage requires additional dependencies. "
+        "Please install them with: pip install 'impresso-pipelines[mallet]'"
+    )
