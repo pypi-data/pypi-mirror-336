30f9c9c5bad0afc6b54aaaa50a99d9764c28b558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 04:05:12 +0000

    Add package x2021-zhiyuan-dahui-guandian-baogao-fix1/2025.3.24.0/x2021-zhiyuan-dahui-guandian-baogao-fix1-2025.3.24.0.tar.gz

---
+++ packages/x2021-zhiyuan-dahui-guandian-baogao-fix1/x2021-zhiyuan-dahui-guandian-baogao-fix1-2025.3.24.0.tar.gz/x2021-zhiyuan-dahui-guandian-baogao-fix1-2025.3.24.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import X2021ZhiyuanDahuiGuandianBaogaoFix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('X2021ZhiyuanDahuiGuandianBaogaoFix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="x2021-zhiyuan-dahui-guandian-baogao-fix1",
+    version=X2021ZhiyuanDahuiGuandianBaogaoFix1.__version__,
+    url="https://github.com/apachecn/x2021-zhiyuan-dahui-guandian-baogao-fix1",
+    author=X2021ZhiyuanDahuiGuandianBaogaoFix1.__author__,
+    author_email=X2021ZhiyuanDahuiGuandianBaogaoFix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="2021智源大会观点报告",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "x2021-zhiyuan-dahui-guandian-baogao-fix1=X2021ZhiyuanDahuiGuandianBaogaoFix1.__main__:main",
+            "X2021ZhiyuanDahuiGuandianBaogaoFix1=X2021ZhiyuanDahuiGuandianBaogaoFix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
