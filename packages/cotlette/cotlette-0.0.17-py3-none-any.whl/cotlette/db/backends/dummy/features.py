0d28b0bf54d12d4e134ff8d955cb2774e8837b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:05:13 +0000

    Add package cotlette/0.0.17/cotlette-0.0.17-py3-none-any.whl

---
+++ packages/cotlette/cotlette-0.0.17-py3-none-any.whl/cotlette/db/backends/dummy/features.py
@@ -0,0 +1,6 @@
+from cotlette.db.backends.base.features import BaseDatabaseFeatures
+
+
+class DummyDatabaseFeatures(BaseDatabaseFeatures):
+    supports_transactions = False
+    uses_savepoints = False
---
+++ packages/cotlette/cotlette-0.0.17-py3-none-any.whl/cotlette/db/backends/signals.py
@@ -0,0 +1,3 @@
+from cotlette.dispatch import Signal
+
+connection_created = Signal()
