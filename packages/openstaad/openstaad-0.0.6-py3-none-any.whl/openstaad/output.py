6c63c5c094405ce010cf8b9f2362323f80413a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 04:04:45 +0000

    Add package openstaad/0.0.6/openstaad-0.0.6-py3-none-any.whl

---
+++ packages/openstaad/openstaad-0.0.6-py3-none-any.whl/openstaad/output.py
@@ -0,0 +1,40 @@
+from openstaad.tools import *
+from comtypes import automation
+from comtypes import client
+
+class Output():
+    def __init__(self):
+        self._staad = client.GetActiveObject("StaadPro.OpenSTAAD")
+        self._output = self._staad.Output
+
+        self._functions= [
+            'GetMemberEndForces',
+            'GetSupportReactions'
+        ]
+
+        for function_name in self._functions:
+            self._output._FlagAsMethod(function_name)
+
+    ## PROPERTIES FUNCTIONS
+
+    def GetMemberEndForces(self,beam, start = True, lc :int= 1,local: int = 0):
+        safe_n1 = make_safe_array_double(6)
+        x = make_variant_vt_ref(safe_n1,  automation.VT_ARRAY |  automation.VT_R8)
+
+        if start:
+            end = 0
+        else:
+            end = 1
+
+        retval = self._output.GetMemberEndForces(beam, end,lc,x,local)
+
+        return x.value[0]
+
+    def GetSupportReactions(self, node,lc :int= 1):
+        
+        safe_n1 = make_safe_array_double(6)
+        x = make_variant_vt_ref(safe_n1,  automation.VT_ARRAY |  automation.VT_R8)
+
+        retval = self._output.GetSupportReactions(node,lc,x)
+
+        return x.value[0]
---
+++ packages/openstaad/openstaad-0.0.6-py3-none-any.whl/openstaad/view.py
@@ -0,0 +1,124 @@
+from openstaad.tools import *
+from comtypes import automation
+from comtypes import client
+
+from .geometry import Geometry
+
+class View():
+    def __init__(self):
+        self._staad = client.GetActiveObject("StaadPro.OpenSTAAD")
+        self._view = self._staad.View
+        self._geometry = Geometry()
+
+        self._functions= [
+            "RefreshView",
+            "ShowAllMembers",
+            "HideAllMembers",
+            "ZoomExtentsMainView",
+            "ShowMembers",
+            "HideMember",
+            "HideMembers",
+            "ShowBack",
+            "ShowBottom",
+            "ShowFront",
+            "ShowIsometric",
+            "ShowLeft",
+            "ShowPlan",
+            "ShowRight",
+            "SpinLeft",
+            "SpinRight",
+            "ZoomAll",
+            "SelectMembersParallelTo"
+        ]
+
+        for function_name in self._functions:
+            self._view._FlagAsMethod(function_name)
+
+    def RefreshView(self):
+        self._view.RefreshView()
+
+    def ShowAllMembers(self):
+        self._view.ShowAllMembers()
+    
+    def HideAllMembers(self):
+        self._view.HideAllMembers()
+
+    def ZoomExtentsMainView(self):
+        self._view.ZoomExtentsMainView()
+
+    def ShowMembers(self,NMembers,NaMemberNos):
+        safe_list = make_safe_array_long_input(NaMemberNos)
+        lista_variant = make_variant_vt_ref(safe_list, automation.VT_ARRAY | automation.VT_I4)
+
+        self._view.ShowAllMembers()
+        self._view.HideAllMembers()
+        self._geometry.ClearMemberSelection() 
+        self._view.ShowMembers(NMembers,lista_variant)
+        self._view.ShowIsometric()
+        self._view.ZoomExtentsMainView()
+        self._view.RefreshView()
+
+    def HideMember(self,IDMember):
+        self._view.HideMember(IDMember)
+        # self._view.RefreshView()
+
+    def HideMembers(self,NMembers,NaMemberNos):
+        safe_list = make_safe_array_long_input(NaMemberNos)
+        lista_variant = make_variant_vt_ref(safe_list, automation.VT_ARRAY | automation.VT_I4)
+
+        self._view.HideMembers(NMembers,lista_variant)
+        self._view.RefreshView()
+
+    def ShowBack(self):
+        self._view.ShowBack()
+        self._view.RefreshView()
+        self._view.ZoomExtentsMainView()
+
+    def ShowBottom(self):
+        self._view.ShowBottom()
+        self._view.RefreshView()
+        self._view.ZoomExtentsMainView()
+
+    def ShowFront(self):
+        self._view.ShowFront()
+        self._view.RefreshView()
+        self._view.ZoomExtentsMainView()
+
+    def ShowIsometric(self):
+        self._view.ShowIsometric()
+        self._view.RefreshView()
+        self._view.ZoomExtentsMainView()
+        
+    def ShowLeft(self):
+        self._view.ShowLeft()
+        self._view.RefreshView()
+        self._view.ZoomExtentsMainView()
+
+    def ShowPlan(self):
+        self._view.ShowPlan()
+        self._view.RefreshView()
+        self._view.ZoomExtentsMainView()
+
+    def ShowRight(self):
+        self._view.ShowRight()
+        self._view.RefreshView()
+        self._view.ZoomExtentsMainView()
+
+    def SpinLeft(self,Degrees):
+        Degrees = float(Degrees)
+        self._view.SpinLeft(Degrees)
+        self._view.RefreshView()
+        self._view.ZoomExtentsMainView()
+                
+    def SpinRight(self,Degrees):
+        Degrees = float(Degrees)
+        self._view.SpinRight(Degrees)
+        self._view.RefreshView()
+        self._view.ZoomExtentsMainView()
+
+    def ZoomAll(self):
+        self._view.ZoomAll()
+
+    def SelectMembersParallelTo(self,axis):
+        self._view.SelectMembersParallelTo(axis)
+    
