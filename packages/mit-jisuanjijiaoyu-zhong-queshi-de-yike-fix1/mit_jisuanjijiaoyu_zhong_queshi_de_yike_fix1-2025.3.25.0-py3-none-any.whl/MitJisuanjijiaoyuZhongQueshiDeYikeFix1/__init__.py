7b22ac2082ae65d3f2a5191a4719fed8e25c90ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:05:34 +0000

    Add package mit-jisuanjijiaoyu-zhong-queshi-de-yike-fix1/2025.3.25.0/mit_jisuanjijiaoyu_zhong_queshi_de_yike_fix1-2025.3.25.0-py3-none-any.whl

---
+++ packages/mit-jisuanjijiaoyu-zhong-queshi-de-yike-fix1/mit_jisuanjijiaoyu_zhong_queshi_de_yike_fix1-2025.3.25.0-py3-none-any.whl/MitJisuanjijiaoyuZhongQueshiDeYikeFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mit-jisuanjijiaoyu-zhong-queshi-de-yike-fix1
+https://github.com/apachecn/mit-jisuanjijiaoyu-zhong-queshi-de-yike-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.25.0"
