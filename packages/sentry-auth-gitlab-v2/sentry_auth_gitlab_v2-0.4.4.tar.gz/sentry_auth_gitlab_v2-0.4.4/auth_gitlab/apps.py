cb01109d3d7fdba3f02b4b6da8c4586fd5a27a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 16:05:32 +0000

    Add package sentry-auth-gitlab-v2/0.4.4/sentry_auth_gitlab_v2-0.4.4.tar.gz

---
+++ packages/sentry-auth-gitlab-v2/sentry_auth_gitlab_v2-0.4.4.tar.gz/sentry_auth_gitlab_v2-0.4.4/auth_gitlab/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+class Config(AppConfig):
+    name = "auth_gitlab"
+
+    def ready(self):
+        from sentry.auth import register
+
+        from .provider import GitLabOAuth2Provider
+
+        register(GitLabOAuth2Provider)
