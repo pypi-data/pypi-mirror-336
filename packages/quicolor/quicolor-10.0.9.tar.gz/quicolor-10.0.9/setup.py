414189643660ac619d78e4790c5fd3b3aa5e5bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 14:05:29 +0000

    Add package quicolor/10.0.9/quicolor-10.0.9.tar.gz

---
+++ packages/quicolor/quicolor-10.0.9.tar.gz/quicolor-10.0.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quicolor",
+    version="10.0.9",
+    packages=["quicolor"],
+    install_requires=[
+        "requests",
+    ],
+    author="Author",
+    author_email="author@example.com",
+    description="Color formatting utility for Python console applications",
+    keywords="color, format, console, utility",
+    url="https://github.com/author/quicolor",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.6",
+) 
