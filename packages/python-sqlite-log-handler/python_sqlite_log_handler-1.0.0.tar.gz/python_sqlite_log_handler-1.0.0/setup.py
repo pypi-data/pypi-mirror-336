baa1d4a0cd600bb3a989a037a9045bacf7bb3bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 03:16:36 +0000

    Add package python-sqlite-log-handler/1.0.0/python_sqlite_log_handler-1.0.0.tar.gz

---
+++ packages/python-sqlite-log-handler/python_sqlite_log_handler-1.0.0.tar.gz/python_sqlite_log_handler-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='python_sqlite_log_handler',
+    packages=['python_sqlite_log_handler'],
+    version='1.0.0',
+    license='MIT',
+    description='SQLite log handler for Python logging module',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Roger Vilà',
+    author_email='rogervila@me.com',
+    url='https://github.com/rogervila/python_sqlite_log_handler',
+    download_url='https://github.com/rogervila/python_sqlite_log_handler/archive/1.0.0.tar.gz',
+    keywords=['sqlite', 'logging', 'handler'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
