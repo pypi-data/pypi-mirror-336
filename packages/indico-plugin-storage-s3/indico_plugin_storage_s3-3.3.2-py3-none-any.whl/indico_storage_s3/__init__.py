1bc035227c823dd96e3f926055a5a0546e625a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:04:29 +0000

    Add package indico-plugin-storage-s3/3.3.2/indico_plugin_storage_s3-3.3.2-py3-none-any.whl

---
+++ packages/indico-plugin-storage-s3/indico_plugin_storage_s3-3.3.2-py3-none-any.whl/indico_storage_s3/__init__.py
@@ -0,0 +1,17 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2002 - 2025 CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
+from indico.core import signals
+from indico.util.i18n import make_bound_gettext
+
+
+_ = make_bound_gettext('storage_s3')
+
+
+@signals.core.import_tasks.connect
+def _import_tasks(sender, **kwargs):
+    import indico_storage_s3.task  # noqa: F401
---
+++ packages/indico-plugin-storage-s3/indico_plugin_storage_s3-3.3.2-py3-none-any.whl/indico_storage_s3/util.py
@@ -0,0 +1,22 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2002 - 2025 CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
+import unicodedata
+from urllib.parse import quote
+
+
+def make_content_disposition_args(attachment_filename):
+    try:
+        attachment_filename.encode('ascii')
+    except UnicodeEncodeError:
+        simple = unicodedata.normalize('NFKD', attachment_filename)
+        simple = simple.encode('ascii', 'ignore').decode('ascii')
+        # safe = RFC 5987 attr-char
+        quoted = quote(attachment_filename, safe='!#$&+-.^_`|~')
+        return {'filename': simple, 'filename*': f"UTF-8''{quoted}"}
+    else:
+        return {'filename': attachment_filename}
