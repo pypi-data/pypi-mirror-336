109e1f0f9beb1974ebbd7bfe340d535e84d70f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 04:04:48 +0000

    Add package DisNet/0.7/DisNet-0.7.tar.gz

---
+++ packages/DisNet/DisNet-0.7.tar.gz/DisNet-0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DisNet",
+    version="0.7",
+    packages=find_packages(),  # Нахождение всех пакетов в проекте
+    install_requires=["discord"],  # Зависимости
+    description="A Discord bot package",  # Описание
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Лицензия
+        "Operating System :: OS Independent",
+    ],
+)
