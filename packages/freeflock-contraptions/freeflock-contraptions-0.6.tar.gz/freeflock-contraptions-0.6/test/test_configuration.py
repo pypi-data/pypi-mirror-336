bf38d2a9648593894af87e77ef150b9d29fb02da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 16:05:46 +0000

    Add package freeflock-contraptions/0.6/freeflock-contraptions-0.6.tar.gz

---
+++ packages/freeflock-contraptions/freeflock-contraptions-0.6.tar.gz/freeflock-contraptions-0.6/test/test_configuration.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_collect_environment_variable(monkeypatch):
+    monkeypatch.delenv("AZURE_KEY_VAULT_NAME", raising=False)
+    monkeypatch.setenv("FREEFLOCK", "FREEFLOCK")
+    from packages.freeflock_contraptions.configuration import collect
+    result = await collect("FREEFLOCK")
+    assert result == "FREEFLOCK"
+
+
+@pytest.mark.asyncio
+async def test_collect_azure_keyvault(monkeypatch):
+    # set env var AZURE_KEY_VAULT_NAME to an existing key vault with a secret named "FREEFLOCK" to run this test
+    from packages.freeflock_contraptions.configuration import collect
+    result = await collect("FREEFLOCK")
+    assert result == "FREEFLOCK"
+
+@pytest.mark.asyncio
+async def test_collect_azure_keyvault_caching(monkeypatch):
+    # set env var AZURE_KEY_VAULT_NAME to an existing key vault with a secret named "FREEFLOCK" to run this test
+    from packages.freeflock_contraptions.configuration import collect
+    result = await collect("FREEFLOCK")
+    assert result == "FREEFLOCK"
+    result = await collect("FREEFLOCK")
+    assert result == "FREEFLOCK"
