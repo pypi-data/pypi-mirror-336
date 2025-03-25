3a640ad8438ddf8964f16653c20afd5ba06b31ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 07:05:22 +0000

    Add package zhmiscellany/4.6.3/zhmiscellany-4.6.3-py3-none-any.whl

---
+++ packages/zhmiscellany/zhmiscellany-4.6.3-py3-none-any.whl/zhmiscellany/list.py
@@ -0,0 +1,31 @@
+def remove_duplicates(l_list):
+    return list(dict.fromkeys(l_list))
+
+
+def subtract_lists(main_list, *other_lists):
+    # Create a set of all items in other lists
+    other_items = set(item for sublist in other_lists for item in sublist)
+
+    # Return items from the main list that are not in other lists
+    return [item for item in main_list if item not in other_items]
+
+
+def remove_duplicates_by_element(tuple_list, element):
+    seen = {}
+    result = []
+    for tpl in tuple_list:
+        if tpl[element] not in seen:
+            seen[tpl[element]] = True
+            result.append(tpl)
+    return result
+
+
+def multi_split(string_list, splits):
+    string = string_list
+    for i in splits:
+        temp = []
+        for stri in string:
+            temp.extend(stri.split(i))
+        string = temp
+
+    return string
