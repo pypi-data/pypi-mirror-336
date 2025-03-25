803321b3a2f73c73be3b1f2628702bd496a7b16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 03:15:58 +0000

    Add package sc-common-interface/2.9.4/sc_common_interface-2.9.4.tar.gz

---
+++ packages/sc-common-interface/sc_common_interface-2.9.4.tar.gz/sc_common_interface-2.9.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sc_common_interface',
+    version='2.9.4',
+    description='SC贴文销售公用的接口',
+    author='river',
+    packages=find_packages(),
+    install_requires=[
+    "requests>=2.0.0",
+    "jsonpath>=0.82",
+    "websocket-client>=1.8.0",
+    "cryptography>=44.0.2"
+             ],
+)
+
