fb0640dea21431b574940a8b133ec90dd0654573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 15:05:24 +0000

    Add package x7-tian-xuehui-ecs-fix1/2025.3.25.0/x7-tian-xuehui-ecs-fix1-2025.3.25.0.tar.gz

---
+++ packages/x7-tian-xuehui-ecs-fix1/x7-tian-xuehui-ecs-fix1-2025.3.25.0.tar.gz/x7-tian-xuehui-ecs-fix1-2025.3.25.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import X7TianXuehuiEcsFix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('X7TianXuehuiEcsFix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="x7-tian-xuehui-ecs-fix1",
+    version=X7TianXuehuiEcsFix1.__version__,
+    url="https://github.com/apachecn/x7-tian-xuehui-ecs-fix1",
+    author=X7TianXuehuiEcsFix1.__author__,
+    author_email=X7TianXuehuiEcsFix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="7天学会ECS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "x7-tian-xuehui-ecs-fix1=X7TianXuehuiEcsFix1.__main__:main",
+            "X7TianXuehuiEcsFix1=X7TianXuehuiEcsFix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
