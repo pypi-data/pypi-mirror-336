cd09f95db15f10445d7e941047e2c968bc6bee95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 07:05:12 +0000

    Add package clustervis/1.0.44/clustervis-1.0.44.tar.gz

---
+++ packages/clustervis/clustervis-1.0.44.tar.gz/clustervis-1.0.44/tests/test_visualization_base_save.py
@@ -0,0 +1,28 @@
+from clustervis import base_classifier_plot
+
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_blobs
+from sklearn.neighbors import KNeighborsClassifier
+
+# Step 1: Generate synthetic data
+X, y = make_blobs(n_samples=300, centers=4, random_state=76, cluster_std=1.0)
+
+# Step 2: Train a base classifier (e.g., a KNN Classifier)
+base_estimator = KNeighborsClassifier(n_neighbors=3)
+base_estimator.fit(X, y)
+
+# Step 3: Define some colors for each class (e.g., for 4 classes)
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]  # Red, Green, Blue, Yellow
+
+# Step 4: Declare the name, the resolution and the visibility of the plot
+plotTitle = 'RGB Clustering Decision Boundaries (KNN Classifier)'
+resolution = 100
+show = True
+
+# Step 5: Declare a path to save the plot
+plotPath = "/data/notebook_files" # Example path for JetBrains Datalore
+filename = "classifier.png"
+
+# Step 6: Plot the decision boundary and save it
+base_classifier_plot(X, base_estimator, colors, resolution, plotTitle, show, plotPath, filename)
