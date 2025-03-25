7f25e07c75d428501169d9b12eaddf386b0537ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:04:54 +0000

    Add package DeviceUAgen/0.1.1/deviceuagen-0.1.1.tar.gz

---
+++ packages/DeviceUAgen/deviceuagen-0.1.1.tar.gz/deviceuagen-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DeviceUAgen",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Fajarrrrky",
+    author_email="saepulfajar440@gmail.com",
+    description="Generator User-Agent untuk berbagai perangkat",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Fajarrrrky/DeviceUAgen",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
