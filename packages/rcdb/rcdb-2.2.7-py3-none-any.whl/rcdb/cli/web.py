2f0fea9626d722e7991683e2af84473334d241a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 15:04:59 +0000

    Add package rcdb/2.2.7/rcdb-2.2.7-py3-none-any.whl

---
+++ packages/rcdb/rcdb-2.2.7-py3-none-any.whl/rcdb/cli/web.py
@@ -0,0 +1,29 @@
+import os
+import click
+
+from rcdb import web as rcdb_web, RCDBProvider
+from .context import pass_rcdb_context
+
+
+@click.command("web")
+@pass_rcdb_context
+def web(context):
+    """
+    Runs the local RCDB web application using the connection string from
+    either the CLI context or the RCDB_CONNECTION environment variable.
+    """
+    # If user provided --connection on the CLI, context.db.connection_str is set:
+    rcdb_provider = context.db
+    if rcdb_provider and rcdb_provider.connection_string:
+        assert isinstance(rcdb_provider, RCDBProvider)
+        rcdb_web.app.config["SQL_CONNECTION_STRING"] = rcdb_provider.connection_string
+    elif "RCDB_CONNECTION" in os.environ:
+        # Otherwise check the environment variable
+        rcdb_web.app.config["SQL_CONNECTION_STRING"] = os.environ["RCDB_CONNECTION"]
+    else:
+        # If neither is present, show an error and exit
+        click.echo("ERROR: no connection string found. Provide via CLI or RCDB_CONNECTION env variable.")
+        raise SystemExit(1)
+
+    # Start the Flask dev server
+    rcdb_web.app.run()
