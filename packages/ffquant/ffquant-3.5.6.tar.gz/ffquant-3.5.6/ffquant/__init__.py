e59950c90eed21f410159c1025f66d427b9532f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 04:04:47 +0000

    Add package ffquant/3.5.6/ffquant-3.5.6.tar.gz

---
+++ packages/ffquant/ffquant-3.5.6.tar.gz/ffquant-3.5.6/ffquant/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from .brokers import *
+from .feeds import *
+from .indicators import *
+from .calendar import *
+from .plot import *
+from .utils import *
