61d8982ad64a8d3143b2262bf0b0f4cba6f6f6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 17:04:59 +0000

    Add package trace-it/0.1.3/trace_it-0.1.3.tar.gz

---
+++ packages/trace-it/trace_it-0.1.3.tar.gz/trace_it-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="trace_it",
+    version="0.1.3",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "opentelemetry-api",
+        "opentelemetry-sdk",
+        "openinference-semantic-conventions",
+        "arize-phoenix",
+    ],
+    python_requires=">=3.8",
+)
