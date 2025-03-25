042ea5bb02c3124eadf82033003e216f345adc1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 15:05:20 +0000

    Add package comfyui-frontend-package/1.15.1/comfyui_frontend_package-1.15.1.tar.gz

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.15.1.tar.gz/comfyui_frontend_package-1.15.1/comfyui_frontend_package/static/assets/DesktopStartView-C9n-sBXZ.js
@@ -0,0 +1,19 @@
+import { d as defineComponent, o as openBlock, y as createBlock, z as withCtx, k as createVNode, j as unref, bV as script } from "./index-o_NbcT3P.js";
+import { _ as _sfc_main$1 } from "./BaseViewTemplate-DUC2ZOkw.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "DesktopStartView",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(_sfc_main$1, { dark: "" }, {
+        default: withCtx(() => [
+          createVNode(unref(script), { class: "m-8 w-48 h-48" })
+        ]),
+        _: 1
+      });
+    };
+  }
+});
+export {
+  _sfc_main as default
+};
+//# sourceMappingURL=DesktopStartView-C9n-sBXZ.js.map
