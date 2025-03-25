19558fda7c4585c3664b69fb396ba732722f0876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 05:05:01 +0000

    Add package indico-plugin-livesync/3.3.2/indico_plugin_livesync-3.3.2-py3-none-any.whl

---
+++ packages/indico-plugin-livesync/indico_plugin_livesync-3.3.2-py3-none-any.whl/indico_livesync/__init__.py
@@ -0,0 +1,25 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2002 - 2025 CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
+from indico.core import signals
+from indico.util.i18n import make_bound_gettext
+
+
+_ = make_bound_gettext('livesync')
+__all__ = ('LiveSyncPluginBase', 'LiveSyncBackendBase', 'AgentForm', 'SimpleChange', 'process_records',
+           'Uploader')
+
+
+from .base import LiveSyncBackendBase, LiveSyncPluginBase  # noqa: E402
+from .forms import AgentForm  # noqa: E402
+from .simplify import SimpleChange, process_records  # noqa: E402
+from .uploader import Uploader  # noqa: E402
+
+
+@signals.core.import_tasks.connect
+def _import_tasks(sender, **kwargs):
+    import indico_livesync.task  # noqa: F401
