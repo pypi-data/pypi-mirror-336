0e7b0d4f66cce154adef682cd93a27910c678d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 05:04:34 +0000

    Add package schubmult/2.0.3/schubmult-2.0.3.tar.gz

---
+++ packages/schubmult/schubmult-2.0.3.tar.gz/schubmult-2.0.3/src/schubmult/sage_integration/__init__.py
@@ -0,0 +1,26 @@
+from sage.all import *  # noqa: F403
+
+
+from ._fast_schubert_polynomial_ring import (
+    FastSchubertPolynomialRing,
+    FastSchubertPolynomial,
+    FastSchubertPolynomialRing_base,
+    FastQuantumSchubertPolynomialRing,
+)
+from ._fast_double_schubert_polynomial_ring import (
+    FastDoubleSchubertPolynomialRing,
+    FastDoubleSchubertPolynomial,
+    FastDoubleSchubertPolynomialRing_base,
+    FastQuantumDoubleSchubertPolynomialRing,    
+)
+
+__all__ = [
+    "FastSchubertPolynomialRing",
+    "FastSchubertPolynomial",
+    "FastSchubertPolynomialRing_base",
+    "FastDoubleSchubertPolynomialRing",
+    "FastDoubleSchubertPolynomial",
+    "FastDoubleSchubertPolynomialRing_base",
+    "FastQuantumSchubertPolynomialRing",
+    "FastQuantumDoubleSchubertPolynomialRing",    
+]
