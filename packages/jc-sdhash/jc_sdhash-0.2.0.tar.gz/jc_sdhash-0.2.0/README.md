45f1bfbe798726779de5d6caefd48321c0976ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 07:05:42 +0000

    Add package jc-sdhash/0.2.0/jc_sdhash-0.2.0.tar.gz

---
+++ packages/jc-sdhash/jc_sdhash-0.2.0.tar.gz/jc_sdhash-0.2.0/README.md
@@ -0,0 +1,1 @@
+# SDhash_Python
---
+++ packages/jc-sdhash/jc_sdhash-0.2.0.tar.gz/jc_sdhash-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jc_sdhash",
+    version="0.2.0",  # Ensure this is 0.2.0
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"jc_sdhash": ["sdhash"]},
+    install_requires=[],
+    author="Mabon Ninan",
+    author_email="mabonmn2002@gmail.com",
+    description="A Python wrapper for the SDHash binary.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Botacin-s-Lab/SDhash_Python",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
