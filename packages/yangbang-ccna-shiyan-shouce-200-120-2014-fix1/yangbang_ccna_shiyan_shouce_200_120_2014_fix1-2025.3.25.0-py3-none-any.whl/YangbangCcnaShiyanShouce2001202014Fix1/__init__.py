72a2efe120dc6c8114664fba8f8fad0bef4e6384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 15:05:04 +0000

    Add package yangbang-ccna-shiyan-shouce-200-120-2014-fix1/2025.3.25.0/yangbang_ccna_shiyan_shouce_200_120_2014_fix1-2025.3.25.0-py3-none-any.whl

---
+++ packages/yangbang-ccna-shiyan-shouce-200-120-2014-fix1/yangbang_ccna_shiyan_shouce_200_120_2014_fix1-2025.3.25.0-py3-none-any.whl/YangbangCcnaShiyanShouce2001202014Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yangbang-ccna-shiyan-shouce-200-120-2014-fix1
+https://github.com/apachecn/yangbang-ccna-shiyan-shouce-200-120-2014-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.25.0"
