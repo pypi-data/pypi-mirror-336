0e127b2a57c46bd2f0f0e57881a9f1d740d10a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 05:05:04 +0000

    Add package VectorCode/0.5.0/vectorcode-0.5.0-py3-none-any.whl

---
+++ packages/VectorCode/vectorcode-0.5.0-py3-none-any.whl/vectorcode/subcommands/__init__.py
@@ -0,0 +1,21 @@
+from vectorcode.subcommands.check import check
+from vectorcode.subcommands.clean import clean
+from vectorcode.subcommands.drop import drop
+from vectorcode.subcommands.init import init
+from vectorcode.subcommands.ls import ls
+from vectorcode.subcommands.prompt import prompts
+from vectorcode.subcommands.query import query
+from vectorcode.subcommands.update import update
+from vectorcode.subcommands.vectorise import vectorise
+
+__all__ = [
+    "drop",
+    "init",
+    "query",
+    "ls",
+    "vectorise",
+    "check",
+    "update",
+    "clean",
+    "prompts",
+]
