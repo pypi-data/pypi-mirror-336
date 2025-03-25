086473cc74ae36793af139d6fcb85c1458333484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 03:15:49 +0000

    Add package StructResult/0.3.2/structresult-0.3.2.tar.gz

---
+++ packages/StructResult/structresult-0.3.2.tar.gz/structresult-0.3.2/src/StructResult/main.py
@@ -0,0 +1,31 @@
+from typing import Optional, TypeVar, Generic, Self
+
+T = TypeVar("T")
+
+
+class Result(Generic[T]):
+    value: Optional[T]
+    err: Optional[list[Exception]]
+    __slots__ = ("value", "err")
+
+    def __init__(self, value: Optional[T], err: list[Exception] = None):
+        self.value = value
+        self.err = err
+
+    def __getitem__(self, item):
+        if item == 0:
+            return self.value
+        elif item == 1:
+            return self.err
+        else:
+            raise StopIteration
+
+    def append_err(self, e: Exception):
+        if self.err is None:
+            self.err = list()
+        self.err.append(e)
+
+    def extend_err(self, e: list[Exception]):
+        if self.err is None:
+            self.err = list()
+        self.err.extend(e)
---
+++ packages/StructResult/structresult-0.3.2.tar.gz/structresult-0.3.2/test/test_Result.py
@@ -0,0 +1,15 @@
+import unittest
+from StructResult import Result
+
+
+class TestType(unittest.TestCase):
+
+    def test_init(self):
+        res = Result[int](1)
+        self.assertEqual(res.value, 1)
+        self.assertEqual(list(res), [1, None])
+
+    def test_Optional(self):
+
+        def foo() -> Result[int]:
+            return Result(None)
