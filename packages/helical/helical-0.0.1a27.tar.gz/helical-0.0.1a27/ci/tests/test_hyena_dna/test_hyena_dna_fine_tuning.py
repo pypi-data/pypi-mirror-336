0135b9a3ba1448f80391ded5ec65989bf7d5f562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 04:05:28 +0000

    Add package helical/0.0.1a27/helical-0.0.1a27.tar.gz

---
+++ packages/helical/helical-0.0.1a27.tar.gz/helical-0.0.1a27/ci/tests/test_hyena_dna/test_hyena_dna_fine_tuning.py
@@ -0,0 +1,35 @@
+import pytest
+import torch
+from helical.models.hyena_dna import HyenaDNAConfig, HyenaDNAFineTuningModel
+
+
+class TestHyenaDNAFineTuning:
+    @pytest.fixture(params=["hyenadna-tiny-1k-seqlen", "hyenadna-tiny-1k-seqlen-d256"])
+    def hyenaDNAFineTune(self, request):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        config = HyenaDNAConfig(
+            model_name=request.param, batch_size=1, device=self.device
+        )
+        return HyenaDNAFineTuningModel(
+            hyena_config=config, fine_tuning_head="classification", output_size=1
+        )
+
+    @pytest.fixture
+    def mock_data(self, hyenaDNAFineTune):
+        input_sequences = ["AAAA", "CCCC", "TTTT", "ACGT", "ACGN", "ANNT"]
+        labels = [0, 0, 0, 0, 0, 0]
+        tokenized_sequences = hyenaDNAFineTune.process_data(input_sequences)
+        return tokenized_sequences, labels
+
+    def test_output_dimensionality_of_fine_tuned_model(
+        self, hyenaDNAFineTune, mock_data
+    ):
+        input_sequences, labels = mock_data
+        hyenaDNAFineTune.train(
+            train_dataset=input_sequences,
+            train_labels=labels,
+            validation_dataset=input_sequences,
+            validation_labels=labels,
+        )
+        outputs = hyenaDNAFineTune.get_outputs(input_sequences)
+        assert outputs.shape == (len(input_sequences), 1)
