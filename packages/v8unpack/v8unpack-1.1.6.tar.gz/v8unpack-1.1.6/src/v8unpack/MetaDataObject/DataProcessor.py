28b3910c286a64a93cdb22e89167e9b73a3a6d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 16:05:10 +0000

    Add package v8unpack/1.1.6/v8unpack-1.1.6.tar.gz

---
+++ packages/v8unpack/v8unpack-1.1.6.tar.gz/v8unpack-1.1.6/src/v8unpack/MetaDataObject/DataProcessor.py
@@ -0,0 +1,17 @@
+from ..MetaDataObject.core.Container import FormContainer
+
+
+class DataProcessor(FormContainer):
+    ext_code = {
+        'mgr': '2',  # модуль менеджера
+        'obj': '0',  # модуль объекта
+    }
+    help_file_number = 1
+
+    @classmethod
+    def get_decode_header(cls, header_data):
+        return header_data[0][1][3][1]
+
+    def encode_header(self):
+        super().encode_header()
+        self.set_product_comment(self.options.get('product_version'))
