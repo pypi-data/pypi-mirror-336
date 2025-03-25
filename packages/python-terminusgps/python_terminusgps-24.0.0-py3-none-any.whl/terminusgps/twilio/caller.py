2162fac6798fd488ea1d4237e37a29d50b2fb629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 05:04:10 +0000

    Add package python-terminusgps/24.0.0/python_terminusgps-24.0.0-py3-none-any.whl

---
+++ packages/python-terminusgps/python_terminusgps-24.0.0-py3-none-any.whl/terminusgps/twilio/caller.py
@@ -0,0 +1,55 @@
+from typing import Any
+
+import asyncio
+import twilio.rest
+
+from django.conf import settings
+from twilio.http.async_http_client import AsyncTwilioHttpClient
+
+
+class TwilioCaller:
+    def __init__(self) -> None:
+        self.client_sid = settings.TWILIO_SID
+        self.client_token = settings.TWILIO_TOKEN
+        self.from_number = settings.TWILIO_FROM_NUMBER
+        self.messaging_sid = settings.TWILIO_MESSAGING_SID
+
+    def __enter__(self) -> "TwilioCaller":
+        self.client = twilio.rest.Client(
+            self.client_sid, self.client_token, http_client=AsyncTwilioHttpClient()
+        )
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb) -> None:
+        return None
+
+    async def create_notification(
+        self, to_number: str, message: str, method: str = "sms"
+    ) -> asyncio.Task[Any]:
+        match method:
+            case "sms":
+                task = asyncio.create_task(
+                    self.create_sms(to_number=to_number, message=message)
+                )
+            case "call" | "phone":
+                task = asyncio.create_task(
+                    self.create_call(to_number=to_number, message=message)
+                )
+            case _:
+                raise ValueError(f"Unsupported TwilioCaller method '{method}'.")
+        return task
+
+    async def create_call(self, to_number: str, message: str) -> None:
+        await self.client.calls.create_async(
+            to=to_number,
+            from_=self.from_number,
+            twiml=f"<Response><Say>{message}</Say></Response>",
+        )
+
+    async def create_sms(self, to_number: str, message: str) -> None:
+        await self.client.messages.create_async(
+            to=to_number,
+            from_=self.from_number,
+            body=message,
+            messaging_service_sid=self.messaging_sid,
+        )
---
+++ packages/python-terminusgps/python_terminusgps-24.0.0-py3-none-any.whl/terminusgps/wialon/items/unit_group.py
@@ -0,0 +1,152 @@
+from terminusgps.wialon import flags
+from terminusgps.wialon.items.base import WialonBase
+
+
+class WialonUnitGroup(WialonBase):
+    def create(self, creator_id: str | int, name: str) -> int | None:
+        """
+        Creates a new Wialon unit group.
+
+        :param creator_id: A Wialon user id.
+        :type creator_id: :py:obj:`str` | :py:obj:`int`
+        :param name: A name for the group.
+        :type name: :py:obj:`str`
+        :raises ValueError: If ``creator_id`` is not a digit.
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: The Wialon id for the new group, if it was created.
+        :rtype: :py:obj:`int` | :py:obj:`None`
+
+        """
+
+        if isinstance(creator_id, str) and not creator_id.isdigit():
+            raise ValueError(f"'creator_id' must be a digit, got '{creator_id}'.")
+
+        response = self.session.wialon_api.core_create_unit_group(
+            **{
+                "creatorId": creator_id,
+                "name": name,
+                "dataFlags": flags.DATAFLAG_UNIT_BASE,
+            }
+        )
+        return int(response.get("item", {}).get("id")) if response.get("item") else None
+
+    def set_items(self, new_items: list[str]) -> None:
+        """
+        Sets this group's members to a list of Wialon unit ids.
+
+        :param new_items: A list of Wialon unit ids.
+        :type new_items: :py:obj:`list`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+
+        self.session.wialon_api.unit_group_update_units(
+            **{"itemId": self.id, "units": new_items}
+        )
+
+    def is_member(self, item: WialonBase) -> bool:
+        """
+        Determines whether or not ``item`` is a member of the group.
+
+        :param item: A Wialon object.
+        :type item: :py:obj:`~terminusgps.wialon.items.base.WialonBase`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: :py:obj:`True` if ``item`` is a member of the group, else :py:obj:`False`.
+        :rtype: :py:obj:`bool`
+
+        """
+        return True if item.id in self.items else False
+
+    def add_item(self, item: WialonBase) -> None:
+        """
+        Adds a Wialon item to the group.
+
+        :param item: A Wialon object.
+        :type item: :py:obj:`~terminusgps.wialon.items.base.WialonBase`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        new_items: list[str] = self.items.copy() + [str(item.id)]
+        self.set_items(new_items)
+
+    def rm_item(self, item: WialonBase) -> None:
+        """
+        Removes a Wialon unit from the group, if it's a member of the group.
+
+        :param item: A Wialon object.
+        :type item: :py:obj:`~terminusgps.wialon.items.base.WialonBase`
+        :raises AssertionError: If the item wasn't in the group.
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        assert self.is_member(item), f"Cannot remove {item}, it's not in the group"
+        new_items: list[str] = self.items.copy()
+        new_items.remove(str(item.id))
+        self.set_items(new_items)
+
+    @property
+    def items(self) -> list[str]:
+        """
+        Returns a list of the group's Wialon unit ids.
+
+        :type: :py:obj:`list`
+
+        """
+        response = self.session.wialon_api.core_search_items(
+            **{
+                "spec": {
+                    "itemsType": "avl_unit_group",
+                    "propName": "sys_id",
+                    "propValueMask": str(self.id),
+                    "sortType": "sys_id",
+                    "propType": "property",
+                    "or_logic": 0,
+                },
+                "force": 1,
+                "flags": flags.DATAFLAG_UNIT_BASE,
+                "from": 0,
+                "to": 0,
+            }
+        )
+        return [unit_id for unit_id in response.get("items")[0].get("u", [])]
+
+
+def main() -> None:
+    import logging
+
+    from django.utils import timezone
+    from django.conf import settings
+    from terminusgps.wialon.session import WialonSession
+    from terminusgps.wialon.items import WialonUnit
+    from terminusgps.wialon.utils import get_hw_type_id
+
+    timestamp = f"{timezone.now():%Y_%m_%d_%H:%M:%S}"
+    with WialonSession(log_level=logging.DEBUG) as session:
+        unit = WialonUnit(
+            id=None,
+            session=session,
+            creator_id=settings.WIALON_ADMIN_ID,
+            name=f"test_unit_{timestamp}",
+            hw_type_id=get_hw_type_id("Test HW", session=session),
+        )
+        group = WialonUnitGroup(
+            id=None,
+            session=session,
+            creator_id=settings.WIALON_ADMIN_ID,
+            name=f"test_group_{timestamp}",
+        )
+        group.add_item(unit)
+        print(f"{group.items = }")
+        unit.delete()
+        group.delete()
+    return
+
+
+if __name__ == "__main__":
+    main()
