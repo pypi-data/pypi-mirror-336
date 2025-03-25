9b18d78a8a9ba2d503e53b61ad01cd5ed67abef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 15:05:05 +0000

    Add package open-webdriver/1.6.4/open_webdriver-1.6.4-py3-none-any.whl

---
+++ packages/open-webdriver/open_webdriver-1.6.4-py3-none-any.whl/open_webdriver/download_chromium.py
@@ -0,0 +1,121 @@
+"""
+Module to download the Chromium browser from the repo.
+"""
+
+# flake8: noqa F401
+# pylint: disable=unused-import
+
+import os
+import subprocess
+import sys
+import zipfile
+
+from download import download  # type: ignore
+
+from open_webdriver.path import WDM_CHROMIUM_DIR
+
+
+def _set_exe_permissions(start_dir: str) -> None:
+    """Sets the permissions on the chromium executable."""
+    if sys.platform == "win32":
+        return
+    for root, _, files in os.walk(start_dir):
+        for file_name in files:
+            path = os.path.join(root, file_name)
+            os.chmod(path, 0o755)
+
+
+def _download(url: str, path: str) -> str:
+    """Downloads a file from a url to a path."""
+
+    path = download(
+        url=url, path=path, kind="file", progressbar=True, replace=True, timeout=5 * 60
+    )
+    return path
+
+
+def _unzip(zip_path: str) -> None:
+    """Unzips a zip file."""
+
+    if sys.platform == "linux":
+        print("Linux detected.")
+        url = "https://github.com/zackees/open-webdriver/raw/main/chromium/7za"
+        dst = os.path.join(WDM_CHROMIUM_DIR, "7za")
+        path_7za = _download(url=url, path=dst)
+        path_7za = os.path.abspath(path_7za)
+        assert os.path.exists(path_7za), f"{path_7za} does not exist."
+        # Add executable permissions.
+        os.chmod(path_7za, 0o755)
+        print("Using 7za tool")
+        try:
+            zip_name = os.path.basename(zip_path)
+            cmd = f"{path_7za} -y x {zip_name}"
+            print(f'Executing: "{cmd}"')
+            _ = subprocess.check_output(cmd, cwd=os.path.dirname(zip_path), shell=True)
+        except subprocess.CalledProcessError:
+            print("Failed to unzip with command line, falling back to python unzip")
+    elif sys.platform == "darwin":
+        print("MacOS detected.")
+        dir_path = os.path.dirname(zip_path)
+        os.makedirs(dir_path, exist_ok=True)
+        cmd = f'py7zr x "{os.path.abspath(zip_path)}" "{os.path.abspath(dir_path)}"'
+        print(f'Executing: "{cmd}"')
+        try:
+            _ = subprocess.check_output(cmd, cwd=os.path.dirname(zip_path), shell=True)
+        except subprocess.CalledProcessError:
+            print(f"Failed to un7z with command line {cmd}")
+        # Remove quarantine attribute.
+        cmd = f'xattr -d com.apple.quarantine "{os.path.abspath(zip_path)}"'
+        print(f'Executing: "{cmd}"')
+        os.system(cmd)
+    else:
+        print("Windows detected.")
+        with zipfile.ZipFile(zip_path, "r") as zipf:
+            zipf.testzip()
+            zipf.extractall(os.path.dirname(zip_path))
+
+
+def get_chromium_exe() -> str:
+    """Fetches the chromium executable."""
+    ext = ".7z" if sys.platform == "darwin" else ".zip"
+    url_src = f"https://github.com/zackees/open-webdriver/raw/main/chromium/{sys.platform}{ext}"
+    platform_dir = os.path.join(WDM_CHROMIUM_DIR, sys.platform)
+    print(f"WDM_CHROMIUM_DIR: {WDM_CHROMIUM_DIR}")
+    finished_stamp = os.path.join(platform_dir, "finished")
+    if not os.path.exists(finished_stamp):
+        ext = ".7z" if sys.platform == "darwin" else ".zip"
+        archive_dst = platform_dir + ext
+        print(f"Download {url_src} to {archive_dst}")
+        _download(url=url_src, path=archive_dst)
+        assert os.path.exists(archive_dst), f"{archive_dst} does not exist."
+        print(f"Unzipping {archive_dst}")
+        _unzip(zip_path=archive_dst)
+        print(f"Fixing permissions {platform_dir}")
+        _set_exe_permissions(platform_dir)
+        # Touch file.
+        print(f"Touching file: {finished_stamp}")
+        os.makedirs(os.path.dirname(finished_stamp), exist_ok=True)
+        with open(finished_stamp, encoding="utf-8", mode="w") as filed:
+            filed.write("")
+        print(f"Removing {archive_dst}")
+        os.remove(archive_dst)
+    exe_path = None
+    if sys.platform == "win32":
+        exe_path = os.path.join(platform_dir, "chrome.exe")
+    elif sys.platform == "linux":
+        exe_path = os.path.join(platform_dir, "chrome")
+    elif sys.platform == "darwin":
+        exe_path = os.path.join(
+            platform_dir, "Chromium.app", "Contents", "MacOS", "Chromium"
+        )
+    else:
+        raise NotImplementedError(f"Unsupported platform: {sys.platform}")
+    print(f"Chromium executable: {exe_path}")
+    assert os.path.exists(
+        exe_path
+    ), f"Chromium executable not found: {os.path.abspath(exe_path)}"
+    return exe_path
+
+
+if __name__ == "__main__":
+    print(get_chromium_exe())
---
+++ packages/open-webdriver/open_webdriver-1.6.4-py3-none-any.whl/open_webdriver/main.py
@@ -0,0 +1,131 @@
+"""
+Handles the creation of the web driver.
+"""
+
+# pylint: disable=protected-access
+
+import os
+import ssl
+import sys
+import traceback
+from typing import Optional
+
+import filelock  # type: ignore
+import urllib3  # type: ignore
+from selenium import webdriver  # type: ignore
+from selenium.webdriver import ChromeOptions  # type: ignore
+from selenium.webdriver.remote.webdriver import WebDriver as Driver  # type: ignore
+from webdriver_manager.chrome import ChromeDriverManager  # type: ignore
+from webdriver_manager.core.driver_cache import DriverCacheManager  # type: ignore
+from webdriver_manager.core.os_manager import ChromeType  # type: ignore
+
+from open_webdriver.download_chromium import get_chromium_exe
+from open_webdriver.path import LOG_FILE, WDM_DIR
+
+urllib3.disable_warnings()
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Turn off SSL verification to allow visiting websites with self-signed certs.
+os.environ["WDM_SSL_VERIFY"] = "0"
+
+FORCE_HEADLESS = sys.platform == "linux" and "DISPLAY" not in os.environ
+
+CACHE_TIMEOUT_DAYS = 7
+
+os.makedirs(WDM_DIR, exist_ok=True)
+LOCK_FILE = os.path.join(WDM_DIR, "lock.file")
+
+INSTALL_TIMEOUT = float(60 * 10)  # Upto 10 minutes of install time.
+
+# CHROME_VERSION = "136.0.7071.0"
+CHROME_VERSION = None
+
+
+def _user_agent(chrome_version: str | None = None) -> str:
+    """Gets the user agent."""
+    chrome_version = chrome_version or "136.0.7071.0"
+    return (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        " AppleWebKit/537.36 (KHTML, like Gecko)"
+        f" Chrome/{chrome_version} Safari/537.36"
+    )
+
+
+def _init_log() -> None:
+    """Initializes the log."""
+    os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+    with open(LOG_FILE, encoding="utf-8", mode="w") as filed:
+        filed.write(f"{__file__}: Starting up web driver.\n")
+        if sys.platform == "linux":
+            if os.geteuid() == 0:
+                filed.write("\n\n  WARNING: Running as root. The driver may crash!\n\n")
+
+
+def open_webdriver(  # pylint: disable=too-many-arguments,too-many-branches
+    headless: bool = True,
+    verbose: bool = False,  # pylint: disable=unused-argument
+    timeout: float = INSTALL_TIMEOUT,
+    disable_gpu: Optional[bool] = None,
+    disable_dev_shm_usage: bool = True,
+    user_agent: str | None = None,
+) -> Driver:
+    """Opens the web driver."""
+    user_agent = user_agent or _user_agent()
+    _init_log()
+    if headless or FORCE_HEADLESS:
+        if FORCE_HEADLESS and not headless:
+            print("\n  WARNING: HEADLESS ENVIRONMENT DETECTED, FORCING HEADLESS")
+        headless = True
+    # For Chrome/Brave just install the driver immediately.
+    opts: ChromeOptions = ChromeOptions()
+    opts.add_argument("--ignore-certificate-errors")
+    opts.add_argument("--no-sandbox")
+    if disable_dev_shm_usage:
+        opts.add_argument("--disable-dev-shm-usage")
+    if headless:
+        opts.add_argument("--headless")
+        if disable_gpu is None or disable_gpu is True:
+            opts.add_argument("--disable-gpu")
+        opts.add_argument("--enable-unsafe-swiftshader")
+    if user_agent:
+        opts.add_argument(f"--user-agent={user_agent}")
+    lock = filelock.FileLock(LOCK_FILE)
+    with lock.acquire(timeout=timeout):
+        if verbose:
+            print("  Installing web driver...")
+        if CHROME_VERSION is not None:
+            chromium_exe = get_chromium_exe()
+            if verbose:
+                print("  Finished installing web driver: ", chromium_exe)
+        cache_manager = DriverCacheManager(
+            root_dir=WDM_DIR, valid_range=CACHE_TIMEOUT_DAYS
+        )
+
+        # opts.binary_location = chromium_exe
+        driver_path = ChromeDriverManager(
+            chrome_type=ChromeType.GOOGLE,
+            cache_manager=cache_manager,
+        ).install()
+    if verbose:
+        print(f"\n  Using ChromeDriver: {driver_path}")
+    try:
+        if os.path.exists(LOG_FILE):
+            os.remove(LOG_FILE)
+        driver = webdriver.Chrome(options=opts)  # , service_log_path=LOG_FILE)
+        if headless:
+            driver.set_window_size(1440, 900)
+        return driver
+    except Exception as err:  # pylint: disable=broad-except
+        traceback.print_exc()
+        log_file_text = ""
+        if os.path.exists(LOG_FILE):
+            with open(LOG_FILE, encoding="utf-8", mode="r") as filed:
+                log_file_text = filed.read()
+        print(f"{__file__}: Error: {err}")
+        print(f"{LOG_FILE}:\n{log_file_text}")
+        raise
+
+
+if __name__ == "__main__":
+    open_webdriver()
