1e7229930df622df7156365875d94f979794271e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 16:05:23 +0000

    Add package pngquant/3.0.2/pngquant-3.0.2.tar.gz

---
+++ packages/pngquant/pngquant-3.0.2.tar.gz/pngquant-3.0.2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+version = '3.0.2'
+
+
+setup(
+    name='pngquant',
+    version=version,
+    keywords='',
+    description='A Python Wrapper of pngquant',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/Brightcells/pngquant',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    packages=['pngquant'],
+    py_modules=[],
+    install_requires=['Pillow', 'puremagic', ],
+    python_requires='>=3.7, <4',
+
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
