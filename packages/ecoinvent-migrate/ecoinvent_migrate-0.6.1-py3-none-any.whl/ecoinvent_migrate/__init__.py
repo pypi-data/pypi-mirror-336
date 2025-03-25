b880f7ec369d43c92a6b6c5db51e78accb90c8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 17:05:34 +0000

    Add package ecoinvent-migrate/0.6.1/ecoinvent_migrate-0.6.1-py3-none-any.whl

---
+++ packages/ecoinvent-migrate/ecoinvent_migrate-0.6.1-py3-none-any.whl/ecoinvent_migrate/__init__.py
@@ -0,0 +1,11 @@
+"""ecoinvent_migrate."""
+
+__all__ = (
+    "__version__",
+    "generate_technosphere_mapping",
+    "generate_biosphere_mapping",
+)
+
+__version__ = "0.6.1"
+
+from ecoinvent_migrate.main import generate_biosphere_mapping, generate_technosphere_mapping
