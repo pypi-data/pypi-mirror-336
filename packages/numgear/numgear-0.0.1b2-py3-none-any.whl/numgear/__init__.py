41e4ae40967613a3bd0872ebff09186b54e33bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 03:16:19 +0000

    Add package numgear/0.0.1b2/numgear-0.0.1b2-py3-none-any.whl

---
+++ packages/numgear/numgear-0.0.1b2-py3-none-any.whl/numgear/__init__.py
@@ -0,0 +1,9 @@
+from .functions import (
+    apply_from_axis,
+    map_range,
+    map_ranges,
+    permute,
+    full_transpose
+)
+from .utils.h5 import load as loadh5, save as saveh5
+from .utils.npk import load as loadk, save as savek, NpkFile
