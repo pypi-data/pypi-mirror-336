6dce1b232cb4f9cf618e9f4ddbf74433ee54e6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 14:05:06 +0000

    Add package netbox-validity/3.1.3/netbox_validity-3.1.3.tar.gz

---
+++ packages/netbox-validity/netbox_validity-3.1.3.tar.gz/netbox_validity-3.1.3/validity/compliance/eval/__init__.py
@@ -0,0 +1,2 @@
+from .eval import EvalWithCompoundTypes, ExplanationalEval
+from .eval_defaults import DEFAULT_NAMES, DEFAULT_OPERATORS, REPR_DEFAULTS, repr_
---
+++ packages/netbox-validity/netbox_validity-3.1.3.tar.gz/netbox_validity-3.1.3/validity/netbox_changes/oldest.py
@@ -0,0 +1,9 @@
+# NetBox 4.0
+from pydoc import locate as __locate
+
+
+enqueue_event = __locate("extras.events.enqueue_object")
+
+
+QUEUE_CREATE_ACTION = "create"
+DEVICE_NAME_ORDER = "_name"
