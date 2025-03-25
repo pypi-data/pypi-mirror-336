87c8000f85ba72a3ff9193ca613649af3a250433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 17:05:43 +0000

    Add package uipath_connectors.sap_odata/1.0.25.dev12/uipath_connectors_sap_odata-1.0.25.dev12-py3-none-any.whl

---
+++ packages/uipath_connectors.sap_odata/uipath_connectors_sap_odata-1.0.25.dev12-py3-none-any.whl/uipath_connectors/sap_odata/api/entity/__init__.py
@@ -0,0 +1,7 @@
+from .list_all_entity_records import sync as list_all_entity_records
+from .list_all_entity_records import asyncio as list_all_entity_records_async
+
+__all__ = [
+    "list_all_entity_records",
+    "list_all_entity_records_async",
+]
