01b8f37eaaa92f77cdaf22a79254ea82faaa4db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 17:05:33 +0000

    Add package trace-it/0.1.5/trace_it-0.1.5-py3-none-any.whl

---
+++ packages/trace-it/trace_it-0.1.5-py3-none-any.whl/trace_it/__init__.py
@@ -0,0 +1,32 @@
+from trace_it.decorators import embedding_tracer 
+from trace_it.decorators import reranking_tracer 
+from trace_it.decorators import vector_db_tracer 
+from trace_it.decorators import llm_tracer 
+from trace_it.tracer import phoenix_tracing
+from trace_it.config import context_config
+from trace_it.config import phoenix_tracer
+
+# Expose the decorators at the top level for easier imports
+embedding_tracing = embedding_tracer.embedding_tracing
+reranking_tracing = reranking_tracer.reranking_tracing
+vectordb_tracing = vector_db_tracer.vectordb_tracing
+llm_tracing = llm_tracer.llm_tracing
+
+# Expose the AIObservability class
+from trace_it.config.phoenix_tracer import AIObservability
+
+__all__ = [
+    'embedding_tracer',
+    'reranking_tracer',
+    'vector_db_tracer',
+    'llm_tracer',
+    'phoenix_tracing',
+    'context_config',
+    'phoenix_tracer',
+    # User-friendly exports
+    'embedding_tracing',
+    'reranking_tracing',
+    'vectordb_tracing',
+    'llm_tracing',
+    'AIObservability'
+] 
