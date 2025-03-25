dbc1ddd300dfbb18d01fd7d15fe8c332f882bce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 04:05:02 +0000

    Add package embetter/0.7.0/embetter-0.7.0.tar.gz

---
+++ packages/embetter/embetter-0.7.0.tar.gz/embetter-0.7.0/embetter/text/__init__.py
@@ -0,0 +1,52 @@
+from embetter.error import NotInstalled
+from embetter.text._model2vec import TextEncoder
+
+try:
+    from embetter.text._sbert import SentenceEncoder, MatrouskaEncoder, MatryoshkaEncoder
+except ModuleNotFoundError:
+    SentenceEncoder = NotInstalled("SentenceEncoder", "sbert")
+    MatrouskaEncoder = NotInstalled("MatrouskaEncoder", "sbert")
+    MatryoshkaEncoder = NotInstalled("MatryoshkaEncoder", "sbert")
+
+try:
+    from embetter.text._s2v import Sense2VecEncoder
+except ModuleNotFoundError:
+    Sense2VecEncoder = NotInstalled("Sense2VecEncoder", "sense2vec")
+
+try:
+    from embetter.text._bpemb import BytePairEncoder
+except ModuleNotFoundError:
+    BytePairEncoder = NotInstalled("BytePairEncoder", "bpemb")
+
+try:
+    from embetter.text._spacy import spaCyEncoder
+except ModuleNotFoundError:
+    spaCyEncoder = NotInstalled("spaCyEncoder", "spacy")
+
+try:
+    from embetter.text._word2vec import GensimEncoder
+except ModuleNotFoundError:
+    GensimEncoder = NotInstalled("GensimEncoder", "gensim")
+
+try:
+    from embetter.text._keras import KerasNLPEncoder
+except (ImportError, ModuleNotFoundError):
+    KerasNLPEncoder = NotInstalled("KerasNLPEncoder", "keras_nlp")
+
+
+from embetter.text._lite import LiteTextEncoder, learn_lite_text_embeddings
+
+
+__all__ = [
+    "TextEncoder",
+    "SentenceEncoder",
+    "MatrouskaEncoder",
+    "MatryoshkaEncoder",
+    "Sense2VecEncoder",
+    "BytePairEncoder",
+    "spaCyEncoder",
+    "GensimEncoder",
+    "KerasNLPEncoder",
+    "LiteTextEncoder",
+    "learn_lite_text_embeddings",
+]
