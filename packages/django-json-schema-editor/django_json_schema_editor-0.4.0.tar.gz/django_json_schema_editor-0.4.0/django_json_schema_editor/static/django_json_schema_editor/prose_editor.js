445348592641f41d3afe39f31cb3e7376140f27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:04:39 +0000

    Add package django-json-schema-editor/0.4.0/django_json_schema_editor-0.4.0.tar.gz

---
+++ packages/django-json-schema-editor/django_json_schema_editor-0.4.0.tar.gz/django_json_schema_editor-0.4.0/django_json_schema_editor/static/django_json_schema_editor/prose_editor.js
@@ -0,0 +1,83 @@
+import {
+  // Always recommended:
+  Document,
+  Dropcursor,
+  Gapcursor,
+  Paragraph,
+  HardBreak,
+  Text,
+  // Add support for a few marks:
+  Bold,
+  Italic,
+  Underline,
+  Subscript,
+  Superscript,
+  // A menu is always nice:
+  Menu,
+  // Useful:
+  createTextareaEditor,
+} from "django-prose-editor/editor"
+
+function createJSONProseEditor(textarea) {
+  const extensions = [
+    Document,
+    Dropcursor,
+    Gapcursor,
+    Paragraph,
+    HardBreak,
+    Text,
+    Bold,
+    Italic,
+    Underline,
+    Subscript,
+    Superscript,
+    Menu,
+  ]
+
+  const editor = createTextareaEditor(textarea, extensions)
+  return () => {
+    editor.destroy()
+  }
+}
+
+JSONEditor.defaults.editors.prose = class extends (
+  JSONEditor.defaults.editors.string
+) {
+  setValue(value, initial, fromTemplate) {
+    const res = super.setValue(value, initial, fromTemplate)
+
+    if (res?.changed) {
+      this.clobber?.()
+      this.clobber = createJSONProseEditor(this.input)
+    }
+  }
+
+  build() {
+    this.options.format = "textarea"
+    super.build()
+    this.input_type = this.schema.format // Restore original format
+  }
+
+  afterInputReady() {
+    this.clobber?.()
+    this.clobber = createJSONProseEditor(this.input)
+
+    this.input.addEventListener("input", (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+
+      this.value = this.input.value
+      this.onChange(true)
+    })
+  }
+
+  getNumColumns() {
+    return 6
+  }
+}
+
+JSONEditor.defaults.resolvers.unshift((schema) => {
+  if (schema.type === "string" && schema.format === "prose") {
+    return "prose"
+  }
+})
