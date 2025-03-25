d1512ce7535cc198369f7a53c1e7628c11ae9676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:04:34 +0000

    Add package kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1/2025.3.25.0/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1-2025.3.25.0.tar.gz

---
+++ packages/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1-2025.3.25.0.tar.gz/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1-2025.3.25.0/KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouceFix1/README.md
@@ -0,0 +1,27 @@
+# Kubernetes 中文指南——云原生应用架构实践手册
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce
+docker run -tid -p <port>:80 apachecn0/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce
+kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce
+kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1-2025.3.25.0.tar.gz/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1-2025.3.25.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouceFix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouceFix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1",
+    version=KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouceFix1.__version__,
+    url="https://github.com/apachecn/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1",
+    author=KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouceFix1.__author__,
+    author_email=KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouceFix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="Kubernetes 中文指南——云原生应用架构实践手册",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce-fix1=KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouceFix1.__main__:main",
+            "KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouceFix1=KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouceFix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
