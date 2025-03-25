75c6565aeb4f012ce2533bbd0448826834a703c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 17:05:27 +0000

    Add package litegen/0.0.97/litegen-0.0.97-py3-none-any.whl

---
+++ packages/litegen/litegen-0.0.97-py3-none-any.whl/litegen/__init__.py
@@ -0,0 +1,13 @@
+from .client_completions import LLM,DSLLM,HFLLM
+from .model import LLM as LM
+from .completions import (
+lazy_completion as completion,
+print_stream_completion as pp_completion,
+genai
+)
+from ._types import ModelType
+
+from .agents import Agent,ModelClient,AgentResponse
+
+from .feats.llmsearch import LLMSearch
+from .feats.ai_search import search_ai
