603b171f6c25e8ab78a40eac60af2493a9b9b0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 17:05:16 +0000

    Add package lionweb-python/0.1.15/lionweb_python-0.1.15-py3-none-any.whl

---
+++ packages/lionweb-python/lionweb_python-0.1.15-py3-none-any.whl/lionwebpython/language/primitive_type.py
@@ -0,0 +1,27 @@
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from lionwebpython.language.concept import Concept
+from lionwebpython.language.data_type import DataType
+from lionwebpython.language.language import Language
+from lionwebpython.lionweb_version import LionWebVersion
+from lionwebpython.self.lioncore import LionCore
+
+
+class PrimitiveType(DataType):
+    def __init__(
+        self,
+        lion_web_version: LionWebVersion = LionWebVersion.current_version(),
+        language: Optional[Language] = None,
+        name: Optional[str] = None,
+        id: Optional[str] = None,
+        key: Optional[str] = None,
+    ):
+        super().__init__(lion_web_version, language, name)
+        if id:
+            self.set_id(id)
+        if key:
+            self.set_key(key)
+
+    def get_classifier(self) -> 'Concept':
+        return LionCore.get_primitive_type(self.get_lionweb_version())
