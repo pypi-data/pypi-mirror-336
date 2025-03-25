ea1f1acbf9fb7713b40c0b7e1d388b8b2753baab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:05:25 +0000

    Add package quack2tex/1.0.7/quack2tex-1.0.7-py3-none-any.whl

---
+++ packages/quack2tex/quack2tex-1.0.7-py3-none-any.whl/quack2tex/windows/ouput_dialog.py
@@ -0,0 +1,25 @@
+from quack2tex.pyqt import QToolBox, QDialog, QVBoxLayout
+
+from quack2tex.widgets import MarkdownViewer
+
+
+class OutputDialog(QDialog):
+    """
+    A window to display the predictions output
+    """
+    def __init__(self, predictions: dict, parent=None):
+        super().__init__(parent)
+
+        # Set window properties
+        self.setWindowTitle("Matrix Theme Window")
+        self.setGeometry(100, 100, 800, 600)
+        toolbox = QToolBox()
+        for model, prediction in predictions.items():
+            viewer = MarkdownViewer()
+            viewer.set_content(prediction)
+            toolbox.addItem(viewer, model)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(toolbox)
+        self.setLayout(self.layout)
+
