b9d4ed45ad76e04413b773b81af5ce84e7f3cfab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 07:05:25 +0000

    Add package hmd-cli-librarian-sync-watcher/0.2.134/hmd_cli_librarian_sync_watcher-0.2.134-py3-none-any.whl

---
+++ packages/hmd-cli-librarian-sync-watcher/hmd_cli_librarian_sync_watcher-0.2.134-py3-none-any.whl/hmd_cli_librarian_sync_watcher/source_handlers/__init__.py
@@ -0,0 +1,32 @@
+import logging
+from pathlib import Path
+from typing import Dict
+
+from hmd_lang_librarian_sync.hmd_lang_librarian_sync_client import (
+    HmdLangLibrarianSyncClient,
+)
+from hmd_lib_librarian_client.hmd_lib_librarian_client import HmdLibrarianClient
+from .manifest_source import ManifestSource
+from .source import Source
+
+logger = logging.getLogger(__name__)
+
+
+def make_source(
+    name: str,
+    source: Dict,
+    librarian_sync_client: HmdLangLibrarianSyncClient,
+    librarian_client: HmdLibrarianClient,
+    hmd_home: Path,
+    hmd_repo_home: Path,
+) -> Source:
+    source["name"] = name
+    source_type = source.get("type", "default")
+    logger.info(f"{source['name']}: creating source of type {source_type}")
+    if source_type == "manifest":
+        return ManifestSource(
+            source, librarian_sync_client, librarian_client, hmd_home, hmd_repo_home
+        )
+    return Source(
+        source, librarian_sync_client, librarian_client, hmd_home, hmd_repo_home
+    )
