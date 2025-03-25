403c5cf031f6c2f7fba88fd3eb1f69e0025b942c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 05:04:43 +0000

    Add package atla/0.5.0/atla-0.5.0.tar.gz

---
+++ packages/atla/atla-0.5.0.tar.gz/atla-0.5.0/src/atla/resources/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .metrics import (
+    MetricsResource,
+    AsyncMetricsResource,
+    MetricsResourceWithRawResponse,
+    AsyncMetricsResourceWithRawResponse,
+    MetricsResourceWithStreamingResponse,
+    AsyncMetricsResourceWithStreamingResponse,
+)
+from .evaluation import (
+    EvaluationResource,
+    AsyncEvaluationResource,
+    EvaluationResourceWithRawResponse,
+    AsyncEvaluationResourceWithRawResponse,
+    EvaluationResourceWithStreamingResponse,
+    AsyncEvaluationResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "EvaluationResource",
+    "AsyncEvaluationResource",
+    "EvaluationResourceWithRawResponse",
+    "AsyncEvaluationResourceWithRawResponse",
+    "EvaluationResourceWithStreamingResponse",
+    "AsyncEvaluationResourceWithStreamingResponse",
+    "MetricsResource",
+    "AsyncMetricsResource",
+    "MetricsResourceWithRawResponse",
+    "AsyncMetricsResourceWithRawResponse",
+    "MetricsResourceWithStreamingResponse",
+    "AsyncMetricsResourceWithStreamingResponse",
+]
