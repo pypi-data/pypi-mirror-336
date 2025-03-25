6b72db0d2ae70e1fe80e13e37c05b6b7479976c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 05:04:42 +0000

    Add package frida-gadget/1.5.5/frida_gadget-1.5.5.tar.gz

---
+++ packages/frida-gadget/frida_gadget-1.5.5.tar.gz/frida_gadget-1.5.5/scripts/__init__.py
@@ -0,0 +1,22 @@
+"""init file for scripts folder."""
+import sys
+import subprocess
+from .logger import logger
+
+
+def import_or_install(package):
+    """Function to install missing packages.
+
+    Args:
+        package (string): Name of the package to install.
+    """
+    try:
+        __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'frida', '--user'])
+        logger.info('Try Again')
+        sys.exit(0)
+
+
+import_or_install('frida')  # Install missing packages
+INSTALLED_FRIDA_VERSION: str = __import__('frida').__version__  # Get installed frida version
