a64a6e42dc9c927feae036d2d68be4ba4f110b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:04:48 +0000

    Add package idi/0.0.250325/idi-0.0.250325.tar.gz

---
+++ packages/idi/idi-0.0.250325.tar.gz/idi-0.0.250325/idi/simulation/__init__.py
@@ -0,0 +1,35 @@
+__all__ = ["cpu", "simobj", "cuda", "common", "time", "cutime", "simple"]
+
+from . import cpu, simobj, time, simple
+from .common import *
+
+try:
+    from . import cuda
+
+    auto = cuda
+except ImportError as _e:
+    if any(x in str(_e.args[0]).lower() for x in ["cuda", "libcu", "cupy"]):
+        import warnings as _w
+
+        _w.warn("cuda error. cuda time dependent simulation not imported. is cuda available and paths set?")
+        auto = cpu
+    else:
+        print(_e)
+except Exception as _e:
+    print("Import exception on cuda:")
+    print(_e)
+try:
+    from . import cutime
+
+    autotime = cutime
+except (AttributeError, ImportError) as _e:
+    if any(x in str(_e.args[0]).lower() for x in ["cuda", "libcu", "cupy"]):
+        import warnings as _w
+
+        _w.warn("cuda error. cuda time dependent simulation not imported. is cuda available and paths set?")
+        autotime = time
+    else:
+        print(_e)
+except Exception as _e:
+    print("Import exception on cutime:")
+    print(_e)
