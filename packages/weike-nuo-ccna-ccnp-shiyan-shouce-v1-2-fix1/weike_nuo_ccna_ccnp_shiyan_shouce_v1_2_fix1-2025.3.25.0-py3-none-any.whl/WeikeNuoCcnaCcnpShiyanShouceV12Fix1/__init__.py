0ce63032ef3a2ada5f6667b8df2421975b89cff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 14:06:01 +0000

    Add package weike-nuo-ccna-ccnp-shiyan-shouce-v1-2-fix1/2025.3.25.0/weike_nuo_ccna_ccnp_shiyan_shouce_v1_2_fix1-2025.3.25.0-py3-none-any.whl

---
+++ packages/weike-nuo-ccna-ccnp-shiyan-shouce-v1-2-fix1/weike_nuo_ccna_ccnp_shiyan_shouce_v1_2_fix1-2025.3.25.0-py3-none-any.whl/WeikeNuoCcnaCcnpShiyanShouceV12Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""weike-nuo-ccna-ccnp-shiyan-shouce-v1-2-fix1
+https://github.com/apachecn/weike-nuo-ccna-ccnp-shiyan-shouce-v1-2-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.25.0"
