f1aa3f8af00f97b5abf80bd1581bb1db3bdfce60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 07:04:40 +0000

    Add package niuke-java-gangwei-mianshi-zhenti-baodian-2021-2022-fix1/2025.3.25.0/niuke-java-gangwei-mianshi-zhenti-baodian-2021-2022-fix1-2025.3.25.0.tar.gz

---
+++ packages/niuke-java-gangwei-mianshi-zhenti-baodian-2021-2022-fix1/niuke-java-gangwei-mianshi-zhenti-baodian-2021-2022-fix1-2025.3.25.0.tar.gz/niuke-java-gangwei-mianshi-zhenti-baodian-2021-2022-fix1-2025.3.25.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import NiukeJavaGangweiMianshiZhentiBaodian20212022Fix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('NiukeJavaGangweiMianshiZhentiBaodian20212022Fix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="niuke-java-gangwei-mianshi-zhenti-baodian-2021-2022-fix1",
+    version=NiukeJavaGangweiMianshiZhentiBaodian20212022Fix1.__version__,
+    url="https://github.com/apachecn/niuke-java-gangwei-mianshi-zhenti-baodian-2021-2022-fix1",
+    author=NiukeJavaGangweiMianshiZhentiBaodian20212022Fix1.__author__,
+    author_email=NiukeJavaGangweiMianshiZhentiBaodian20212022Fix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="牛客 Java 岗位面试真题宝典 2021~2022",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "niuke-java-gangwei-mianshi-zhenti-baodian-2021-2022-fix1=NiukeJavaGangweiMianshiZhentiBaodian20212022Fix1.__main__:main",
+            "NiukeJavaGangweiMianshiZhentiBaodian20212022Fix1=NiukeJavaGangweiMianshiZhentiBaodian20212022Fix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
