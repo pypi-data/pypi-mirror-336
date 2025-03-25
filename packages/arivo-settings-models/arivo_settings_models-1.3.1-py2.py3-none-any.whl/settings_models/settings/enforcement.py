a871d80d1a919a1e0a47fd58dc46175a8d2c1459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 16:05:24 +0000

    Add package arivo-settings-models/1.3.1/arivo_settings_models-1.3.1-py2.py3-none-any.whl

---
+++ packages/arivo-settings-models/arivo_settings_models-1.3.1-py2.py3-none-any.whl/settings_models/settings/enforcement.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from settings_models._combat import SettingsModel, Field
+from settings_models.validators import timezone_validator
+
+
+class EnforcementSettings(SettingsModel):
+    """
+    Settings for enforcement service
+    """
+    payment_deadline_hours: int = Field(..., gt=0, le=960,
+                                        description="How long someone can pay after exiting parking lot "
+                                                    "(not including generally applied additional day)")
+    strictness: int = Field(..., description="Strictness level for enforcement. Values to be defined")
+    enabled: bool = Field(..., description="If enforcement is enabled")
+    last_edited: datetime = Field(..., description="Last time (UTC) when enabled setting was changed")
+
+    last_edited_validator = timezone_validator("last_edited")
