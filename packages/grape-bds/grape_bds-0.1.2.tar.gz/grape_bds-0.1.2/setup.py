86b9ea3559de7f0f85c129f07195945a727e3da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:04:50 +0000

    Add package grape-bds/0.1.2/grape_bds-0.1.2.tar.gz

---
+++ packages/grape-bds/grape_bds-0.1.2.tar.gz/grape_bds-0.1.2/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# Read the README file for the long description
+HERE = pathlib.Path(__file__).parent
+long_description = (HERE / "README.md").read_text()
+
+setup(
+    name="grape_bds",
+    version="0.1.2",
+    author="Allan de Lima",
+    description="Grammatical Evolution framework built with DEAP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "deap"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
