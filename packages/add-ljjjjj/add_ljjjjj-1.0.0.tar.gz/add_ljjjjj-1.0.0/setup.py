76c72e5af33adbfa6848fbf9aa277f80cca8bec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 15:05:23 +0000

    Add package add-ljjjjj/1.0.0/add_ljjjjj-1.0.0.tar.gz

---
+++ packages/add-ljjjjj/add_ljjjjj-1.0.0.tar.gz/add_ljjjjj-1.0.0/setup.py
@@ -0,0 +1,40 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '1.0.0'
+DESCRIPTION = 'A light weight command line menu that supports Windows, MacOS, and Linux'
+LONG_DESCRIPTION = 'A light weight command line menu. Supporting Windows, MacOS, and Linux. It has support for hotkeys'
+
+# Setting up
+setup(
+    name="add_ljjjjj",
+    version=VERSION,
+    author="clever chen",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        'getch; platform_system=="Unix"',
+        'getch; platform_system=="MacOS"',
+    ],
+    keywords=['python', 'menu', 'dumb_menu', 'windows', 'mac', 'linux'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
