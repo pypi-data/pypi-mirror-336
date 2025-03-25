4a56b036ccf381708c3190de7baaf78f998ee653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 03:16:17 +0000

    Add package test-my-binding/1.0.87/test_my_binding-1.0.87-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/test-my-binding/test_my_binding-1.0.87-cp39-cp39-macosx_11_0_arm64.whl/test_my_binding/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+
+__author__ = ["tom van mele", "petras vestartas"]
+__copyright__ = "Block Research Group - ETH Zurich"
+__license__ = "MIT License"
+__email__ = ["van.mele@arch.ethz.ch", "vestartas@arch.ethz.ch"]
+__version__ = "1.0.87"
+
+HERE = os.path.dirname(__file__)
+
+HOME = os.path.abspath(os.path.join(HERE, "../../"))
+DATA = os.path.abspath(os.path.join(HOME, "data"))
+DOCS = os.path.abspath(os.path.join(HOME, "docs"))
+TEMP = os.path.abspath(os.path.join(HOME, "temp"))
+
+
+__all_plugins__ = [
+    "test_my_binding.booleans",
+    "test_my_binding.intersections",
+    "test_my_binding.meshing",
+    "test_my_binding.measure",
+    "test_my_binding.reconstruction",
+    "test_my_binding.triangulation",
+    "test_my_binding.slicer",
+    "test_my_binding.subdivision",
+]
+
+__all__ = ["HOME", "DATA", "DOCS", "TEMP"]
