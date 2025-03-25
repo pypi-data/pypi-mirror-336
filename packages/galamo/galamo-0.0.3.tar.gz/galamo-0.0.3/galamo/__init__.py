9cc1f08091d05cb4f716923f90f62af0329d2dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:05:00 +0000

    Add package galamo/0.0.3/galamo-0.0.3.tar.gz

---
+++ packages/galamo/galamo-0.0.3.tar.gz/galamo-0.0.3/galamo/__init__.py
@@ -0,0 +1,18 @@
+import os
+import tensorflow as tf
+from .model import galaxy_morph, preprocess_image
+from . import bpt  # Import the BPT module
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow warnings
+tf.get_logger().setLevel('ERROR')  # Suppress other TensorFlow logs
+# Get the absolute model path inside the package
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "model.keras")
+
+# Load the model globally
+if os.path.exists(MODEL_PATH):
+    model = tf.keras.models.load_model(MODEL_PATH)
+else:
+    raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
+
+# Expose key functions and modules
+__all__ = ["galaxy_morph", "preprocess_image", "model", "bpt"]
