e6a7e5c2e3758b77f3911db9053d8c7bf5ef33bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:04:33 +0000

    Add package djhierarchical/0.1.1/djhierarchical-0.1.1-py2.py3-none-any.whl

---
+++ packages/djhierarchical/djhierarchical-0.1.1-py2.py3-none-any.whl/hierarchical/__init__.py
@@ -0,0 +1,16 @@
+"""
+Django Hierarchical Configuration.
+
+A Django app that provides a hierarchical configuration pattern with 
+automatic inheritance between related models.
+"""
+
+__version__ = '0.1.1'
+
+# Import main components for ease of use
+from .models import (
+    HierarchicalModelMixin,
+    HierarchicalManager,
+    HierarchicalManagerMixin,
+    has_hierarchical_field
+) 
