3c5b9c24569144dab4838fc8390476631a526c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 07:04:34 +0000

    Add package Qubx/0.6.16/qubx-0.6.16-cp312-cp312-manylinux_2_39_x86_64.whl

---
+++ packages/Qubx/qubx-0.6.16-cp312-cp312-manylinux_2_39_x86_64.whl/qubx/core/exceptions.py
@@ -0,0 +1,42 @@
+class BaseError(Exception):
+    pass
+
+
+class ExchangeError(BaseError):
+    pass
+
+
+class BadRequest(ExchangeError):
+    pass
+
+
+class InvalidOrder(ExchangeError):
+    pass
+
+
+class InvalidOrderParameters(ExchangeError):
+    pass
+
+
+class OrderNotFound(InvalidOrder):
+    pass
+
+
+class NotSupported(ExchangeError):
+    pass
+
+
+class QueueTimeout(BaseError):
+    pass
+
+
+class StrategyExceededMaxNumberOfRuntimeFailuresError(Exception):
+    pass
+
+
+class SimulationError(Exception):
+    pass
+
+
+class SimulationConfigError(Exception):
+    pass
