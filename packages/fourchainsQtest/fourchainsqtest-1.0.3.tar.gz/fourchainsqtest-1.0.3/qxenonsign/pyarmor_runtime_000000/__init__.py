3c18cc89f73d4c1247bfa43b0d5326e37cbfc229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 16:05:37 +0000

    Add package fourchainsQtest/1.0.3/fourchainsqtest-1.0.3.tar.gz

---
+++ packages/fourchainsQtest/fourchainsqtest-1.0.3.tar.gz/fourchainsqtest-1.0.3/qxenonsign/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.1.2 (trial), 000000, 2025-03-25T15:49:20.462130
+from .pyarmor_runtime import __pyarmor__
---
+++ packages/fourchainsQtest/fourchainsqtest-1.0.3.tar.gz/fourchainsqtest-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fourchainsQtest",
+    version="1.0.3",
+    packages=find_packages(include=['qxenonsign', 'qxenonsign.*']),
+    install_requires=[
+        "numpy",
+        "requests",
+        "pydantic",
+        "pyngrok",
+        "flask"
+    ],
+    include_package_data=True,
+    author="fourchains",
+    author_email="fourchains.work@gmail.com",
+    description="fourchainsQtest",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
