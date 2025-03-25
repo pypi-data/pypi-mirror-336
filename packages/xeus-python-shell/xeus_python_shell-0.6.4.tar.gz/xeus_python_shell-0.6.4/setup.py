1940d52b9c9024f4b9933385faad96ae9d8c43b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 17:05:41 +0000

    Add package xeus-python-shell/0.6.4/xeus_python_shell-0.6.4.tar.gz

---
+++ packages/xeus-python-shell/xeus_python_shell-0.6.4.tar.gz/xeus_python_shell-0.6.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+__AUTHOR__ = 'QuantStack dev team'
+
+setup(
+    name='xeus-python-shell',
+    version='0.6.4',
+    description='The xeus-python core python logic.',
+    author=__AUTHOR__,
+    maintainer=__AUTHOR__,
+    url='https://github.com/jupyter-xeus/xeus-python-shell',
+    license='BSD 3-Clause',
+    keywords='python ipython xeus-python',
+    packages=find_packages(exclude=['test']),
+    python_requires='>=3.6',
+    install_requires=[
+        'debugpy>=1.1.0,<2'
+    ],
+    extras_require={
+        'ipython': ['ipython>=7.21'],
+        'wasm': ['pyjs>=1.1.0,<2.0.0'],
+    },
+    platforms=['any'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
