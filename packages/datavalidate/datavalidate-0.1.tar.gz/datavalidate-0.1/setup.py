3760eef8fa6c7c38566ee72b3e1689f1f85c7dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 15:04:56 +0000

    Add package datavalidate/0.1/datavalidate-0.1.tar.gz

---
+++ packages/datavalidate/datavalidate-0.1.tar.gz/datavalidate-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-16") as f:
+    long_description = f.read()
+setup(
+    name='datavalidate',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Okoli Ogechukwu Abimbola',
+    author_email='okoliogechi74@gmail.com',
+    description="A simple datavalidator for validating email, phone numbers, dates, and URLs.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/Data-Epic/data-validator-ogechukwu-okoli",
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    license_file='LICENSE',
+)
