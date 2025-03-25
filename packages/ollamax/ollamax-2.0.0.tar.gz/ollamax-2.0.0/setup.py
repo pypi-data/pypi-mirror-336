9d54e10159969a58585c54bffd4a437a3a601df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 06:05:09 +0000

    Add package ollamax/2.0.0/ollamax-2.0.0.tar.gz

---
+++ packages/ollamax/ollamax-2.0.0.tar.gz/ollamax-2.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ollamax',
+    version='2.0.0',
+    description='A package to manage Ollama service and models.',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=['ollama'],
+    author='John Codes',
+    author_email='efexzium@gmail.com',
+)
