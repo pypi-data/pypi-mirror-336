dc7f0b484f6d05fd7989d49fc6b7d5f9451b2abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 15:05:22 +0000

    Add package zai-spring-shengtai-zhong-wanzhuan-rockmq-fix1/2025.3.25.0/zai_spring_shengtai_zhong_wanzhuan_rockmq_fix1-2025.3.25.0-py3-none-any.whl

---
+++ packages/zai-spring-shengtai-zhong-wanzhuan-rockmq-fix1/zai_spring_shengtai_zhong_wanzhuan_rockmq_fix1-2025.3.25.0-py3-none-any.whl/ZaiSpringShengtaiZhongWanzhuanRockmqFix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zai-spring-shengtai-zhong-wanzhuan-rockmq-fix1
+https://github.com/apachecn/zai-spring-shengtai-zhong-wanzhuan-rockmq-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2025.3.25.0"
