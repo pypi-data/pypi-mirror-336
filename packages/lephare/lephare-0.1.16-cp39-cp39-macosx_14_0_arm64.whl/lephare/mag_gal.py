bb803b07789b3c67ded1e50495b8c00d47cbe9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 05:04:28 +0000

    Add package lephare/0.1.16/lephare-0.1.16-cp39-cp39-macosx_14_0_arm64.whl

---
+++ packages/lephare/lephare-0.1.16-cp39-cp39-macosx_14_0_arm64.whl/lephare/mag_gal.py
@@ -0,0 +1,108 @@
+from lephare._lephare import GalMag, QSOMag, StarMag, keyword
+from lephare.runner import Runner
+
+__all__ = [
+    "MagGal",
+]
+
+global mag_gal_config_keys
+mag_gal_config_keys = [
+    "COSMOLOGY",
+    "FILTER_FILE",
+    "MAGTYPE",
+    "EXTINC_LAW",
+    "EB_V",
+    "MOD_EXTINC",
+    "ZGRID_TYPE",
+    "Z_STEP",
+    "GAL_LIB_IN",
+    "QSO_LIB_IN",
+    "STAR_LIB_IN",
+    "GAL_LIB_OUT",
+    "QSO_LIB_OUT",
+    "STAR_LIB_OUT",
+    "LIB_ASCII",
+    "EM_LINES",
+    "EM_DISPERSION",
+    "ADD_DUSTEM",
+    "VERBOSE",
+]
+
+
+class MagGal(Runner):
+    """Use the galaxy templates and filters to derive a library of predicted magnitudes.
+
+    The run method of this class is equivalent to the mag_gal command line.
+
+    Parameters
+    ----------
+    config_file : `string` or `None`, optional
+        Path to config file in LePHARE .para format
+    config_keymap : `dict` or `None`, optional
+        Dictionary of all config values as alternative to config file.
+    """
+
+    def __init__(self, config_file=None, config_keymap=None):
+        super().__init__(mag_gal_config_keys, config_file, config_keymap)
+
+    def run(self, **kwargs):
+        """Compute the model magnitudes across the redshift grid.
+
+        Returns
+        -------
+        None
+        """
+        # update keymap and verbosity based on call arguments
+        # this is only when the code is called from python session
+        self.verbose = kwargs.pop("verbose", self.verbose)
+        for k, v in kwargs.items():
+            if k == "typ":
+                self.typ = v.upper()
+                continue
+            # if k == "config":
+            #     self.config = config_file
+            #     continue
+            if k.upper() in self.keymap:
+                self.keymap[k.upper()] = keyword(k.upper(), str(v))
+
+        # needed to comply with the C++ interface
+        # Define the type (Galaxy, QSO, Stars)
+        self.keymap["t"] = keyword("t", self.typ)
+        # Parameter file
+        self.keymap["c"] = keyword("c", self.config)
+
+        if self.typ[0] == "G":
+            mag = GalMag(self.keymap)
+        elif self.typ[0] == "Q":
+            mag = QSOMag(self.keymap)
+        elif self.typ[0] == "S":
+            mag = StarMag(self.keymap)
+        else:
+            raise KeyError("-t arg must start with G/g Q/q or S/s for Galaxy QSO and Star respectively.")
+        mag.open_files()
+        mag.print_info()
+        # Read dust extinction laws
+        mag.read_ext()
+        # Define the redshift grid
+        mag.def_zgrid()
+        # Read B12 templates to add dust emission to BC03
+        mag.read_B12()
+        # Read sed, apply extinction and IGM opacity
+        mag.read_SED()
+        mag.write_doc()
+        # we need to call the close method here because run can
+        # be called within a python session that stays alive afterwards
+        mag.close_files()
+
+        self.Mag = mag
+        return
+
+
+def main():
+    runner = MagGal()
+    runner.run()
+    runner.end()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/lephare/lephare-0.1.16-cp39-cp39-macosx_14_0_arm64.whl/lephare/zphota.py
@@ -0,0 +1,125 @@
+import time
+
+from lephare._lephare import (  # , read_lib, read_doc_filters, readOutKeywords, GalMag, bestFilter, maxkcolor
+    GalMag,
+    PhotoZ,
+    keyword,
+)
+from lephare.runner import Runner
+
+__all__ = [
+    "Zphota",
+]
+
+global zphota_config_keys
+zphota_config_keys = [
+    "CAT_IN",
+    "INP_TYPE",
+    "CAT_MAG",
+    "CAT_FMT",
+    "CAT_LINES",
+    "ZPHOTLIB",
+    "PARA_OUT",
+    "CAT_OUT",
+    "VERBOSE",
+    "CAT_TYPE",
+    "ERR_SCALE",
+    "ERR_FACTOR",
+    "BD_SCALE",
+    "GLB_CONTEXT",
+    "FORB_CONTEXT",
+    "MASS_SCALE",
+    "MAG_ABS",
+    "MAG_ABS_AGN",
+    "MAG_REF",
+    "NZ_PRIOR",
+    "ZFIX",
+    "Z_INTERP",
+    "Z_METHOD",
+    "DZ_WIN",
+    "MIN_THRES",
+    "PROB_INTZ",
+    "SPEC_OUT",
+    "FIR_LIB",
+    "FIR_LMIN",
+    "FIR_CONT",
+    "FIR_SCALE",
+    "FIR_FREESCALE",
+    "FIR_SUBSTELLAR",
+    "MABS_METHOD",
+    "MABS_CONTEXT",
+    "MABS_REF",
+    "MABS_FILT",
+    "MABS_ZBIN",
+    "RF_COLORS",
+    "M_REF",
+    "APPLY_SYSSHIFT",
+    "AUTO_ADAPT",
+    "ADAPT_BAND",
+    "ADAPT_LIM",
+    "ADAPT_CONTEXT",
+    "ADAPT_ZBIN",
+    "PDZ_OUT",
+    "PDZ_TYPE",
+    "PDZ_MABS_FILT",
+    "ADD_EMLINES",
+    "ADDITIONAL_MAG",
+    "LIMITS_ZBIN",
+    "LIMITS_MAPP_REF",
+    "LIMITS_MAPP_SEL",
+    "LIMITS_MAPP_CUT",
+    "Z_STEP",
+]
+
+nonestring = "NONE"
+
+
+class Zphota(Runner):
+    """Performs chisquare minimization in order to derive photometric redshifts and
+    other physical parameters.
+
+    Equivalent to zphota command.
+
+    Parameters
+    ----------
+    config_file : `str`
+        Path to config file.
+    """
+
+    def __init__(self, config_file=None):
+        super().__init__(zphota_config_keys, config_file)
+
+    def run(self, **kwargs):
+        # update keymap and verbosity based on call arguments
+        # this is only when the code is called from python session
+        self.verbose = kwargs.pop("verbose", self.verbose)
+        self.keymap["c"] = keyword("c", self.config)
+
+        photoz = PhotoZ(self.keymap)
+        autoadapt = (self.keymap["AUTO_ADAPT"]).split_bool("NO", 1)[0]
+        if autoadapt:
+            adapt_srcs = photoz.read_autoadapt_sources()
+            a0 = photoz.run_autoadapt(adapt_srcs)
+        else:
+            a0 = []
+            for _ in range(photoz.imagm):
+                a0.append(0.0)
+
+        opa_out = GalMag.read_opa()  # noqa: F841
+
+        fit_srcs = photoz.read_photoz_sources()
+        photoz.run_photoz(fit_srcs, a0)
+        photoz.write_outputs(fit_srcs, int(time.time()))
+
+    def end(self):
+        super().end()
+
+
+def main():
+    runner = Zphota()
+    runner.run()
+    runner.end()
+
+
+if __name__ == "__main__":
+    main()
