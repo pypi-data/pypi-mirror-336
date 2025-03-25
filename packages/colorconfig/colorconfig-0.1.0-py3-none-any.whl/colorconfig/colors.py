43a646cffa1990047784b6ea8767356ab692158f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-25 17:05:51 +0000

    Add package colorconfig/0.1.0/colorconfig-0.1.0-py3-none-any.whl

---
+++ packages/colorconfig/colorconfig-0.1.0-py3-none-any.whl/colorconfig/colors.py
@@ -0,0 +1,12 @@
+from colorama import Fore, Style
+
+class Colors:
+    header = Fore.YELLOW
+    prompt = Fore.CYAN
+    success = Fore.GREEN
+    error = Fore.RED
+    info = Fore.BLUE
+    highlight = Fore.MAGENTA
+    reset = Style.RESET_ALL
+
+colors = Colors()
