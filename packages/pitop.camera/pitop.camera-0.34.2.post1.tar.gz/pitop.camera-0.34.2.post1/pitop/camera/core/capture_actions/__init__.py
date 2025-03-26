8442e51a40334f2334ca4113b8ea53a0428b4567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 04:05:27 +0000

    Add package pitop.camera/0.34.2.post1/pitop.camera-0.34.2.post1.tar.gz

---
+++ packages/pitop.camera/pitop.camera-0.34.2.post1.tar.gz/pitop.camera-0.34.2.post1/pitop/camera/core/capture_actions/__init__.py
@@ -0,0 +1,9 @@
+# classes to perform actions with frames
+from .capture_action_base import CaptureActionBase
+
+# enum with available capture actions
+from .capture_actions import CaptureActions
+from .generic_action import GenericAction
+from .motion_detector import MotionDetector
+from .store_frame import StoreFrame
+from .video_capture import VideoCapture
