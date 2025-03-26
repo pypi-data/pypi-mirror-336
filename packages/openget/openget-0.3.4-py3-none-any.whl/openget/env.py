05a23db61b073ad73c044f0c25842fa7f6262856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:07:41 +0000

    Add package openget/0.3.4/openget-0.3.4-py3-none-any.whl

---
+++ packages/openget/openget-0.3.4-py3-none-any.whl/openget/env.py
@@ -0,0 +1,46 @@
+# coding:utf8
+"""
+Get all environment variable
+
+"""
+import sys
+import os
+from os import path
+from dotenv import load_dotenv
+
+
+# All env defines
+env = {
+    # PROD / DEV / ...
+    "OPENGET_ENV": "DEV",
+    # default mysql config
+    "OPENGET_MYSQL_HOST": None,
+    "OPENGET_MYSQL_PORT": "3306",
+    "OPENGET_MYSQL_USER": None,
+    "OPENGET_MYSQL_PASSWD": None,
+    "OPENGET_MYSQL_CHARSET": "utf8mb4",
+    "OPENGET_MYSQL_DB": "openget",
+    # default redis config
+    "OPENGET_REDIS_URI": "redis://localhost:6379/0",
+    # default proxy service url
+    "OPENGET_PROXY_SERVICE_URL": None,
+    # docker mode
+    "OPENGET_IN_DOCKER": "false",
+    # logging
+    # forbidden better_exceptions
+    "OPENGET_FORBIDDED_BETTER_EXCEPTIONS": "false",
+}
+
+
+# from ~/.openget/.env
+GLOBAL_ENV_FILE = path.join(path.expanduser("~"), ".openget/.env")
+if GLOBAL_ENV_FILE:
+    load_dotenv(GLOBAL_ENV_FILE)
+
+# from $(pwd)/.env
+if sys.argv[0]:
+    LOCAL_ENV_FILE = path.join(
+        path.dirname(path.join(os.getcwd(), sys.argv[0])), ".env"
+    )
+    if LOCAL_ENV_FILE:
+        load_dotenv(LOCAL_ENV_FILE, override=True)
