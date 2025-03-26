335d89ad4b87269af7731aa8a8def70c039ebaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:05:31 +0000

    Add package quartic-transformer/0.0.18/quartic_transformer-0.0.18.tar.gz

---
+++ packages/quartic-transformer/quartic_transformer-0.0.18.tar.gz/quartic_transformer-0.0.18/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'quartic-transformer',
+  packages = find_packages(exclude=[]),
+  version = '0.0.18',
+  license='MIT',
+  description = 'Quartic Transformer',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/quartic-transformer',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformer',
+    'attention'
+  ],
+  install_requires=[
+    'colt5-attention',
+    'einops>=0.7.0',
+    'einx[torch]>=0.1.3',
+    'hyper-connections>=0.1.15',
+    'taylor-series-linear-attention',
+    'torch>=2.0',
+    'x-transformers'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
