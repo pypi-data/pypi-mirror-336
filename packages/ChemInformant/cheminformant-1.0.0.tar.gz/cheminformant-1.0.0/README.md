7a035354b895bd283114c399920a535ea5364b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 04:05:33 +0000

    Add package ChemInformant/1.0.0/cheminformant-1.0.0.tar.gz

---
+++ packages/ChemInformant/cheminformant-1.0.0.tar.gz/cheminformant-1.0.0/README.md
@@ -0,0 +1,6 @@
+﻿# ChemInformant
+![ChemInformant Logo](https://github.com/HzaCode/ChemInformant/raw/main/images/logo.png)
+This is a placeholder package for the name ChemInformant on PyPI.
+
+## License
+Distributed under the MIT License. See LICENSE for more information.
---
+++ packages/ChemInformant/cheminformant-1.0.0.tar.gz/cheminformant-1.0.0/setup.py
@@ -0,0 +1,22 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="ChemInformant",
+    version="1.0.0",
+    author="Ang",
+    author_email="ang@hezhiang.com",
+    description="A placeholder package for ChemInformant on PyPI.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/HzaCode/ChemInformant",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["requests>=2.31.0"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="placeholder cheminformant",
+)
