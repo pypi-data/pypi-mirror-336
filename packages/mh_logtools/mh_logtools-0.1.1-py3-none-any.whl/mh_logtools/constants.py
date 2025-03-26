052b22750bca9397538908b7e394549950d85088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:06:34 +0000

    Add package mh_logtools/0.1.1/mh_logtools-0.1.1-py3-none-any.whl

---
+++ packages/mh_logtools/mh_logtools-0.1.1-py3-none-any.whl/mh_logtools/constants.py
@@ -0,0 +1,12 @@
+"""
+Macros for mh_logtools.
+
+Author: Johannes Traa, 2025
+"""
+
+LOG_CLASS = 'class'
+LOG_FUNCTION = 'function'
+LOG_LEVEL = 'level'
+LOG_MESSAGE = 'message'
+LOG_TIMESTAMP = 'timestamp'
+
