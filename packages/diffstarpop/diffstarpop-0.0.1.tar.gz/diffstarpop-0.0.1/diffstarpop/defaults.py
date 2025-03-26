3adfc9e9e9504dcdf388024c6eff682e27082a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:05:56 +0000

    Add package diffstarpop/0.0.1/diffstarpop-0.0.1.tar.gz

---
+++ packages/diffstarpop/diffstarpop-0.0.1.tar.gz/diffstarpop-0.0.1/diffstarpop/defaults.py
@@ -0,0 +1,11 @@
+"""
+"""
+
+# flake8: noqa
+
+from .kernels.defaults_tpeak_line import (
+    DEFAULT_DIFFSTARPOP_PARAMS,
+    DEFAULT_DIFFSTARPOP_U_PARAMS,
+    DiffstarPopParams,
+    DiffstarPopUParams,
+)
