04a80977d2999588cbd469d3011f2afcfa13b1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:05:14 +0000

    Add package css-make/1.3.0/css_make-1.3.0.tar.gz

---
+++ packages/css-make/css_make-1.3.0.tar.gz/css_make-1.3.0/test/test_CssCode.py
@@ -0,0 +1,21 @@
+from ldpc.codes import hamming_code
+
+import css_make as cm
+
+H = hamming_code(16)
+qcode = cm.CssCode(hx=H, hz=H, compute_distance_timeout=2.0)
+
+print(f"qcode: {qcode}")
+print(f"N: {qcode.N}")
+print(f"K: {qcode.K}")
+print(f"L: {qcode.L}")
+print(f"Q: {qcode.Q}")
+print(f"h: {qcode.h}")
+print(f"lx: {qcode.lx}")
+print(f"lz: {qcode.lz}")
+print(f"l: {qcode.l}")
+print(f"canonical_lx: {qcode.canonical_lx}")
+print(f"canonical_lz: {qcode.canonical_lz}")
+print(f"D: {qcode.D}")
+print(f"code_params: {qcode.code_params}")
+qcode.test()
