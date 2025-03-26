77af7312b8fdc02cbf4e52ed14afe1c38e8e7b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:07:39 +0000

    Add package opengate/10.0.2/opengate-10.0.2.tar.gz

---
+++ packages/opengate/opengate-10.0.2.tar.gz/opengate-10.0.2/opengate/exception.py
@@ -0,0 +1,69 @@
+import inspect
+import colored
+
+import opengate_core as g4
+from .logger import global_log
+
+
+class GateDeprecationError(Exception):
+    """Raise this if a deprecated feature is used.
+    Provide the user with information on how to update their code.
+    """
+
+
+class GateFeatureUnavailableError(Exception):
+    """Raise this if a feature is used that is (currently) unavailable.
+    Ideally, provide the user with information about alternatives.
+    Can be used as temporary workaround during refactorings.
+    """
+
+
+class GateImplementationError(Exception):
+    """Raise this if a feature is used that is (currently) unavailable.
+    Ideally, provide the user with information about alternatives.
+    Can be used as temporary workaround during refactorings.
+    """
+
+
+color_error = colored.fore("red") + colored.style("bold")
+color_warning = colored.fore("orange_1")
+color_ok = colored.fore("green")
+
+
+def fatal(s):
+    caller = inspect.getframeinfo(inspect.stack()[1][0])
+    ss = f"(in {caller.filename} line {caller.lineno})"
+    ss = colored.stylize(ss, color_error)
+    global_log.critical(ss)
+    s = colored.stylize(s, color_error)
+    global_log.critical(s)
+    raise Exception(s)
+
+
+def warning(s):
+    s = colored.stylize(s, color_warning)
+    global_log.warning(s)
+
+
+def raise_except(s):
+    s = colored.stylize(s, color_error)
+    raise Exception(s)
+
+
+class ExceptionHandler(g4.GateExceptionHandler):
+    """
+    Geant4 exception handler. Inherit form GateExceptionHandler
+    The function 'Notify' will be called by G4 when an exception occurs.
+    Should be created after initialization.
+    Will be automatically stored in the G4 stateManager
+    """
+
+    def Notify(self, originOfException, exceptionCode, severity, description):
+        s = f"G4Exception origin: {originOfException}\n"
+        s += f"G4Exception code: {exceptionCode}\n"
+        s += f"G4Exception severity: {severity}\n"
+        s += f"G4Exception: {description}"
+        if severity == g4.FatalException or severity == g4.FatalErrorInArgument:
+            fatal(s)
+        warning(s)
+        return False
---
+++ packages/opengate/opengate-10.0.2.tar.gz/opengate-10.0.2/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+with open("VERSION", "r") as fh:
+    version = fh.read()[:-1]
+
+# For windows, this package is needed
+install_requires_windows = []
+if os.name == "nt":
+    install_requires_windows = ["msvc-runtime"]
+
+setup(
+    install_requires=[
+        "colored>1.5",
+        "opengate-core==" + version,
+        "gatetools",
+        "click",
+        "python-box<7.0.0",
+        "anytree",
+        "numpy<2.0.0",
+        "itk",
+        "uproot",
+        "scipy",
+        "matplotlib",
+        "GitPython",
+        "colorlog",
+        "numpy-stl",
+        "radioactivedecay",
+        "jsonpickle",
+        "pandas",
+        "requests",
+        "PyYAML",
+        "SimpleITK",
+        "spekpy",
+        "icrp107-database",
+    ]
+    + install_requires_windows,
+)
