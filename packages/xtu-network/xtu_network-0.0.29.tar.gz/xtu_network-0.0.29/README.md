d96a86908a3790b7abdecdc51a9e88530b4fb3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:06:03 +0000

    Add package xtu-network/0.0.29/xtu_network-0.0.29.tar.gz

---
+++ packages/xtu-network/xtu_network-0.0.29.tar.gz/xtu_network-0.0.29/README.md
@@ -0,0 +1,68 @@
+# 湘潭大学 校园网 API
+
+## 安装
+
+从 PyPI 安装：
+```bash
+pip install xtu-network
+```
+
+从 GitHub 安装：
+```bash
+pip install git+https://github.com/xtu-hit/xtu-network-python.git@main#egg=xtu-network
+```
+
+## 示例
+
+简单代码示例
+```python
+from xtu.network import XtuNetwork
+from xtu.network import NoLoginError
+from xtu.network import OnlineUserInfo
+
+async def main():
+    async with XtuNetwork(202400001111, "rsa_password") as xtu:
+        print(await xtu.login())
+        print(await xtu.checkOnline())
+
+        print(await xtu.getErrorMsg())
+        print(res := await xtu.getOnlineUserInfo())
+
+        print(OnlineUserInfo.get_online_count_from_ball_info(res["ballInfo"]))
+
+        print(await xtu.checkNetwork())
+
+        try:
+            print(await xtu.logout())
+        except NoLoginError:
+            pass
+```
+详见 [example.py](https://github.com/xtu-org/xtu-network/blob/main/example/main.py) 用法
+
+## 说明
+
+- `XtuNetwork` 类用于连接校园网。
+- `getErrorMsg()` 乍一看是获取错误信息，实际上什么也不返回，但是不调用此 API 的话 `getOnlineUserInfo` 就无法正确返回。
+- `getOnlineUserInfo()` 用于获取在线用户信息，返回一个字典。
+- `checkNetwork()` 用于检查网络状态，返回一个布尔值。
+- `logout()` 用于退出校园网，无返回值，如果尚未登录则抛出 `NoLoginError` 异常。
+
+**如何获得 RSA 加密后的密码？**
+
+使用 [security.html](https://github.com/xtu-org/xtu-network/blob/main/example/web/security.html) 获取，或者 F12 查看校园网登录请求。
+
+## 开源协议
+
+本仓库使用 [AGPL-3.0](https://www.gnu.org/licenses/agpl-3.0.en.html) 协议，注意事项：
+
+1. **源代码共享义务**：使用或修改此软件进行开发或提供网络服务时，必须提供完整的源代码。
+
+2. **网络应用条款**：若通过网络向用户提供此程序的功能，必须确保用户能够获取到相应的源代码。
+
+3. **许可证继承**：任何分发的修改版必须继续使用AGPL-3.0许可证，确保后续版本的开放性和共享要求。
+
+4. **修改版标识**：任何分发的修改版本须在显著位置标明已修改，并附上具体修改的内容和日期。
+
+5. **专利权警示**：使用此软件即表明你接受并遵循软件中规定的专利权限。
+
+6. **免责条款**：软件按“现状”提供，不提供任何明示或暗示的保证。使用软件所产生的任何风险由用户自行承担。
---
+++ packages/xtu-network/xtu_network-0.0.29.tar.gz/xtu_network-0.0.29/xtu/network/api.py
@@ -0,0 +1,223 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Protocol
+from json.decoder import JSONDecodeError
+from typing_extensions import override
+from urllib.parse import quote
+from functools import partial
+import asyncio
+import string
+import random
+import httpx
+
+
+from .exception import NoLoginError, LoginReadyError
+from .const import NETWORK_TEST_URLS, RETRY_COUNT
+from .models import LoginResult, OnlineUserInfo
+from .encrypt import encryptedPassword
+from .utils import logger
+
+if TYPE_CHECKING:
+
+    class _ApiCall(Protocol):
+        async def __call__(self, **kwargs: Any) -> Any: ...
+
+
+HEADERS = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Safari/605.1.15",
+}
+
+
+class XtuNetwork:
+    """校园网 API"""
+
+    def __init__(self, username: int, password: str, **args):
+        """
+        :param username: 学号
+        :param password: RSA 加密后的密码
+        """
+        self.username = username
+        self.client = httpx.AsyncClient(timeout=None, headers=HEADERS, **args)
+
+        if len(password) == 256:  # RSA 加密后的密码
+            self.password = password
+        else:  # 未加密的密码
+            mac = "".join(random.choice(string.hexdigits.lower()) for _ in range(32))
+            self.password = encryptedPassword(f"{password}>{mac}")
+
+    @override
+    def __getattr__(self, name: str, **data: Any) -> "_ApiCall":
+        """调用 /eportal/InterFace.do 的 API"""
+        if name.startswith("_") or name.endswith("_"):
+            raise AttributeError(f"Attribute {name} not found")
+        url = "http://172.16.0.32:8080/eportal/InterFace.do"
+        data["params"] = {
+            "method": name,
+        }
+        return partial(self.client.post, url, **data)
+
+    async def aclose(self):
+        await self.client.aclose()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.aclose()
+
+    async def login(self) -> "LoginResult":
+        """登录校园网"""
+        if not self.password or not self.username:
+            raise ValueError("用户名或密码不能为空")
+        resp = await self.client.post(
+            url="http://172.16.0.32:8080/eportal/InterFace.do",
+            params={
+                "method": "login",
+            },
+            data={
+                "userId": self.username,
+                "password": self.password,
+                "service": "",
+                "queryString": quote(await self.getQueryString()),
+                "operatorPwd": "",
+                "operatorUserId": "",
+                "validcode": "",
+                "passwordEncrypt": True,
+            },
+        )
+        res: "LoginResult" = resp.json()
+        self._userIndex = res.get("userIndex", None)
+
+        return res
+
+    async def getUserIndex(self) -> str:
+        """获取用户索引
+        注意：登录后才能调用该 API
+        """
+        if not hasattr(self, "_userIndex") or not self._userIndex:
+            resp = await self.client.get(
+                url="http://172.16.0.32:8080/eportal/redirectortosuccess.jsp",
+            )
+            if resp.next_request.url.host == "123.123.123.123":
+                raise NoLoginError
+            self._userIndex = resp.next_request.url.params.get("userIndex", None)
+            return self._userIndex
+        else:
+            return self._userIndex
+
+    async def getQueryString(self) -> str:
+        """获取 queryString
+        注意：登录后无法调用该 API
+        """
+        try:
+            resp = await self.client.get(
+                url="http://123.123.123.123",
+            )
+        except httpx.ReadError:
+            raise LoginReadyError
+        else:
+            queryString = resp.text.replace(
+                "<script>top.self.location.href='http://172.16.0.32:8080/eportal/index.jsp?",
+                "",
+            ).replace("'</script>", "")
+            return queryString.replace("\r\n", "")
+
+    async def logout(self):
+        """注销校园网"""
+        await self.client.post(
+            url="http://172.16.0.32:8080/eportal/InterFace.do",
+            params={
+                "method": "logout",
+            },
+            data={
+                "userIndex": await self.getUserIndex(),
+            },
+        )
+        self._userIndex = None
+
+    async def getOnlineUserInfo(self) -> "OnlineUserInfo" | dict:
+        """获取在线用户信息
+        注意：登录后才能调用该 API
+        """
+        for index in range(RETRY_COUNT):
+            resp = await self.client.post(
+                url="http://172.16.0.32:8080/eportal/InterFace.do",
+                params={
+                    "method": "getOnlineUserInfo",
+                },
+                data={
+                    "userIndex": await self.getUserIndex(),
+                },
+            )
+            try:
+                res: "OnlineUserInfo" = resp.json()
+            except JSONDecodeError:
+                logger.warning(f"getOnlineUserInfo response is not json: {resp.text}")
+                return {}
+
+            if res["result"] == "success" or index == RETRY_COUNT - 1:
+                return res
+
+            await asyncio.sleep(random.randint(3, 8))
+
+    async def getErrorMsg(self) -> str:
+        """获取错误信息"""
+        resp = await self.client.post(
+            url="http://172.16.0.32:8080/eportal/userV2.do",
+            params={
+                "method": "getErrorMsg",
+            },
+            data={
+                "userIndex": await self.getUserIndex(),
+            },
+        )
+        return resp.text
+
+    async def checkOnline(self) -> bool:
+        """检查在线状态"""
+        try:
+            resp = await self.client.get(
+                url="http://172.16.0.32:8080/eportal/redirectortosuccess.jsp",
+            )
+        except httpx.NetworkError:
+            return True  # 无法连接到认证服务器，避免重复登录
+        else:
+            if resp.next_request.url.host == "123.123.123.123":
+                return False
+            else:
+                return True
+
+    async def checkNetwork(self) -> bool:
+        """检查实际网络状态"""
+
+        async def _request(client: httpx.AsyncClient, url: str) -> bool:
+            """请求"""
+            try:
+                resp = await client.get(url, timeout=3.0)
+            except (httpx.NetworkError, httpx.TimeoutException):
+                return False
+            else:
+                if "<script>top.self.location.href='http://172.16.0.32:8080/eportal/index.jsp" in resp.text:
+                    return False
+                else:
+                    return True
+
+        result = await asyncio.gather(
+            *[_request(self.client, url) for url in random.sample(NETWORK_TEST_URLS, 3)]
+        )
+        return any(item for item in result)
+
+    async def loop(self, interval: float = 5):
+        """循环检测在线状态"""
+        while True:
+            await asyncio.sleep(max(interval, 1.5))
+
+            if (await self.checkNetwork()) or (await self.checkOnline()):
+                logger.info(f"在线 {self.username}")
+                continue
+
+            try:
+                logger.warning("离线，自动登录")
+                await self.login()
+            except LoginReadyError:
+                pass
