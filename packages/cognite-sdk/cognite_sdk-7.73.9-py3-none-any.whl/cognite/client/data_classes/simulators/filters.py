2d2954cdda34e242d2ec4f3b4d41dfb503bbf91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:15:14 +0000

    Add package cognite-sdk/7.73.9/cognite_sdk-7.73.9-py3-none-any.whl

---
+++ packages/cognite-sdk/cognite_sdk-7.73.9-py3-none-any.whl/cognite/client/data_classes/simulators/filters.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import Any, Literal
+
+from cognite.client.data_classes._base import CogniteFilter, CogniteSort
+
+
+class SimulatorIntegrationFilter(CogniteFilter):
+    def __init__(
+        self,
+        simulator_external_ids: str | Sequence[str] | None = None,
+        active: bool | None = None,
+    ) -> None:
+        self.simulator_external_ids = (
+            [simulator_external_ids] if isinstance(simulator_external_ids, str) else simulator_external_ids
+        )
+        self.active = active
+
+
+class SimulatorModelsFilter(CogniteFilter):
+    def __init__(
+        self,
+        simulator_external_ids: str | Sequence[str] | None = None,
+    ) -> None:
+        self.simulator_external_ids = (
+            [simulator_external_ids] if isinstance(simulator_external_ids, str) else simulator_external_ids
+        )
+
+
+class SimulatorModelRevisionsFilter(CogniteFilter):
+    def __init__(
+        self,
+        model_external_ids: str | Sequence[str] | None = None,
+        all_versions: bool | None = None,
+    ) -> None:
+        self.model_external_ids = [model_external_ids] if isinstance(model_external_ids, str) else model_external_ids
+
+
+class SimulatorRoutinesFilter(CogniteFilter):
+    def __init__(
+        self,
+        model_external_ids: Sequence[str] | None = None,
+        simulator_integration_external_ids: Sequence[str] | None = None,
+    ) -> None:
+        self.model_external_ids = model_external_ids
+        self.simulator_integration_external_ids = simulator_integration_external_ids
+
+
+class PropertySort(CogniteSort):
+    def __init__(
+        self,
+        property: Literal["createdTime"] = "createdTime",
+        order: Literal["asc", "desc"] = "asc",
+    ):
+        super().__init__(property, order)
+
+    def dump(self, camel_case: bool = True) -> dict[str, Any]:
+        dumped = super().dump(camel_case=camel_case)
+        dumped["property"] = self.property
+        return dumped
