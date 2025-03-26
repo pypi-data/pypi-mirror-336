c7dba2254478097c37278f5aa9cde21dae963f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:06:21 +0000

    Add package pyhalboy/1.2.0/pyhalboy-1.2.0.tar.gz

---
+++ packages/pyhalboy/pyhalboy-1.2.0.tar.gz/pyhalboy-1.2.0/src/pyhalboy/types.py
@@ -0,0 +1,18 @@
+from collections.abc import Mapping
+from typing import Sequence
+
+type JsonObject = Mapping[str, JsonValue]
+type JsonArray = Sequence[JsonValue]
+type JsonPrimitive = str | int | float | bool | None
+type JsonValue = JsonObject | JsonArray | JsonPrimitive
+
+JsonValueType = JsonValue.__value__
+
+
+PropertyName = str
+PropertyValue = JsonValue
+ResourceRel = str
+LinkRel = str
+Href = str
+
+StatusCode = int
