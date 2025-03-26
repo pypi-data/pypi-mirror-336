0de72ee34c845774c750de3d5309734d167245ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:06:23 +0000

    Add package webknossos/2.0.6/webknossos-2.0.6.tar.gz

---
+++ packages/webknossos/webknossos-2.0.6.tar.gz/webknossos-2.0.6/webknossos/annotation/annotation_info.py
@@ -0,0 +1,58 @@
+from typing import List, Optional
+
+import attr
+
+from ..client.api_client.models import ApiAnnotation
+from .annotation import Annotation, AnnotationState, AnnotationType
+
+
+@attr.frozen
+class AnnotationInfo:
+    """Data class containing information about a WEBKNOSSOS annotation"""
+
+    id: str
+    owner_id: Optional[str]
+    name: str
+    description: str
+    type: AnnotationType
+    state: AnnotationState
+    duration_in_seconds: Optional[float]
+    modified: Optional[int]
+
+    def download_annotation(self) -> Annotation:
+        """Downloads and returns the annotation that is described by this AnnotationInfo object"""
+        return Annotation.download(self.id)
+
+    @classmethod
+    def _from_api_annotation(cls, api_annotation: ApiAnnotation) -> "AnnotationInfo":
+        owner_id = api_annotation.owner.id if api_annotation.owner is not None else None
+        return AnnotationInfo(
+            id=api_annotation.id,
+            owner_id=owner_id,
+            name=api_annotation.name,
+            description=api_annotation.description,
+            type=AnnotationType(api_annotation.typ),
+            state=AnnotationState(api_annotation.state),
+            duration_in_seconds=(api_annotation.tracing_time / 1000)
+            if api_annotation.tracing_time is not None
+            else None,
+            modified=api_annotation.modified,
+        )
+
+    @classmethod
+    def get_remote_annotations(
+        cls, is_finished: Optional[bool] = False, owner: Optional[str] = None
+    ) -> List["AnnotationInfo"]:
+        """Returns a list of AnnotationInfo objects for all annotations that belong to the current user (if owner is None).
+        If owner is not None, only annotations of the specified owner are returned."""
+        from ..client.context import _get_api_client
+
+        client = _get_api_client(True)
+        if owner is None:
+            owner = client.user_current().id
+
+        return [
+            cls._from_api_annotation(api_annotation)
+            for api_annotation in client.annotation_list(is_finished)
+            if api_annotation.owner.id == owner
+        ]
---
+++ packages/webknossos/webknossos-2.0.6.tar.gz/webknossos-2.0.6/webknossos/skeleton/__init__.py
@@ -0,0 +1,9 @@
+# ruff: noqa: F401 imported but unused
+
+from os import PathLike
+from typing import Union
+
+from .group import Group
+from .node import Node
+from .skeleton import Skeleton
+from .tree import Tree
