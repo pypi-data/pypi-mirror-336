18c9c0bf401ae88b6d9ddc0e1cf1897beb82d5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 07:06:40 +0000

    Add package pixeltable/0.3.8/pixeltable-0.3.8.tar.gz

---
+++ packages/pixeltable/pixeltable-0.3.8.tar.gz/pixeltable-0.3.8/pixeltable/catalog/path.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import logging
+
+from pixeltable import exceptions as excs
+
+from .globals import is_valid_path
+
+_logger = logging.getLogger('pixeltable')
+
+
+class Path:
+    def __init__(self, path: str, empty_is_valid: bool = False):
+        if not is_valid_path(path, empty_is_valid):
+            raise excs.Error(f"Invalid path format: '{path}'")
+        self.components = path.split('.')
+
+    @property
+    def len(self) -> int:
+        return 0 if self.is_root else len(self.components)
+
+    @property
+    def name(self) -> str:
+        assert len(self.components) > 0
+        return self.components[-1]
+
+    @property
+    def is_root(self) -> bool:
+        return self.components[0] == ''
+
+    @property
+    def parent(self) -> Path:
+        if len(self.components) == 1:
+            if self.is_root:
+                return self
+            else:
+                return Path('', empty_is_valid=True)
+        else:
+            return Path('.'.join(self.components[:-1]))
+
+    def append(self, name: str) -> Path:
+        if self.is_root:
+            return Path(name)
+        else:
+            return Path(f'{str(self)}.{name}')
+
+    def is_ancestor(self, other: Path, is_parent: bool = False) -> bool:
+        """
+        True if self as an ancestor path of other.
+        """
+        if self.len >= other.len or other.is_root:
+            return False
+        if self.is_root and (other.len == 1 or not is_parent):
+            return True
+        is_prefix = self.components == other.components[: self.len]
+        return is_prefix and (self.len == (other.len - 1) or not is_parent)
+
+    def __str__(self) -> str:
+        return '.'.join(self.components)
