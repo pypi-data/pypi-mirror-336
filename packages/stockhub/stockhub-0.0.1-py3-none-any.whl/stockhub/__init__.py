21c41d3163c55a293b2f15b0480afd468781fd46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:14:34 +0000

    Add package stockhub/0.0.1/stockhub-0.0.1-py3-none-any.whl

---
+++ packages/stockhub/stockhub-0.0.1-py3-none-any.whl/stockhub/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#===================================================================================================
+#---    copyright 2017~2025, OBCon Inc.
+#---    author gye hyun james kim <pnuskgh@gmail.com>
+#---    license GNU GENERAL PUBLIC LICENSE v3.0 (https://github.com/jopenbusiness/StockHub?tab=GPL-3.0-1-ov-file)
+#===================================================================================================
+
+__version__ = '0.0.1'
