9635d8984c8b635dae8d9290edfd90bde3fef8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 16:06:25 +0000

    Add package agilab/0.0.1a9/agilab-0.0.1a9.tar.gz

---
+++ packages/agilab/agilab-0.0.1a9.tar.gz/agilab-0.0.1a9/src/apps/my-code-project/test/test_my_code_worker.py
@@ -0,0 +1,15 @@
+from agi_core.workers.agi_worker import AgiWorker
+
+cmd = "uv build"
+res = AgiWorker.exec(cmd, ".", "localhost")
+
+cmd = "uv sync --group dag-worker;uv pip install -e \.;"
+res = AgiWorker.exec(cmd, ".", "localhost")
+
+args = {'param1': 0, 'param2': "some text", 'param3': 3.14, 'param4': True}
+
+# AgiWorker.run('my_code', mode=0, verbose=3, args=args)
+# AgiWorker.run('my_code', mode=1, verbose=3, args=args)
+
+# compilation cython and dask are managed by Agi so mode > 1 iare not available for unary test
+# AgiWorker.run('my_code', mode=2, verbose=3, args=args)
