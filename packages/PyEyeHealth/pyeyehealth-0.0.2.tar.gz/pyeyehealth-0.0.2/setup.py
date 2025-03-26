c4859d0e9978f4ad776fafd5bb1360d2026af0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 16:08:22 +0000

    Add package PyEyeHealth/0.0.2/pyeyehealth-0.0.2.tar.gz

---
+++ packages/PyEyeHealth/pyeyehealth-0.0.2.tar.gz/pyeyehealth-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="PyEyeHealth",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[
+	"PyQt6","imutils"
+
+    ]
+)
