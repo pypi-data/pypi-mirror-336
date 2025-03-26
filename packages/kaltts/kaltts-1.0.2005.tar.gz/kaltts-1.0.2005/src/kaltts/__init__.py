ad7cac5124ebe48deb7ad3aa50be7a38ffee6a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:06:10 +0000

    Add package kaltts/1.0.2005/kaltts-1.0.2005.tar.gz

---
+++ packages/kaltts/kaltts-1.0.2005.tar.gz/kaltts-1.0.2005/src/kaltts/__init__.py
@@ -0,0 +1,7 @@
+from .model import create_tacotron2_model
+from .train import train_model
+from .synthesize import synthesize_speech
+from .controller import main_controller
+from .data_processing import process_data
+from .utils import utility_function
+from .vocoder import griffin_lim
