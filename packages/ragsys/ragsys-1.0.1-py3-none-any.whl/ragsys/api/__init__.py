a4e38304edeff6f852bd2ba54702aec36295eabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 08:05:04 +0000

    Add package ragsys/1.0.1/ragsys-1.0.1-py3-none-any.whl

---
+++ packages/ragsys/ragsys-1.0.1-py3-none-any.whl/ragsys/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from ragsys.api.account_api import AccountApi
+from ragsys.api.completion_api import CompletionApi
+from ragsys.api.database_api import DatabaseApi
+from ragsys.api.few_shot_example_api import FewShotExampleApi
+from ragsys.api.task_api import TaskApi
+
