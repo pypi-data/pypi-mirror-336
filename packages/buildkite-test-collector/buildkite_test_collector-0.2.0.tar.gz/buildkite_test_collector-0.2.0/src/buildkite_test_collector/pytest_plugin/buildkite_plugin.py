86a84298f2d3ce6a1db456e05c0d75fb4a87aff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:05:34 +0000

    Add package buildkite-test-collector/0.2.0/buildkite_test_collector-0.2.0.tar.gz

---
+++ packages/buildkite-test-collector/buildkite_test_collector-0.2.0.tar.gz/buildkite_test_collector-0.2.0/src/buildkite_test_collector/pytest_plugin/buildkite_plugin.py
@@ -0,0 +1,58 @@
+"""Buildkite test collector plugin for Pytest"""
+import json
+
+from uuid import uuid4
+
+from ..collector.payload import TestData
+
+class BuildkitePlugin:
+    """Buildkite test collector plugin for Pytest"""
+
+    def __init__(self, payload):
+        self.payload = payload
+        self.in_flight = {}
+        self.spans = {}
+
+    def pytest_runtest_logstart(self, nodeid, location):
+        """pytest_runtest_logstart hook callback"""
+        if not self.payload.is_started():
+            self.payload = self.payload.started()
+
+        chunks = nodeid.split("::")
+
+        test_data = TestData.start(
+            uuid4(),
+            scope="::".join(chunks[:-1]),
+            name=chunks[-1],
+            file_name=location[0],
+            location=f"{location[0]}:{location[1]}"
+        )
+        self.in_flight[nodeid] = test_data
+
+    def pytest_runtest_logreport(self, report):
+        """pytest_runtest_logreport hook callback"""
+        if report.when != 'call':
+            return
+
+        nodeid = report.nodeid
+        test_data = self.in_flight.get(nodeid)
+
+        if test_data:
+            test_data = test_data.finish()
+
+            if report.passed:
+                test_data = test_data.passed()
+
+            if report.failed:
+                test_data = test_data.failed(report.longreprtext)
+
+            if report.skipped:
+                test_data = test_data.skipped()
+
+            del self.in_flight[nodeid]
+            self.payload = self.payload.push_test_data(test_data)
+
+    def save_payload_as_json(self, path):
+        """ Save payload into a json file """
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(self.payload.as_json()["data"], f)
