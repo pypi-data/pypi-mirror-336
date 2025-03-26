a655228cd2fbdf72ca4f695fecad7ef35fd78fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:06:09 +0000

    Add package apertium-apy/0.13.0/apertium_apy-0.13.0.tar.gz

---
+++ packages/apertium-apy/apertium_apy-0.13.0.tar.gz/apertium_apy-0.13.0/apertium_apy/handlers/base.py
@@ -0,0 +1,213 @@
+import json
+import logging
+import os
+import sys
+from datetime import datetime, timedelta
+
+import tornado
+import tornado.gen
+import tornado.web
+from tornado.escape import utf8
+from tornado.locks import Semaphore
+
+from apertium_apy.utils import to_fallback_code
+# Typing imports that flake8 doesn't understand:
+from typing import Union, Dict, Optional, List, Any, Tuple  # noqa: F401
+from apertium_apy.utils.translation import FlushingPipeline, SimplePipeline  # noqa: F401
+
+
+def dump_json(data):
+    # This acts very similarly to Tornado's escape.json_encode but doesn't
+    # result in ugly \u codes. Tornado does not support this natively.
+    return json.dumps(data, ensure_ascii=False).replace('</', '<\\/')
+
+
+class Stats:
+    startdate = datetime.now()
+    usecount = {}               # type: Dict[Tuple[str, str], int]
+    vmsize = 0
+    timing = []                 # type: List[Tuple[datetime, datetime, int]]
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    pairs = {}  # type: Dict[str, str]
+    analyzers = {}  # type: Dict[str, Tuple[str, str]]
+    generators = {}  # type: Dict[str, Tuple[str, str]]
+    taggers = {}  # type: Dict[str, Tuple[str, str]]
+    spellers = {}  # type: Dict[str, Tuple[str, str]]
+    guessers = {}  # type: Dict[str, Tuple[str, str]]
+    pairprefs = {}        # type: Dict[str, Dict[str, Dict[str, str]]]
+    # (l1, l2): [translation.Pipeline], only contains flushing pairs!
+    pipelines = {}  # type: Dict[Tuple[str, str], List[Union[FlushingPipeline, SimplePipeline]]]
+    pipelines_holding = []  # type: List
+    callback = None
+    timeout = 10
+    lang_names = None           # type: Optional[str]
+    scale_mt_logs = False
+    verbosity = 0
+    api_keys_conf = None
+    stat_period_max_age = timedelta.max
+
+    # dict representing a graph of translation pairs; keys are source languages
+    # e.g. pairs_graph['eng'] = ['fra', 'spa']
+    pairs_graph = {}  # type: Dict[str, List[str]]
+    # 2-D dict storing the shortest path for a chained translation pair
+    # keys are source and target languages
+    # e.g. paths['eng']['fra'] = ['eng', 'spa', 'fra']
+    paths = {}  # type: Dict[str, Dict[str, List[str]]]
+
+    # dict representing equivalent languages and pairs
+    # keys are languages or pairs with variants
+    fallback_codes = {}  # type: Dict[str, str]
+
+    stats = Stats()
+
+    # (l1, l2): translation.ParsedModes
+    pipeline_cmds = {}  # type: Dict
+    max_pipes_per_pair = 1
+    min_pipes_per_pair = 0
+    max_users_per_pipe = 5
+    max_idle_secs = 0
+    restart_pipe_after = 1000
+    doc_pipe_sem = Semaphore(3)
+    # Empty the url_cache[pair] when it's this full:
+    max_inmemory_url_cache = 1000  # type: int
+    url_cache = {}       # type: Dict[Tuple[str, str], Dict[str, Tuple[str, str]]]
+    url_cache_path = None  # type: Optional[str]
+    # Keep half a gig free when storing url_cache to disk:
+    min_free_space_disk_url_cache = 512 * 1024 * 1024  # type: int
+
+    def initialize(self):
+        self.callback = self.get_argument('callback', default=None)
+
+    @classmethod
+    def init_pairs_graph(cls):
+        for pair in cls.pairs:
+            lang1, lang2 = pair.split('-')
+            if lang1 in cls.pairs_graph:
+                cls.pairs_graph[lang1].append(lang2)
+            else:
+                cls.pairs_graph[lang1] = [lang2]
+
+    @classmethod
+    def calculate_paths(cls, start):
+        nodes = set()
+        for pair in map(lambda x: x.split('-'), cls.pairs):
+            nodes.add(pair[0])
+            nodes.add(pair[1])
+        dists = {}
+        prevs = {}
+        dists[start] = 0
+
+        while nodes:
+            u = min(nodes, key=lambda u: dists.get(u, sys.maxsize))
+            nodes.remove(u)
+            for v in cls.pairs_graph.get(u, []):
+                if v in nodes:
+                    other = dists.get(u, sys.maxsize) + 1   # TODO: weight(u, v) -- lower weight = better translation
+                    if other < dists.get(v, sys.maxsize):
+                        dists[v] = other
+                        prevs[v] = u
+
+        cls.paths[start] = {}
+        for u in prevs:
+            prev = prevs[u]     # type: Optional[str]
+            path = [u]
+            while prev:
+                path.append(prev)
+                prev = prevs.get(prev)
+            cls.paths[start][u] = list(reversed(path))
+
+    @classmethod
+    def init_paths(cls):
+        for lang in cls.pairs_graph:
+            cls.calculate_paths(lang)
+
+    def log_vmsize(self):
+        if self.verbosity < 1:
+            return
+        scale = {'kB': 1024, 'mB': 1048576,
+                 'KB': 1024, 'MB': 1048576}
+        try:
+            for line in open('/proc/%d/status' % os.getpid()):
+                if line.startswith('VmSize:'):
+                    _, num, unit = line.split()
+                    break
+            vmsize = int(num) * scale[unit]
+            if vmsize > self.stats.vmsize:
+                logging.warning('VmSize of %s from %d to %d', os.getpid(), self.stats.vmsize, vmsize)
+                self.stats.vmsize = vmsize
+        except Exception as e:
+            # Don't fail just because we couldn't log:
+            logging.info('Exception in log_vmsize: %s', e)
+
+    def send_response(self, data):
+        self.log_vmsize()
+        if isinstance(data, dict) or isinstance(data, list):
+            data = dump_json(data)
+            self.set_header('Content-Type', 'application/json; charset=UTF-8')
+
+        if self.callback:
+            self.set_header('Content-Type', 'application/javascript; charset=UTF-8')
+            self._write_buffer.append(utf8('%s(%s)' % (self.callback, data)))
+        else:
+            self._write_buffer.append(utf8(data))
+        self.finish()
+
+    def write_error(self, status_code, **kwargs):
+        http_explanations = {
+            400: 'Request not properly formatted or contains languages that Apertium APy does not support',
+            404: 'Resource requested does not exist. URL may have been mistyped',
+            408: 'Server did not receive a complete request within the time it was prepared to wait. Try again',
+            500: 'Unexpected condition on server. Request could not be fulfilled.',
+        }
+        explanation = kwargs.get('explanation', http_explanations.get(status_code, ''))
+        if 'exc_info' in kwargs and len(kwargs['exc_info']) > 1:
+            exception = kwargs['exc_info'][1]
+            if hasattr(exception, 'log_message') and exception.log_message:
+                explanation = exception.log_message % exception.args
+            elif hasattr(exception, 'reason'):
+                explanation = exception.reason or tornado.httputil.responses.get(status_code, 'Unknown')
+            else:
+                explanation = tornado.httputil.responses.get(status_code, 'Unknown')
+
+        result = {
+            'status': 'error',
+            'code': status_code,
+            'message': tornado.httputil.responses.get(status_code, 'Unknown'),
+            'explanation': explanation,
+        }
+
+        data = dump_json(result)
+        self.set_header('Content-Type', 'application/json; charset=UTF-8')
+
+        if self.callback:
+            self.set_header('Content-Type', 'application/javascript; charset=UTF-8')
+            self._write_buffer.append(utf8('%s(%s)' % (self.callback, data)))
+        else:
+            self._write_buffer.append(utf8(data))
+        self.finish()
+
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+        self.set_header('Access-Control-Allow-Headers', 'accept, cache-control, origin, x-requested-with, x-file-name, content-type')
+
+    def find_fallback_mode(self, in_mode, installed_modes):
+        if in_mode not in installed_modes and '_' in in_mode:
+            if in_mode in self.fallback_codes:
+                in_mode = self.fallback_codes[in_mode]
+            else:
+                fallback_mode = to_fallback_code(in_mode, installed_modes)
+                if fallback_mode is not None:
+                    self.fallback_codes[in_mode] = fallback_mode
+                    in_mode = fallback_mode
+        return in_mode
+
+    @tornado.gen.coroutine
+    def post(self):
+        yield self.get()
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
---
+++ packages/apertium-apy/apertium_apy-0.13.0.tar.gz/apertium_apy-0.13.0/apertium_apy/mode_search.py
@@ -0,0 +1,115 @@
+import re
+import os
+import logging
+try:
+    from lxml import etree
+except ImportError:
+    etree = None
+
+from apertium_apy.utils import to_alpha3_code
+
+if False:
+    from typing import Dict, List, Tuple  # noqa: F401
+
+
+def is_loop(dirpath, rootpath, real_root=None):
+    if os.path.islink(dirpath):
+        # We just descended into a directory via a symbolic link
+        # Check if we're referring to a directory that is
+        # a parent of our nominal directory
+        if not real_root:
+            real_root = os.path.abspath(os.path.realpath(rootpath))
+        relative = os.path.relpath(dirpath, rootpath)
+        nominal_path = os.path.join(real_root, relative)
+        real_path = os.path.abspath(os.path.realpath(dirpath))
+        for nominal, real in zip(nominal_path.split(os.sep),
+                                 real_path.split(os.sep)):
+            if nominal != real:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def search_path(rootpath, include_pairs=True, verbosity=1):
+    lang_code = r'[a-z]{2,3}(?:_[A-Za-z0-9]+)*'
+    type_re = {
+        'pair': re.compile(r'({0})-({0})\.mode'.format(lang_code)),
+        'analyzer': re.compile(r'(({0}(-{0})?)-(an)?mor(ph)?)\.mode'.format(lang_code)),
+        'generator': re.compile(r'(({0}(-{0})?)-gener[A-z]*)\.mode'.format(lang_code)),
+        'tagger': re.compile(r'(({0}(-{0})?)-tagger)\.mode'.format(lang_code)),
+        'spell': re.compile(r'(({0}(-{0})?)-spell)\.mode'.format(lang_code)),
+        'tokenise': re.compile(r'(({0}(-{0})?)-tokenise)\.mode'.format(lang_code)),
+        'guesser': re.compile(r'(({0}(-{0})?)-guess(er)?)\.mode'.format(lang_code)),
+    }
+    modes = {
+        'pair': [],
+        'analyzer': [],
+        'generator': [],
+        'tagger': [],
+        'spell': [],
+        'tokenise': [],
+        'guesser': [],
+    }  # type: Dict[str, List[Tuple[str, str, str]]]
+
+    real_root = os.path.abspath(os.path.realpath(rootpath))
+
+    for dirpath, dirnames, files in os.walk(rootpath, followlinks=True):
+        if is_loop(dirpath, rootpath, real_root):
+            dirnames[:] = []
+            continue
+        for filename in [f for f in files if f.endswith('.mode')]:
+            for mtype, regex in type_re.items():
+                m = regex.match(filename)
+                if m:
+                    if mtype != 'pair':
+                        modename = m.group(1)  # e.g. en-es-anmorph
+                        langlist = [to_alpha3_code(x) for x in m.group(2).split('-')]
+                        lang_pair = '-'.join(langlist)  # e.g. en-es
+                        dir_of_modes = os.path.dirname(dirpath)
+                        mode = (dir_of_modes,
+                                modename,
+                                lang_pair)
+                        modes[mtype].append(mode)
+                    elif include_pairs:
+                        lang_src = m.group(1)
+                        lang_trg = m.group(2)
+                        mode = (os.path.join(dirpath, filename),
+                                to_alpha3_code(lang_src),
+                                to_alpha3_code(lang_trg))
+                        modes[mtype].append(mode)
+
+    if verbosity > 1:
+        _log_modes(modes)
+
+    return modes
+
+
+def search_prefs(rootpath):
+    if etree is None:
+        logging.warning('Please install python3-lxml to enable /pairprefs endpoint')
+        return
+    real_root = os.path.abspath(os.path.realpath(rootpath))
+    prefspath = real_root + '/prefs'
+    pairprefs = {}        # type: Dict[str, Dict[str, Dict[str, str]]]
+    if not os.path.exists(prefspath):
+        return pairprefs
+    for f in os.listdir(prefspath):
+        fp = os.path.join(prefspath, f)
+        try:
+            mode = re.sub(r'[.]xml$', '', f)
+            pairprefs[mode] = {pref.get('id'): {dsc.get('lang'): dsc.text
+                               for dsc in pref.xpath('./description')}
+                               for pref
+                               in etree.parse(fp).xpath('//preference')}
+        except Exception:
+            logging.warning('Exception on parsing preferences file {}'.format(fp))
+    return pairprefs
+
+
+def _log_modes(modes):
+    """Print given modes to log."""
+    for mtype in modes:
+        if modes[mtype]:
+            logging.info('"%s" modes found:\n%s', mtype, '\n'.join(['\t'.join(m) for m in modes[mtype]]))
