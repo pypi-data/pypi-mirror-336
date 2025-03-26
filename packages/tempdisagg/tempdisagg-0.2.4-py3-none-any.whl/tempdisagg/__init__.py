eb45a93900827dfbb343210c688d47b6410d1034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:05:26 +0000

    Add package tempdisagg/0.2.4/tempdisagg-0.2.4-py3-none-any.whl

---
+++ packages/tempdisagg/tempdisagg-0.2.4-py3-none-any.whl/tempdisagg/__init__.py
@@ -0,0 +1,28 @@
+# tempdisagg/__init__.py
+
+"""
+tempdisagg: Temporal disaggregation of low-frequency time series using various statistical methods.
+"""
+
+# Public API
+from .model.tempdisagg_model import TempDisaggModel
+
+# Define what gets imported with `from tempdisagg import *`
+__all__ = [
+    "TempDisaggModel",
+    "ModelFitter",
+    "EnsemblePredictor",
+    "TempDisaggVisualizer",
+    "TempDisaggReporter",
+    "TempDisaggAdjuster",
+    "DisaggInputPreparer",
+    "InputPreprocessor",
+    "TimeSeriesCompleter",
+    "ConversionMatrixBuilder",
+    "ModelsHandler",
+    "RhoOptimizer",
+    "NumericUtils",
+    "PostEstimation",
+    "EnsemblePrediction",
+    "TemporalAggregator",
+]
