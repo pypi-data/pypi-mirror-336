998f2288d47431bf98886b9748271fa01741742a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:06:48 +0000

    Add package xmanage/0.5.0/xmanage-0.5.0-py2.py3-none-any.whl

---
+++ packages/xmanage/xmanage-0.5.0-py2.py3-none-any.whl/xmanage/xsystem/__init__.py
@@ -0,0 +1,43 @@
+# coding:utf-8
+
+from typing import List
+from typing import Optional
+from typing import Sequence
+
+from xkits import add_command
+from xkits import argp
+from xkits import commands
+from xkits import run_command
+
+from xmanage.attribute import __description__
+from xmanage.attribute import __project__
+from xmanage.attribute import __urlhome__
+from xmanage.attribute import __version__
+
+subs: List[add_command] = list()
+
+try:
+    from xmanage.xsystem.path import add_cmd_path
+    subs.append(add_cmd_path)
+except Exception:  # pragma: no cover
+    pass  # pragma: no cover
+
+
+@add_command(__project__)
+def add_cmd(_arg: argp):
+    pass
+
+
+@run_command(add_cmd, *subs)
+def run_cmd(cmds: commands) -> int:
+    return 0
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    cmds = commands()
+    cmds.version = __version__
+    return cmds.run(
+        root=add_cmd,
+        argv=argv,
+        description=__description__,
+        epilog=f"For more, please visit {__urlhome__}.")
