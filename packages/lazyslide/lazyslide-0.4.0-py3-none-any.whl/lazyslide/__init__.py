7105b8ec1f9400937861ed2a885afa5bdf5e8f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 04:05:13 +0000

    Add package lazyslide/0.4.0/lazyslide-0.4.0-py3-none-any.whl

---
+++ packages/lazyslide/lazyslide-0.4.0-py3-none-any.whl/lazyslide/__init__.py
@@ -0,0 +1,35 @@
+"""Efficient and Scalable Whole Slide Image (WSI) processing library."""
+
+__version__ = "0.4.0"
+
+
+import sys
+
+# Re-export the public API
+from wsidata import open_wsi, agg_wsi
+
+from . import cv
+from . import io
+from . import models
+from . import plotting as pl
+from . import preprocess as pp
+from . import segmentation as seg
+from . import tools as tl
+from . import datasets
+
+# Inject the aliases into the current module
+sys.modules.update({f"{__name__}.{m}": globals()[m] for m in ["tl", "pp", "pl", "seg"]})
+del sys
+
+
+__all__ = [
+    "open_wsi",
+    "agg_wsi",
+    "pp",
+    "tl",
+    "pl",
+    "seg",
+    "cv",
+    "models",
+    "io",
+]
---
+++ packages/lazyslide/lazyslide-0.4.0-py3-none-any.whl/lazyslide/segmentation/__init__.py
@@ -0,0 +1,2 @@
+from ._run_seg import cells, semantic
+from ._tissue_seg import tissue
