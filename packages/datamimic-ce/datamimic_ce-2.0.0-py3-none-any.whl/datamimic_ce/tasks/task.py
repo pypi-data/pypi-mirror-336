d883d8154adad3fdec92209ba2bd936e4f685dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:06:44 +0000

    Add package datamimic-ce/2.0.0/datamimic_ce-2.0.0-py3-none-any.whl

---
+++ packages/datamimic-ce/datamimic_ce-2.0.0-py3-none-any.whl/datamimic_ce/tasks/task.py
@@ -0,0 +1,37 @@
+# DATAMIMIC
+# Copyright (c) 2023-2025 Rapiddweller Asia Co., Ltd.
+# This software is licensed under the MIT License.
+# See LICENSE file for the full text of the license.
+# For questions and support, contact: info@rapiddweller.com
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+from datamimic_ce.contexts.geniter_context import GenIterContext
+from datamimic_ce.contexts.setup_context import SetupContext
+from datamimic_ce.statements.statement import Statement
+
+
+class Task(ABC):
+    @property
+    @abstractmethod
+    def statement(self) -> Statement:
+        pass
+
+
+class SetupSubTask(Task, ABC):
+    @abstractmethod
+    def execute(self, ctx: SetupContext) -> Any:
+        pass
+
+
+class GenIterSubTask(Task, ABC):
+    @abstractmethod
+    def execute(self, ctx: GenIterContext) -> Any:
+        pass
+
+
+class CommonSubTask(Task, ABC):
+    @abstractmethod
+    def execute(self, ctx: SetupContext | GenIterContext) -> Any:
+        pass
