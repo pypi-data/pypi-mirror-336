dc7fadae8104151900e15f50ab91336c416dfefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:06:43 +0000

    Add package SPEARS/2.0.1/spears-2.0.1.tar.gz

---
+++ packages/SPEARS/spears-2.0.1.tar.gz/spears-2.0.1/CHANGELOG.md
@@ -0,0 +1,9 @@
+
+# NOTE: Version 2 Improvements
+    # Introduced the SPEAR class, storing local variables instead of utilizing functions
+    # Massively improved computational effecientcy
+    # Better improved modularity, and client-side customization
+    # Improved simplicity and ease-of-use
+    # (actually works well)
+    # Officially supports simple multithreading
+    # So much more, a complete rehaul from V1, utilize this version above V1 (V1 is very rough and unlikely to work propertly)
---
+++ packages/SPEARS/spears-2.0.1.tar.gz/spears-2.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the long description from README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="SPEARS",
+    version="2.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "SPEARS": ["data/*.json"],
+    },
+    author="Justin Domagala-Tang",
+    url="https://pypi.org/project/SPEARS/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
