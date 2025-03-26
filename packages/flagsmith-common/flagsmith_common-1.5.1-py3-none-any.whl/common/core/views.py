04a53d1be840fa767a476b87de3eee33b2d445bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:06:33 +0000

    Add package flagsmith-common/1.5.1/flagsmith_common-1.5.1-py3-none-any.whl

---
+++ packages/flagsmith-common/flagsmith_common-1.5.1-py3-none-any.whl/common/core/views.py
@@ -0,0 +1,23 @@
+import logging
+
+import prometheus_client
+from django.http import HttpResponse, JsonResponse
+from rest_framework.request import Request
+
+from common.core import utils
+from common.prometheus.utils import get_registry
+
+logger = logging.getLogger(__name__)
+
+
+def version_info(request: Request) -> JsonResponse:
+    return JsonResponse(utils.get_version_info())
+
+
+def metrics(request: Request) -> HttpResponse:
+    registry = get_registry()
+    metrics_page = prometheus_client.generate_latest(registry)
+    return HttpResponse(
+        metrics_page,
+        content_type=prometheus_client.CONTENT_TYPE_LATEST,
+    )
