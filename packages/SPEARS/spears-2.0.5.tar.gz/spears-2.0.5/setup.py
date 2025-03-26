481be4946dbb4f61197e162a709dc2604677e1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:06:15 +0000

    Add package SPEARS/2.0.5/spears-2.0.5.tar.gz

---
+++ packages/SPEARS/spears-2.0.5.tar.gz/spears-2.0.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+# Read the long description from README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="SPEARS",
+    version="2.0.5",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "SPEARS": ["data/*.json"],
+    },
+    author="Justin Domagala-Tang",
+    url="https://pypi.org/project/SPEARS/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+        "requests",                  # likely required by sparcl
+        "sparclclient",                    # your custom or external dependency
+        "specutils>=1.0",            # for Spectrum1D and find_lines_derivative
+        "astropy>=5.0",              # for units, table, fits, etc.
+        "scipy>=1.7",                # for median_filter
+        "numpy>=1.21",               # for numerical operations
+        "alive-progress",           # for alive_bar
+        "matplotlib>=3.0",          # for plotting
+    ],
+    python_requires=">=3.7",
+)
