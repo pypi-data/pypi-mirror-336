69c443572799830638af01c0a065e825916459c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:06:17 +0000

    Add package tradx/0.8.2/tradx-0.8.2-py3-none-any.whl

---
+++ packages/tradx/tradx-0.8.2-py3-none-any.whl/tradx/baseClass/position.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+
+
+class Position(BaseModel):
+    """
+    A class to represent a financial position.
+    Attributes:
+    -----------
+    ExchangeSegment : str
+        The segment of the exchange where the instrument is traded.
+    ExchangeInstrumentID : int
+        The unique identifier of the instrument on the exchange.
+    ProductType : str
+        The type of the financial product.
+    Quantity : int
+        The quantity of the financial instrument. Default is 0.
+    Methods:
+    --------
+    __init__(self, ExchangeSegment: str, ExchangeInstrumentID: int, ProductType: str, Quantity: int = 0):
+        Constructs all the necessary attributes for the Position object.
+    """
+
+    ExchangeSegment: str
+    ExchangeInstrumentID: int
+    ProductType: str
+    Quantity: int
+
+    def __init__(
+        self,
+        ExchangeSegment: str,
+        ExchangeInstrumentID: int,
+        ProductType: str,
+        Quantity: int = 0,
+    ):
+        assert isinstance(ExchangeSegment, str), "ExchangeSegment must be a string"
+        assert isinstance(
+            ExchangeInstrumentID, int
+        ), "ExchangeInstrumentID must be an integer"
+        assert isinstance(ProductType, str), "ProductType must be a string"
+        assert isinstance(Quantity, int), "Quantity must be an integer"
+        super().__init__(
+            ExchangeSegment=ExchangeSegment,
+            ExchangeInstrumentID=ExchangeInstrumentID,
+            ProductType=ProductType,
+            Quantity=Quantity,
+        )
