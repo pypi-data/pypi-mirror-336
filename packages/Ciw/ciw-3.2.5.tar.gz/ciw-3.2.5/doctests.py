718f7d309ce9dde48a7c1f33781994d31a83b8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:14:50 +0000

    Add package Ciw/3.2.5/ciw-3.2.5.tar.gz

---
+++ packages/Ciw/ciw-3.2.5.tar.gz/ciw-3.2.5/doctests.py
@@ -0,0 +1,22 @@
+import doctest
+import os
+import unittest
+import sys
+
+
+def load_tests(loader, tests, ignorex):
+    for root, dirs, files in os.walk("."):
+        for f in files:
+            if f.endswith(".rst"):
+                tests.addTests(
+                    doctest.DocFileSuite(
+                        os.path.join(root, f), optionflags=doctest.ELLIPSIS
+                    )
+                )
+
+    return tests
+
+
+if __name__ == "__main__":
+    if sys.version_info >= (3, 6):
+        unittest.main()
