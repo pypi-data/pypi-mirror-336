3c687e18bba4b4ddb66f603c21ac7b9b77e7c704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:06:04 +0000

    Add package pyprofqueue/0.3.9a0/pyprofqueue-0.3.9a0.tar.gz

---
+++ packages/pyprofqueue/pyprofqueue-0.3.9a0.tar.gz/pyprofqueue-0.3.9a0/pyprofqueue/__init__.py
@@ -0,0 +1,13 @@
+from .submission import *
+from .script import *
+
+"""
+PyProfQueue.
+
+PyProfQueue serves as a python package that can take in existing bash scripts, add initialisations and calls to use 
+profilers on the bash script and submit the script to an HPC queue system on the users' behalf.
+"""
+
+__version__ = "0.3.9a0"
+__author__ = 'Marcus Keil'
+__credits__ = 'University College London, UK SKA Regional Centre'
