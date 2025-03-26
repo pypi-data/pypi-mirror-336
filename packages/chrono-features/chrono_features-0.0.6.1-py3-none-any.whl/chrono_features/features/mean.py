80610d2dda85f466c838082b0a992bf3d506ae10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:06:20 +0000

    Add package chrono-features/0.0.6.1/chrono_features-0.0.6.1-py3-none-any.whl

---
+++ packages/chrono-features/chrono_features-0.0.6.1-py3-none-any.whl/chrono_features/features/mean.py
@@ -0,0 +1,25 @@
+import numba
+import numpy as np
+
+from chrono_features.features._base import _FromNumbaFuncWithoutCalculatedForEachTSPoint
+from chrono_features.window_type import WindowType
+
+
+class Mean(_FromNumbaFuncWithoutCalculatedForEachTSPoint):
+    def __init__(
+        self,
+        columns: list[str] | str,
+        window_types: list[WindowType] | WindowType,
+        out_column_names: list[str] | str | None = None,
+    ):
+        super().__init__(
+            columns=columns,
+            window_types=window_types,
+            out_column_names=out_column_names,
+            func_name="mean",
+        )
+
+    @staticmethod
+    @numba.njit
+    def _numba_func(xs: np.ndarray) -> np.ndarray:
+        return np.mean(xs)
