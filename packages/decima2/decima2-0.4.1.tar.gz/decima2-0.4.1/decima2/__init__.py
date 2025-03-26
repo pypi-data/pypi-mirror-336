a240eda48814bda71fc0858f7d7df94782bed9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:07:39 +0000

    Add package decima2/0.4.1/decima2-0.4.1.tar.gz

---
+++ packages/decima2/decima2-0.4.1.tar.gz/decima2-0.4.1/decima2/__init__.py
@@ -0,0 +1,5 @@
+# decima2/__init__.py
+from .model.explanation.explanation_generation import model_feature_importance
+from .model.explanation.explanation_generation import grouped_feature_importance
+from .outcome.nlp_explanations.explanation_generation import individual_nlp_explanation
+from .outcome.tabular_explanations.explanation_generation import individual_feature_importance
---
+++ packages/decima2/decima2-0.4.1.tar.gz/decima2-0.4.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+def read_requirements(file_path):
+    """Read the requirements from a requirements.txt file."""
+    with open(file_path) as f:
+        # Strip any whitespace and skip empty lines
+        return [line.strip() for line in f if line and not line.startswith("#")]
+
+# Read the requirements
+requirements = read_requirements('requirements.txt')
+
+setup(
+    name="decima2",
+    version="0.4.1",
+    packages=find_packages(),
+    install_requires=requirements,
+    # Other metadata options
+    author="Torty Sivill",
+    author_email="torty.sivill@decima2.co.uk",
+    description="Evaluation Toolkit for Machine Learning Models",
+    long_description=open('pypi_info.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Decima2/Decima2Toolkit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Corrected classifier
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7, <=3.12.7",
+)
