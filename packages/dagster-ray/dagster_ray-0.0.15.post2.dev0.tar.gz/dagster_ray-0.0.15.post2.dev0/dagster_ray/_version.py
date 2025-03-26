21aec02d0ed494321127fa9c2bc76d6e41bece03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 16:08:54 +0000

    Add package dagster-ray/0.0.15.post2.dev0/dagster_ray-0.0.15.post2.dev0.tar.gz

---
+++ packages/dagster-ray/dagster_ray-0.0.15.post2.dev0.tar.gz/dagster_ray-0.0.15.post2.dev0/dagster_ray/_version.py
@@ -0,0 +1,1 @@
+__version__ = "0.0.15.post2.dev0"  # managed by hatch
---
+++ packages/dagster-ray/dagster_ray-0.0.15.post2.dev0.tar.gz/dagster_ray-0.0.15.post2.dev0/tests/kuberay/test_utils.py
@@ -0,0 +1,21 @@
+from dagster_ray.kuberay.utils import normalize_k8s_label_values
+
+
+def test_normalize_k8s_label_values():
+    assert normalize_k8s_label_values(
+        {
+            "foo": "bar",
+            "my/label": "my/value",
+            "user": "daniel@my.org",
+            "user-dirty": "daniel!`~@my.org",
+            "alphanumeric": "abc123",
+            "long": 64 * "a",
+        }
+    ) == {
+        "foo": "bar",
+        "my/label": "myvalue",
+        "user": "daniel-my-org",
+        "user-dirty": "daniel-my-org",
+        "alphanumeric": "abc123",
+        "long": 63 * "a",
+    }
