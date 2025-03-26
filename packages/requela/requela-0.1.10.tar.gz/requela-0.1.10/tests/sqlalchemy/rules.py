7b01a155af2449a536540aa3f133715f7db15f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:14:47 +0000

    Add package requela/0.1.10/requela-0.1.10.tar.gz

---
+++ packages/requela/requela-0.1.10.tar.gz/requela-0.1.10/tests/sqlalchemy/rules.py
@@ -0,0 +1,49 @@
+from requela.dataclasses import Operator
+from requela.rules import FieldRule, ModelRQLRules, RelationshipRule
+from tests.sqlalchemy.models import Account, Actor, User
+
+
+class NameMixin:
+    name = FieldRule()
+
+
+class ActorRules(NameMixin, ModelRQLRules):
+    __model__ = Actor
+
+
+class TimestampMixin:
+    created_at = FieldRule(
+        alias="events.created.at",
+    )
+
+
+class AuditableMixin(TimestampMixin):
+    created_by = RelationshipRule(
+        alias="events.created.by",
+        rules=ActorRules(),
+    )
+
+
+class AccountRules(ModelRQLRules, AuditableMixin, NameMixin):
+    __model__ = Account
+
+    description = FieldRule()
+    status = FieldRule()
+    balance = FieldRule()
+    datasource_id = FieldRule()
+
+
+class UserRules(ModelRQLRules):
+    __model__ = User
+
+    name = FieldRule()
+    role = FieldRule(
+        allowed_operators=[Operator.IN, Operator.OUT],
+    )
+    is_active = FieldRule()
+    birth_date = FieldRule(
+        alias="events.born.at",
+    )
+    account = RelationshipRule(
+        rules=AccountRules(),
+    )
