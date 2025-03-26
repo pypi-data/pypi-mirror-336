cbeca19ecff5b698c5875cfdb47ab784ab17c8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:05:58 +0000

    Add package opera-utils/0.18.1/opera_utils-0.18.1-py3-none-any.whl

---
+++ packages/opera-utils/opera_utils-0.18.1-py3-none-any.whl/opera_utils/cli.py
@@ -0,0 +1,114 @@
+"""Command-line interface for opera-utils."""
+
+from __future__ import annotations
+
+import json
+import logging
+
+import click
+
+from .burst_frame_db import get_frame_bbox
+
+
+@click.group()
+@click.version_option()
+@click.option("--debug", is_flag=True, default=False)
+@click.pass_context
+def cli_app(ctx, debug):
+    """opera-utils command-line interface."""
+    level = logging.DEBUG if debug else logging.INFO
+    handler = logging.StreamHandler()
+    logging.basicConfig(level=level, handlers=[handler])
+
+
+@cli_app.command()
+@click.argument("frame_id")
+@click.option(
+    "--latlon",
+    "-l",
+    is_flag=True,
+    help="Output the bounding box in latitude/longitude (EPSG:4326)",
+)
+@click.option(
+    "--bounds-only",
+    "-b",
+    is_flag=True,
+    help="Output only (left, bottom, right, top) and omit EPSG",
+)
+def frame_bbox(frame_id, latlon: bool, bounds_only: bool):
+    """Look up the EPSG/bounding box for FRAME_ID.
+
+    Outputs as JSON string to stdout like
+    {"epsg": 32618, "bbox": [157140.0, 4145220.0, 440520.0, 4375770.0]}
+
+    Unless `--bounds-only` is given
+    """
+    epsg, bounds = get_frame_bbox(frame_id=frame_id)
+    if latlon:
+        from opera_utils._helpers import reproject_bounds
+
+        bounds = reproject_bounds(bounds, epsg, 4326)
+    if bounds_only:
+        click.echo(list(bounds))
+    else:
+        obj = dict(epsg=epsg, bbox=bounds)
+        click.echo(json.dumps(obj))
+
+
+@cli_app.command()
+@click.argument("namelist", type=click.File("r"))
+@click.option(
+    "--write-options/--no-write-options",
+    default=True,
+    help="Write out each option to a text file.",
+)
+@click.option(
+    "--output-prefix",
+    type=str,
+    default="option_",
+    help="Prefix for output filenames.",
+    show_default=True,
+)
+@click.option(
+    "--max-options", type=int, default=5, help="Maximum number of options to show"
+)
+def missing_data_options(
+    namelist, write_options: bool, output_prefix: str, max_options: int
+):
+    """Get a list of options for how to handle missing data.
+
+    Prints a table of options to stdout, and writes the subset
+    of files to disk for each option with names like
+
+    option_1_bursts_1234_burst_ids_27_dates_10.txt
+    """
+    from opera_utils import filter_by_burst_id, filter_by_date, get_missing_data_options
+    from opera_utils.missing_data import print_plain, print_with_rich
+
+    file_list = [f.strip() for f in namelist.read().splitlines()]
+    options = get_missing_data_options(file_list)[:max_options]
+
+    try:
+        print_with_rich(options)
+    except ImportError:
+        print_plain(options)
+    if not write_options:
+        return
+
+    # Now filter the files by the burst ids in each, and output to separate
+    for idx, option in enumerate(options, start=1):
+        cur_burst_ids = option.burst_ids
+        # The selected files are those which match the selected date + burst IDs
+        valid_date_files = filter_by_date(file_list, option.dates)
+        valid_files = filter_by_burst_id(valid_date_files, cur_burst_ids)
+
+        cur_output = (
+            f"{output_prefix}{idx}"
+            f"_bursts_{option.total_num_bursts}"
+            f"_burst_ids_{len(cur_burst_ids)}"
+            f"_dates_{option.num_dates}.txt"
+        )
+        click.echo(f"Writing {len(valid_files)} files to {cur_output}")
+        with open(cur_output, "w") as f:
+            f.write("\n".join(valid_files))
+            f.write("\n")
---
+++ packages/opera-utils/opera_utils-0.18.1-py3-none-any.whl/opera_utils/constants.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import re
+
+SPEED_OF_LIGHT = 299_792_458
+
+# Specific to OPERA CSLC products:
+OPERA_DATASET_NAME = "/data/VV"
+OPERA_IDENTIFICATION = "/identification"
+NISAR_BOUNDING_POLYGON = "/science/LSAR/identification/boundingPolygon"
+
+# It should match either or these within a filename:
+# t087_185684_iw2 (which comes from COMPASS)
+# T087-165495-IW3 (which is the official product naming scheme)
+# e.g.
+# OPERA_L2_CSLC-S1_T078-165495-IW3_20190906T232711Z_20230101T100506Z_S1A_VV_v1.0.h5
+
+OPERA_BURST_RE = re.compile(
+    r"[tT](?P<track>\d{3})[-_](?P<burst_id>\d{6})[-_](?P<subswath>iw[1-3])",
+    re.IGNORECASE,
+)
+CSLC_S1_FILE_REGEX = (
+    r"(?P<project>OPERA)_"
+    r"(?P<level>L2)_"
+    r"(?P<product_type>CSLC-S1)_"
+    r"(?P<burst_id>T\d{3}-\d+-IW\d)_"
+    r"(?P<start_datetime>\d{8}T\d{6}Z)_"
+    r"(?P<generation_datetime>\d{8}T\d{6}Z)_"
+    r"(?P<sensor>S1[ABCDE])_"
+    r"(?P<polarization>VV|HH)_"
+    r"v(?P<product_version>\d+\.\d+)"
+)
+
+NISAR_FILE_REGEX = (
+    r"(?P<project>NISAR)_"
+    r"(?P<level>L2)_"
+    r"(?P<product_type>GSLC)_"
+    r"(?P<sensor>NI)_"
+    r"(?P<frame_id>F\d{3})_"
+    r"(?P<start_datetime>\d{8}T\d{6}Z)_"
+    r"(?P<generation_datetime>\d{8}T\d{6}Z)_"
+    r"(?P<sensor_repeat>NI)_"
+    r"(?P<polarization>VV|HH)_"
+    r"v(?P<product_version>\d+\.\d+)"
+)
+# https://github.com/opera-adt/COMPASS/blob/16a3c1da2a5db69b9e2007d798a1110d3a6c5f9f/src/compass/utils/runconfig.py#L316-L318
+# {burst_id_str}_{date_str}
+COMPASS_FILE_REGEX = r"(?P<burst_id>t\d{3}_\d+_iw\d)_(?P<start_datetime>\d{8}).h5"
