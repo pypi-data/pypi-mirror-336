0c68b06987a39b5754e58c7608daae1952e7ad5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:14:59 +0000

    Add package persian-sentiment-analyzer/0.1.1/persian_sentiment_analyzer-0.1.1-py3-none-any.whl

---
+++ packages/persian-sentiment-analyzer/persian_sentiment_analyzer-0.1.1-py3-none-any.whl/persian_sentiment_analyzer/preprocessor.py
@@ -0,0 +1,27 @@
+import re
+from hazm import Normalizer, word_tokenize, Stemmer
+from hazm import stopwords_list
+
+class TextPreprocessor:
+    def __init__(self):
+        self.stopwords = set(stopwords_list())
+        self.normalizer = Normalizer()
+        self.stemmer = Stemmer()
+        self.punctuations = r'[!()-\[\]{};:\'",؟<>./?@#$%^&*_~]'
+        self.numbers_regex = r'[۰-۹\d]+'
+        self.white_space = r'\s+'
+    
+    def preprocess_text(self, text):
+        text = self.normalizer.normalize(str(text))
+        text = re.sub(self.numbers_regex, '', text)
+        text = re.sub(self.punctuations, ' ', text)
+        text = re.sub(self.white_space, ' ', text).strip()
+        
+        tokens = word_tokenize(text)
+        processed_tokens = [
+            self.stemmer.stem(token)
+            for token in tokens
+            if token not in self.stopwords and token.strip()
+        ]
+        
+        return processed_tokens
