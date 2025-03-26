3ae0c57d54dfc2c2301e6fb584a0d01d55f3779a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:07:52 +0000

    Add package apphelpers/0.97.1/apphelpers-0.97.1.tar.gz

---
+++ packages/apphelpers/apphelpers-0.97.1.tar.gz/apphelpers-0.97.1/apphelpers/rest/endpoint.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+
+def login_required(func):
+    """Auth token is required for this endpoint."""
+
+    func.login_required = True
+    return func
+
+
+def any_group_required(*groups):
+    """Any of the mentioned groups is required."""
+
+    def decorator(func):
+        func.any_group_required = set(groups)
+        return func
+
+    return decorator
+
+
+def all_groups_required(*groups):
+    """All of the mentioned groups are required."""
+
+    def decorator(func):
+        func.all_groups_required = set(groups)
+        return func
+
+    return decorator
+
+
+def groups_forbidden(*groups):
+    """None of the mentioned groups are allowed."""
+
+    def decorator(func):
+        func.groups_forbidden = set(groups)
+        return func
+
+    return decorator
+
+
+def authorizer(authorizer):
+    """Custom the authorizer for this endpoint."""
+
+    def decorator(func):
+        func.authorizer = authorizer
+        return func
+
+    return decorator
+
+
+def ignore_site_ctx(func):
+    """Ignore site context for this endpoint."""
+
+    func.ignore_site_ctx = True
+    return func
+
+
+def not_found_on_none(func):
+    """If the return value is None, return HTTP 404 Not Found error."""
+
+    func.not_found_on_none = True
+    return func
+
+
+def response_model(response_model):
+    """[FastAPI only] Set response model for this endpoint."""
+
+    def decorator(func):
+        func.response_model = response_model
+        return func
+
+    return decorator
+
+
+def skip_dbtransaction(func):
+    """Skip database transaction for this endpoint."""
+
+    func.skip_dbtransaction = True
+    return func
