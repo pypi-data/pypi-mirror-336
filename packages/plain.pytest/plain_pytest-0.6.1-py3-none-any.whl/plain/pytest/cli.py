04dd7f713039d41fa9467da784baa54aa63365bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 07:06:30 +0000

    Add package plain.pytest/0.6.1/plain_pytest-0.6.1-py3-none-any.whl

---
+++ packages/plain.pytest/plain_pytest-0.6.1-py3-none-any.whl/plain/pytest/cli.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+import click
+from dotenv import load_dotenv
+
+import pytest
+from plain.cli import register_cli
+
+
+@register_cli("test")
+@click.command(
+    context_settings={
+        "ignore_unknown_options": True,
+    }
+)
+@click.argument("pytest_args", nargs=-1, type=click.UNPROCESSED)
+def cli(pytest_args):
+    """Run tests with pytest"""
+
+    if os.path.exists(".env.test"):
+        click.secho("Loading environment variables from .env.test", fg="yellow")
+        load_dotenv(".env.test")
+
+    returncode = pytest.main(list(pytest_args))
+    if returncode:
+        sys.exit(returncode)
