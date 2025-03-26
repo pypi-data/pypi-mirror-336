5ec487e0fbba2902a2ab6145c6b71edd04878e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:14:51 +0000

    Add package xgrammar/0.1.17/xgrammar-0.1.17-cp39-cp39-win_amd64.whl

---
+++ packages/xgrammar/xgrammar-0.1.17-cp39-cp39-win_amd64.whl/xgrammar/kernels/apply_token_bitmask_inplace_cpu.py
@@ -0,0 +1,36 @@
+"""CPU implementation for in-place applying token mask."""
+
+from typing import List, Optional, Union
+
+import torch
+
+from ..base import _core
+
+
+def apply_token_bitmask_inplace_cpu(
+    logits: torch.Tensor,
+    bitmask: torch.Tensor,
+    indices: Optional[Union[List[int], torch.Tensor]] = None,
+) -> None:
+    """Apply token bitmask in-place on CPU."""
+    if logits.device.type != "cpu":
+        raise ValueError("logits must be on CPU")
+    if bitmask.device.type != "cpu":
+        raise ValueError("bitmask must be on CPU")
+    if logits.dtype != torch.float32:
+        raise ValueError("logits must be of type float32")
+    if bitmask.dtype != torch.int32:
+        raise ValueError("bitmask must be of type int32")
+    if logits.dim() != 1 and logits.dim() != 2:
+        raise ValueError("logits should be 1D or 2D, but got {}D".format(logits.dim()))
+    if bitmask.dim() != 1 and bitmask.dim() != 2:
+        raise ValueError("bitmask should be 1D or 2D, but got {}D".format(bitmask.dim()))
+
+    logits_shape = (1, logits.shape[0]) if logits.dim() == 1 else (logits.shape[0], logits.shape[1])
+    bitmask_shape = (
+        (1, bitmask.shape[0]) if bitmask.dim() == 1 else (bitmask.shape[0], bitmask.shape[1])
+    )
+
+    _core.kernels.apply_token_bitmask_inplace_cpu(
+        logits.data_ptr(), logits_shape, bitmask.data_ptr(), bitmask_shape, indices
+    )
