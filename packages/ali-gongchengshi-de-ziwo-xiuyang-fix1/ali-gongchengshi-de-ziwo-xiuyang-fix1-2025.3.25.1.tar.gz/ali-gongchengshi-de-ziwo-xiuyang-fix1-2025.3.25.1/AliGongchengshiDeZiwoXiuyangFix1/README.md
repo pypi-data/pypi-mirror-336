eed9e6c9aab80d78d343a3e756d75ad36fc9bd4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 04:05:14 +0000

    Add package ali-gongchengshi-de-ziwo-xiuyang-fix1/2025.3.25.1/ali-gongchengshi-de-ziwo-xiuyang-fix1-2025.3.25.1.tar.gz

---
+++ packages/ali-gongchengshi-de-ziwo-xiuyang-fix1/ali-gongchengshi-de-ziwo-xiuyang-fix1-2025.3.25.1.tar.gz/ali-gongchengshi-de-ziwo-xiuyang-fix1-2025.3.25.1/AliGongchengshiDeZiwoXiuyangFix1/README.md
@@ -0,0 +1,27 @@
+# 阿里工程师的自我修养
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/ali-gongchengshi-de-ziwo-xiuyang
+docker run -tid -p <port>:80 apachecn0/ali-gongchengshi-de-ziwo-xiuyang
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install ali-gongchengshi-de-ziwo-xiuyang
+ali-gongchengshi-de-ziwo-xiuyang <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g ali-gongchengshi-de-ziwo-xiuyang
+ali-gongchengshi-de-ziwo-xiuyang <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/ali-gongchengshi-de-ziwo-xiuyang-fix1/ali-gongchengshi-de-ziwo-xiuyang-fix1-2025.3.25.1.tar.gz/ali-gongchengshi-de-ziwo-xiuyang-fix1-2025.3.25.1/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import AliGongchengshiDeZiwoXiuyangFix1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('AliGongchengshiDeZiwoXiuyangFix1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="ali-gongchengshi-de-ziwo-xiuyang-fix1",
+    version=AliGongchengshiDeZiwoXiuyangFix1.__version__,
+    url="https://github.com/apachecn/ali-gongchengshi-de-ziwo-xiuyang-fix1",
+    author=AliGongchengshiDeZiwoXiuyangFix1.__author__,
+    author_email=AliGongchengshiDeZiwoXiuyangFix1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="阿里工程师的自我修养",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "ali-gongchengshi-de-ziwo-xiuyang-fix1=AliGongchengshiDeZiwoXiuyangFix1.__main__:main",
+            "AliGongchengshiDeZiwoXiuyangFix1=AliGongchengshiDeZiwoXiuyangFix1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
