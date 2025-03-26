635065100a357e58d25419199942b33461474409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:15:05 +0000

    Add package guppylang/0.18.0/guppylang-0.18.0.tar.gz

---
+++ packages/guppylang/guppylang-0.18.0.tar.gz/guppylang-0.18.0/tests/integration/test_result.py
@@ -0,0 +1,54 @@
+import pytest
+
+from guppylang.std.builtins import result, nat, array
+from tests.util import compile_guppy
+
+
+def test_basic(validate):
+    @compile_guppy
+    def main(x: int) -> None:
+        result("foo", x)
+
+    validate(main)
+
+
+def test_multi(validate):
+    @compile_guppy
+    def main(w: nat, x: int, y: float, z: bool) -> None:
+        result("a", w)
+        result("b", x)
+        result("c", y)
+        result("d", z)
+
+    validate(main)
+
+
+@pytest.mark.skip("See https://github.com/CQCL/guppylang/issues/631")
+def test_array(validate):
+    @compile_guppy
+    def main(
+        w: array[nat, 42], x: array[int, 5], y: array[float, 1], z: array[bool, 0]
+    ) -> None:
+        result("a", w)
+        result("b", x)
+        result("c", y)
+        result("d", z)
+
+    validate(main)
+
+
+def test_same_tag(validate):
+    @compile_guppy
+    def main(x: int, y: float, z: bool) -> None:
+        result("foo", x)
+        result("foo", y)
+        result("foo", z)
+
+    validate(main)
+
+def test_py_tag(validate):
+    @compile_guppy
+    def main(x: int) -> None:
+        result(py("a" + "b"), x)
+
+    validate(main)
