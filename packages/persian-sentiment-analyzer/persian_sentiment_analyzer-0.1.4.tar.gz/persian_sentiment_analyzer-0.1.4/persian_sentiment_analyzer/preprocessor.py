c9f4767cf79e6d2b58588c57c4af1c5e0579b66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:15:10 +0000

    Add package persian-sentiment-analyzer/0.1.4/persian_sentiment_analyzer-0.1.4.tar.gz

---
+++ packages/persian-sentiment-analyzer/persian_sentiment_analyzer-0.1.4.tar.gz/persian_sentiment_analyzer-0.1.4/persian_sentiment_analyzer/preprocessor.py
@@ -0,0 +1,20 @@
+from hazm import Normalizer, word_tokenize, Stemmer, stopwords_list
+import re
+
+class TextPreprocessor:
+    def __init__(self):
+        self.normalizer = Normalizer()
+        self.stemmer = Stemmer()
+        self.stopwords = set(hazm.stopwords_list())
+    
+    def preprocess_text(self, text):
+        """پردازش کامل متن فارسی"""
+        text = self.normalizer.normalize(str(text))
+        text = re.sub(r'[\d۰-۹]+', '', text)  # حذف اعداد
+        text = re.sub(r'[!()\-\[\]{};:\'"<>?@#$%^&*_~]', ' ', text)  # حذف علائم
+        tokens = word_tokenize(text)
+        return [
+            self.stemmer.stem(token)
+            for token in tokens
+            if token not in self.stopwords and len(token) > 1
+        ]
