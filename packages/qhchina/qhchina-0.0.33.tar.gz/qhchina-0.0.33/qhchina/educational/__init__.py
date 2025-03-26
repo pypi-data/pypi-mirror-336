c2bca952fc43123a7f4ed4aef50b1633d5e1ff45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 16:06:37 +0000

    Add package qhchina/0.0.33/qhchina-0.0.33.tar.gz

---
+++ packages/qhchina/qhchina-0.0.33.tar.gz/qhchina-0.0.33/qhchina/educational/__init__.py
@@ -0,0 +1,15 @@
+"""Educational tools for visualization and learning.
+
+This module provides:
+- Vector visualization tools
+- Educational plotting utilities
+"""
+
+# qhchina/educational/__init__.py
+from .visuals import show_vectors
+
+# Make all functions available at module level
+__all__ = [
+    # Visuals
+    'show_vectors',
+]
