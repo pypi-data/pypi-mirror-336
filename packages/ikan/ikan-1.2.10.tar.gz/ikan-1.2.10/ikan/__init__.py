4d0525083dffec29a76badfc3918b4e751659886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:06:46 +0000

    Add package ikan/1.2.10/ikan-1.2.10.tar.gz

---
+++ packages/ikan/ikan-1.2.10.tar.gz/ikan-1.2.10/ikan/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "1.2.10"
+
+from .KAN import KANLinear, KAN
+from .ChebyKAN import ChebyKANLinear, ChebyKAN
+from .FourierKAN import FourierKANLinear, FourierKAN
+from .JacobiKAN import JacobiKANLinear, JacobiKAN
+from .TaylorKAN import TaylorKANLinear, TaylorKAN
+from .WaveletKAN import WaveletKANLinear, WaveletKAN
+from .GroupKAN import GroupKANLinear, GroupKAN
+__all__ = [
+    "KANLinear",
+    "KAN",
+    "ChebyKANLinear",
+    "ChebyKAN",
+    "FourierKANLinear",
+    "FourierKAN",
+    "JacobiKANLinear",
+    "JacobiKAN",
+    "TaylorKANLinear",
+    "TaylorKAN",
+    "WaveletKANLinear",
+    "WaveletKAN",
+    "GroupKANLinear",
+    "GroupKAN",
+]
