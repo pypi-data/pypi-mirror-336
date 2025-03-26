3ad8569a906c0d008b430edde9c9051c43f27deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:07:41 +0000

    Add package pyloggering/1.0.1/pyloggering-1.0.1-py3-none-any.whl

---
+++ packages/pyloggering/pyloggering-1.0.1-py3-none-any.whl/pyloggering/__init__.py
@@ -0,0 +1,32 @@
+
+'''
+
+Simple module for logging
+
+'''
+
+
+from pyloggering.pyloggering import Utility, Log
+
+
+utility = Utility()
+log = Log(utility)
+
+def create_file(name,location,type):
+
+    utility.create_file(name,location,type)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
