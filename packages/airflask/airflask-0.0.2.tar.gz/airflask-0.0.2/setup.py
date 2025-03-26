bc69fa474d427ca1071ec300b3f5d0cfbb101ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:07:28 +0000

    Add package airflask/0.0.2/airflask-0.0.2.tar.gz

---
+++ packages/airflask/airflask-0.0.2.tar.gz/airflask-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="airflask",  # Change to your package name
+    version="0.0.2",
+    author="Naitik Mundra",
+    author_email="naitikmundra18@gmail.com",
+    description="Simplest way to host your flask web app!",
+    url="https://github.com/naitikmundra/FlaskAir",
+    packages=find_packages(),
+    install_requires=[
+       "flask"
+    ],
+        entry_points={
+        "console_scripts": [
+            "flaskair=airflask.__main__:cli"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
