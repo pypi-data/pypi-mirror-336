1f683ee387f5d45bb6d3148461d6ef76f331f90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:15:10 +0000

    Add package matchescu-base/0.16.0/matchescu_base-0.16.0.tar.gz

---
+++ packages/matchescu-base/matchescu_base-0.16.0.tar.gz/matchescu_base-0.16.0/src/matchescu/typing/__init__.py
@@ -0,0 +1,22 @@
+from matchescu.typing._data import Record, RecordSampler, DataSource, Trait, TRecord
+from matchescu.typing._references import (
+    EntityReferenceIdentifier,
+    EntityReference,
+    EntityReferenceIdFactory,
+    EntityProfile,
+    RecordAdapter,
+)
+
+
+__all__ = [
+    "DataSource",
+    "EntityReference",
+    "EntityReferenceIdentifier",
+    "EntityReferenceIdFactory",
+    "EntityProfile",
+    "Record",
+    "RecordSampler",
+    "RecordAdapter",
+    "Trait",
+    "TRecord",
+]
