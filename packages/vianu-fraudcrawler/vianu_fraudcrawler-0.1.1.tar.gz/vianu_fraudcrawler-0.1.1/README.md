59b0dafdf2d4a24675ba8c5e372996cefa15b7e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:07:14 +0000

    Add package vianu-fraudcrawler/0.1.1/vianu_fraudcrawler-0.1.1.tar.gz

---
+++ packages/vianu-fraudcrawler/vianu_fraudcrawler-0.1.1.tar.gz/vianu_fraudcrawler-0.1.1/README.md
@@ -0,0 +1,5 @@
+# vianu-fraudcrawler
+Intelligent Market Monitoring
+
+## Async Setup
+![Async Setup](docs/assets/images/Fraudcrawler_Async_Setup.svg)
---
+++ packages/vianu-fraudcrawler/vianu_fraudcrawler-0.1.1.tar.gz/vianu_fraudcrawler-0.1.1/fraudcrawler/__init__.py
@@ -0,0 +1,10 @@
+from fraudcrawler.collect.serp import SerpApi
+from fraudcrawler.collect.enrich import Enricher
+from fraudcrawler.collect.zyte import ZyteApi
+
+
+__all__ = [
+    "SerpApi",
+    "Enricher",
+    "ZyteApi",
+]
