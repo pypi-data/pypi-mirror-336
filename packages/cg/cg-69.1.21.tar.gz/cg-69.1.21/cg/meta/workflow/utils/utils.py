4f8d95530d127bc99076d30f7473cb8d31ac4819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 04:05:34 +0000

    Add package cg/69.1.21/cg-69.1.21.tar.gz

---
+++ packages/cg/cg-69.1.21.tar.gz/cg-69.1.21/cg/meta/workflow/utils/utils.py
@@ -0,0 +1,9 @@
+from cg.constants.priority import TrailblazerPriority
+
+MAP_TO_TRAILBLAZER_PRIORITY: dict[int, TrailblazerPriority] = {
+    0: TrailblazerPriority.LOW,
+    1: TrailblazerPriority.NORMAL,
+    2: TrailblazerPriority.HIGH,
+    3: TrailblazerPriority.EXPRESS,
+    4: TrailblazerPriority.NORMAL,
+}
