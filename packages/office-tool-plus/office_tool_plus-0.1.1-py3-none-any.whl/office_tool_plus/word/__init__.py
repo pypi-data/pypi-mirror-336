02123d6ad1ee0d614ef8ee4ecdc803cffb936f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:05:48 +0000

    Add package office-tool-plus/0.1.1/office_tool_plus-0.1.1-py3-none-any.whl

---
+++ packages/office-tool-plus/office_tool_plus-0.1.1-py3-none-any.whl/office_tool_plus/word/__init__.py
@@ -0,0 +1,15 @@
+from .app import WordTools
+
+word = WordTools()
+
+
+def single_to_pdf(word_path, pdf_dir=None):
+    return word.single_to_pdf(word_path, pdf_dir)
+
+
+def many_to_pdf(word_dir, suffix=None, recursive=True, pdf_dir=None):
+    return word.many_to_pdf(word_dir, suffix, recursive, pdf_dir)
+
+
+def from_template(template_file, labor_datas, output_dir=None):
+    return word.from_template(template_file, labor_datas, output_dir)
