fa6090d65e46ed6a40c415bba2baa9766b9e912c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:07:14 +0000

    Add package py-epic/0.1.18/py_epic-0.1.18-py3-none-any.whl

---
+++ packages/py-epic/py_epic-0.1.18-py3-none-any.whl/pyepic/_types.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, TypeVar
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Coroutine
+    from typing import Any, Literal, TypedDict
+
+    from .account import PartialAccount
+    from .auth import AuthSession
+    from .fortnite import SaveTheWorldItem
+    from .route import Route
+
+    URL = Route | str
+
+    Dict = dict[str, Any]
+    List = list[Dict]
+    Json = Dict | List
+
+    DCo = Coroutine[Any, Any, Dict]
+    JCo = Coroutine[Any, Any, Json]
+    NCo = Coroutine[Any, Any, None]
+
+    Attributes = dict[str, Any]
+
+    FriendType = Literal[
+        "friends", "incoming", "outgoing", "suggested", "blocklist"
+    ]
+
+    Listener = Callable[..., Coroutine[..., ..., ...]]
+    ListenerDeco = Callable[[Listener], Listener]
+
+    class PartialCacheEntry(TypedDict):
+        account: PartialAccount
+        expires: float
+
+    STWItemT_co = TypeVar(
+        "STWItemT_co", covariant=True, bound=SaveTheWorldItem
+    )
+
+    AuthT = TypeVar("AuthT", bound=AuthSession)
+    AccountT = TypeVar("AccountT", bound=PartialAccount)
+
+else:
+    AuthT = TypeVar("AuthT", bound="AuthSession")
+    AccountT = TypeVar("AccountT", bound="PartialAccount")
