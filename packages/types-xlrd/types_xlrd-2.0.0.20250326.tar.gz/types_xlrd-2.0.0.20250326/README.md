dcd8a5d05092c0283906b3044359fec51235d6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:05:49 +0000

    Add package types-xlrd/2.0.0.20250326/types_xlrd-2.0.0.20250326.tar.gz

---
+++ packages/types-xlrd/types_xlrd-2.0.0.20250326.tar.gz/types_xlrd-2.0.0.20250326/README.md
@@ -0,0 +1,26 @@
+## Typing stubs for xlrd
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`xlrd`](https://github.com/python-excel/xlrd) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `xlrd`. This version of
+`types-xlrd` aims to provide accurate annotations for
+`xlrd==2.0.*`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/xlrd`](https://github.com/python/typeshed/tree/main/stubs/xlrd)
+directory.
+
+This package was tested with
+mypy 1.15.0,
+pyright 1.1.397,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`4fff7b7d0165120bf79928534c88dffffdcb5da2`](https://github.com/python/typeshed/commit/4fff7b7d0165120bf79928534c88dffffdcb5da2).
---
+++ packages/types-xlrd/types_xlrd-2.0.0.20250326.tar.gz/types_xlrd-2.0.0.20250326/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+name = "types-xlrd"
+description = "Typing stubs for xlrd"
+long_description = '''
+## Typing stubs for xlrd
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`xlrd`](https://github.com/python-excel/xlrd) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `xlrd`. This version of
+`types-xlrd` aims to provide accurate annotations for
+`xlrd==2.0.*`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/xlrd`](https://github.com/python/typeshed/tree/main/stubs/xlrd)
+directory.
+
+This package was tested with
+mypy 1.15.0,
+pyright 1.1.397,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`4fff7b7d0165120bf79928534c88dffffdcb5da2`](https://github.com/python/typeshed/commit/4fff7b7d0165120bf79928534c88dffffdcb5da2).
+'''.lstrip()
+
+setup(name=name,
+      version="2.0.0.20250326",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/xlrd.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['xlrd-stubs'],
+      package_data={'xlrd-stubs': ['__init__.pyi', 'biffh.pyi', 'book.pyi', 'compdoc.pyi', 'formatting.pyi', 'formula.pyi', 'info.pyi', 'sheet.pyi', 'timemachine.pyi', 'xldate.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.9",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
