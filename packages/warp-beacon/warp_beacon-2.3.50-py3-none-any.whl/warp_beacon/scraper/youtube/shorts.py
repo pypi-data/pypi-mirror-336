4748a64de794cc55f1dffa6958f8bc9108e6ace3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 08:05:01 +0000

    Add package warp-beacon/2.3.50/warp_beacon-2.3.50-py3-none-any.whl

---
+++ packages/warp-beacon/warp_beacon-2.3.50-py3-none-any.whl/warp_beacon/scraper/youtube/shorts.py
@@ -0,0 +1,45 @@
+import logging
+
+from warp_beacon.jobs.types import JobType
+from warp_beacon.scraper.youtube.abstract import YoutubeAbstract
+from warp_beacon.scraper.exceptions import NotFound
+
+from warp_beacon.mediainfo.video import VideoInfo
+
+class YoutubeShortsScraper(YoutubeAbstract):
+	YT_MAX_RETRIES_DEFAULT = 8
+	YT_PAUSE_BEFORE_RETRY_DEFAULT = 3
+	YT_TIMEOUT_DEFAULT = 2
+	YT_TIMEOUT_INCREMENT_DEFAULT = 60
+
+	def _download(self, url: str, session: bool = True, timeout: int = 0) -> list:
+		res = []
+		thumbnail = None
+		yt = self.build_yt(url, session=session)
+		stream = yt.streams.get_highest_resolution()
+
+		if not stream:
+			raise NotFound("No suitable video stream found")
+
+		local_file = stream.download(
+			output_path=self.DOWNLOAD_DIR,
+			max_retries=0,
+			timeout=timeout,
+			skip_existing=False,
+			filename_prefix="yt_download_"
+		)
+
+		local_file = self.rename_local_file(local_file)
+		vinfo = VideoInfo(local_file)
+		thumbnail = self.download_hndlr(self.download_thumbnail, video_id=yt.video_id, crop_center=vinfo.get_demensions())
+
+		logging.debug("Temp filename: '%s'", local_file)
+		res.append({
+			"local_media_path": local_file,
+			"performer": yt.author,
+			"thumb": thumbnail,
+			"canonical_name": stream.title,
+			"media_type": JobType.VIDEO
+		})
+
+		return res
