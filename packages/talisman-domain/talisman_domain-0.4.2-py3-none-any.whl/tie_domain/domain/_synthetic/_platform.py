47080b04f314fc2d8cfcb44b10f91c2a19cf92eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:15:22 +0000

    Add package talisman-domain/0.4.2/talisman_domain-0.4.2-py3-none-any.whl

---
+++ packages/talisman-domain/talisman_domain-0.4.2-py3-none-any.whl/tie_domain/domain/_synthetic/_platform.py
@@ -0,0 +1,48 @@
+from tdm.datamodel.domain import AccountType, PlatformType
+from tdm.datamodel.values import StringValue
+
+from tp_interfaces.domain.model.types import AtomValueType, IdentifyingPropertyType, PropertyType, RelationType
+
+_PLATFORM_TYPE_ENUM = (
+    'government',
+    'fileStorage',
+    'database',
+    'procurement',
+    'review',
+    'forum',
+    'newsAggregator',
+    'media',
+    'blog',
+    'messenger',
+    'socialNetwork'
+)
+
+# VALUES
+
+PLATFORM_VALUE = AtomValueType("platform_value", StringValue, id='platform_value', value_restriction=_PLATFORM_TYPE_ENUM)
+STR_VALUE = AtomValueType("str", StringValue, id='str')
+
+# CONCEPTS
+PLATFORM = PlatformType('platform', id='platform')
+ACCOUNT = AccountType('account', id='account')
+
+# RELATIONS
+ACCOUNT_PLATFORM = RelationType("account_platform", ACCOUNT, PLATFORM, id="account_platform")
+
+# PROPERTIES
+ACCOUNT_KEY = IdentifyingPropertyType("Название", ACCOUNT, STR_VALUE, id="account_key")
+ACCOUNT_NAME = PropertyType("account_name", ACCOUNT, STR_VALUE, id="account_name")
+ACCOUNT_URL = PropertyType("account_url", ACCOUNT, STR_VALUE, id="account_url")
+
+PLATFORM_KEY = IdentifyingPropertyType("Название", PLATFORM, STR_VALUE, id="platform_key")
+PLATFORM_NAME = PropertyType("platform_name", PLATFORM, STR_VALUE, id="platform_name")
+PLATFORM_URL = PropertyType("platform_url", PLATFORM, STR_VALUE, id="platform_url")
+PLATFORM_TYPE = PropertyType("platform_type", PLATFORM, PLATFORM_VALUE, id="platform_type")
+
+TYPES = (
+    PLATFORM_VALUE, STR_VALUE,
+    PLATFORM, ACCOUNT,
+    ACCOUNT_PLATFORM,
+    ACCOUNT_KEY, ACCOUNT_NAME, ACCOUNT_URL,
+    PLATFORM_KEY, PLATFORM_NAME, PLATFORM_URL, PLATFORM_TYPE
+)
