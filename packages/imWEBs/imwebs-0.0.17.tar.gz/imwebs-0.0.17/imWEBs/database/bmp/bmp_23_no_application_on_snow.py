e719c880b77e5998b909c284068aadff3100a7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:06:07 +0000

    Add package imWEBs/0.0.17/imwebs-0.0.17.tar.gz

---
+++ packages/imWEBs/imwebs-0.0.17.tar.gz/imwebs-0.0.17/imWEBs/database/bmp/bmp_23_no_application_on_snow.py
@@ -0,0 +1,6 @@
+from .bmp_management_base import BMPManagementBase
+
+class ManureNoApplicaitonOnSnowManagement(BMPManagementBase):
+    """Distribution Table for BMP: No application on snow (23)"""
+    def __init__(self):
+        super().__init__()
---
+++ packages/imWEBs/imwebs-0.0.17.tar.gz/imwebs-0.0.17/imWEBs/database/bmp/bmp_24_spring_application_rather_than_fall_application.py
@@ -0,0 +1,6 @@
+from .bmp_management_base import BMPManagementBase
+
+class ManureSpringApplicationRatherThanFallApplicationManagement(BMPManagementBase):
+    """Distribution Table for BMP: Spring application rather than fall application (24)"""
+    def __init__(self):
+        super().__init__()
