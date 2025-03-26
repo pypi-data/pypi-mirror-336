7e7c5061b04108f031e71c53a477620057877797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:06:04 +0000

    Add package hotpod-cli/0.1.0/hotpod_cli-0.1.0.tar.gz

---
+++ packages/hotpod-cli/hotpod_cli-0.1.0.tar.gz/hotpod_cli-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hotpod-cli",
+    version="0.1.0",
+    author="JDCloud AIDC Team",
+    author_email="your.email@example.com",
+    description="A command line tool for GCS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/hotpod-cli",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "click>=7.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "hotpod=gcs_cli.cli:cli",
+        ],
+    },
+)
