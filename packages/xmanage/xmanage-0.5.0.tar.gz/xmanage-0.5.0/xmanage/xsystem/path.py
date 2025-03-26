8be71ef9e0efdc9781e4ac520b2e9de75059d97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:06:48 +0000

    Add package xmanage/0.5.0/xmanage-0.5.0.tar.gz

---
+++ packages/xmanage/xmanage-0.5.0.tar.gz/xmanage-0.5.0/xmanage/xsystem/path.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+
+from tabulate import tabulate
+from xkits import add_command
+from xkits import argp
+from xkits import commands
+from xkits import run_command
+
+try:
+    from xmanage.systemd import systemd_path
+
+    @add_command("path", help="show system and user paths")
+    def add_cmd_path(_arg: argp):
+        _arg.add_argument(dest="sd_path_titles", type=str, nargs="*",
+                          default=[], action="extend", metavar="NAME",
+                          choices=systemd_path.titles + [[]],
+                          help="the path value with this name is shown")
+
+    @run_command(add_cmd_path)
+    def run_cmd_path(cmds: commands) -> int:
+        if not cmds.has_sub(add_cmd_path):
+            titles = cmds.args.sd_path_titles
+            values = titles if len(titles) > 0 else systemd_path
+            tabular = [(n, "\n".join(systemd_path[n])) for n in values]
+            tstring = tabulate(tabular, headers=["name", "paths"])
+            cmds.stdout(tstring)
+        return 0
+except Exception:  # pragma: no cover
+    pass  # pragma: no cover
