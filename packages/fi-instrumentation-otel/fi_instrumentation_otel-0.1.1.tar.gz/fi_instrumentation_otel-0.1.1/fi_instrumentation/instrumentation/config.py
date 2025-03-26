c5c046deb253c917a4dc901c1b0342e44a235e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:05:11 +0000

    Add package fi-instrumentation-otel/0.1.1/fi_instrumentation_otel-0.1.1.tar.gz

---
+++ packages/fi-instrumentation-otel/fi_instrumentation_otel-0.1.1.tar.gz/fi_instrumentation_otel-0.1.1/fi_instrumentation/instrumentation/config.py
@@ -0,0 +1,419 @@
+import os
+from contextlib import contextmanager
+from dataclasses import dataclass, field, fields
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterator,
+    Optional,
+    Sequence,
+    Union,
+    cast,
+    get_args,
+)
+
+import wrapt
+from fi_instrumentation.fi_types import (
+    EmbeddingAttributes,
+    ImageAttributes,
+    MessageAttributes,
+    MessageContentAttributes,
+    SpanAttributes,
+)
+from fi_instrumentation.instrumentation.logging import logger
+from fi_instrumentation.settings import UuidIdGenerator
+from opentelemetry.context import (
+    _SUPPRESS_INSTRUMENTATION_KEY,
+    Context,
+    attach,
+    detach,
+    set_value,
+)
+from opentelemetry.sdk.trace import IdGenerator
+from opentelemetry.trace import Link, Span, SpanKind, Tracer, use_span
+from opentelemetry.util.types import Attributes, AttributeValue
+
+
+class suppress_tracing:
+    """
+    Context manager to pause OpenTelemetry instrumentation.
+
+    Examples:
+        with suppress_tracing():
+            # No tracing will occur within this block
+            ...
+    """
+
+    def __enter__(self) -> "suppress_tracing":
+        self._token = attach(set_value(_SUPPRESS_INSTRUMENTATION_KEY, True))
+        return self
+
+    def __aenter__(self) -> "suppress_tracing":
+        self._token = attach(set_value(_SUPPRESS_INSTRUMENTATION_KEY, True))
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        detach(self._token)
+
+    def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        detach(self._token)
+
+
+FI_HIDE_INPUTS = "FI_HIDE_INPUTS"
+# Hides input value & messages
+FI_HIDE_OUTPUTS = "FI_HIDE_OUTPUTS"
+# Hides output value & messages
+FI_HIDE_INPUT_MESSAGES = "FI_HIDE_INPUT_MESSAGES"
+# Hides all input messages
+FI_HIDE_OUTPUT_MESSAGES = "FI_HIDE_OUTPUT_MESSAGES"
+# Hides all output messages
+FI_HIDE_INPUT_IMAGES = "FI_HIDE_INPUT_IMAGES"
+# Hides images from input messages
+FI_HIDE_INPUT_TEXT = "FI_HIDE_INPUT_TEXT"
+# Hides text from input messages
+FI_HIDE_OUTPUT_TEXT = "FI_HIDE_OUTPUT_TEXT"
+# Hides text from output messages
+FI_HIDE_EMBEDDING_VECTORS = "FI_HIDE_EMBEDDING_VECTORS"
+# Hides embedding vectors
+FI_BASE64_IMAGE_MAX_LENGTH = "FI_BASE64_IMAGE_MAX_LENGTH"
+# Limits characters of a base64 encoding of an image
+REDACTED_VALUE = "__REDACTED__"
+# When a value is hidden, it will be replaced by this redacted value
+
+DEFAULT_HIDE_INPUTS = False
+DEFAULT_HIDE_OUTPUTS = False
+
+DEFAULT_HIDE_INPUT_MESSAGES = False
+DEFAULT_HIDE_OUTPUT_MESSAGES = False
+
+DEFAULT_HIDE_INPUT_IMAGES = False
+DEFAULT_HIDE_INPUT_TEXT = False
+DEFAULT_HIDE_OUTPUT_TEXT = False
+
+DEFAULT_HIDE_EMBEDDING_VECTORS = False
+DEFAULT_BASE64_IMAGE_MAX_LENGTH = 32_000
+
+
+@dataclass(frozen=True)
+class TraceConfig:
+    """
+    TraceConfig helps you modify the observability level of your tracing.
+    For instance, you may want to keep sensitive information from being logged
+    for security reasons, or you may want to limit the size of the base64
+    encoded images to reduce payloads.
+
+    For those attributes not passed, this object tries to read from designated
+    environment variables and, if not found, has default values that maximize
+    observability.
+    """
+
+    hide_inputs: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": FI_HIDE_INPUTS,
+            "default_value": DEFAULT_HIDE_INPUTS,
+        },
+    )
+    """Hides input value & messages"""
+    hide_outputs: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": FI_HIDE_OUTPUTS,
+            "default_value": DEFAULT_HIDE_OUTPUTS,
+        },
+    )
+    """Hides output value & messages"""
+    hide_input_messages: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": FI_HIDE_INPUT_MESSAGES,
+            "default_value": DEFAULT_HIDE_INPUT_MESSAGES,
+        },
+    )
+    """Hides all input messages"""
+    hide_output_messages: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": FI_HIDE_OUTPUT_MESSAGES,
+            "default_value": DEFAULT_HIDE_OUTPUT_MESSAGES,
+        },
+    )
+    """Hides all output messages"""
+    hide_input_images: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": FI_HIDE_INPUT_IMAGES,
+            "default_value": DEFAULT_HIDE_INPUT_IMAGES,
+        },
+    )
+    """Hides images from input messages"""
+    hide_input_text: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": FI_HIDE_INPUT_TEXT,
+            "default_value": DEFAULT_HIDE_INPUT_TEXT,
+        },
+    )
+    """Hides text from input messages"""
+    hide_output_text: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": FI_HIDE_OUTPUT_TEXT,
+            "default_value": DEFAULT_HIDE_OUTPUT_TEXT,
+        },
+    )
+    """Hides text from output messages"""
+    hide_embedding_vectors: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": FI_HIDE_EMBEDDING_VECTORS,
+            "default_value": DEFAULT_HIDE_EMBEDDING_VECTORS,
+        },
+    )
+    """Hides embedding vectors"""
+    base64_image_max_length: Optional[int] = field(
+        default=None,
+        metadata={
+            "env_var": FI_BASE64_IMAGE_MAX_LENGTH,
+            "default_value": DEFAULT_BASE64_IMAGE_MAX_LENGTH,
+        },
+    )
+    """Limits characters of a base64 encoding of an image"""
+
+    def __post_init__(self) -> None:
+        for f in fields(self):
+            expected_type = get_args(f.type)[0]
+            # Optional is Union[T,NoneType]. get_args()returns (T, NoneType).
+            # We collect the first type
+            self._parse_value(
+                f.name,
+                expected_type,
+                f.metadata["env_var"],
+                f.metadata["default_value"],
+            )
+
+    def mask(
+        self,
+        key: str,
+        value: Union[AttributeValue, Callable[[], AttributeValue]],
+    ) -> Optional[AttributeValue]:
+        if self.hide_inputs and key == SpanAttributes.INPUT_VALUE:
+            value = REDACTED_VALUE
+        elif self.hide_inputs and key == SpanAttributes.INPUT_MIME_TYPE:
+            return
+        elif self.hide_outputs and key == SpanAttributes.OUTPUT_VALUE:
+            value = REDACTED_VALUE
+        elif self.hide_outputs and key == SpanAttributes.OUTPUT_MIME_TYPE:
+            return
+        elif (
+            self.hide_inputs or self.hide_input_messages
+        ) and SpanAttributes.LLM_INPUT_MESSAGES in key:
+            return
+        elif (
+            self.hide_outputs or self.hide_output_messages
+        ) and SpanAttributes.LLM_OUTPUT_MESSAGES in key:
+            return
+        elif (
+            self.hide_input_text
+            and SpanAttributes.LLM_INPUT_MESSAGES in key
+            and MessageAttributes.MESSAGE_CONTENT in key
+            and MessageAttributes.MESSAGE_CONTENTS not in key
+        ):
+            value = REDACTED_VALUE
+        elif (
+            self.hide_output_text
+            and SpanAttributes.LLM_OUTPUT_MESSAGES in key
+            and MessageAttributes.MESSAGE_CONTENT in key
+            and MessageAttributes.MESSAGE_CONTENTS not in key
+        ):
+            value = REDACTED_VALUE
+        elif (
+            self.hide_input_text
+            and SpanAttributes.LLM_INPUT_MESSAGES in key
+            and MessageContentAttributes.MESSAGE_CONTENT_TEXT in key
+        ):
+            value = REDACTED_VALUE
+        elif (
+            self.hide_output_text
+            and SpanAttributes.LLM_OUTPUT_MESSAGES in key
+            and MessageContentAttributes.MESSAGE_CONTENT_TEXT in key
+        ):
+            value = REDACTED_VALUE
+        elif (
+            self.hide_input_images
+            and SpanAttributes.LLM_INPUT_MESSAGES in key
+            and MessageContentAttributes.MESSAGE_CONTENT_IMAGE in key
+        ):
+            return
+        elif (
+            is_base64_url(value)  # type:ignore
+            and len(value) > self.base64_image_max_length  # type:ignore
+            and SpanAttributes.LLM_INPUT_MESSAGES in key
+            and MessageContentAttributes.MESSAGE_CONTENT_IMAGE in key
+            and key.endswith(ImageAttributes.IMAGE_URL)
+        ):
+            value = REDACTED_VALUE
+        elif (
+            self.hide_embedding_vectors
+            and SpanAttributes.EMBEDDING_EMBEDDINGS in key
+            and EmbeddingAttributes.EMBEDDING_VECTOR in key
+        ):
+            return
+        return value() if callable(value) else value
+
+    def _parse_value(
+        self,
+        field_name: str,
+        expected_type: Any,
+        env_var: str,
+        default_value: Any,
+    ) -> None:
+        type_name = expected_type.__name__
+        init_value = getattr(self, field_name, None)
+        if init_value is None:
+            env_value = os.getenv(env_var)
+            if env_value is None:
+                object.__setattr__(self, field_name, default_value)
+            else:
+                try:
+                    env_value = self._cast_value(env_value, expected_type)
+                    object.__setattr__(self, field_name, env_value)
+                except Exception:
+                    logger.warning(
+                        f"Could not parse '{env_value}' to {type_name} "
+                        f"for the environment variable '{env_var}'. "
+                        f"Using default value instead: {default_value}."
+                    )
+                    object.__setattr__(self, field_name, default_value)
+        else:
+            if not isinstance(init_value, expected_type):
+                raise TypeError(
+                    f"The field {field_name} must be of type '{type_name}' "
+                    f"but '{type(init_value).__name__}' was found."
+                )
+
+    def _cast_value(
+        self,
+        value: Any,
+        cast_to: Any,
+    ) -> None:
+        if cast_to is bool:
+            if isinstance(value, str) and value.lower() == "true":
+                return True
+            if isinstance(value, str) and value.lower() == "false":
+                return False
+            raise
+        else:
+            return cast_to(value)
+
+
+_IMPORTANT_ATTRIBUTES = [
+    SpanAttributes.FI_SPAN_KIND,
+]
+
+
+class _WrappedSpan(wrapt.ObjectProxy):  # type: ignore[misc]z
+    def __init__(self, wrapped: Span, config: TraceConfig) -> None:
+        super().__init__(wrapped)
+        self._self_config = config
+        self._self_important_attributes: Dict[str, AttributeValue] = {}
+
+    def set_attributes(self, attributes: Dict[str, AttributeValue]) -> None:
+        for k, v in attributes.items():
+            self.set_attribute(k, v)
+
+    def set_attribute(
+        self,
+        key: str,
+        value: Union[AttributeValue, Callable[[], AttributeValue]],
+    ) -> None:
+        value = self._self_config.mask(key, value)
+        if value is not None:
+            if key in _IMPORTANT_ATTRIBUTES:
+                self._self_important_attributes[key] = value
+            else:
+                span = cast(Span, self.__wrapped__)
+                span.set_attribute(key, value)
+
+    def end(self, end_time: Optional[int] = None) -> None:
+        span = cast(Span, self.__wrapped__)
+        for k, v in reversed(self._self_important_attributes.items()):
+            span.set_attribute(k, v)
+        span.end(end_time)
+
+
+class FITracer(wrapt.ObjectProxy):  # type: ignore[misc]
+    def __init__(self, wrapped: Tracer, config: TraceConfig) -> None:
+        super().__init__(wrapped)
+        self._self_config = config
+        self._self_id_generator = UuidIdGenerator()
+
+    @property
+    def id_generator(self) -> IdGenerator:
+        return self._self_id_generator
+
+    @contextmanager
+    def start_as_current_span(
+        self,
+        name: str,
+        context: Optional[Context] = None,
+        kind: SpanKind = SpanKind.INTERNAL,
+        attributes: Attributes = None,
+        links: Optional[Sequence[Link]] = (),
+        start_time: Optional[int] = None,
+        record_exception: bool = True,
+        set_status_on_exception: bool = True,
+        end_on_exit: bool = True,
+    ) -> Iterator[Span]:
+        span = self.start_span(
+            name=name,
+            context=context,
+            kind=kind,
+            attributes=attributes,
+            links=links,
+            start_time=start_time,
+            record_exception=record_exception,
+            set_status_on_exception=set_status_on_exception,
+        )
+        with use_span(
+            span,
+            end_on_exit=end_on_exit,
+            record_exception=record_exception,
+            set_status_on_exception=set_status_on_exception,
+        ) as span:
+            yield span
+
+    def start_span(
+        self,
+        name: str,
+        context: Optional[Context] = None,
+        kind: SpanKind = SpanKind.INTERNAL,
+        attributes: Attributes = None,
+        links: Optional[Sequence[Link]] = (),
+        start_time: Optional[int] = None,
+        record_exception: bool = True,
+        set_status_on_exception: bool = True,
+    ) -> Span:
+        tracer = cast(Tracer, self.__wrapped__)
+        span = tracer.__class__.start_span(
+            self,
+            name=name,
+            context=context,
+            kind=kind,
+            attributes=None,
+            links=links,
+            start_time=start_time,
+            record_exception=record_exception,
+            set_status_on_exception=set_status_on_exception,
+        )
+        span = _WrappedSpan(span, config=self._self_config)
+        if attributes:
+            span.set_attributes(attributes)
+        return span
+
+
+def is_base64_url(url: str) -> bool:
+    if not isinstance(url, str):
+        return False
+    return url.startswith("data:image/") and "base64" in url
