9d2ac9bd430908bba37479c748a41c141bacd9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:05:26 +0000

    Add package pymusiclooper/3.5.1/pymusiclooper-3.5.1-py3-none-any.whl

---
+++ packages/pymusiclooper/pymusiclooper-3.5.1-py3-none-any.whl/pymusiclooper/__main__.py
@@ -0,0 +1,14 @@
+import logging
+
+from pymusiclooper.cli import cli_main
+
+
+def cli():
+    try:
+        cli_main(prog_name="pymusiclooper")
+    except Exception as e:
+        logging.error(e)
+
+
+if __name__ == "__main__":
+    cli()
