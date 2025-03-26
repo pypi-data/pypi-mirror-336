e7057ecb52177f08ad8accbcd92897ceb25c4cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 08:05:04 +0000

    Add package pipen-report/0.23.2/pipen_report-0.23.2-py3-none-any.whl

---
+++ packages/pipen-report/pipen_report-0.23.2-py3-none-any.whl/pipen_report/frontend/src/components/index.js
@@ -0,0 +1,5 @@
+export { Image } from "./Image";
+export { DataTable } from "./DataTable";
+export { default as Descr } from "./Descr.svelte";
+export { default as Iframe } from "./Iframe.svelte";
+export { default as Plotly } from "./Plotly.svelte";
