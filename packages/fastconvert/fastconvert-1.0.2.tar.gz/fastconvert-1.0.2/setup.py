4bdec7d2537f5ae11eba0896075c5411c2f76983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 07:06:40 +0000

    Add package fastconvert/1.0.2/fastconvert-1.0.2.tar.gz

---
+++ packages/fastconvert/fastconvert-1.0.2.tar.gz/fastconvert-1.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fastconvert',
+    version='1.0.2',
+    description='A powerful CLI tool for converting files between different formats.',
+    long_description=open('README_PYPI.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='qwez',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'Pillow',
+        'python-magic-bin',
+        'opencv-python'
+    ],
+    entry_points={
+        'console_scripts': [
+            'fastconvert=fastconvert:convert'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
