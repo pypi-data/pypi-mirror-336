d47e74a31f017691e5a1e15f41d852025e767be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 03:14:43 +0000

    Add package tg-bot-screen/1.4.6/tg_bot_screen-1.4.6.tar.gz

---
+++ packages/tg-bot-screen/tg_bot_screen-1.4.6.tar.gz/tg_bot_screen-1.4.6/tg_bot_screen/ptb/bot_manager.py
@@ -0,0 +1,31 @@
+from telegram import Update
+from telegram import Message as TgMessage
+from telegram.ext import Application, CallbackQueryHandler, MessageHandler
+from ..user_data import UserDataManager
+from ..bot_manager import BotManager as BaseBotManager
+from .user_screen import UserScreen
+
+class BotManager(BaseBotManager):
+    def __init__(self, application: Application):
+        super().__init__()
+        self.bot = application.bot
+        self.application = application
+    
+    def get_callback_query_handler(self):
+        async def callback(update: Update, _):
+            user_id = update.callback_query.from_user.id
+            query_data = update.callback_query.data
+            await self._handle_callback_query(user_id, query_data)
+            await update.callback_query.answer()
+        return CallbackQueryHandler(callback)
+
+    def get_message_handler(self):
+        async def callback(update: Update, _):
+            user_id = update.message.from_user.id
+            await self._handle_message(user_id, update=update
+                , message=update.message)
+            
+        return MessageHandler(None, callback)
+    
+    async def delete_message(self, message: TgMessage, **kwargs):
+        await message.delete()
