9fa2e295e50a960a08ed4b38947dce19afce7198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 07:06:59 +0000

    Add package vencordthemer-simplecli/0.1.0/vencordthemer_simplecli-0.1.0.tar.gz

---
+++ packages/vencordthemer-simplecli/vencordthemer_simplecli-0.1.0.tar.gz/vencordthemer_simplecli-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vencordthemer-simplecli",
+    version="0.1.0",
+    author="VencordThemer",
+    author_email="vencordthemer@users.noreply.github.com",
+    description="A simple framework for building interactive CLI menus in Python",
+    packages=["cli"],
+    python_requires=">=3.6",
+)
