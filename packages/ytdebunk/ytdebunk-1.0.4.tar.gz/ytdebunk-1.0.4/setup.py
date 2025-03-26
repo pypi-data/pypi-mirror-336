b4e468fceb7c48f9d02233568b582c622ba037c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 07:07:05 +0000

    Add package ytdebunk/1.0.4/ytdebunk-1.0.4.tar.gz

---
+++ packages/ytdebunk/ytdebunk-1.0.4.tar.gz/ytdebunk-1.0.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ytdebunk",
+    version="1.0.4",
+    packages=find_packages(),
+    install_requires=[
+        "numpy<2",
+        "python-dotenv==1.0.1",
+        "google-generativeai==0.8.4",
+        "yt-dlp==2025.3.21",
+        "torch==2.1.0",
+        "torchaudio==2.1.0",
+        "librosa==0.11.0",
+        "transformers==4.36.2",
+        "streamlit",
+    ],
+    entry_points={"console_scripts": ["ytdebunk=ytdebunk.ytdebunk:main"]},
+    author="Md. Sazzad Hissain Khan",
+    author_email='hissain.khan@gmail.com',
+    description="A CLI tool to download audio from a YouTube video, transcribe it, and refine the transcription using AI.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hissain/youtuber-debunked',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords=[
+        "youtube", 
+        "transcription", 
+        "audio", 
+        "refinement", 
+        "ai", 
+        "bangla", 
+        "bengali", 
+        "geminai",
+        "librosa",
+        "streamlit",
+        ],
+)
---
+++ packages/ytdebunk/ytdebunk-1.0.4.tar.gz/ytdebunk-1.0.4/ytdebunk/settings.py
@@ -0,0 +1,7 @@
+OUTPUT_DIRECTORY = 'output'
+AUDIO_FILE = OUTPUT_DIRECTORY + '/audio.mp3'
+VIDEO_FILE = OUTPUT_DIRECTORY + '/video.mp4'
+TRANSCRIPTION_FILE = OUTPUT_DIRECTORY + '/transcription.txt'
+REFINED_TRANSCRIPTION_FILE = OUTPUT_DIRECTORY + '/refined_transcription.txt'
+LOGICAL_FAULTS_FILE = OUTPUT_DIRECTORY + '/logical_faults.txt'
+TRANSCRIPTION_MODEL = 'bangla-speech-processing/BanglaASR'
