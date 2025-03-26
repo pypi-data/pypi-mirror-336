3c98276ca96870f90986a83c57d421d879c9b5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:06:19 +0000

    Add package meds-torch-data/0.4/meds_torch_data-0.4.tar.gz

---
+++ packages/meds-torch-data/meds_torch_data-0.4.tar.gz/meds_torch_data-0.4/src/meds_torchdata/__init__.py
@@ -0,0 +1,15 @@
+from importlib.metadata import PackageNotFoundError, version
+
+__package_name__ = "meds_torchdata"
+
+try:
+    __version__ = version(__package_name__)
+except PackageNotFoundError:  # pragma: no cover
+    # package is not installed
+    __version__ = "unknown"
+
+from .config import MEDSTorchDataConfig  # noqa: F401
+from .pytorch_dataset import MEDSPytorchDataset  # noqa: F401
+from .types import MEDSTorchBatch  # noqa: F401
+
+__all__ = ["MEDSPytorchDataset", "MEDSTorchDataConfig", "MEDSTorchBatch"]
