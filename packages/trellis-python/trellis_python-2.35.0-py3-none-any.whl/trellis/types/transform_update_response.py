c5318d766579fc255e1a1814d950955c2fad80e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:05:41 +0000

    Add package trellis-python/2.35.0/trellis_python-2.35.0-py3-none-any.whl

---
+++ packages/trellis-python/trellis_python-2.35.0-py3-none-any.whl/trellis/types/transform_update_response.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["TransformUpdateResponse", "Data"]
+
+
+class Data(BaseModel):
+    status: Literal["running", "failed", "completed", "not_started"]
+    """An enumeration."""
+
+    transform_id: str
+
+
+class TransformUpdateResponse(BaseModel):
+    data: Data
+
+    message: str
