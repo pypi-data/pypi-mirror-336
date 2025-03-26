e24fa46244917fbf542fa3d9c32ae9f0e7573e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:05:23 +0000

    Add package sltkpy/0.1.0b1/sltkpy-0.1.0b1.tar.gz

---
+++ packages/sltkpy/sltkpy-0.1.0b1.tar.gz/sltkpy-0.1.0b1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0-beta-1' 
+DESCRIPTION = 'Sinhala Language Tool Kit'
+LONG_DESCRIPTION = open("README.md", 'r', encoding="utf-8").read()
+
+# Setting up
+
+# python setup.py sdist bdist_wheel
+# python -m twine upload dist/*
+
+setup(
+        name="sltkpy", 
+        version=VERSION,
+        author="Buddhi Kavindra Ranasinghe",
+        author_email="info@buddhilive.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        packages=find_packages(exclude=['tests']),
+        install_requires=['regex'],
+        package_data={"sltkpy": ["*.txt", "*.json"]},
+        include_package_data=True,
+        data_files=[('shared', ['sltkpy/models/vocab.json'])],
+        keywords=['python', 'Sinhala Tokenizer'],
+        license='MIT',
+        url='https://github.com/buddhilive/sltkpy',
+        classifiers= [
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Education",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: Text Processing :: Linguistic",
+        ]
+)
