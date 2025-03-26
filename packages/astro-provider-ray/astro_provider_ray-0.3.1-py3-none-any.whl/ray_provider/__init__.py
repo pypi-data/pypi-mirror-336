04e5979b362006375332e61e9d0cc19d2a99a432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:05:30 +0000

    Add package astro-provider-ray/0.3.1/astro_provider_ray-0.3.1-py3-none-any.whl

---
+++ packages/astro-provider-ray/astro_provider_ray-0.3.1-py3-none-any.whl/ray_provider/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+__version__ = "0.3.1"
+
+from typing import Any
+
+
+# This is needed to allow Airflow to pick up specific metadata fields it needs for certain features.
+def get_provider_info() -> dict[str, Any]:
+    return {
+        "package-name": "astro-provider-ray",  # Required
+        "name": "Ray",  # Required
+        "description": "An integration between airflow and ray",  # Required
+        "connection-types": [{"connection-type": "ray", "hook-class-name": "ray_provider.hooks.RayHook"}],
+        "versions": [__version__],  # Required
+    }
