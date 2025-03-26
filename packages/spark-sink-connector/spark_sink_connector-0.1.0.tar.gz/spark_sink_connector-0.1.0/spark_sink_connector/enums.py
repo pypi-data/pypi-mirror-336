8bf785bb28441b736b5552e3b4441519318c77d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 16:06:35 +0000

    Add package spark-sink-connector/0.1.0/spark_sink_connector-0.1.0.tar.gz

---
+++ packages/spark-sink-connector/spark_sink_connector-0.1.0.tar.gz/spark_sink_connector-0.1.0/spark_sink_connector/enums.py
@@ -0,0 +1,16 @@
+from enum import Enum, auto
+
+
+class SchemaKind(Enum):
+    AVRO = auto()
+    PROTOBUF = auto()
+
+
+class ConnectorOutputMode(str, Enum):
+    APPEND = "append"
+    OVERWRITE = "overwrite"
+
+
+class ConnectorMode(Enum):
+    STREAM = auto()
+    BATCH = auto()
