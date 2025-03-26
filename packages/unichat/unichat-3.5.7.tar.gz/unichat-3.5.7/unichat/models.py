2296c5269fb98bac39bae815f95e0128e6548f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 08:04:57 +0000

    Add package unichat/3.5.7/unichat-3.5.7.tar.gz

---
+++ packages/unichat/unichat-3.5.7.tar.gz/unichat-3.5.7/unichat/models.py
@@ -0,0 +1,75 @@
+MODELS_LIST =  {
+    "anthropic_models": [
+        "claude-3-5-haiku-latest",
+        "claude-3-5-sonnet-latest",
+        "claude-3-7-sonnet-latest",
+        "claude-3-7-sonnet-latest",
+        "claude-3-opus-latest"
+    ],
+    "mistral_models": [
+        "mistral-small-latest",
+        "mistral-large-latest",
+        "codestral-latest",
+        "pixtral-large-latest"
+    ],
+    "openai_models": [
+        "chatgpt-4o-latest",
+        "gpt-4o-mini",
+        "gpt-4o",
+        "gpt-4o-2024-11-20",
+        "gpt-4.5-preview",
+        "o1-mini",
+        "o1-preview",
+        "o1",
+        "o3-mini"
+    ],
+    "grok_models": [
+        "grok-2-latest",
+        "grok-2-vision-latest"
+    ],
+    "gemini_models": [
+        "gemini-2.0-flash-lite-preview-02-05",
+        "gemini-2.0-flash",
+        "gemini-2.0-flash-thinking-exp",
+        "gemini-2.5-pro-exp-03-25"
+    ],
+    "deepseek_models": [
+        "deepseek-chat",
+        "deepseek-reasoner"
+    ],
+    "alibaba_models": [
+        "qwen-turbo-latest",
+        "qwen-plus-latest",
+        "qwen-max-latest"
+    ]
+}
+MODELS_MAX_TOKEN =  {
+    "chatgpt-4o-latest": 16384,
+    "gpt-4o-mini": 16384,
+    "gpt-4o": 16384,
+    "gpt-4o-2024-11-20": 16384,
+    "gpt-4.5-preview": 16384,
+    "o1-mini": 65536,
+    "o1-preview": 32768,
+    "o1": 100000,
+    "o3-mini": 100000,
+    "mistral-small-latest": 32000,
+    "mistral-large-latest": 128000,
+    "codestral-latest": 256000,
+    "pixtral-large-latest": 128000,
+    "claude-3-5-haiku-latest": 8192,
+    "claude-3-5-sonnet-latest": 8192,
+    "claude-3-7-sonnet-latest": 8192,
+    "claude-3-opus-latest": 4096,
+    "grok-2-latest": 131072,
+    "grok-2-vision-latest": 32768,
+    "gemini-2.0-flash-lite-preview-02-05": 8192,
+    "gemini-2.0-flash": 8192,
+    "gemini-2.0-flash-thinking-exp": 8192,
+    "gemini-2.5-pro-exp-03-25": 8192,
+    "deepseek-chat": 8192,
+    "deepseek-reasoner": 8192,
+    "qwen-turbo-latest": 8192,
+    "qwen-plus-latest": 8192,
+    "qwen-max-latest": 8192
+}
