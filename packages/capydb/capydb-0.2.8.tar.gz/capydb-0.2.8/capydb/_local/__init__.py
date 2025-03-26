7aea048775fa9e03157bbf7fea748ad460f9ca2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:06:47 +0000

    Add package capydb/0.2.8/capydb-0.2.8.tar.gz

---
+++ packages/capydb/capydb-0.2.8.tar.gz/capydb-0.2.8/capydb/_local/__init__.py
@@ -0,0 +1,12 @@
+# _local package
+from ._collection import Collection, APIClientError, AuthenticationError, ClientRequestError, ServerError
+from ._database import Database
+
+__all__ = [
+    "Collection", 
+    "Database", 
+    "APIClientError", 
+    "AuthenticationError", 
+    "ClientRequestError", 
+    "ServerError"
+] 
