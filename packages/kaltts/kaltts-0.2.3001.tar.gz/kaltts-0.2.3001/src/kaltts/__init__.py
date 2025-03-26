77cabb13213fa2fe0b83d00e6137a9aac5d51302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:06:39 +0000

    Add package kaltts/0.2.3001/kaltts-0.2.3001.tar.gz

---
+++ packages/kaltts/kaltts-0.2.3001.tar.gz/kaltts-0.2.3001/src/kaltts/__init__.py
@@ -0,0 +1,9 @@
+# This file makes kaltts a package
+
+from . import synthesize
+from . import controller
+from . import data_processing
+from . import model
+from . import train
+from . import utils
+from . import vocoder
