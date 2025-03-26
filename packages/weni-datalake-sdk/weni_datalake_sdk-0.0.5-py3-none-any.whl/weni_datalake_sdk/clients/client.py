584a216ff4503404644d347a7bb3988cd4f349c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 05:05:30 +0000

    Add package weni-datalake-sdk/0.0.5/weni_datalake_sdk-0.0.5-py3-none-any.whl

---
+++ packages/weni-datalake-sdk/weni_datalake_sdk-0.0.5-py3-none-any.whl/weni_datalake_sdk/clients/client.py
@@ -0,0 +1,16 @@
+import grpc
+from weni_datalake_sdk.clients import msgs_pb2, msgs_pb2_grpc
+
+SERVER_ADDRESS = "localhost:50051"
+
+def send_data(path, data):
+    channel = grpc.insecure_channel(SERVER_ADDRESS)
+    stub = msgs_pb2_grpc.DatalakeManagerServiceStub(channel)
+
+    request = msgs_pb2.InsertRequest(
+        path=path,
+        data=data
+    )
+
+    response = stub.InsertData(request)
+    print("Server response:", response.status)
