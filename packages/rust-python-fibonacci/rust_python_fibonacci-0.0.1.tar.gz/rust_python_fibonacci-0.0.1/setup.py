e375aae28be8c077669c4d3d0b234e8d19a69c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 06:06:05 +0000

    Add package rust-python-fibonacci/0.0.1/rust_python_fibonacci-0.0.1.tar.gz

---
+++ packages/rust-python-fibonacci/rust_python_fibonacci-0.0.1.tar.gz/rust_python_fibonacci-0.0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="rust_python_fibonacci",
+    version="0.0.1",
+    author="SportyGeek2015",
+    author_email="this_is_not_a_real_emall_address@hotmail.com",
+    description="Calculates a Fibonacci number",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SportyGeek2015/rust_python_fibonacci",
+    install_requires=[
+        "PyYAML>=4.1.2",
+        "dill>=0.2.8"
+    ],
+    extras_require={
+     'server': ["Flask>=1.0.0"]
+    },
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'fib-number = rust_python_fibonacci.cmd.fib_numb:fib_numb',
+        ],
+    },
+    python_requires='>=3.10',
+    tests_require=['pytest'],
+)
