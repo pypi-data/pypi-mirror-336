6264d1fafb89c18cb7062b261c2a6c39d2880c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 14:05:38 +0000

    Add package opensubmarine/0.2.0/opensubmarine-0.2.0-py3-none-any.whl

---
+++ packages/opensubmarine/opensubmarine-0.2.0-py3-none-any.whl/opensubmarine/__init__.py
@@ -0,0 +1,42 @@
+from opensubmarine.contracts.access.Ownable.contract import Ownable, OwnableInterface
+from opensubmarine.contracts.participation.Stakable.contract import Stakeable
+from opensubmarine.contracts.update.Upgradeable.contract import Upgradeable
+from opensubmarine.contracts.factory.Deployable.contract import Deployable
+from opensubmarine.contracts.factory.Factory.contract import BaseFactory, FactoryCreated
+from opensubmarine.contracts.token.ARC200.src.contract import (
+    ARC200Token,
+    ARC200TokenInterface,
+    arc200_Transfer,
+)
+from opensubmarine.contracts.token.ARC72.src.contract import (
+    ARC72Token,
+    ARC72TokenInterface,
+    arc72_Transfer,
+    arc72_nft_data,
+)
+from opensubmarine.contracts.utils.introspection.arc73.contract import (
+    ARC73SupportsInterface,
+)
+
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "ARC200Token",
+    "ARC200TokenInterface",
+    "ARC72Token",
+    "ARC72TokenInterface",
+    "ARC73SupportsInterface",
+    "BaseFactory",
+    "Deployable",
+    "FactoryCreated",
+    "Ownable",
+    "OwnableInterface",
+    "Stakeable",
+    "Upgradeable",
+    "arc200_Transfer",
+    "arc72_Transfer",
+    "arc72_nft_data",
+]
+
+OpenSubmarine_version = __version__
---
+++ packages/opensubmarine/opensubmarine-0.2.0-py3-none-any.whl/opensubmarine/contracts/participation/Stakable/__init__.py
@@ -0,0 +1,11 @@
+"""
+OpenSubmarine - Stakeable contract
+"""
+
+__version__ = "0.1.0"
+
+from .contract import Stakeable
+
+__all__ = ["Stakeable"]
+
+Stakeable_version = __version__
