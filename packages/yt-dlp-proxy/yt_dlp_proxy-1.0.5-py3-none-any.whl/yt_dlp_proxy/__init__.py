e47c11a7607f3aa0bf1b6fb3c2d80b74eaf926e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-26 15:06:31 +0000

    Add package yt-dlp-proxy/1.0.5/yt_dlp_proxy-1.0.5-py3-none-any.whl

---
+++ packages/yt-dlp-proxy/yt_dlp_proxy-1.0.5-py3-none-any.whl/yt_dlp_proxy/__init__.py
@@ -0,0 +1,24 @@
+from .api import execute_yt_dlp_command, get_proxy_strings, run_yt_dlp, update_proxies
+
+
+class YtDLPProxy:
+
+    @staticmethod
+    def execute_raw(proxy_str: str, args: list[str]) -> None:
+        execute_yt_dlp_command(proxy_str=proxy_str, args=args)
+
+    @staticmethod
+    def load_proxy_strings() -> list[str]:
+        return get_proxy_strings()
+
+    @staticmethod
+    def execute(args: list[str]) -> bool:
+        """If proxy.json is not found it will be created"""
+        return run_yt_dlp(args)  # not sure how this works yet
+
+    @staticmethod
+    def update() -> None:
+        update_proxies()
+
+
+__all__ = ["YtDLPProxy"]
