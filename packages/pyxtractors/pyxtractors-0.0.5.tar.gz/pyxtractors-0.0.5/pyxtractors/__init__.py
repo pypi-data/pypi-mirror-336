673e5654995246e4dff64414ba2530aee6bddb0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:40 +0000

    Add package pyxtractors/0.0.5/pyxtractors-0.0.5.tar.gz

---
+++ packages/pyxtractors/pyxtractors-0.0.5.tar.gz/pyxtractors-0.0.5/pyxtractors/__init__.py
@@ -0,0 +1,4 @@
+from .extractor import \
+	Extractor, Returner, KeyExtractor, FuncExtractor, PlainFuncExtractor, \
+	ExtractorWithDefault, ExtractorWithErrDefault, TransformExtractor, \
+	MassExtractor, ExtractorChain
---
+++ packages/pyxtractors/pyxtractors-0.0.5.tar.gz/pyxtractors-0.0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+	name="pyxtractors",
+	version="0.0.5",
+	packages=find_packages(),
+	author="Alexander Smirnov",
+	author_email="alexsm.w@yandex.ru",
+	description="Tools to configure a mapping extraction policy"
+)
