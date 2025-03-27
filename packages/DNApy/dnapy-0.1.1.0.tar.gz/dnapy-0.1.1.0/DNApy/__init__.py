45a037fa5790a52f8ad861f91dadf57834eb0bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 16:06:35 +0000

    Add package DNApy/0.1.1.0/dnapy-0.1.1.0.tar.gz

---
+++ packages/DNApy/dnapy-0.1.1.0.tar.gz/dnapy-0.1.1.0/DNApy/__init__.py
@@ -0,0 +1,3 @@
+from .common import *
+from .clustering import *
+from .parsers import *
---
+++ packages/DNApy/dnapy-0.1.1.0.tar.gz/dnapy-0.1.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+pack_name="DNApy"
+
+setup(
+  name = pack_name,
+  packages = [pack_name], # this must be the same as the name above
+  version = '0.1.1.0',
+  description = 'A bioinformatics library containing common file parsers and others!',
+  author = 'Naman Jain',
+  author_email = 'namanjn07@hotmail.com',
+  url = 'https://github.com/Namanjn/namuskj', # use the URL to the github repo
+  download_url = 'https://github.com/Namanjn/PyNamu/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'bioinformatics', 'example'], # arbitrary keywords
+  classifiers = [],
+)
