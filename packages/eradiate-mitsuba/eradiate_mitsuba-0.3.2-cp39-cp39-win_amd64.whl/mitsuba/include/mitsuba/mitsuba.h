edb094c89da8485b43ad81a015381acd7a4b2823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:56 +0000

    Add package eradiate-mitsuba/0.3.2/eradiate_mitsuba-0.3.2-cp39-cp39-win_amd64.whl

---
+++ packages/eradiate-mitsuba/eradiate_mitsuba-0.3.2-cp39-cp39-win_amd64.whl/mitsuba/include/mitsuba/mitsuba.h
@@ -0,0 +1,41 @@
+/*
+    Mitsuba 3: A Retargetable Forward and Inverse Renderer
+    Copyright 2021, Realistic Graphics Lab, EPFL.
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE.txt file.
+*/
+
+#pragma once
+
+#define MI_VERSION_MAJOR 3
+#define MI_VERSION_MINOR 5
+#define MI_VERSION_PATCH 2
+
+#define MI_STRINGIFY(x) #x
+#define MI_TOSTRING(x)  MI_STRINGIFY(x)
+
+/// Current release of Mitsuba
+#define MI_VERSION                                                             \
+    MI_TOSTRING(MI_VERSION_MAJOR) "."                                          \
+    MI_TOSTRING(MI_VERSION_MINOR) "."                                          \
+    MI_TOSTRING(MI_VERSION_PATCH)
+
+/// Year of the current release
+#define MI_YEAR "2023"
+
+#define ERD_MI_VERSION_MAJOR 0
+#define ERD_MI_VERSION_MINOR 3
+#define ERD_MI_VERSION_PATCH 2
+
+/// Current release of the Eradiate patch for Mitsuba
+#define ERD_MI_VERSION                                                             \
+    MI_TOSTRING(ERD_MI_VERSION_MAJOR) "."                                          \
+    MI_TOSTRING(ERD_MI_VERSION_MINOR) "."                                          \
+    MI_TOSTRING(ERD_MI_VERSION_PATCH)
+
+/// Authors list
+#define MI_AUTHORS "Realistic Graphics Lab, EPFL; Rayference"
+
+#include <mitsuba/core/config.h>
+#include <mitsuba/core/platform.h>
