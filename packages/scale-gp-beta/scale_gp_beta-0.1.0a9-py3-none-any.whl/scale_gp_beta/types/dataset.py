f971975dbb0ddea44d8d0db05e2c69676df8447c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 21:07:01 +0000

    Add package scale-gp-beta/0.1.0a9/scale_gp_beta-0.1.0a9-py3-none-any.whl

---
+++ packages/scale-gp-beta/scale_gp_beta-0.1.0a9-py3-none-any.whl/scale_gp_beta/types/dataset.py
@@ -0,0 +1,27 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["Dataset"]
+
+
+class Dataset(BaseModel):
+    id: str
+
+    created_at: datetime
+
+    created_by_user_id: str
+
+    current_version_num: int
+
+    name: str
+
+    archived_at: Optional[datetime] = None
+
+    description: Optional[str] = None
+
+    object: Optional[Literal["dataset"]] = None
