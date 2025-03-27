404e66eeec76e1e1b8d9e61d3a844b365aeeaa65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 20:06:22 +0000

    Add package yutipy/1.4.1/yutipy-1.4.1-py3-none-any.whl

---
+++ packages/yutipy/yutipy-1.4.1-py3-none-any.whl/yutipy/utils/__init__.py
@@ -0,0 +1,13 @@
+from .cheap_utils import (
+    guess_album_type,
+    are_strings_similar,
+    is_valid_string,
+    separate_artists,
+)
+
+__all__ = [
+    "guess_album_type",
+    "are_strings_similar",
+    "is_valid_string",
+    "separate_artists",
+]
