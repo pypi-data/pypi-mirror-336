def84729af5310be7816a6ed2a828c610c09aefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:20 +0000

    Add package fed-rag/0.0.6.post1/fed_rag-0.0.6.post1.tar.gz

---
+++ packages/fed-rag/fed_rag-0.0.6.post1.tar.gz/fed_rag-0.0.6.post1/src/fed_rag/utils/data/finetuning_datasets/pytorch.py
@@ -0,0 +1,20 @@
+"""PyTorch RAG Finetuning Dataset"""
+
+from typing import Any
+
+import torch
+from torch.utils.data import Dataset
+
+
+class PyTorchRAGFinetuningDataset(Dataset):
+    def __init__(
+        self, input_ids: list[torch.Tensor], target_ids: list[torch.Tensor]
+    ):
+        self.input_ids = input_ids
+        self.target_ids = target_ids
+
+    def __len__(self) -> int:
+        return len(self.input_ids)
+
+    def __getitem__(self, idx: int) -> Any:
+        return self.input_ids[idx], self.target_ids[idx]
