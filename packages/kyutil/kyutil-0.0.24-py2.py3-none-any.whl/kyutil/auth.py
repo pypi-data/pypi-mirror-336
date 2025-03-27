bbeb32ac35a1fa687d301f7f80f4f51e71cc780f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 22:07:36 +0000

    Add package kyutil/0.0.24/kyutil-0.0.24-py2.py3-none-any.whl

---
+++ packages/kyutil/kyutil-0.0.24-py2.py3-none-any.whl/kyutil/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：kyutil 
+@File    ：auth.py.py
+@IDE     ：PyCharm 
+@Author  ：xuyong@kylinos.cn
+@Date    ：2025/3/27 下午3:24 
+@Desc    ：说明：
+"""
+
