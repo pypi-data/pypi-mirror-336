2ffd91e27e788a03ae60306260d6ad4a91cab18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:19 +0000

    Add package vision-unlearning/0.1.1/vision_unlearning-0.1.1-py3-none-any.whl

---
+++ packages/vision-unlearning/vision_unlearning-0.1.1-py3-none-any.whl/vision_unlearning/integrations/__init__.py
@@ -0,0 +1,4 @@
+# integrations are not loaded in the main code
+# You should import one-by-one as needed
+# Example: `if is_wandb_available(): from vision_unlearning.integrations.wandb import log_image`
+# Also, their interfaces are similar, but consistency is not guaranteed
