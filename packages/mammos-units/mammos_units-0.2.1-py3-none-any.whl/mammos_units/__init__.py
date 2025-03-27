a6856bf3b37c2a4575a93bbbb9140cac7db843ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:39 +0000

    Add package mammos-units/0.2.1/mammos_units-0.2.1-py3-none-any.whl

---
+++ packages/mammos-units/mammos_units-0.2.1-py3-none-any.whl/mammos_units/__init__.py
@@ -0,0 +1,5 @@
+from astropy.units import *
+import astropy.constants as constants
+
+def_unit(['f_u_', 'Formula_Unit'], dimensionless_unscaled, namespace=globals())
+def_unit('mu_B', constants.muB, namespace=globals())
