a2039c1929885fb3603c9775240c29e406b52ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:07:11 +0000

    Add package drkbloodbarometer/0.1.2/drkbloodbarometer-0.1.2.tar.gz

---
+++ packages/drkbloodbarometer/drkbloodbarometer-0.1.2.tar.gz/drkbloodbarometer-0.1.2/src/const.py
@@ -0,0 +1,37 @@
+states = {
+    "Baden-Würtemberg": {"url": "https://www.blutspende.de/startseite"},
+    "Hessen": {"url": "https://www.blutspende.de/startseite"},
+    "Bayern": {"url": ""},
+    "Berlin": {"url": "https://www.blutspende-nordost.de/"},
+    "Brandenburg": {"url": "https://www.blutspende-nordost.de/"},
+    "Bremen": {"url": "https://www.blutspende-leben.de/blut-spenden"},
+    "Hamburg": {"url": "https://www.blutspende-nordost.de/"},
+    "Mecklenburg-Vorpommern": {"url": "https://www.blutspende-leben.de/blut-spenden"},
+    "Niedersachsen": {"url": "https://www.blutspende-leben.de/blut-spenden"},
+    "Nordrhein-Westfalen": {},
+    "Rheinland-Pfalz": {},
+    "Saarland": {},
+    "Sachsen": {"url": "https://www.blutspende-nordost.de/"},
+    "Sachsen-Anhalt": {"url": "https://www.blutspende-leben.de/blut-spenden"},
+    "Schleswig-Holstein": {"https://www.blutspende-nordost.de/"},
+    "Thüringen": {"url": "https://www.blutspende-leben.de/blut-spenden"},
+}
+
+bloodtypes = {
+    "a_plus": "A+",
+    "b_plus": "B+",
+    "ab_plus": "AB+",
+    "zero_plus": "0+",
+    "a_neg": "A-",
+    "b_neg": "B-",
+    "ab_neg": "AB-",
+    "zero_neg": "0-",
+}
+
+ratings = {
+    "Hoch": 100,
+    "Normal": 51,
+    "Niedrig": 39,
+    "Kritisch": 27,
+    "Bedrohlich": 16
+}
