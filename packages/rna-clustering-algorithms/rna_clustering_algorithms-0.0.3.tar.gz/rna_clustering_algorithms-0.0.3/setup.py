2959f374d0a95dde3621da074e0d65a906386f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 15:06:15 +0000

    Add package rna-clustering-algorithms/0.0.3/rna_clustering_algorithms-0.0.3.tar.gz

---
+++ packages/rna-clustering-algorithms/rna_clustering_algorithms-0.0.3.tar.gz/rna_clustering_algorithms-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rna_clustering_algorithms",
+    version="0.1.1",
+    description="Modular clustering extension for scanpy",
+    author=["Daniel Sutton", "Laurits Madsen", "Sebastian Svendsen", "Valdemar fuglsang"],
+    packages=find_packages(),
+    install_requires=[
+        "scanpy>=1.9.0",
+        "numpy>=1.20.0",
+        "anndata>=0.8.0",
+        "scipy>=1.6.0",
+    ],
+    python_requires=">=3.8",
+) 
