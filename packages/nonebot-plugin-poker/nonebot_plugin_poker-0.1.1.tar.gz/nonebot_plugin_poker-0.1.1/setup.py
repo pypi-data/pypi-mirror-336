c13d7fadad861c43898b6a8457bca8d0d5aefd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 15:06:37 +0000

    Add package nonebot-plugin-poker/0.1.1/nonebot_plugin_poker-0.1.1.tar.gz

---
+++ packages/nonebot-plugin-poker/nonebot_plugin_poker-0.1.1.tar.gz/nonebot_plugin_poker-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nonebot_plugin_poker',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        "nonebot2>=2.0.0",
+    ],
+    author='MoonofBridge24',
+    author_email='moonofbridge24@foxmail.com',
+    description='详见readme',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MoonofBridge24/nonebot_plugin_poker',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
