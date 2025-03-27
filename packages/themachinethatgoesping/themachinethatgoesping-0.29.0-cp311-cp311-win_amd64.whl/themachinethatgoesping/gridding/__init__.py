dbb556edd07bb5cf0701e3def871a668ab8dda7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:41 +0000

    Add package themachinethatgoesping/0.29.0/themachinethatgoesping-0.29.0-cp311-cp311-win_amd64.whl

---
+++ packages/themachinethatgoesping/themachinethatgoesping-0.29.0-cp311-cp311-win_amd64.whl/themachinethatgoesping/gridding/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2022 - 2023 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: MPL-2.0
+
+# folders
+from . import functions
+
+# modules
+from .echogrid import EchoGrid
+from .forwardgridder import ForwardGridder
+
+__version__ = "0.3.0"
