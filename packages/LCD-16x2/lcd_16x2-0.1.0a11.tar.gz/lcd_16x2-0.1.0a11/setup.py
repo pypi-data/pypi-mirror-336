90f028a0e5812205e49936b7bb1f21b4a19eee94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 21:09:29 +0000

    Add package LCD-16x2/0.1.0a11/lcd_16x2-0.1.0a11.tar.gz

---
+++ packages/LCD-16x2/lcd_16x2-0.1.0a11.tar.gz/lcd_16x2-0.1.0a11/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="LCD_16x2",
+    version="0.1.0-alpha11",
+    packages=["lcd_16x2",
+              "lcd_16x2.drivers",
+              "lcd_16x2.emulators",
+              "demo"],
+    install_requires=['requests~=2.32.3',
+                      'beautifulsoup4~=4.13.3',
+                      'setuptools~=78.1.0'],
+    author="Matthew Timmons-Brown (the-raspberry-pi-guy)",
+    maintainer="https://github.com/the-raspberry-pi-guy/lcd/contributors",
+    description="Python package for interfacing with a 16x2 character I2C liquid-crystal display (LCD).",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/the-raspberry-pi-guy/lcd",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'lcd-setup=install:run_install'
+        ]
+    },
+    include_package_data=True
+)
