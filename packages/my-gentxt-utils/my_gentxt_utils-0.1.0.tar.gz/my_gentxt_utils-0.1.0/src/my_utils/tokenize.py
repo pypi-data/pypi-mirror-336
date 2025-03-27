ea9f90250d8d9980a91ba2b3b0b04579a494cb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:42 +0000

    Add package my-gentxt-utils/0.1.0/my_gentxt_utils-0.1.0.tar.gz

---
+++ packages/my-gentxt-utils/my_gentxt_utils-0.1.0.tar.gz/my_gentxt_utils-0.1.0/src/my_utils/tokenize.py
@@ -0,0 +1,11 @@
+import tiktoken
+
+class Tokenizer:
+    def __init__(self, model_name):
+        self.encoding = tiktoken.encoding_for_model(model_name)
+
+    def tokenize(self, text):
+        return self.encoding.encode(text)
+
+    def detokenize(self, tokens):
+        return self.encoding.decode(tokens)
