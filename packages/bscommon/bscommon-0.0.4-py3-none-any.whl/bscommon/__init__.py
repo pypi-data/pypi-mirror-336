04efad5b5a54ed26c846f5f0c45be1f9ae8ec622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 16:07:35 +0000

    Add package bscommon/0.0.4/bscommon-0.0.4-py3-none-any.whl

---
+++ packages/bscommon/bscommon-0.0.4-py3-none-any.whl/bscommon/__init__.py
@@ -0,0 +1,17 @@
+import json
+
+# 读取配置文件路径
+ReadConfigFilePath=None
+
+# 读取配置文件
+def readConfig(filePath):
+    ReadConfigFilePath=filePath
+    with open(filePath, 'r') as file:
+        return json.load(file)
+    
+# 保存配置文件，如果filePath为None，则保存到readConfig中读取的文件中
+def saveConfig(strJson, filePath=None):
+    global ReadConfigFilePath
+    if filePath is None:filePath=ReadConfigFilePath
+    with open(filePath, 'w') as file:
+        json.dump(strJson, file)
