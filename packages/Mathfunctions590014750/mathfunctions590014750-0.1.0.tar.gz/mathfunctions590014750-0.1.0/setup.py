1cf613bfa3c6ba9eb8f90d74e4a18c0fe409239f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:19 +0000

    Add package Mathfunctions590014750/0.1.0/mathfunctions590014750-0.1.0.tar.gz

---
+++ packages/Mathfunctions590014750/mathfunctions590014750-0.1.0.tar.gz/mathfunctions590014750-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Mathfunctions590014750",  
+    version="0.1.0",
+    author="garima arora ",
+    author_email="garima217arora@gmail.com",
+    description="Maths Functions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
