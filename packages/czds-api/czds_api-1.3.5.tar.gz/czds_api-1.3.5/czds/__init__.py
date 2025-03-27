d3afede5d96d042ebe228f23d144223346dd48e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:40 +0000

    Add package czds-api/1.3.5/czds_api-1.3.5.tar.gz

---
+++ packages/czds-api/czds_api-1.3.5.tar.gz/czds_api-1.3.5/czds/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# ICANN API for the Centralized Zones Data Service - developed by acidvegas (https://git.acid.vegas/czds)
+# czds/__init__.py
+
+__version__ = '1.3.5'
+__author__  = 'acidvegas'
+__email__   = 'acid.vegas@acid.vegas'
+__github__  = 'https://github.com/acidvegas/czds'
