bcd6e69b4068e079522b30665e830489e9cccdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 22:06:45 +0000

    Add package bi-code/0.3/bi_code-0.3.tar.gz

---
+++ packages/bi-code/bi_code-0.3.tar.gz/bi_code-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bi_code",
+    version="0.3",
+    packages=find_packages(),
+    install_requires=[],
+    author="Mia Khalifa",
+    author_email="khalifa@gmail.com",
+    description="BI codes for Bsc it sem 6",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/mypackage",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
