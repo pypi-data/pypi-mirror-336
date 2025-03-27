10d4b987e2ca5e7de3e698b8dc0d5ec2ed82abd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:55 +0000

    Add package swaglib/0.1.7/swaglib-0.1.7-py3-none-any.whl

---
+++ packages/swaglib/swaglib-0.1.7-py3-none-any.whl/swaglib/modules.py
@@ -0,0 +1,43 @@
+import os
+import requests
+import subprocess
+
+version = "0.1.7"
+
+def greet(name):
+    print(f"hello {name}!!")
+
+def fetchTextFromUrl(url: str) -> str:
+    response = requests.get(url)
+
+    print(response.text)
+
+def getResponseStatusFromUrl(url: str) -> str:
+    response = requests.get(url)
+    print(response.status_code)
+
+def playAudio(file_path: str) -> str:
+    subprocess.run(["ffplay", "-nodisp", "-autoexit", file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def checkWifi():
+    try:
+        requests.get("https://google.com", timeout=2)
+        return True
+    except requests.exceptions.RequestException:
+        return False 
+
+def writeToFile(path, content):
+    if not os.path.exists(path):
+        with open(path, 'w') as file:
+            file.write(content)
+    else:
+        with open(path, 'a') as file:
+            file.write(content)
+
+def createFile(file_name, file_extension):
+    file = f"{file_name}.{file_extension}"
+    with open(file, 'a') as file:
+        return
+    
+def classic_greet(name):
+    print(f"Hello, {name}!")
