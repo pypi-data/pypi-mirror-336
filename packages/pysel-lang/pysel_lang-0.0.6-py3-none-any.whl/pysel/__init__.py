2f51bda05b1658344fe1e263fd1ed3f599be2f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 22:06:59 +0000

    Add package pysel-lang/0.0.6/pysel_lang-0.0.6-py3-none-any.whl

---
+++ packages/pysel-lang/pysel_lang-0.0.6-py3-none-any.whl/pysel/__init__.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022-present tandemdude
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""A simple but powerful embedded expression language for Python."""
+
+import typing as t
+
+from pysel import ast
+from pysel import errors
+from pysel import lexer
+from pysel import tokens
+from pysel import vm
+
+__all__ = ["Expression", "ast", "lexer", "tokens"]
+
+__version__ = "0.0.6"
+
+T = t.TypeVar("T")
+
+
+class Expression(t.Generic[T]):
+    __slots__ = ("ast", "bytecode", "raw", "vm")
+
+    def __init__(self, raw: str) -> None:
+        self.raw: str = raw
+
+        self.ast: t.Optional[ast.Node] = None
+        self.bytecode: t.Optional[t.List[vm.Instruction]] = None
+        self.vm: t.Optional[vm.VirtualMachine[T]] = None
+
+    def compile(self) -> vm.VirtualMachine[T]:
+        if self.vm is not None:
+            return self.vm
+
+        self.ast = ast.Parser(self.raw, lexer.Lexer(self.raw).tokenize()).compilation_unit()
+
+        symbol_table = vm.SymbolTable()
+        self.bytecode = self.ast.compile(symbol_table)
+        self.vm = vm.VirtualMachine(self.bytecode, symbol_table)
+
+        return self.vm
+
+    def evaluate(self, env: t.Optional[t.Dict[str, t.Any]] = None, use_ast: bool = False) -> T:
+        """
+        Evaluate this expression under the given environment.
+
+        Args:
+            env (dict | None): Environment to use to resolve references in the expression.
+            use_ast (bool): Whether to run as an AST walker instead of compiling to PySEL bytecode and using
+                the bytecode virtual machine.
+
+        Returns:
+            The output of the expression.
+        """
+        if self.ast is None:
+            self.compile()
+
+        env = env or {}
+        for primitive in (bool, float, int, str):
+            env.setdefault(primitive.__name__, primitive)
+        env.setdefault("None", None)
+
+        if use_ast:
+            assert self.ast is not None
+            return t.cast("T", self.ast.evaluate(env))
+
+        assert self.vm is not None
+        return self.vm.run(env)
---
+++ packages/pysel-lang/pysel_lang-0.0.6-py3-none-any.whl/pysel/tokens.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022-present tandemdude
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import abc
+import enum
+import typing as t
+
+__all__ = [
+    "ErrorToken",
+    "FloatToken",
+    "IdentifierToken",
+    "IntToken",
+    "LiteralMixin",
+    "OperatorToken",
+    "StringToken",
+    "Token",
+    "TokenType",
+]
+
+
+class TokenType(enum.Enum):
+    # Compound tokens
+    IDENTIFIER = "\\w[\\w\\d]+"
+    INT_LITERAL = "[\\d]+"
+    FLOAT_LITERAL = "[\\d]+\\.[\\d]*"
+    STRING_LITERAL = "'.*'"
+    # Special tokens
+    UNKNOWN = "UNKNOWN"
+    # Operators
+    OPERATOR_EQ = "=="
+    OPERATOR_NOT_EQ = "!="
+    OPERATOR_GREATER_THAN = ">"
+    OPERATOR_LESS_THAN = "<"
+    OPERATOR_GREATER_EQUALS = ">="
+    OPERATOR_LESS_EQUALS = "<="
+    OPERATOR_NOT = "!"
+    OPERATOR_AND = "&&"
+    OPERATOR_OR = "||"
+    OPERATOR_ADD = "+"
+    OPERATOR_SUB = "-"
+    OPERATOR_MULTIPLY = "*"
+    OPERATOR_DIVIDE = "/"
+    OPERATOR_INTEGER_DIVIDE = "//"
+    OPERATOR_MODULO = "%"
+    OPERATOR_POW = "**"
+    # Special characters
+    QUESTION_MARK = "?"
+    COLON = ":"
+    ACCESSOR = "."
+    COMMA = ","
+    L_PAREN = "("
+    R_PAREN = ")"
+    L_SQUARE_BRACKET = "["
+    R_SQUARE_BRACKET = "]"
+
+
+EXCLUDED_TOKEN_TYPES = {
+    TokenType.IDENTIFIER,
+    TokenType.INT_LITERAL,
+    TokenType.FLOAT_LITERAL,
+    TokenType.STRING_LITERAL,
+    TokenType.UNKNOWN,
+}
+SORTED_TOKEN_TYPES = sorted(
+    set(TokenType).difference(EXCLUDED_TOKEN_TYPES),
+    key=lambda itm: len(itm.value),
+    reverse=True,
+)
+
+
+class Token(abc.ABC):
+    __slots__ = ("at", "type", "value")
+
+    value: t.Any
+    at: int
+    type: TokenType
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(value={self.value!r}, type={self.type})"
+
+
+class LiteralMixin:
+    __slots__ = ()
+
+
+class OperatorToken(Token):
+    __slots__ = ()
+
+    def __init__(self, type: TokenType, at: int) -> None:
+        self.value = type.value
+        self.at = at
+        self.type = type
+
+
+class IdentifierToken(Token):
+    __slots__ = ()
+
+    type: TokenType = TokenType.IDENTIFIER
+
+    def __init__(self, value: str, at: int) -> None:
+        self.value = value
+        self.at = at
+
+
+class StringToken(Token, LiteralMixin):
+    __slots__ = ()
+
+    type: TokenType = TokenType.STRING_LITERAL
+
+    def __init__(self, value: str, at: int) -> None:
+        self.value = value.replace("\\", "")
+        self.at = at
+
+
+class IntToken(Token, LiteralMixin):
+    __slots__ = ()
+
+    type: TokenType = TokenType.INT_LITERAL
+
+    def __init__(self, value: str, at: int) -> None:
+        self.value: int = int(value)
+        self.at = at
+
+
+class FloatToken(Token, LiteralMixin):
+    __slots__ = ()
+
+    type: TokenType = TokenType.FLOAT_LITERAL
+
+    def __init__(self, value: str, at: int) -> None:
+        self.value = float(value)
+        self.at = at
+
+
+class ErrorToken(Token):
+    __slots__ = ()
+
+    type: TokenType = TokenType.UNKNOWN
+
+    def __init__(self, value: str, at: int) -> None:
+        self.value: str = value
+        self.at = at
