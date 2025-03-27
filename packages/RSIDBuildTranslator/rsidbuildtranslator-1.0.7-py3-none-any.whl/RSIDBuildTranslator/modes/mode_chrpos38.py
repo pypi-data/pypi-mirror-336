af808cd5d9bbe88ed3e5a82c02481369f7d4f4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 15:06:51 +0000

    Add package RSIDBuildTranslator/1.0.7/rsidbuildtranslator-1.0.7-py3-none-any.whl

---
+++ packages/RSIDBuildTranslator/rsidbuildtranslator-1.0.7-py3-none-any.whl/RSIDBuildTranslator/modes/mode_chrpos38.py
@@ -0,0 +1,35 @@
+from RSIDBuildTranslator.utils import (
+    cleanup_query_df,
+    create_ids_to_search,
+    load_gtex_data,
+    make_checks_chrpos,
+    query_to_df,
+    read_input_file,
+    write_output_file,
+)
+
+
+def run(args):
+    """Handles mode "chrpos38" logic."""
+
+    input_data = read_input_file(args.input)
+
+    if not make_checks_chrpos(input_data, args.chr38, args.pos38):
+        return
+
+    input_data, ids_to_search = create_ids_to_search(input_data, [args.chr38, args.pos38])
+
+    with load_gtex_data() as gtex_con:
+        if gtex_con:
+            gtex_cur = gtex_con.cursor()
+
+            BATCH_SIZE = 500
+            results_df = query_to_df("GTEx_lookup", ids_to_search, "chrpos38", gtex_cur, BATCH_SIZE)
+
+            final_df = cleanup_query_df(
+                results_df, input_data, "new_ids", "chrpos38", args.exclude_ref_alt
+            )
+
+            print("Output file head:\n")
+            print(final_df.head())
+            write_output_file(final_df, args.output)
