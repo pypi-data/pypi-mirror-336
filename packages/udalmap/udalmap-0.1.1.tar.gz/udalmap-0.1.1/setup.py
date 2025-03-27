5354752a96ebbc921c5d9b593f5fcd117b949f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 15:06:17 +0000

    Add package udalmap/0.1.1/udalmap-0.1.1.tar.gz

---
+++ packages/udalmap/udalmap-0.1.1.tar.gz/udalmap-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setup(
+    author="Mikel Imaz",
+    description="A wrapper for Udalmap API",
+    name="udalmap",
+    version="0.1.1",
+    url="https://github.com/mikel-imaz/udalmap",
+    keywords=["udalmap", "api", "euskadi", "basque", "opendata"],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["requests", "pandas", "matplotlib"],
+    python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
