c37be01533d9cbb63c7fb4eeb16b0ad33a923b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 15:06:52 +0000

    Add package dbnd-airflow-monitor/1.0.27.18/dbnd_airflow_monitor-1.0.27.18.tar.gz

---
+++ packages/dbnd-airflow-monitor/dbnd_airflow_monitor-1.0.27.18.tar.gz/dbnd_airflow_monitor-1.0.27.18/src/airflow_monitor/multiserver/airflow_integration.py
@@ -0,0 +1,69 @@
+# © Copyright Databand.ai, an IBM Company 2022
+import logging
+
+from typing import Optional
+
+from airflow_monitor.adapter.airflow_adapter import AirflowAdapter
+from airflow_monitor.common.config_data import AirflowIntegrationConfig
+from airflow_monitor.config_updater.runtime_config_updater import (
+    AirflowRuntimeConfigUpdater,
+)
+from airflow_monitor.data_fetcher.db_data_fetcher import DbFetcher
+from airflow_monitor.syncer.runtime_syncer import AirflowRuntimeSyncer
+from airflow_monitor.tracking_service.airflow_tracking_service import (
+    AirflowTrackingService,
+)
+from dbnd_monitor.adapter.adapter import ThirdPartyInfo
+from dbnd_monitor.base_integration import BaseIntegration
+
+
+logger = logging.getLogger(__name__)
+
+
+class AirflowIntegration(BaseIntegration):
+    MONITOR_TYPE = "airflow"
+    CONFIG_CLASS = AirflowIntegrationConfig
+    config: AirflowIntegrationConfig
+
+    def get_components_dict(self):
+        return {
+            "state_sync": AirflowRuntimeSyncer,
+            "config_updater": AirflowRuntimeConfigUpdater,
+        }
+
+    def get_components(self):
+        tracking_service = self.get_tracking_service()
+        data_fetcher = self.get_data_fetcher()
+        components_dict = self.get_components_dict()
+        all_components = []
+        for _, syncer_class in components_dict.items():
+            syncer_instance = syncer_class(
+                config=self.config,
+                tracking_service=tracking_service,
+                reporting_service=self.reporting_service,
+                data_fetcher=data_fetcher,
+            )
+            all_components.append(syncer_instance)
+
+        return all_components
+
+    def get_data_fetcher(self):
+        return DbFetcher(self.config)
+
+    def get_tracking_service(self) -> AirflowTrackingService:
+        return AirflowTrackingService(
+            monitor_type=self.MONITOR_TYPE,
+            tracking_source_uid=str(self.config.tracking_source_uid),
+        )
+
+    def get_third_party_info(self) -> Optional[ThirdPartyInfo]:
+        return AirflowAdapter().get_third_party_info()
+
+    def on_integration_disabled(self):
+        tracking_service = self.get_tracking_service()
+
+        logger.info("Running runtime_config_updater last time before stopping")
+        updater = AirflowRuntimeConfigUpdater(
+            self.config, tracking_service, self.reporting_service
+        )
+        updater.sync_once()
---
+++ packages/dbnd-airflow-monitor/dbnd_airflow_monitor-1.0.27.18.tar.gz/dbnd_airflow_monitor-1.0.27.18/src/airflow_monitor/syncer/runtime_syncer.py
@@ -0,0 +1,216 @@
+# © Copyright Databand.ai, an IBM Company 2022
+import datetime
+import logging
+import sys
+
+from typing import List
+
+from airflow_monitor.common.airflow_data import (
+    AirflowDagRun,
+    AirflowDagRunsResponse,
+    DagRunsStateData,
+)
+from airflow_monitor.common.config_data import AirflowIntegrationConfig
+from airflow_monitor.data_fetcher.base_data_fetcher import AirflowDataFetcher
+from airflow_monitor.data_fetcher.plugin_metadata import get_plugin_metadata
+from airflow_monitor.tracking_service.airflow_tracking_service import (
+    AirflowTrackingService,
+)
+from dbnd._core.utils.timezone import utcnow
+from dbnd_monitor.base_component import BaseComponent
+
+
+logger = logging.getLogger(__name__)
+
+VIEW_ONLY_EVENTS = {
+    "duration",
+    "elasticsearch",
+    "extra_links",
+    "gantt",
+    "get_logs_with_metadata",
+    "graph",
+    "landing_times",
+    "log",
+    "rendered",
+    "task",
+    "task_instances",
+    "tree",
+    "tries",
+    "variables",
+    "varimport",
+    "xcom",
+}
+
+
+def is_view_only_events(dr: AirflowDagRun):
+    if not dr.events:
+        return False
+    non_view_events = set(dr.events.split(",")) - VIEW_ONLY_EVENTS
+    return len(non_view_events) == 0
+
+
+def categorize_dag_runs(
+    airflow_dag_runs: List[AirflowDagRun], dbnd_dag_run_ids: List[int]
+):
+    dagruns_to_init = []
+    dagruns_to_update = []
+    dagruns_to_skip = []
+    for dr in airflow_dag_runs:
+        is_finished = dr.state != "RUNNING"
+        if is_finished and is_view_only_events(dr):
+            dagruns_to_update.append(dr)
+        elif dr.id not in dbnd_dag_run_ids:
+            dagruns_to_init.append(dr)
+        elif is_finished or dr.has_updated_task_instances or dr.is_paused:
+            dagruns_to_update.append(dr)
+        else:
+            dagruns_to_skip.append(dr)
+    return dagruns_to_init, dagruns_to_update
+
+
+class AirflowRuntimeSyncer(BaseComponent):
+    SYNCER_TYPE = "runtime_syncer"
+
+    tracking_service: AirflowTrackingService
+    config: AirflowIntegrationConfig
+    data_fetcher: AirflowDataFetcher
+
+    last_success_sync: datetime.datetime = None
+    last_sync_heartbeat: datetime.datetime = None
+
+    def _sync_once(self):
+        if (
+            self.last_success_sync
+            and self.config.restart_after_not_synced_minutes
+            and self.last_sync_heartbeat - self.last_success_sync
+            > datetime.timedelta(minutes=self.config.restart_after_not_synced_minutes)
+        ):
+            # this mechanism works only after first successful attempt, hoping that
+            # restart will help. If we didn't had even one successful attempt - the
+            # probability that restart will help is small
+            logger.fatal(
+                "Didn't sync data for last %s minutes, restarting",
+                self.config.restart_after_not_synced_minutes,
+            )
+            # unfortunately, currently no easy way to restart monitor from _sync_once
+            sys.exit(42)
+
+        self.last_sync_heartbeat = utcnow()
+
+        synced_new_data = self._actual_sync_once()
+
+        self.last_success_sync = utcnow()
+        # update last_sync_heartbeat as well to prevent false positives for cases
+        # when iteration takes too much time
+        self.last_sync_heartbeat = self.last_success_sync
+
+        self.reporting_service.report_monitor_time_data(
+            self.config.uid, synced_new_data=synced_new_data
+        )
+
+    def _actual_sync_once(self):
+        dbnd_response = self.tracking_service.get_active_dag_runs(
+            start_time_window=self.config.start_time_window,
+            dag_ids=self.config.dag_ids,
+            excluded_dag_ids=self.config.excluded_dag_ids,
+        )
+        if (
+            dbnd_response.last_seen_dag_run_id is None
+            or dbnd_response.last_seen_log_id is None
+        ):
+            last_seen_values = self.data_fetcher.get_last_seen_values()
+            if last_seen_values.last_seen_log_id is None:
+                last_seen_values.last_seen_log_id = -1
+            if last_seen_values.last_seen_dag_run_id is None:
+                last_seen_values.last_seen_dag_run_id = -1
+
+            self.tracking_service.update_last_seen_values(last_seen_values)
+
+            dbnd_response = self.tracking_service.get_active_dag_runs(
+                start_time_window=self.config.start_time_window,
+                dag_ids=self.config.dag_ids,
+                excluded_dag_ids=self.config.excluded_dag_ids,
+            )
+
+        logger.debug(
+            "Getting new dag runs from Airflow with parameters last_seen_dag_run_id=%s, last_seen_log_id=%s, extra_dag_run_ids=%s, dag_ids=%s, excluded_dag_ids=%s",
+            dbnd_response.last_seen_dag_run_id,
+            dbnd_response.last_seen_log_id,
+            ",".join(map(str, dbnd_response.dag_run_ids)),
+            self.config.dag_ids,
+            self.config.excluded_dag_ids,
+        )
+        airflow_response = self.data_fetcher.get_airflow_dagruns_to_sync(
+            last_seen_dag_run_id=dbnd_response.last_seen_dag_run_id,
+            last_seen_log_id=dbnd_response.last_seen_log_id,
+            extra_dag_run_ids=dbnd_response.dag_run_ids,
+            dag_ids=self.config.dag_ids,
+            excluded_dag_ids=self.config.excluded_dag_ids,
+        )  # type: AirflowDagRunsResponse
+
+        dagruns_to_init, dagruns_to_update = categorize_dag_runs(
+            airflow_response.dag_runs, dbnd_response.dag_run_ids
+        )
+
+        self.init_runs(dagruns_to_init)
+        self.update_runs(dagruns_to_update)
+
+        synced_new_data = len(dagruns_to_init) > 0 or len(dagruns_to_update) > 0
+        return synced_new_data
+
+    def init_runs(self, run_ids_to_init: List[AirflowDagRun]):
+        if not run_ids_to_init:
+            return
+
+        plugin_metadata = get_plugin_metadata()
+        dagruns: List[AirflowDagRun] = sorted(run_ids_to_init, key=lambda dr: dr.id)
+
+        bulk_size = self.config.dag_run_bulk_size or len(dagruns)
+        for i in range(0, len(dagruns), bulk_size):
+            dagruns_chunk = dagruns[i : i + bulk_size]
+            dag_run_ids = [dr.id for dr in dagruns_chunk]
+            dag_runs_full_data = self.data_fetcher.get_full_dag_runs(
+                dag_run_ids, self.config.include_sources
+            )
+            logger.info(
+                "Syncing new %d dag runs and %d task instances",
+                len(dag_runs_full_data.dag_runs),
+                len(dag_runs_full_data.task_instances),
+            )
+            self.tracking_service.init_dagruns(
+                dag_runs_full_data, max(dag_run_ids), self.SYNCER_TYPE, plugin_metadata
+            )
+
+    def update_runs(self, run_ids_to_update: List[AirflowDagRun]):
+        # update_dagruns will fetch updated states for given dagruns and send them
+        # to webserver (in chunks). As a side effect - webserver will also update
+        # last_sync_time which is used as heartbeat. We do it as part of update_dagruns
+        # to be sure that we update last_sync_time only if data is being updated.
+        if not run_ids_to_update:
+            # if there is no dagruns to update - do empty update to set last_sync_time
+            # otherwise we might get false alarms that monitor is not syncing
+            self.tracking_service.update_dagruns(
+                DagRunsStateData(task_instances=[], dag_runs=[]), None, self.SYNCER_TYPE
+            )
+            return
+
+        dagruns = sorted(
+            run_ids_to_update, key=lambda dr: (dr.max_log_id is None, dr.max_log_id)
+        )  # type: List[AirflowDagRun]
+
+        bulk_size = self.config.dag_run_bulk_size or len(dagruns)
+        for i in range(0, len(dagruns), bulk_size):
+            dagruns_chunk = dagruns[i : i + bulk_size]
+            dag_run_ids = [dr.id for dr in dagruns_chunk]
+            dag_runs_state_data = self.data_fetcher.get_dag_runs_state_data(dag_run_ids)
+            logger.info(
+                "Updating states for %d dag runs and %d task instances",
+                len(dag_runs_state_data.dag_runs),
+                len(dag_runs_state_data.task_instances),
+            )
+            max_logs_ids = [dr.max_log_id for dr in dagruns_chunk if dr.max_log_id]
+            self.tracking_service.update_dagruns(
+                dag_runs_state_data,
+                max(max_logs_ids) if max_logs_ids else None,
+                self.SYNCER_TYPE,
+            )
