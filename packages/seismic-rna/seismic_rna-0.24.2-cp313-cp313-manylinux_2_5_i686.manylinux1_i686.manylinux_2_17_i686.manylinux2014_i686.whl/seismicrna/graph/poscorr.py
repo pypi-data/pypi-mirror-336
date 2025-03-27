af0a94e3e90720ec7fad1ca458515527ccdf8dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:23 +0000

    Add package seismic-rna/0.24.2/seismic_rna-0.24.2-cp313-cp313-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/seismic-rna/seismic_rna-0.24.2-cp313-cp313-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/seismicrna/graph/poscorr.py
@@ -0,0 +1,92 @@
+import os
+from abc import ABC
+
+import numpy as np
+import pandas as pd
+from click import command
+
+from .pospair import PositionPairGraph, PositionPairWriter, PositionPairRunner
+from ..core.run import log_command
+
+COMMAND = __name__.split(os.path.extsep)[-1]
+
+
+def calc_phi(n: int | float | np.ndarray | pd.Series,
+             a: int | float | np.ndarray | pd.Series | pd.DataFrame,
+             b: int | float | np.ndarray | pd.Series | pd.DataFrame,
+             a_and_b: int | float | np.ndarray | pd.Series | pd.DataFrame):
+    """ Calculate the phi correlation coefficient for a 2x2 matrix.
+
+    +----+----+
+    | AB | AO | A.
+    +----+----+
+    | OB | OO | O.
+    +----+----+
+      .B   .O   ..
+
+    where
+      A. = AB + AO
+      .B = AB + OB
+      .. = A. + O. = .B + .O
+
+    Parameters
+    ----------
+    n: int | float | np.ndarray | pd.Series
+        Observations in total (..)
+    a: int | float | np.ndarray | pd.Series | pd.DataFrame
+        Observations for which A is true, regardless of B (A.)
+    b: int | float | np.ndarray | pd.Series | pd.DataFrame
+        Observations for which B is true, regardless of A (.B)
+    a_and_b: int | float | np.ndarray | pd.Series | pd.DataFrame
+        Observations for which A and B are both true (AB)
+
+    Returns
+    -------
+    float | np.ndarray | pd.Series | pd.DataFrame
+        Phi correlation coefficient
+    """
+    a_x_b = a * b
+    with np.errstate(divide="ignore", invalid="ignore"):
+        return (n * a_and_b - a_x_b) / np.sqrt(a_x_b * (n - a) * (n - b))
+
+
+class PositionCorrelationGraph(PositionPairGraph, ABC):
+    """ Phi correlations between pairs of positions. """
+
+    @classmethod
+    def graph_kind(cls):
+        return COMMAND
+
+    @classmethod
+    def what(cls):
+        return "Phi correlation between"
+
+    @classmethod
+    def get_pair_func(cls):
+        """ Function to compare each pair of positions. """
+        return calc_phi
+
+
+class PositionCorrelationWriter(PositionPairWriter):
+
+    @classmethod
+    def graph_type(cls):
+        return PositionCorrelationGraph
+
+
+class PositionCorrelationRunner(PositionPairRunner):
+
+    @classmethod
+    def get_writer_type(cls):
+        return PositionCorrelationWriter
+
+    @classmethod
+    @log_command(COMMAND)
+    def run(cls, *args, **kwargs):
+        return super().run(*args, **kwargs)
+
+
+@command(COMMAND, params=PositionCorrelationRunner.params())
+def cli(*args, **kwargs):
+    """ Phi correlations between pairs of positions. """
+    return PositionCorrelationRunner.run(*args, **kwargs)
