723e786f94ca562b5dd12c38a020adc660e27469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 20:06:47 +0000

    Add package aiosplunkbase/0.1.1/aiosplunkbase-0.1.1-py3-none-any.whl

---
+++ packages/aiosplunkbase/aiosplunkbase-0.1.1-py3-none-any.whl/aiosplunkbase/exceptions.py
@@ -0,0 +1,27 @@
+from httpx import Response
+
+
+class ApiException(Exception):
+    def __init__(self, response: Response):
+        self.status_code = response.status_code
+        self.headers = response.headers
+        self.text = response.text
+
+        message = f"HTTP {self.status_code} - {self.text}"
+        if len(message) > 1000:
+            message = message[:1000] + "..."
+        super().__init__(message)
+
+
+class AppNotFound(Exception):
+    pass
+
+class AuthenticationError(ApiException):
+    pass
+
+class NoReleaseFound(Exception):
+    pass
+
+
+class VersionNotFound(Exception):
+    pass
