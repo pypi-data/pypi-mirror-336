15b3b392986e4c25bd8e485dde681b79baa7b186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 21:07:13 +0000

    Add package ovos-solver-bm25-plugin/0.0.1a1/ovos-solver-bm25-plugin-0.0.1a1.tar.gz

---
+++ packages/ovos-solver-bm25-plugin/ovos-solver-bm25-plugin-0.0.1a1.tar.gz/ovos-solver-bm25-plugin-0.0.1a1/ovos_bm25_solver/__init__.py
@@ -0,0 +1,4 @@
+from ovos_bm25_solver.base import BM25MultipleChoiceSolver, BM25EvidenceSolverPlugin
+from ovos_bm25_solver.corpus import BM25CorpusSolver, BM25QACorpusSolver
+from ovos_bm25_solver.embed import JsonEmbeddingsDB, BM25TextEmbeddingsStore
+from ovos_bm25_solver.demo import BM25FreebaseQASolver, BM25SquadQASolver
