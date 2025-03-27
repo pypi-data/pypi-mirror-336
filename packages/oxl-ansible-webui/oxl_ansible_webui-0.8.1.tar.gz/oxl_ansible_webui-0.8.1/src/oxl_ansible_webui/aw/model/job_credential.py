4eca683d0680971463f67ae0caf3e02d07d8ceca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 17:06:38 +0000

    Add package oxl-ansible-webui/0.8.1/oxl_ansible_webui-0.8.1.tar.gz

---
+++ packages/oxl-ansible-webui/oxl_ansible_webui-0.8.1.tar.gz/oxl_ansible_webui-0.8.1/src/oxl_ansible_webui/aw/model/job_credential.py
@@ -0,0 +1,152 @@
+from django.db import models
+
+from aw.model.base import BaseModel, DEFAULT_NONE
+from aw.utils.util import is_null, is_set
+from aw.utils.crypto import decrypt, encrypt
+from aw.base import USERS
+
+
+class BaseJobCredentials(BaseModel):
+    SECRET_ATTRS = ['become_pass', 'vault_pass', 'connect_pass', 'ssh_key']
+    PUBLIC_ATTRS_ARGS = {
+        'connect_user': '--user',
+        'become_user': '--become-user',
+        'vault_file': '--vault-password-file',
+        'vault_id': '--vault-id',
+    }
+    EMPTY_ATTRS = [
+        'connect_user', 'vault_file', 'vault_id', 'vault_pass', 'become_pass', 'connect_pass', 'ssh_key',
+    ]
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    connect_user = models.CharField(max_length=100, **DEFAULT_NONE)
+    become_user = models.CharField(max_length=100, default='root', null=True, blank=True)
+    # default become_user according to ansible-playbook docs
+    vault_file = models.CharField(max_length=300, **DEFAULT_NONE)
+    vault_id = models.CharField(max_length=50, **DEFAULT_NONE)
+
+    _enc_vault_pass = models.CharField(max_length=500, **DEFAULT_NONE)
+    _enc_become_pass = models.CharField(max_length=500, **DEFAULT_NONE)
+    _enc_connect_pass = models.CharField(max_length=500, **DEFAULT_NONE)
+    _enc_ssh_key = models.CharField(max_length=5000, **DEFAULT_NONE)
+
+    @property
+    def vault_pass(self) -> str:
+        if is_null(self._enc_vault_pass):
+            return ''
+
+        return decrypt(self._enc_vault_pass)
+
+    @vault_pass.setter
+    def vault_pass(self, value: str):
+        if is_null(value):
+            self._enc_vault_pass = None
+            return
+
+        self._enc_vault_pass = encrypt(value)
+
+    @property
+    def vault_pass_is_set(self) -> bool:
+        return is_set(self._enc_vault_pass)
+
+    @property
+    def become_pass(self) -> str:
+        if is_null(self._enc_become_pass):
+            return ''
+
+        return decrypt(self._enc_become_pass)
+
+    @become_pass.setter
+    def become_pass(self, value: str):
+        if is_null(value):
+            self._enc_become_pass = None
+            return
+
+        self._enc_become_pass = encrypt(value)
+
+    @property
+    def become_pass_is_set(self) -> bool:
+        return is_set(self._enc_become_pass)
+
+    @property
+    def connect_pass(self) -> str:
+        if is_null(self._enc_connect_pass):
+            return ''
+
+        return decrypt(self._enc_connect_pass)
+
+    @connect_pass.setter
+    def connect_pass(self, value: str):
+        if is_null(value):
+            self._enc_connect_pass = None
+            return
+
+        self._enc_connect_pass = encrypt(value)
+
+    @property
+    def connect_pass_is_set(self) -> bool:
+        return is_set(self._enc_connect_pass)
+
+    @property
+    def ssh_key(self) -> str:
+        if is_null(self._enc_ssh_key):
+            return ''
+
+        return decrypt(self._enc_ssh_key)
+
+    @ssh_key.setter
+    def ssh_key(self, value: str):
+        if is_null(value):
+            self._enc_ssh_key = None
+            return
+
+        self._enc_ssh_key = encrypt(value)
+
+    @property
+    def ssh_key_is_set(self) -> bool:
+        return is_set(self._enc_ssh_key)
+
+    def _get_set_creds_str(self) -> str:
+        creds_set = [attr for attr in self.SECRET_ATTRS if is_set(getattr(self, attr))]
+        creds_set_str = ''
+        if len(creds_set) > 0:
+            creds_set_str = f" ({', '.join(creds_set)})"
+
+        return creds_set_str
+
+    class Meta:
+        abstract = True
+
+
+class JobGlobalCredentials(BaseJobCredentials):
+    api_fields_read = [
+        'id', 'name', 'become_user', 'connect_user', 'vault_file', 'vault_id',
+    ]
+    api_fields_write = api_fields_read.copy()
+    api_fields_write.extend(BaseJobCredentials.SECRET_ATTRS)
+    for secret_attr in BaseJobCredentials.SECRET_ATTRS:
+        api_fields_read.append(f'{secret_attr}_is_set')
+    form_fields = api_fields_write
+
+    def __str__(self) -> str:
+        return f"Global credentials '{self.name}'{self._get_set_creds_str()}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name'], name='jobcreds_name')
+        ]
+
+
+class JobUserCredentials(BaseJobCredentials):
+    api_fields_read = JobGlobalCredentials.api_fields_read.copy()
+    api_fields_read.extend(['user', 'category'])
+    api_fields_write = JobGlobalCredentials.api_fields_write.copy()
+    api_fields_write.extend(['category'])
+    form_fields = JobGlobalCredentials.api_fields_write.copy()
+    form_fields.append('category')
+
+    user = models.ForeignKey(USERS, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100, **DEFAULT_NONE)
+
+    def __str__(self) -> str:
+        return f"Credentials '{self.name}' of user '{self.user.username}'{self._get_set_creds_str()}"
---
+++ packages/oxl-ansible-webui/oxl_ansible_webui-0.8.1.tar.gz/oxl_ansible_webui-0.8.1/src/oxl_ansible_webui/db.py
@@ -0,0 +1,244 @@
+from pathlib import Path
+from shutil import copy, move
+from datetime import datetime
+from sys import exit as sys_exit
+from secrets import choice as random_choice
+from string import digits, ascii_letters
+from os import listdir, remove
+from time import time
+from sqlite3 import connect as db_connect
+from sqlite3 import OperationalError, DatabaseError
+
+from aw.config.main import VERSION
+from aw.settings import DB_FILE
+from aw.utils.subps import process
+from aw.utils.debug import log, log_error, log_warn
+from aw.utils.deployment import deployment_prod, is_release_version
+from aw.config.hardcoded import FILE_TIME_FORMAT, GRP_MANAGER
+from aw.config.environment import check_aw_env_var_true, get_aw_env_var, check_aw_env_var_is_set
+
+# pylint: disable=C0415
+
+DB_BACKUP_EXT = '.auto.bak'
+DB_BACKUP_RETENTION_DAYS = 7
+
+if not deployment_prod():
+    DB_BACKUP_RETENTION_DAYS = 1
+
+DB_BACKUP_RETENTION = DB_BACKUP_RETENTION_DAYS * 24 * 60 * 60
+
+
+def _check_if_writable():
+    try:
+        test_file = DB_FILE.parent / '.awtest'
+        with open(test_file, 'w', encoding='utf-8') as _file:
+            _file.write('TEST')
+
+        remove(test_file)
+
+    except PermissionError:
+        log(msg=f"Error: DB directory is not writable: '{DB_FILE.parent}'")
+        sys_exit(1)
+
+
+def _schema_up_to_date_base() -> bool:
+    try:
+        with db_connect(DB_FILE) as conn:
+            return conn.execute('SELECT schema_version FROM aw_schemametadata').fetchall()[0][0] == VERSION
+
+    except (IndexError, OperationalError):
+        return False
+
+
+def _schema_up_to_date() -> bool:
+    if not Path(DB_FILE).is_file():
+        return False
+
+    try:
+        return _schema_up_to_date_base()
+
+    except DatabaseError as err:
+        # this may happen if WAL or SHM got corrupted (p.e. connection was not closed gracefully)
+        log_warn(msg=f"Trying to fix database error: '{err}'", _stderr=True)
+        backup_ext = f".{datetime.now().strftime(FILE_TIME_FORMAT)}{DB_BACKUP_EXT}"
+        db_shm = f'{DB_FILE}-shm'
+        db_wal = f'{DB_FILE}-wal'
+
+        if Path(db_shm).is_file():
+            move(db_shm, f'{db_shm}{backup_ext}')
+
+        if Path(db_wal).is_file():
+            move(db_wal, f'{db_wal}{backup_ext}')
+
+        return _schema_up_to_date_base()
+
+
+def _get_current_schema_version() -> (str, None):
+    try:
+        with db_connect(DB_FILE) as conn:
+            return conn.execute('SELECT schema_version FROM aw_schemametadata').fetchall()[0][0]
+
+    except (IndexError, OperationalError):
+        return None
+
+
+def _update_schema_version() -> None:
+    previous = _get_current_schema_version()
+
+    with db_connect(DB_FILE) as conn:
+        try:
+            if previous is None:
+                conn.execute(
+                    "INSERT INTO aw_schemametadata (created, updated, schema_version) VALUES "
+                    f"(DATETIME('now'), DATETIME('now'), '{VERSION}')",
+                )
+
+            else:
+                conn.execute(
+                    "UPDATE aw_schemametadata SET "
+                    f"schema_version = '{VERSION}', schema_version_prev = '{previous}', "
+                    "updated = DATETIME('now') WHERE id = 1"
+                )
+
+            conn.commit()
+
+        except (IndexError, OperationalError) as err:
+            log(msg=f"Error updating database schema version: '{err}'", level=3)
+
+
+def install_or_migrate_db():
+    log(msg=f"Using DB: {DB_FILE}", level=4)
+    _check_if_writable()
+    if not Path(DB_FILE).is_file():
+        return install()
+
+    return migrate()
+
+
+def _manage_db(action: str, cmd: list, backup: str = None) -> dict:
+    cmd2 = ['python3', 'manage.py']
+    cmd2.extend(cmd)
+
+    result = process(cmd=cmd2)
+
+    if result['rc'] != 0:
+        log_error(f'Database {action} failed!')
+        log(msg=f"Error:\n{result['stderr']}", level=1)
+        log(msg=f"Output:\n{result['stdout']}", level=3)
+
+        if backup is not None:
+            log_warn(
+                msg=f"Trying to restore database from automatic backup: {backup} => {DB_FILE}",
+                _stderr=True,
+            )
+            copy(src=DB_FILE, dst=f'{backup}.failed')
+            copy(src=backup, dst=DB_FILE)
+
+        else:
+            sys_exit(1)
+
+    return result
+
+
+def _clean_old_db_backups():
+    possible_db_backup_files = listdir(DB_FILE.parent)
+    for file in possible_db_backup_files:
+        if file.startswith(DB_FILE.name) and file.endswith(DB_BACKUP_EXT):
+            backup_file = DB_FILE.parent / file
+            backup_age = time() - backup_file.stat().st_mtime
+            if backup_age > DB_BACKUP_RETENTION:
+                log(msg=f"Cleaning old backup file: '{backup_file}'", level=4)
+                remove(backup_file)
+
+
+def install():
+    log(msg=f"Initializing database {DB_FILE}..", level=3)
+    _migration_needed()
+    _manage_db(action='initialization', cmd=['migrate'])
+    _update_schema_version()
+
+
+def migrate():
+    _clean_old_db_backups()
+
+    if _migration_needed() and check_aw_env_var_true(var='db_migrate', fallback=True):
+        backup = f"{DB_FILE}.{datetime.now().strftime(FILE_TIME_FORMAT)}{DB_BACKUP_EXT}"
+        log(msg=f"Creating database backup: '{backup}'", level=6)
+        copy(src=DB_FILE, dst=backup)
+
+        log(msg=f"Upgrading database {DB_FILE}", level=3)
+        if _manage_db(action='migration', cmd=['migrate'], backup=backup)['rc'] == 0:
+            _update_schema_version()
+
+
+def _migration_needed() -> bool:
+    if is_release_version():
+        # stable versions should only have released migrations
+        return not _schema_up_to_date()
+
+    changed = False
+
+    for stdout in [
+        _manage_db(action='schema-creation', cmd=['makemigrations'])['stdout'],
+        _manage_db(action='schema-creation', cmd=['makemigrations', 'aw'])['stdout'],
+    ]:
+        if stdout.find('No changes detected') == -1:
+            changed = True
+
+    return changed or not _schema_up_to_date()
+
+
+def _get_random_pwd() -> str:
+    return ''.join(random_choice(ascii_letters + digits + '!.-+') for _ in range(14))
+
+
+def create_first_superuser():
+    from aw.base import USERS
+    if len(USERS.objects.filter(is_superuser=True)) == 0:
+        name = get_aw_env_var('init_admin')
+        pwd = get_aw_env_var('init_admin_pwd')
+
+        if name is None:
+            name = 'ansible'
+
+        if pwd is None:
+            pwd = _get_random_pwd()
+
+        USERS.objects.create_superuser(
+            username=name,
+            email=f"{name}@localhost",
+            password=pwd
+        )
+
+        log_warn('No admin was found in the database!')
+        if check_aw_env_var_is_set('init_admin_pwd'):
+            log(msg=f"The user '{name}' was created!", level=4)
+
+        else:
+            log(msg=f"Generated user: '{name}'", level=3)
+            log(msg=f"Generated pwd: '{pwd}'", level=3)
+            log_warn('Make sure to change the password!')
+
+
+def create_manager_groups():
+    from django.contrib.auth.models import Group
+    for grp in GRP_MANAGER.values():
+        Group.objects.get_or_create(name=grp)
+
+
+def create_schedule_user():
+    # just to reserve the username as it is referenced internally
+    from aw.base import USERS
+    if len(USERS.objects.filter(username='schedule')) == 0:
+        USERS.objects.create(
+            username='schedule',
+            email='schedule@localhost',
+            password=_get_random_pwd(),
+        )
+
+
+def cleanup_job_stati():
+    from aw.model.base import JOB_EXEC_STATI_ACTIVE, JOB_EXEC_STATUS_FAILED
+    from aw.model.job import JobExecution
+
+    JobExecution.objects.filter(status__in=JOB_EXEC_STATI_ACTIVE).update(status=JOB_EXEC_STATUS_FAILED)
