ac3d98dfe92523773ec28a7a450efa946c7005ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:49 +0000

    Add package Mathmodule590011276/0.1.0/mathmodule590011276-0.1.0.tar.gz

---
+++ packages/Mathmodule590011276/mathmodule590011276-0.1.0.tar.gz/mathmodule590011276-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Mathmodule590011276",  
+    version="0.1.0",
+    author="Shivangi Singh",
+    author_email="shivangisingh200603@gmail.com",
+    description="Maths Functions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
