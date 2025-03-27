d25b929967afec29ef45eb2e0ac60418293a3eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:40 +0000

    Add package clinicaltrials-interact/0.1.5/clinicaltrials_interact-0.1.5.tar.gz

---
+++ packages/clinicaltrials-interact/clinicaltrials_interact-0.1.5.tar.gz/clinicaltrials_interact-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="clinicaltrials_interact",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+    "requests>=2.32.3,<3.0.0",
+    "pandas>=2.2.0,<3.0.0",
+    "sentence-transformers>=3.4.1,<4.0.0",
+    "scikit-learn>=1.6.1,<2.0.0",
+    "matplotlib>=3.10.1,<4.0.0",
+    "seaborn>=0.13.2,<1.0.0",
+    "plotly>=5.0.0",
+    # Add other runtime dependencies as needed...
+    ],
+    author="KeyVuLee",
+    description="A Python package to interact with ClinicalTrials.gov API v2",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hsph-bst236/midterm-project-keyvulee-innovations/tree/api/clinicaltrials_interact",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6, <3.13',
+)
