a4cb718271b772cdd1d5ce4103ca668346c09cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 20:06:41 +0000

    Add package java-gradescope-autograder-helper/1.2.3/java_gradescope_autograder_helper-1.2.3.tar.gz

---
+++ packages/java-gradescope-autograder-helper/java_gradescope_autograder_helper-1.2.3.tar.gz/java_gradescope_autograder_helper-1.2.3/src/java_gradescope_autograder_helper/helpers.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+
+
+class ConfigurationError(Exception):
+    """
+    Raised when there is an error in the user's configuration.
+    """
+
+    pass
+
+
+def find_absolute_path(search, cwd=None):
+    """
+    Searches for an absolute path ending with the specified string within a
+    directory tree.
+
+    Raises:
+        ConfigurationError: If no file or directory ending with `search` is
+            found in the specified directory tree, or if multiple matching
+            instances are found.
+    """
+
+    if cwd is None:
+        cwd = Path.cwd()
+
+    instances = []
+    for root, dirs, files in os.walk(cwd):
+        for thing in dirs + files:
+            absolute_thing_path = os.path.join(root, thing)
+            if absolute_thing_path.endswith(search):
+                instances.append(absolute_thing_path)
+
+    if len(instances) == 1:
+        return instances[0]
+
+    if len(instances) > 1:
+        raise ConfigurationError(
+            f'Tried finding only one instance of the required file "{search}" in "{cwd}" but found multiple.'
+        )
+
+    raise ConfigurationError(
+        f'Tried finding the required file "{search}" in "{cwd}" but it was not there.'
+    )
+
+
+# Gradescope autograder file structure: https://gradescope-autograders.readthedocs.io/en/latest/specs/#file-hierarchy
+ABSOLUTE_SOURCE_DIR = "/autograder/source"
+ABSOLUTE_SUBMISSION_DIR = "/autograder/submission"
+ABSOLUTE_RESULTS_DIR = "/autograder/results"
