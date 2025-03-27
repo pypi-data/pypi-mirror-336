773a7f40aebc2fd2370f02940cbdfc2263bec53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:48 +0000

    Add package maravilla/0.0.0/maravilla-0.0.0-py3-none-any.whl

---
+++ packages/maravilla/maravilla-0.0.0-py3-none-any.whl/num.py
@@ -0,0 +1,12 @@
+def pgcd(a,b):
+    while a > 1 && b > 1:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    if a == 1 || b == 1:
+        return 1
+    if a == 0:
+        return b
+    if b == 0:
+        return a
