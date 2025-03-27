6d3623bea27458ebde66d25bf3a31200478f24c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 21:09:31 +0000

    Add package aar-doc/2.1.0/aar_doc-2.1.0.tar.gz

---
+++ packages/aar-doc/aar_doc-2.1.0.tar.gz/aar_doc-2.1.0/aar_doc/core.py
@@ -0,0 +1,321 @@
+"""
+Core module for aar_doc.
+
+This module provides utilities for processing Ansible role meta files
+and rendering jinja2 templates from processing data.
+"""
+
+import json
+import pathlib
+import re
+from enum import Enum
+
+import jinja2
+import typer
+from ruamel.yaml import YAML, YAMLError
+
+yaml = YAML()
+yaml.indent(mapping=2, sequence=2, offset=2)
+yaml.version = (1, 1)
+yaml.encoding = "utf-8"
+yaml.allow_unicode = True
+
+
+def ansible_doc_markup(text):
+    """
+    Create Ansible specific markup
+    Regular expressions copied from ansible-doc:
+    https://github.com/ansible/ansible/blob/devel/lib/ansible/cli/doc.py#L436
+    """
+    out = re.sub(r"\bI\(([^)]+)\)", r"*\1*", text)  # I(text)     -> *text*
+    out = re.sub(r"\bB\(([^)]+)\)", r"**\1**", out)  # B(text)     -> **text**
+    out = re.sub(r"\bC\(([^)]+)\)", r"`\1`", out)  # C(text)     -> `text`
+    out = re.sub(r"\bM\(([^)]+)\)", r"`\1`", out)  # M(module)   -> `module`
+    out = re.sub(r"\bO\(((?:[^\\)]+|\\.)+)\)", r"`\1`", out)  # O(option)   -> `option`
+    out = re.sub(r"\bV\(((?:[^\\)]+|\\.)+)\)", r"`\1`", out)  # V(value)    -> `value`
+    out = re.sub(r"\bV\(((?:[^\\)]+|\\.)+)\)", r"`\1`", out)  # E(env)      -> `env`
+    out = re.sub(r"\bV\(((?:[^\\)]+|\\.)+)\)", r"`\1`", out)  # RV(retval)  -> `retval`
+    out = re.sub(r"\bU\(([^)]+)\)", r"[\1]", out)  # U(url)      -> [url]
+    out = re.sub(
+        r"\bL\(([^)]+), *([^)]+)\)",
+        r"[\1](\2)",
+        out,
+    )  # L(text,url) -> [text](url)
+    out = re.sub(
+        r"\bR\(([^)]+), *([^)]+)\)",
+        r"[\1](#\2)",
+        out,
+    )  # R(text,frag) -> [text](#frag)
+    out = re.sub(r"\bHORIZONTALLINE\b", r"\n\n---\n", out)  # HORIZONTALLINE -> ---
+    return out
+
+
+class OutputMode(Enum):
+    """
+    Defines the options for the output mode.
+    """
+
+    INJECT = "inject"
+    REPLACE = "replace"
+
+
+def parse_config(
+    ctx: typer.Context,
+    config_file: pathlib.Path,
+) -> None:
+    """
+    Parses the configuration file
+    """
+    # This will change the defaults when called with --help
+    # See: https://github.com/tiangolo/typer/issues/347
+
+    config_file_full_path: pathlib.Path = config_file.expanduser()
+
+    if config_file_full_path.exists():
+        try:
+            with open(config_file_full_path, "r", encoding="utf-8") as f:
+                content = yaml.load(f)
+
+                ctx.default_map = ctx.default_map or {}
+                ctx.default_map.update(content)
+        except Exception as ex:
+            raise typer.BadParameter(str(ex))
+
+
+def parse_meta(ctx: typer.Context) -> tuple[dict, dict]:
+    """
+    Parses Ansible role metadata from YAML files in the meta/ directory.
+    """
+
+    meta: pathlib.Path = ctx.obj["config"]["role_path"] / "meta"
+    argument_specs: dict = {}
+
+    try:
+        argument_specs_yml: pathlib.Path = list(meta.glob("argument_specs.y*ml"))[0]
+        with open(argument_specs_yml, "r", encoding="utf-8") as f:
+            try:
+                argument_specs = yaml.load(f)
+            except YAMLError:
+                typer.echo("Not a valid YAML file: meta/argument_specs.y[a]ml")
+            try:
+                argument_specs = argument_specs.get("argument_specs", {})
+            except TypeError:
+                typer.echo("Could not read meta/argument_specs.y[a]ml")
+    except (UnboundLocalError, IndexError):
+        pass
+
+    try:
+        main_yml = list(meta.glob("main.y*ml"))[0]
+    except IndexError as exc:
+        typer.echo("Could not find meta/main.y[a]ml")
+        raise typer.Exit(code=1) from exc
+
+    with open(main_yml, "r", encoding="utf-8") as f:
+        try:
+            main = yaml.load(f)
+        except YAMLError as exc:
+            typer.echo("Not a valid YAML file: meta/main.y[a]ml")
+            raise typer.Exit(1) from exc
+        if not argument_specs:
+            try:
+                argument_specs = main["argument_specs"]
+            except TypeError as exc:
+                typer.echo(
+                    "Could not read meta/main.y[a]ml or meta/argument_specs.y[a]ml",
+                )
+                raise typer.Exit(1) from exc
+    return main, argument_specs
+
+
+def parse_collection(ctx: typer.Context) -> dict:
+    """
+    Parses Ansible Galaxy collection metadata from YAML files.
+    """
+
+    collection_dir = ctx.obj["config"]["role_path"].parent.parent
+
+    galaxy_files = list(collection_dir.glob("galaxy.y*ml"))
+
+    if galaxy_files:
+        galaxy_yml = galaxy_files[0]
+
+        with open(galaxy_yml, "r", encoding="utf-8") as f:
+            collection = yaml.load(f)
+
+        return collection
+    return {}
+
+
+def gather_choices(path: list[str], arguments: dict) -> list[tuple[list[str], list]]:
+    """
+    Walks through an argument_specs 'options' section gathering choices for options
+    and adding them to a list with a 'path' describing the entrypoint/options they
+    belong to.
+    """
+    results = []
+    if "options" in arguments:
+        options = arguments["options"]
+        for name, details in options.items():
+            if "choices" in details:
+                results.append((path + [name], details["choices"]))
+            if "options" in details:
+                results.extend(gather_choices(path + [name], details))
+
+    return results
+
+
+def parse_choices(ctx: typer.Context) -> dict:
+    """
+    Parses argument_specs into options structure, with sections for subptions
+    and translation of formats into display formats.
+    """
+    entrypoint_choices = {}
+    for entrypoint, arguments in ctx.obj["data"]["argument_specs"].items():
+        gathered_choices = gather_choices([entrypoint], arguments)
+        entrypoint_choices[entrypoint] = gathered_choices
+
+    return entrypoint_choices
+
+
+def gather_options(path: list[str], arguments: dict) -> list[tuple[list[str], dict]]:
+    """
+    Walks through an argument_specs 'options' section gathering groups of suboptions
+    and adding them to a list with a 'path' describing the entrypoint/options they
+    belong to.
+    """
+    results = []
+    if "options" in arguments:
+        options = arguments["options"]
+        results.append((path, options))
+        for name, details in options.items():
+            if "options" in details:
+                results.extend(gather_options(path + [name], details))
+
+    return results
+
+
+def parse_options(ctx: typer.Context) -> dict:
+    """
+    Parses argument_specs into options structure, with sections for subptions
+    and translation of formats into display formats.
+    """
+    entrypoint_options = {}
+    for entrypoint, arguments in ctx.obj["data"]["argument_specs"].items():
+        gathered_options = gather_options([entrypoint], arguments)
+        for _, options in gathered_options:
+            for option, details in options.items():
+                details["display_required"] = (
+                    "yes" if details.get("required", False) else "no"
+                )
+                description = details["description"] if "description" in details else ""
+                details["display_description"] = (
+                    (
+                        description
+                        if isinstance(description, str)
+                        else (" ").join(description)
+                    )
+                    .replace("\n", " ")
+                    .strip()
+                )
+
+                display_type = details.get("type", "str")
+
+                if display_type == "list":
+                    elements = details.get("elements", "")
+                    if elements == "dict" and "options" in details:
+                        display_type = f"list of dicts of '{option}' options"
+                    else:
+                        display_type = f"list of '{elements}'"
+                elif display_type == "dict":
+                    if "options" in details:
+                        display_type = f"dict of '{option}' options"
+
+                details["display_type"] = display_type
+
+                if "default" in details:
+                    default_value = details.get("default", "")
+                    details["display_default"] = str(default_value).strip()
+
+                    if display_type in ["list", "dict"]:
+                        details["display_default"] = (
+                            json.dumps(default_value) if default_value else ""
+                        )
+
+                    elif display_type == "str":
+                        if not isinstance(default_value, str):
+                            typer.echo(
+                                f"The default value of the argument {option} "
+                                f"is of type {type(default_value).__name__}, need str",
+                            )
+                            raise typer.Exit(1)
+
+                else:
+                    details["display_default"] = ""
+
+        entrypoint_options[entrypoint] = gathered_options
+
+    return entrypoint_options
+
+
+def render_content(ctx: typer.Context, content_template: str) -> str:
+    """
+    Renders Jinja2 templates twice, the first time to get the parsed
+    data into {{ content }} and finally to render the user provided
+    (or default) output template to be written to the file.
+    """
+
+    env = jinja2.Environment(
+        loader=jinja2.PackageLoader("aar_doc"),
+        autoescape=jinja2.select_autoescape(),
+        undefined=jinja2.StrictUndefined,
+    )
+    env.filters["ansible_doc_markup"] = ansible_doc_markup
+
+    role = ctx.obj["config"]["role"]
+    metadata = ctx.obj["data"]["metadata"]
+    argument_specs = ctx.obj["data"]["argument_specs"]
+    collection = ctx.obj["data"]["galaxy_collection"]
+    entrypoint_options = ctx.obj["data"]["entrypoint_options"]
+    entrypoint_choices = ctx.obj["data"]["entrypoint_choices"]
+
+    if "namespace" in collection:
+        role = collection["namespace"] + "." + collection["name"] + "." + role
+
+    content = env.get_template(content_template).render(
+        role=role,
+        metadata=metadata,
+        argument_specs=argument_specs,
+        galaxy_collection=collection,
+        entrypoint_options=entrypoint_options,
+        entrypoint_choices=entrypoint_choices,
+    )
+
+    role_path: pathlib.Path = ctx.obj["config"]["role_path"]
+    output_template: str = ctx.obj["config"]["output_template"].replace("\\n", "\n")
+
+    try:
+        output_template_file = pathlib.Path(output_template).resolve(strict=True)
+
+        env = jinja2.Environment(
+            keep_trailing_newline=True,
+            loader=jinja2.FileSystemLoader([role_path, output_template_file.parent]),
+        )
+        env.filters["ansible_doc_markup"] = ansible_doc_markup
+        template = env.get_template(output_template_file.name)
+    except (FileNotFoundError, OSError):
+        env = jinja2.Environment(
+            keep_trailing_newline=True,
+            loader=jinja2.FileSystemLoader(role_path),
+        )
+        env.filters["ansible_doc_markup"] = ansible_doc_markup
+        template = env.from_string(source=output_template)
+
+    return template.render(
+        content=content,
+        role=role,
+        metadata=metadata,
+        argument_specs=argument_specs,
+        galaxy_collection=collection,
+        entrypoint_options=entrypoint_options,
+        entrypoint_choices=entrypoint_choices,
+    )
---
+++ packages/aar-doc/aar_doc-2.1.0.tar.gz/aar_doc-2.1.0/aar_doc/defaults.py
@@ -0,0 +1,160 @@
+"""
+Defaults module for aar_doc.
+
+This module provides utilities for collecting the default values
+of an argument_spec and writing the final defaults file.
+"""
+
+from dataclasses import dataclass, field
+from os import linesep
+from pathlib import Path
+from typing import Any, Union
+
+from ruamel.yaml import YAML
+from ruamel.yaml.comments import CommentedMap
+from ruamel.yaml.scalarstring import LiteralScalarString, SingleQuotedScalarString
+
+yaml = YAML()
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.encoding = "utf-8"
+yaml.allow_unicode = True
+
+
+@dataclass
+class RoleDefault:
+    """
+    Class that represents a role default.
+    """
+
+    name: str
+    value: Any
+    description: Union[str, list[str]]
+
+
+@dataclass
+class RoleDefaultsManager:
+    """Class for managing all the role defaults of a role.
+
+    Args:
+        _overwrite (bool): Whether a default should be overwritten,
+        if it's already known. Defaults to False.
+    """
+
+    _defaults: dict[str, RoleDefault] = field(default_factory=lambda: {}, init=False)
+    _overwrite: bool = False
+
+    @property
+    def defaults(self) -> list[RoleDefault]:
+        """Get the list of tracked defaults.
+
+        Returns:
+            list[RoleDefault]: List of role defaults.
+        """
+        return list(self._defaults.values())
+
+    def add_default(
+        self,
+        name: str,
+        value: Any,
+        description: Union[str, list],
+    ) -> None:
+        """Add a default.
+
+        Args:
+            name (str): Variable name of the default.
+            value (Any): Value of the default.
+            description (str, optional): Description of the default.
+        """
+        if isinstance(value, str):
+            value = value.strip()
+
+        if self._overwrite:
+            self._defaults[name] = RoleDefault(name, value, description)
+        else:
+            self._defaults.setdefault(name, RoleDefault(name, value, description))
+
+    def safe_quote_recursive(self, value):
+        if isinstance(value, list):
+            return [self.safe_quote_recursive(v) for v in value]
+        elif isinstance(value, dict):
+            return {k: self.safe_quote_recursive(v) for k, v in value.items()}
+        elif isinstance(value, str):
+            if value in ("yes", "no"):
+                return SingleQuotedScalarString(value)
+            elif "\n" in value:
+                return LiteralScalarString(value)
+            elif ":" in value:
+                return SingleQuotedScalarString(value)
+        return value
+
+    def to_commented_map(self) -> CommentedMap:
+        """
+        Returns all tracked defaults as a CommentedMap.
+        """
+        commented_defaults = CommentedMap()
+        for role_default in self.defaults:
+            value = self.safe_quote_recursive(role_default.value)
+            commented_defaults[role_default.name] = value
+            description_items = (
+                role_default.description
+                if isinstance(role_default.description, list)
+                else [role_default.description]
+            )
+
+            for description_item in description_items:
+                commented_defaults.yaml_set_comment_before_after_key(
+                    key=role_default.name,
+                    before=description_item,
+                )
+
+        return commented_defaults
+
+
+def generate_commented_defaults(
+    argument_spec_data: dict,
+    overwrite_duplicate_defaults: bool,
+) -> CommentedMap:
+    """Generates the inital RoleDefaults"""
+    defaults_manager = RoleDefaultsManager(overwrite_duplicate_defaults)
+
+    for entry_point in argument_spec_data:
+        options: dict[str, Any] = argument_spec_data.get(entry_point, {}).get("options")
+        if not options:
+            continue
+        for name, spec in options.items():
+            value = spec.get("default")
+            if value is not None:
+                description = spec.get("description")
+                defaults_manager.add_default(
+                    name,
+                    value,
+                    description,
+                )
+    return defaults_manager.to_commented_map()
+
+
+def write_defaults(
+    output_file_path: Path,
+    role_path: Path,
+    role_defaults: CommentedMap,
+) -> None:
+    """
+    Writes the generated defaults CommentedMap to the given file.
+    """
+    if output_file_path.name == "README.md":
+        output: Path = role_path / "defaults" / "main.yml"
+    elif output_file_path.is_absolute():
+        output = output_file_path
+    else:
+        output = role_path / "defaults" / output_file_path
+    output.resolve()
+
+    # Create parent directories if they don't exist yet.
+    # Needed if the <role_path>/defaults was not created yet.
+    output.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(output, "w", encoding="utf-8") as defaults_file:
+        defaults_file.writelines(
+            ["---" + linesep, "# Automatically generated by aar-doc" + linesep],
+        )
+        yaml.dump(role_defaults, defaults_file)
