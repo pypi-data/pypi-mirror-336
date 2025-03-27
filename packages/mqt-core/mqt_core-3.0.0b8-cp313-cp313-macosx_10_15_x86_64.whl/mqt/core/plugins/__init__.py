9009fe7829bd0329d8c4d8e1add6466c25ef6ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:28 +0000

    Add package mqt-core/3.0.0b8/mqt_core-3.0.0b8-cp313-cp313-macosx_10_15_x86_64.whl

---
+++ packages/mqt-core/mqt_core-3.0.0b8-cp313-cp313-macosx_10_15_x86_64.whl/mqt/core/plugins/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2025 Chair for Design Automation, TUM
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""Plugins for the core package."""
+
+from __future__ import annotations
+
+from . import qiskit
+
+__all__ = [
+    "qiskit",
+]
