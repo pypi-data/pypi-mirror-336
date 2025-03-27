171aa172c000c2536301d4121964a0619d1436b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 15:06:52 +0000

    Add package dbnd-monitor/1.0.27.18/dbnd_monitor-1.0.27.18.tar.gz

---
+++ packages/dbnd-monitor/dbnd_monitor-1.0.27.18.tar.gz/dbnd_monitor-1.0.27.18/src/dbnd_monitor/base_integration_config.py
@@ -0,0 +1,60 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+from typing import Optional
+from uuid import UUID
+
+import attr
+
+from dbnd_monitor.base_monitor_config import BaseMonitorConfig
+
+
+DEFAULT_SYNC_INTERVAL_IN_SECONDS = 10
+DEFAULT_SYNC_BULK_SIZE = 10
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class BaseIntegrationConfig:
+    uid: UUID
+    source_name: str
+    source_type: str
+    tracking_source_uid: UUID
+
+    sync_interval: int = DEFAULT_SYNC_INTERVAL_IN_SECONDS
+    sync_bulk_size: int = DEFAULT_SYNC_BULK_SIZE
+    fetcher_type: Optional[str] = None
+
+    log_level: str = None
+    syncer_max_retries: int = 5
+
+    credentials: str = ""
+    integration_config: dict = {}
+
+    @classmethod
+    def create(cls, config: dict, monitor_config: Optional[BaseMonitorConfig] = None):
+        if "integration_config" not in config:
+            raise KeyError(
+                f"Unable to create server config for integration with uid={config.get('uid')}, name={config.get('name')} integration_config is not found."
+            )
+        integration_config = config.get("integration_config")
+        tracking_source_uid = None
+        if "tracking_source_uid" in config:
+            tracking_source_uid = config["tracking_source_uid"]
+        elif len(integration_config.get("synced_entities", [])) > 0:
+            tracking_source_uid = integration_config["synced_entities"][0][
+                "tracking_source_uid"
+            ]
+        return BaseIntegrationConfig(
+            uid=config["uid"],
+            source_type=config["integration_type"],
+            source_name=config["name"],
+            credentials=config["credentials"],
+            tracking_source_uid=tracking_source_uid,
+            integration_config=integration_config,
+            sync_interval=integration_config.get(
+                "sync_interval", DEFAULT_SYNC_INTERVAL_IN_SECONDS
+            ),
+            # currently some existing integrations has "runs_bulk_size" field, fix it :)
+            sync_bulk_size=integration_config.get(
+                "runs_bulk_size", DEFAULT_SYNC_BULK_SIZE
+            ),
+        )
---
+++ packages/dbnd-monitor/dbnd_monitor-1.0.27.18.tar.gz/dbnd_monitor-1.0.27.18/src/dbnd_monitor/error_handler.py
@@ -0,0 +1,94 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+import logging
+import traceback
+
+from contextlib import contextmanager
+from datetime import timedelta
+from typing import Optional
+from uuid import UUID
+
+from dbnd._core.utils.timezone import utcnow
+from dbnd._vendor.cachetools import TTLCache, cached
+from dbnd_monitor.base_component import BaseComponent
+from dbnd_monitor.errors import ClientConnectionError
+from dbnd_monitor.reporting_service import ReportingService
+
+
+logger = logging.getLogger(__name__)
+ERRORS_TO_IGNORE = [ClientConnectionError]
+
+
+@contextmanager
+def capture_component_exception(component: BaseComponent, function_name: str):
+    syncer_logger = getattr(component.__module__, "logger", None) or logging.getLogger(
+        component.__module__
+    )
+    class_name = component.__class__.__name__
+    full_function_name = f"{class_name}.{function_name}"
+
+    syncer_logger.debug("Running function %s from %s", function_name, component)
+
+    try:
+        yield
+        # No errors, send error None to clean the existing error if any
+        _report_error(
+            component.reporting_service, component.config.uid, full_function_name, None
+        )
+
+        component.report_sync_metrics(is_success=True)
+
+    except Exception as exc:
+        syncer_logger.exception(
+            "Error when running function %s from %s, integration_uid: %s, tracking_source_uid: %s",
+            function_name,
+            class_name,
+            component.config.uid,
+            str(component.config.tracking_source_uid),
+        )
+
+        err_message = traceback.format_exc()
+        if not should_ignore_error(exc):
+            _log_exception_to_server(err_message, component.reporting_service)
+
+        err_message += f"\nTimestamp: {utcnow()}"
+
+        _report_error(
+            component.reporting_service,
+            component.config.uid,
+            full_function_name,
+            err_message,
+        )
+
+        component.report_sync_metrics(is_success=False)
+
+
+log_exception_cache = TTLCache(maxsize=5, ttl=timedelta(hours=1).total_seconds())
+
+
+# cached in order to avoid logging same messages over and over again
+@cached(log_exception_cache)
+def _log_exception_to_server(
+    exception_message: str, reporting_service: ReportingService
+):
+    try:
+        reporting_service.report_exception_to_web_server(exception_message)
+    except Exception:
+        logger.warning("Error sending monitoring exception message")
+
+
+def _report_error(
+    reporting_service: ReportingService,
+    syncer_id: UUID,
+    function_name: str,
+    err_message: Optional[str],
+):
+    try:
+        reporting_service.report_error(syncer_id, function_name, err_message)
+    except Exception:
+        logger.warning("Error sending error message", exc_info=True)
+
+
+def should_ignore_error(exc: Exception) -> bool:
+    """returns whether an error should be logged to server"""
+    return any([isinstance(exc, error) for error in ERRORS_TO_IGNORE])
