47afb54ac574c04ae6fff0fe6fa2c634ae1ff1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 19:06:32 +0000

    Add package ExoJAX/2.0/ExoJAX-2.0-py2.py3-none-any.whl

---
+++ packages/ExoJAX/ExoJAX-2.0-py2.py3-none-any.whl/exojax/spec/__init__.py
@@ -0,0 +1,21 @@
+__all__ = []
+
+__version__ = '1.6'
+__uri__ = ''
+__author__ = 'ExoJAX contributors'
+__email__ = 'divrot@gmail.com'
+__license__ = ''
+__description__ = 'auto-differentiable spectral modules in exojax'
+
+from exojax.spec.hitran import (line_strength, doppler_sigma,
+                                gamma_natural, normalized_doppler_sigma)
+
+from exojax.spec.lpf import (
+    hjert,
+    voigt,
+    voigtone,
+    vvoigt,
+)
+
+from exojax.spec.make_numatrix import (
+    make_numatrix0, )
