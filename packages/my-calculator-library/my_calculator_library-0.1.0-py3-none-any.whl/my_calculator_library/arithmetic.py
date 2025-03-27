11bfba2dc7e75e6e5a321efe54c0987642403aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:27 +0000

    Add package my-calculator-library/0.1.0/my_calculator_library-0.1.0-py3-none-any.whl

---
+++ packages/my-calculator-library/my_calculator_library-0.1.0-py3-none-any.whl/my_calculator_library/arithmetic.py
@@ -0,0 +1,19 @@
+# calculator/arithmetic.py
+
+def add(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Returns the difference between two numbers."""
+    return a - b
+
+def multiply(a, b):
+    """Returns the product of two numbers."""
+    return a * b
+
+def divide(a, b):
+    """Returns the quotient of two numbers. Raises an error if dividing by zero."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
