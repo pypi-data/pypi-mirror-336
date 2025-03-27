307f73ae135aa828e63d9c6ba14fe6939bb61dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:35 +0000

    Add package zepben.evolve/0.45.0b6/zepben.evolve-0.45.0b6-py3-none-any.whl

---
+++ packages/zepben.evolve/zepben.evolve-0.45.0b6-py3-none-any.whl/zepben/evolve/model/cim/iec61970/base/core/power_system_resource.py
@@ -0,0 +1,40 @@
+#  Copyright 2024 Zeppelin Bend Pty Ltd
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from __future__ import annotations
+
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from zepben.evolve.model.cim.iec61968.assets.asset_info import AssetInfo
+    from zepben.evolve.model.cim.iec61968.common.location import Location
+from zepben.evolve.model.cim.iec61970.base.core.identified_object import IdentifiedObject
+
+__all__ = ['PowerSystemResource']
+
+
+class PowerSystemResource(IdentifiedObject):
+    """
+    Abstract class, should only be used through subclasses.
+    A power system resource can be an item of equipment such as a switch, an equipment container containing many individual
+    items of equipment such as a substation, or an organisational entity such as sub-control area. Power system resources
+    can have measurements associated.
+    """
+
+    location: Optional[Location] = None
+    """A `zepben.evolve.iec61968.common.location.Location` for this resource."""
+
+    asset_info: Optional[AssetInfo] = None
+    """A subclass of `zepben.evolve.iec61968.assets.asset_info.AssetInfo` providing information about the asset associated with this PowerSystemResource."""
+
+    num_controls: int = 0
+    """Number of Control's known to associate with this [PowerSystemResource]"""
+
+    @property
+    def has_controls(self) -> bool:
+        """
+        * :return: True if this [PowerSystemResource] has at least 1 Control associated with it, false otherwise.
+        """
+        return self.num_controls > 0
