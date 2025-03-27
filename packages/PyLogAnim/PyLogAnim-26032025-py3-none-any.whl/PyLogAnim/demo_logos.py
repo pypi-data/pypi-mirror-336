62a8f33bf4657d59499cc3193fb7ca25b923841d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 19:06:40 +0000

    Add package PyLogAnim/26032025/PyLogAnim-26032025-py3-none-any.whl

---
+++ packages/PyLogAnim/PyLogAnim-26032025-py3-none-any.whl/PyLogAnim/demo_logos.py
@@ -0,0 +1,10 @@
+DEMOLOGO = [
+    "       PyLogAnim       ",
+    " PYthon LOGo ANIMation ",
+    "     by lz in 2025     ",
+    "      MIT License      "
+]
+
+DEMOLOGO_FILL = [
+    "████████████████████"
+]
