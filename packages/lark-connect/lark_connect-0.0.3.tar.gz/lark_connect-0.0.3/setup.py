471efcad7fe6d5cf8b93df7b641395b952bd9f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 21:09:00 +0000

    Add package lark-connect/0.0.3/lark_connect-0.0.3.tar.gz

---
+++ packages/lark-connect/lark_connect-0.0.3.tar.gz/lark_connect-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lark-connect',
+    version='0.0.3',
+    description="A Python package for integrating with Lark API to send messages, manage bots, "
+                "and interact with Lark apps.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author='wontae.jeon',
+    author_email='wontae@aidenlab.io',
+    install_requires=['requests'],
+    packages=find_packages(exclude=[]),
+    keywords=['lark', 'incode8'],
+    python_requires='>=3.8',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
