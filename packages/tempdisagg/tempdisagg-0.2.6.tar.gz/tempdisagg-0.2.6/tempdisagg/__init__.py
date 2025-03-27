f566b114ba3133dba64b99c6b4380478d3eaea42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 20:06:18 +0000

    Add package tempdisagg/0.2.6/tempdisagg-0.2.6.tar.gz

---
+++ packages/tempdisagg/tempdisagg-0.2.6.tar.gz/tempdisagg-0.2.6/tempdisagg/__init__.py
@@ -0,0 +1,37 @@
+"""
+tempdisagg: Temporal disaggregation of low-frequency time series using various statistical methods.
+"""
+
+# Public API
+from .model.tempdisagg_model import TempDisaggModel
+from .utils.retropolarizer import Retropolarizer
+from .core.temporal_aggregator import TemporalAggregator
+
+# Define what gets imported with `from tempdisagg import *`
+__all__ = [
+    "ConversionMatrixBuilder",
+    "DisaggInputPreparer",
+    "EnsemblePrediction",
+    "EnsemblePredictor",
+    "InputPreprocessor",
+    "ModelsHandler",
+    "ModelFitter",
+    "NumericUtils",
+    "PostEstimation",
+    "Retropolarizer",
+    "RhoOptimizer",
+    "TempDisaggAdjuster",
+    "TempDisaggModel",
+    "TempDisaggReporter",
+    "TempDisaggVisualizer",
+    "TemporalAggregator",
+    "TimeSeriesCompleter",
+]
+
+# Version handling using importlib.metadata
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("tempdisagg")
+except PackageNotFoundError:
+    __version__ = "unknown"
