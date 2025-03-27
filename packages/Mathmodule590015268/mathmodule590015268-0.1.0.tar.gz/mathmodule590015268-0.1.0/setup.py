1a85b8843f8975f7e49663a316388b097c4b2d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:31 +0000

    Add package Mathmodule590015268/0.1.0/mathmodule590015268-0.1.0.tar.gz

---
+++ packages/Mathmodule590015268/mathmodule590015268-0.1.0.tar.gz/mathmodule590015268-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Mathmodule590015268",  
+    version="0.1.0",
+    author="Anushka Parashar",
+    author_email="anushkas2323@gmail.com",
+    description="Maths Functions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
