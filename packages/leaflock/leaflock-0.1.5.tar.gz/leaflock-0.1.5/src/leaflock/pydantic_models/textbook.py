602e331cab0bb777114d836e60d9d9e72222061b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 17:06:45 +0000

    Add package leaflock/0.1.5/leaflock-0.1.5.tar.gz

---
+++ packages/leaflock/leaflock-0.1.5.tar.gz/leaflock-0.1.5/src/leaflock/pydantic_models/textbook.py
@@ -0,0 +1,19 @@
+import uuid
+
+from pydantic import BaseModel
+
+from .activity import Activity
+from .topic import Topic
+
+
+class Textbook(BaseModel):
+    guid: uuid.UUID
+    title: str
+    prompt: str
+    authors: str
+
+    activities: set[Activity]
+    topics: set[Topic]
+
+    def __hash__(self) -> int:
+        return hash(self.guid)
