b042ae706d30b548d8e3052f002298c36fe651ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 22:07:25 +0000

    Add package coocan/0.0.1/coocan-0.0.1.tar.gz

---
+++ packages/coocan/coocan-0.0.1.tar.gz/coocan-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("readme.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="coocan",
+    version="0.0.1",
+    author="wauo",
+    author_email="markadc@126.com",
+    description="Air Spider Framework",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
