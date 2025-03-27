6acf7adb24000a06e2590bf825c1cc5d69f06f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:31 +0000

    Add package nto218145/1.1.25/nto218145-1.1.25.tar.gz

---
+++ packages/nto218145/nto218145-1.1.25.tar.gz/nto218145-1.1.25/README.md
@@ -0,0 +1,22 @@
+# This is Python Reader 1.1.25
+
+```markdown
+realpython-reader/
+│
+├── src/
+│   └── reader/
+│       ├── __init__.py
+│       ├── __main__.py
+│       ├── config.toml
+│       ├── feed.py
+│       └── viewer.py
+│
+├── tests/
+│   ├── test_feed.py
+│   └── test_viewer.py
+│
+├── LICENSE
+├── MANIFEST.in
+├── README.md
+└── pyproject.toml
+```
---
+++ packages/nto218145/nto218145-1.1.25.tar.gz/nto218145-1.1.25/src/reader/__init__.py
@@ -0,0 +1,15 @@
+# __init__.py
+
+from importlib import resources
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Version of the realpython-reader package
+__version__ = "1.1.25"
+
+# Read URL of the Real Python feed from config file
+_cfg = tomllib.loads(resources.read_text("reader", "config.toml"))
+URL = _cfg["feed"]["url"]
