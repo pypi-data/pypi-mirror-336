997869681e8ebfff31c81d6ea67ddd93b0700158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 18:06:43 +0000

    Add package hurst-calc/0.1.4/hurst_calc-0.1.4.tar.gz

---
+++ packages/hurst-calc/hurst_calc-0.1.4.tar.gz/hurst_calc-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hurst_calc',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas"
+    ],
+    author="Jeremy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
