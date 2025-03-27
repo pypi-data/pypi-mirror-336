8d73773e1cb0fe0dd1692f5a2afde52cd7cf8b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 16:06:36 +0000

    Add package cardamom/0.0.3/cardamom-0.0.3-py3-none-any.whl

---
+++ packages/cardamom/cardamom-0.0.3-py3-none-any.whl/cardamom/__init__.py
@@ -0,0 +1,14 @@
+"""
+CARDAMOM
+========
+
+Gene regulatory network inference from single-cell data
+-------------------------------------------------------
+
+Author: Elias Ventre
+"""
+__version__ = '1.0'
+
+from .model import NetworkModel
+
+__all__ = ['NetworkModel']
