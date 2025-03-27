8b167700f55cc1abc3a6fc8b5ce101d1a690b84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 21:07:14 +0000

    Add package NoWebsocket/2.0.1/nowebsocket-2.0.1.tar.gz

---
+++ packages/NoWebsocket/nowebsocket-2.0.1.tar.gz/nowebsocket-2.0.1/NoWebsocket/application.py
@@ -0,0 +1,22 @@
+# websocket/application.py
+class WebSocketApplication:
+    """WebSocket应用基类"""
+    def __init__(self):
+        self.connection = None
+        self.path_params = None
+
+    def on_open(self):
+        """连接建立时触发"""
+        pass
+
+    def on_message(self, message):
+        """收到文本消息时触发"""
+        pass
+
+    def on_binary(self, data):
+        """收到二进制消息时触发"""
+        pass
+
+    def on_close(self):
+        """连接关闭时触发"""
+        pass
