96e43a028a498507fca931bb75494b524dee2ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 22:07:03 +0000

    Add package XtendR/0.2.1/xtendr-0.2.1-py3-none-any.whl

---
+++ packages/XtendR/xtendr-0.2.1-py3-none-any.whl/xtendr/xtendrbase.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class XtendRBase(ABC):
+    """Abstract base class for all plugins.
+    
+    Example:
+    >>> class TestPlugin(XtendRBase):
+    ...     def run(self):
+    ...         print("Running TestPlugin")
+    ...     def stop(self):
+    ...         print("Stopping TestPlugin")
+    
+    >>> plugin = TestPlugin()
+    >>> plugin.run()
+    Running TestPlugin
+    >>> plugin.stop()
+    Stopping TestPlugin
+    """
+    @abstractmethod
+    def run(self, *args, **kwargs):
+        pass
+    
+    @abstractmethod
+    def stop(self):
+        pass
