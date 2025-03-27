52c6fbecc52be8f971a776664f04bbc035d0b6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:19 +0000

    Add package s2fft/1.2.2/s2fft-1.2.2.tar.gz

---
+++ packages/s2fft/s2fft-1.2.2.tar.gz/s2fft-1.2.2/benchmarks/precompute_spherical.py
@@ -0,0 +1,114 @@
+"""Benchmarks for precompute spherical transforms."""
+
+import numpy as np
+from benchmarking import (
+    BenchmarkSetup,
+    benchmark,
+    parse_args_collect_and_run_benchmarks,
+    skip,
+)
+
+import s2fft
+import s2fft.precompute_transforms
+
+L_VALUES = [8, 16, 32, 64, 128, 256]
+SPIN_VALUES = [0]
+SAMPLING_VALUES = ["mw"]
+METHOD_VALUES = ["numpy", "jax"]
+REALITY_VALUES = [True]
+RECURSION_VALUES = ["auto"]
+
+
+def setup_forward(method, L, sampling, spin, reality, recursion):
+    if reality and spin != 0:
+        skip("Reality only valid for scalar fields (spin=0).")
+    rng = np.random.default_rng()
+    flm = s2fft.utils.signal_generator.generate_flm(rng, L, spin=spin, reality=reality)
+    f = s2fft.transforms.spherical.inverse(
+        flm,
+        L=L,
+        spin=spin,
+        sampling=sampling,
+        reality=reality,
+    )
+    kernel_function = (
+        s2fft.precompute_transforms.construct.spin_spherical_kernel_jax
+        if method == "jax"
+        else s2fft.precompute_transforms.construct.spin_spherical_kernel
+    )
+    kernel = kernel_function(
+        L=L,
+        spin=spin,
+        reality=reality,
+        sampling=sampling,
+        forward=True,
+        recursion=recursion,
+    )
+    return BenchmarkSetup({"f": f, "kernel": kernel}, flm, "jax" in method)
+
+
+@benchmark(
+    setup_forward,
+    method=METHOD_VALUES,
+    L=L_VALUES,
+    sampling=SAMPLING_VALUES,
+    spin=SPIN_VALUES,
+    reality=REALITY_VALUES,
+    recursion=RECURSION_VALUES,
+)
+def forward(f, kernel, method, L, sampling, spin, reality, recursion):
+    return s2fft.precompute_transforms.spherical.forward(
+        f=f,
+        L=L,
+        spin=spin,
+        kernel=kernel,
+        sampling=sampling,
+        reality=reality,
+        method=method,
+    )
+
+
+def setup_inverse(method, L, sampling, spin, reality, recursion):
+    if reality and spin != 0:
+        skip("Reality only valid for scalar fields (spin=0).")
+    rng = np.random.default_rng()
+    flm = s2fft.utils.signal_generator.generate_flm(rng, L, spin=spin, reality=reality)
+    kernel_function = (
+        s2fft.precompute_transforms.construct.spin_spherical_kernel_jax
+        if method == "jax"
+        else s2fft.precompute_transforms.construct.spin_spherical_kernel
+    )
+    kernel = kernel_function(
+        L=L,
+        spin=spin,
+        reality=reality,
+        sampling=sampling,
+        forward=False,
+        recursion=recursion,
+    )
+    return BenchmarkSetup({"flm": flm, "kernel": kernel}, None, "jax" in method)
+
+
+@benchmark(
+    setup_inverse,
+    method=METHOD_VALUES,
+    L=L_VALUES,
+    sampling=SAMPLING_VALUES,
+    spin=SPIN_VALUES,
+    reality=REALITY_VALUES,
+    recursion=RECURSION_VALUES,
+)
+def inverse(flm, kernel, method, L, sampling, spin, reality, recursion):
+    return s2fft.precompute_transforms.spherical.inverse(
+        flm=flm,
+        L=L,
+        spin=spin,
+        kernel=kernel,
+        sampling=sampling,
+        reality=reality,
+        method=method,
+    )
+
+
+if __name__ == "__main__":
+    results = parse_args_collect_and_run_benchmarks()
---
+++ packages/s2fft/s2fft-1.2.2.tar.gz/s2fft-1.2.2/benchmarks/precompute_wigner.py
@@ -0,0 +1,104 @@
+"""Benchmarks for precompute Wigner-d transforms."""
+
+import numpy as np
+from benchmarking import (
+    BenchmarkSetup,
+    benchmark,
+    parse_args_collect_and_run_benchmarks,
+)
+
+import s2fft
+import s2fft.precompute_transforms
+from s2fft.base_transforms import wigner as base_wigner
+
+L_VALUES = [16, 32, 64, 128, 256]
+N_VALUES = [2]
+L_LOWER_VALUES = [0]
+SAMPLING_VALUES = ["mw"]
+METHOD_VALUES = ["numpy", "jax"]
+REALITY_VALUES = [True]
+MODE_VALUES = ["auto"]
+
+
+def setup_forward(method, L, N, L_lower, sampling, reality, mode):
+    rng = np.random.default_rng()
+    flmn = s2fft.utils.signal_generator.generate_flmn(rng, L, N, reality=reality)
+    f = base_wigner.inverse(
+        flmn,
+        L,
+        N,
+        L_lower=L_lower,
+        sampling=sampling,
+        reality=reality,
+    )
+    kernel_function = (
+        s2fft.precompute_transforms.construct.wigner_kernel_jax
+        if "jax" in method
+        else s2fft.precompute_transforms.construct.wigner_kernel
+    )
+    kernel = kernel_function(
+        L=L, N=N, reality=reality, sampling=sampling, forward=True, mode=mode
+    )
+    return BenchmarkSetup({"f": f, "kernel": kernel}, flmn, "jax" in method)
+
+
+@benchmark(
+    setup_forward,
+    method=METHOD_VALUES,
+    L=L_VALUES,
+    N=N_VALUES,
+    L_lower=L_LOWER_VALUES,
+    sampling=SAMPLING_VALUES,
+    reality=REALITY_VALUES,
+    mode=MODE_VALUES,
+)
+def forward(f, kernel, method, L, N, L_lower, sampling, reality, mode):
+    return s2fft.precompute_transforms.wigner.forward(
+        f=f,
+        L=L,
+        N=N,
+        kernel=kernel,
+        sampling=sampling,
+        reality=reality,
+        method=method,
+    )
+
+
+def setup_inverse(method, L, N, L_lower, sampling, reality, mode):
+    rng = np.random.default_rng()
+    flmn = s2fft.utils.signal_generator.generate_flmn(rng, L, N, reality=reality)
+    kernel_function = (
+        s2fft.precompute_transforms.construct.wigner_kernel_jax
+        if method == "jax"
+        else s2fft.precompute_transforms.construct.wigner_kernel
+    )
+    kernel = kernel_function(
+        L=L, N=N, reality=reality, sampling=sampling, forward=False, mode=mode
+    )
+    return BenchmarkSetup({"flmn": flmn, "kernel": kernel}, None, "jax" in method)
+
+
+@benchmark(
+    setup_inverse,
+    method=METHOD_VALUES,
+    L=L_VALUES,
+    N=N_VALUES,
+    L_lower=L_LOWER_VALUES,
+    sampling=SAMPLING_VALUES,
+    reality=REALITY_VALUES,
+    mode=MODE_VALUES,
+)
+def inverse(flmn, kernel, method, L, N, L_lower, sampling, reality, mode):
+    return s2fft.precompute_transforms.wigner.inverse(
+        flmn=flmn,
+        L=L,
+        N=N,
+        kernel=kernel,
+        sampling=sampling,
+        reality=reality,
+        method=method,
+    )
+
+
+if __name__ == "__main__":
+    results = parse_args_collect_and_run_benchmarks()
