79af0f98f6627494c24e28ac25d4550c3cbf8ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 15:06:24 +0000

    Add package pythermogis/0.1.3/pythermogis-0.1.3-py3-none-any.whl

---
+++ packages/pythermogis/pythermogis-0.1.3-py3-none-any.whl/pythermogis/pythermogis.py
@@ -0,0 +1,6 @@
+from pythermogis.thermogis_classes.doublet import calculate_performance_across_dimensions
+
+
+def calculate_doublet_performance(input_data):
+    """Testing API"""
+    return calculate_performance_across_dimensions(input_data)
