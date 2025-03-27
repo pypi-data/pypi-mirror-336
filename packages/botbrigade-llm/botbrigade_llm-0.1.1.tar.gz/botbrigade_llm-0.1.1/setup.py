add6c93600e043d6a5758bfb83407bb033e695df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 17:06:04 +0000

    Add package botbrigade-llm/0.1.1/botbrigade_llm-0.1.1.tar.gz

---
+++ packages/botbrigade-llm/botbrigade_llm-0.1.1.tar.gz/botbrigade_llm-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="botbrigade-llm",
+    version="0.1.1",
+    author="Lantip",
+    author_email="lantip@gmail.com",
+    description="A Python SDK for interacting with the BotBrigade LLM API.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bot-brigade-studio/botbrigade-llm",
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
