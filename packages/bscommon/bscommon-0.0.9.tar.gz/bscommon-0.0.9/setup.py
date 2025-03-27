4291237d4d9b5cf6a1681f7fda770da403f1128a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 20:06:53 +0000

    Add package bscommon/0.0.9/bscommon-0.0.9.tar.gz

---
+++ packages/bscommon/bscommon-0.0.9.tar.gz/bscommon-0.0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bscommon",
+    version="0.0.9",
+    packages=find_packages("src"),
+    # package_dir={'': 'src'},
+    install_requires=["requests>=2.25", "numpy"],
+    author="bs",
+    description="冰鼠常用操作库"
+)
