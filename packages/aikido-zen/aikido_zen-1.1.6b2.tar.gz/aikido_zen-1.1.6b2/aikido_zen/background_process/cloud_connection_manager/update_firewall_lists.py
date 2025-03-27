a472061f8106f1c07aa05d63ce2c3480f0676137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 14:06:23 +0000

    Add package aikido-zen/1.1.6b2/aikido_zen-1.1.6b2.tar.gz

---
+++ packages/aikido-zen/aikido_zen-1.1.6b2.tar.gz/aikido_zen-1.1.6b2/aikido_zen/background_process/cloud_connection_manager/update_firewall_lists.py
@@ -0,0 +1,33 @@
+"""
+Exports update_firewall_lists
+"""
+
+from aikido_zen.helpers.logging import logger
+
+
+def update_firewall_lists(connection_manager):
+    """Fetches firewall lists from core and updates config (blocked/allowed IPs, blocked user agents)"""
+    if not connection_manager.token or connection_manager.serverless:
+        # We need a token, and support in serverless mode is off for this feature
+        return
+    try:
+        res = connection_manager.api.fetch_firewall_lists(connection_manager.token)
+        blocked_ips = res.get("blockedIPAddresses")
+        allowed_ips = res.get("allowedIPAddresses")
+        blocked_user_agents = res.get("blockedUserAgents")
+
+        # Validate response
+        if (
+            not res.get("success")
+            and blocked_ips
+            and allowed_ips
+            and blocked_user_agents
+        ):
+            return
+
+        connection_manager.conf.set_blocked_ips(blocked_ips)
+        connection_manager.conf.set_allowed_ips(allowed_ips)
+        connection_manager.conf.set_blocked_user_agents(blocked_user_agents)
+
+    except Exception as e:
+        logger.debug("Exception in update_firewall_lists: %s", e)
