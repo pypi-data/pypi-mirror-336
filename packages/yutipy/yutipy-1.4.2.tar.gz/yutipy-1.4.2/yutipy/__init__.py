21c315df32f451f9ed03f6dc12d8f8be89e95338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 21:09:28 +0000

    Add package yutipy/1.4.2/yutipy-1.4.2.tar.gz

---
+++ packages/yutipy/yutipy-1.4.2.tar.gz/yutipy-1.4.2/yutipy/__init__.py
@@ -0,0 +1,17 @@
+from .deezer import Deezer
+from .itunes import Itunes
+from .kkbox import KKBox
+from .models import MusicInfo
+from .musicyt import MusicYT
+from .spotify import Spotify
+from .yutipy_music import YutipyMusic
+
+__all__ = [
+    "Deezer",
+    "Itunes",
+    "KKBox",
+    "MusicInfo",
+    "MusicYT",
+    "Spotify",
+    "YutipyMusic",
+]
