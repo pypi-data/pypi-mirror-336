d78b585c361649a2745a06922864ac67512b511c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 15:06:15 +0000

    Add package variant-extractor/4.0.9/variant_extractor-4.0.9.tar.gz

---
+++ packages/variant-extractor/variant_extractor-4.0.9.tar.gz/variant_extractor-4.0.9/src/variant_extractor/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2022 - Barcelona Supercomputing Center
+# Author: Rodrigo Martin
+# MIT License
+from .VariantExtractor import VariantExtractor
+
+__version__ = '4.0.9'
+__author__ = 'Rapsssito'
