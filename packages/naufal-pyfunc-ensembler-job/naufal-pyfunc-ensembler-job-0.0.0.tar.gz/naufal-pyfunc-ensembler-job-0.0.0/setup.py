1859246a04042f87734cb5900beb136196c51935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 22:07:20 +0000

    Add package naufal-pyfunc-ensembler-job/0.0.0/naufal-pyfunc-ensembler-job-0.0.0.tar.gz

---
+++ packages/naufal-pyfunc-ensembler-job/naufal-pyfunc-ensembler-job-0.0.0.tar.gz/naufal-pyfunc-ensembler-job-0.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import pathlib
+import pkg_resources
+
+
+with pathlib.Path("requirements.txt").open() as requirements_txt:
+    requirements = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+with pathlib.Path("requirements.dev.txt").open() as dev_requirements_test:
+    dev_requirements = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(dev_requirements_test)
+    ]
+
+setuptools.setup(
+    name="naufal-pyfunc-ensembler-job",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    dev_requirements=dev_requirements,
+    python_requires=">=3.8,<3.11",
+    setup_requires=["setuptools_scm"],
+)
