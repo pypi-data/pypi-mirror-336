706ec3ee2e802e056eb782baaff727156ddf841f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 17:06:33 +0000

    Add package forecastingapi/0.5.1/forecastingapi-0.5.1.tar.gz

---
+++ packages/forecastingapi/forecastingapi-0.5.1.tar.gz/forecastingapi-0.5.1/forecastingapi/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for forecastingAPI."""
+
+__author__ = """T. Moudiki"""
+__email__ = "thierry.moudiki@gmail.com"
+__version__ = "0.3.0"
+
+from .forecastingapi import get_forecast
+
+__all__ = [get_forecast]
