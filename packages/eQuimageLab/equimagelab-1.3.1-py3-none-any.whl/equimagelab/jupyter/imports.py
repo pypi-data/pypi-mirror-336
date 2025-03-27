c5e164d11cd1ff29c6da45f33c0d5213254a5fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 19:05:54 +0000

    Add package eQuimageLab/1.3.1/equimagelab-1.3.1-py3-none-any.whl

---
+++ packages/eQuimageLab/equimagelab-1.3.1-py3-none-any.whl/equimagelab/jupyter/imports.py
@@ -0,0 +1,12 @@
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+# Author: Yann-Michel Niquet (contact@ymniquet.fr).
+# Version: 1.3.1 / 2025.03.26
+# Doc OK.
+
+"""Jupyter Lab interface top-level symbols."""
+
+from .utils import filter, shadowed, highlighted, differences
+from .backend_plotly import show, show_t, show_histograms, show_statistics, light_curve
+from .backend_dash import Dashboard
