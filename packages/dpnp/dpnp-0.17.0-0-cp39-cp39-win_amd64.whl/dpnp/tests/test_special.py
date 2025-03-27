50d4c90fe54b512f8db3804364848ba24f7c39af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 15:06:29 +0000

    Add package dpnp/0.17.0/dpnp-0.17.0-0-cp39-cp39-win_amd64.whl

---
+++ packages/dpnp/dpnp-0.17.0-0-cp39-cp39-win_amd64.whl/dpnp/tests/test_special.py
@@ -0,0 +1,34 @@
+import math
+
+import numpy
+from numpy.testing import assert_allclose
+
+import dpnp
+
+
+def test_erf():
+    a = numpy.linspace(2.0, 3.0, num=10)
+    ia = dpnp.linspace(2.0, 3.0, num=10)
+
+    assert_allclose(a, ia)
+
+    expected = numpy.empty_like(a)
+    for idx, val in enumerate(a):
+        expected[idx] = math.erf(val)
+
+    result = dpnp.erf(ia)
+
+    assert_allclose(result, expected)
+
+
+def test_erf_fallback():
+    a = numpy.linspace(2.0, 3.0, num=10)
+    dpa = dpnp.linspace(2.0, 3.0, num=10)
+
+    expected = numpy.empty_like(a)
+    for idx, val in enumerate(a):
+        expected[idx] = math.erf(val)
+
+    result = dpnp.erf(dpa)
+
+    assert_allclose(result, expected)
