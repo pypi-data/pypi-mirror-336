e4665325552bf6e27708d2200d582e15541afdc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-27 21:07:02 +0000

    Add package DrissionPagePro/0.0.1/drissionpagepro-0.0.1.tar.gz

---
+++ packages/DrissionPagePro/drissionpagepro-0.0.1.tar.gz/drissionpagepro-0.0.1/setup.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="DrissionPagePro",
+    version='0.0.1',
+    author="g1879",
+    author_email="g1879@qq.com",
+    description="Python based web automation tool. It can control the browser and send and receive data packets.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # license="BSD",
+    keywords="DrissionPage",
+    url="https://DrissionPage.cn",
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'lxml',
+        'requests',
+        'cssselect',
+        'DownloadKit>=2.0.7',
+        'websocket-client',
+        'click',
+        'tldextract>=3.4.4',
+        'psutil'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        # "License :: OSI Approved :: BSD License",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'dp = DrissionPage._functions.cli:main',
+        ],
+    },
+)
