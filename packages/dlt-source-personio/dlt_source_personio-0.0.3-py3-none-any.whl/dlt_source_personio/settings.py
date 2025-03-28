270baad6c0c5d5c399d98bac5adde7c83d0ff6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:18:17 +0000

    Add package dlt-source-personio/0.0.3/dlt_source_personio-0.0.3-py3-none-any.whl

---
+++ packages/dlt-source-personio/dlt_source_personio-0.0.3-py3-none-any.whl/dlt_source_personio/settings.py
@@ -0,0 +1,11 @@
+"""personio source settings and constants"""
+
+API_BASE = "https://api.personio.de"
+
+V1_EMPLOYEES = "/v1/company/employees"
+
+V2_AUTH_TOKEN = "/v2/auth/token"
+V2_AUTH_REVOKE = "/v2/auth/revoke"
+V2_PERSONS = "/v2/persons"
+
+X_PERSONIO_APP_ID = "DLT_SOURCE_PERSONIO"
