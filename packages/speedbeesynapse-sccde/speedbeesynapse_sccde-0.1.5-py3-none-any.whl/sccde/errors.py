86372d9b6c806f10ce37e7acd57b960ea43ffeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:18:12 +0000

    Add package speedbeesynapse-sccde/0.1.5/speedbeesynapse_sccde-0.1.5-py3-none-any.whl

---
+++ packages/speedbeesynapse-sccde/speedbeesynapse_sccde-0.1.5-py3-none-any.whl/sccde/errors.py
@@ -0,0 +1,40 @@
+"""Errors definition."""
+from pathlib import Path
+
+# ruff: noqa: D101, D105, D107
+
+class SccdeError(Exception):
+    def __str__(self) -> str:
+        args_str = ' '.join([str(x) for x in self.args])
+        return f'{self.__class__.__name__}: {args_str}'
+
+
+class AlreadyInitializedError(SccdeError):
+    def __init__(self, path: Path):
+        self.path = path
+    def __str__(self) -> str:
+        return f'The directory {self.path} is initialized for SCCDE already.'
+
+
+class NotInitializedError(SccdeError):
+    def __init__(self, path: Path):
+        self.path = path
+    def __str__(self) -> str:
+        return f'The directory {self.path} is not initialized for SCCDE yet.'
+
+
+class SccInfoNotWritableError(SccdeError):
+    def __init__(self, info_path: Path):
+        self.info_path = info_path
+    def __str__(self) -> str:
+        return f'Could not write data into {self.info_path}.'
+
+
+class SynapseNotInstalledError(SccdeError):
+    def __str__(self) -> str:
+        return 'SpeeDBee Synapse not found in this system.'
+
+
+class UnknownPlatformError(SccdeError):
+    def __str__(self) -> str:
+        return 'This platform is not supported.'
